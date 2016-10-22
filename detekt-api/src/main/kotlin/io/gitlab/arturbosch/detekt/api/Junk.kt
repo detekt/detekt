f8f695e903a07d0c2260082fd6cea50fcf2d6826
ArtiSmarti <arturbosch@gmx.de>
2016-10-22 16:48:18 +0200

    Make visit methods internal

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Junk.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Junk.kt
@@ -14,11 +14,11 @@ fun validateIdentifier(id: String) {
 	require(id.matches(identifierRegex), { "id must match [aA-zZ]+([-][aA-zZ]+)*" })
 }
 
-fun ASTNode.visitTokens(currentNode: (node: ASTNode) -> Unit) {
+internal fun ASTNode.visitTokens(currentNode: (node: ASTNode) -> Unit) {
 	currentNode(this)
 	getChildren(null).forEach { it.visitTokens(currentNode) }
 }
 
-fun ASTNode.visit(visitor: DetektVisitor) {
+internal fun ASTNode.visit(visitor: DetektVisitor) {
 	KtPsiUtil.visitChildren(this.psi as KtElement, visitor, null)
 }
