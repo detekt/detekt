53eb1cf952bd3027c00e9727aa79b2a30225a244
abosch <arturbosch@gmx.de>
2016-11-14 22:13:40 +0100

    Comment auto correct of trailing spaces as they modify the underlying file each time - #49

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/formatting/TrailingSpaces.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/formatting/TrailingSpaces.kt
@@ -3,7 +3,6 @@ package io.gitlab.arturbosch.detekt.rules.formatting
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
-import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
@@ -19,20 +18,21 @@ import org.jetbrains.kotlin.psi.psiUtil.startOffset
  */
 class TrailingSpaces(config: Config) : TokenRule("TrailingSpaces", Severity.Style, config) {
 
+	// TODO#49
 	override fun procedure(node: ASTNode) {
 		if (node is PsiWhiteSpace) {
 			val split = node.getText().split("\n")
 			if (split.size > 1) {
 				checkForTrailingSpaces(split.head(), node)
-				withAutoCorrect {
-					(node as LeafPsiElement).replaceWithText("\n".repeat(split.size - 1) + split.last())
-				}
+//				withAutoCorrect {
+//					(node as LeafPsiElement).replaceWithText("\n".repeat(split.size - 1) + split.last())
+//				}
 			} else
 				if (PsiTreeUtil.nextLeaf(node) == null /* eof */) {
 					checkForTrailingSpaces(split, node)
-					withAutoCorrect {
-						(node as LeafPsiElement).replaceWithText("\n".repeat(split.size - 1))
-					}
+//					withAutoCorrect {
+//						(node as LeafPsiElement).replaceWithText("\n".repeat(split.size - 1))
+//					}
 				}
 		}
 	}
