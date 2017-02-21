8e5b36489764018ca412e5f650096afb93cdb027
abosch <arturbosch@gmx.de>
2017-02-21 17:26:20 +0100

    Check override-modifier before checking the parameter list - #88

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/LongParameterList.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/LongParameterList.kt
@@ -5,6 +5,8 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Metric
 import io.gitlab.arturbosch.detekt.api.ThresholdedCodeSmell
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtParameterList
 
 /**
@@ -12,10 +14,15 @@ import org.jetbrains.kotlin.psi.KtParameterList
  */
 class LongParameterList(config: Config = Config.empty, threshold: Int = 5) : CodeSmellThresholdRule("LongParameterList", config, threshold) {
 
-	override fun visitParameterList(list: KtParameterList) {
-		val size = list.parameters.size
+	override fun visitNamedFunction(function: KtNamedFunction) {
+		if (function.hasModifier(KtTokens.OVERRIDE_KEYWORD)) return
+		function.valueParameterList?.checkThreshold()
+	}
+
+	private fun KtParameterList.checkThreshold() {
+		val size = parameters.size
 		if (size > threshold) {
-			addFindings(ThresholdedCodeSmell(id, Entity.from(list), Metric("SIZE", size, threshold)))
+			addFindings(ThresholdedCodeSmell(id, Entity.from(this), Metric("SIZE", size, threshold)))
 		}
 	}
 }
