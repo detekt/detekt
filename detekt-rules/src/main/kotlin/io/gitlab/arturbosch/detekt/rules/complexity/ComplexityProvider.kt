1e06c152864b2fa25477aa61fc56a1f95b9048cc
abosch <arturbosch@gmx.de>
2017-03-23 21:53:49 +0100

    Add complexity provider

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/ComplexityProvider.kt
@@ -0,0 +1,23 @@
+package io.gitlab.arturbosch.detekt.rules.complexity
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
+/**
+ * @author Artur Bosch
+ */
+class ComplexityProvider : RuleSetProvider {
+
+	override val ruleSetId: String = "complexity"
+
+	override fun instance(config: Config): RuleSet {
+		return RuleSet(ruleSetId, listOf(
+				LongParameterList(config),
+				LongMethod(config),
+				LargeClass(config),
+				ComplexMethod(config),
+				NestedBlockDepth(config)
+		))
+	}
+}
--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/CodeSmellProvider.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/CodeSmellProvider.kt
@@ -3,11 +3,6 @@ package io.gitlab.arturbosch.detekt.rules.providers
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
-import io.gitlab.arturbosch.detekt.rules.complexity.ComplexMethod
-import io.gitlab.arturbosch.detekt.rules.complexity.LargeClass
-import io.gitlab.arturbosch.detekt.rules.complexity.LongMethod
-import io.gitlab.arturbosch.detekt.rules.complexity.LongParameterList
-import io.gitlab.arturbosch.detekt.rules.complexity.NestedBlockDepth
 
 /**
  * @author Artur Bosch
@@ -18,12 +13,7 @@ class CodeSmellProvider : RuleSetProvider {
 
 	override fun instance(config: Config): RuleSet {
 		return RuleSet(ruleSetId, listOf(
-				LongParameterList(config),
-				LongMethod(config),
-				LargeClass(config),
-				ComplexMethod(config),
-				NestedBlockDepth(config)/*,
-				FeatureEnvy(config)*/
+				//FeatureEnvy(config)
 		))
 	}
 }
