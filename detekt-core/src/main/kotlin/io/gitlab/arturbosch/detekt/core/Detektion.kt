c86a99ea52826f87084598eab6d864ea5aee6b01
abosch <arturbosch@gmx.de>
2017-03-31 22:10:04 +0200

    Enhance Detektion object to include user specific data

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/Detektion.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/Detektion.kt
@@ -1,5 +1,7 @@
 package io.gitlab.arturbosch.detekt.core
 
+import com.intellij.openapi.util.Key
+import com.intellij.util.keyFMap.KeyFMap
 import io.gitlab.arturbosch.detekt.api.Finding
 
 /**
@@ -8,7 +10,19 @@ import io.gitlab.arturbosch.detekt.api.Finding
 interface Detektion {
 	val findings: Map<String, List<Finding>>
 	val notifications: List<Notification>
+
+	fun <V> getData(key: Key<V>): V?
+	fun <V> addData(key: Key<V>, value: V)
 }
 
 data class DetektResult(override val findings: Map<String, List<Finding>>,
-						override val notifications: List<Notification>) : Detektion
+						override val notifications: List<Notification>) : Detektion {
+
+	private var userData = KeyFMap.EMPTY_MAP
+
+	override fun <V> getData(key: Key<V>): V? = userData.get(key)
+
+	override fun <V> addData(key: Key<V>, value: V) {
+		userData = userData.plus(key, value)
+	}
+}
