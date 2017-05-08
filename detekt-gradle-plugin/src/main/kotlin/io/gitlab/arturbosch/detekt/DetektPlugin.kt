1dcab776f8f5e38e91b916b43295245359c2d510
abosch <arturbosch@gmx.de>
2017-05-08 21:15:38 +0200

    Fix gradle plugin by using JavaExec task to run detekt - #16

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektPlugin.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektPlugin.kt
@@ -1,40 +1,61 @@
 package io.gitlab.arturbosch.detekt
 
-import io.gitlab.arturbosch.detekt.cli.Main
-import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
+import org.gradle.api.tasks.JavaExec
 
 class DetektPlugin : Plugin<Project> {
 
 	override fun apply(project: Project) {
-		project.task(mapOf("type" to DetektTask::class.java), "detekt")
-		project.extensions.create("detekt", DetektConfig::class.java)
-	}
+		val detektConfig = project.extensions.create("detekt", DetektConfig::class.java)
+		val configuration = project.configurations.maybeCreate("detekt")
 
-}
+		project.dependencies.add(configuration.name, DefaultExternalModuleDependency(
+				"io.gitlab.arturbosch.detekt", "detekt-cli", detektConfig.version))
 
-open class DetektTask : DefaultTask() {
+		detektConfig.input = project.projectDir.absolutePath
 
-	@TaskAction
-	fun detekt() {
-		val input = project.projectDir.absolutePath
-		val detektConfig = project.extensions.getByName("detekt") as DetektConfig
-		val args = mutableListOf<String>()
-		with(detektConfig) {
-			input?.let { args.add("--project"); args.add(it) }
-			config?.let { args.add("--config"); args.add(it) }
-			configResource?.let { args.add("--config-resource"); args.add(it) }
-			filters?.let { args.add("--filters"); args.add(it) }
-			rulesets?.let { args.add("--rules"); args.add(it) }
+		project.afterEvaluate {
+
+			val args = mutableListOf<String>()
+
+			with(detektConfig) {
+				input?.let { args.add("--project"); args.add(it) }
+				config?.let { args.add("--config"); args.add(it) }
+				configResource?.let { args.add("--config-resource"); args.add(it) }
+				filters?.let { args.add("--filters"); args.add(it) }
+				rulesets?.let { args.add("--rules"); args.add(it) }
+				report?.let { args.add("--report"); args.add(it) }
+				output?.let { args.add("--output"); args.add(it) }
+				baseline?.let { args.add("--baseline"); args.add(it) }
+				parallel?.let { args.add("--parallel"); args.add(it) }
+				format?.let { args.add("--format"); args.add(it) }
+				useTabs?.let { args.add("--useTabs"); args.add(it) }
+			}
+
+			project.tasks.create("detekt", JavaExec::class.java) {
+				it.description = "Analyze your kotlin code with detekt!"
+				it.main = "io.gitlab.arturbosch.detekt.cli.Main"
+				it.classpath = configuration
+				it.args(args)
+			}
 		}
-		Main.main(args.toTypedArray())
+
 	}
+
 }
 
-open class DetektConfig(var message: String = "World",
+@Suppress("LongParameterList")
+open class DetektConfig(var version: String = "1.0.0.M10",
+						var input: String? = null,
 						var config: String? = null,
 						var configResource: String? = null,
 						var filters: String? = null,
-						var rulesets: String? = null)
+						var rulesets: String? = null,
+						var report: String? = null,
+						var output: String? = null,
+						var baseline: String? = null,
+						var parallel: String? = null,
+						var format: String? = null,
+						var useTabs: String? = null)
