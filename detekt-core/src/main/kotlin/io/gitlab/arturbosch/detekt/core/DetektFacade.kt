1146b591e66659fd006292ce1bac1b41f9dd5936
abosch <arturbosch@gmx.de>
2017-05-09 15:36:22 +0200

    Implement more instance methods for DetektFacade - #29

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/DetektFacade.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/DetektFacade.kt
@@ -1,15 +1,24 @@
 package io.gitlab.arturbosch.detekt.core
 
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
 /**
  * @author Artur Bosch
  */
 object DetektFacade {
 
 	fun instance(settings: ProcessingSettings): Detektor {
-		val compiler = KtTreeCompiler.instance(settings)
 		val locator = RuleSetLocator.instance(settings)
 		val providers = locator.loadProviders()
-		return Detektor(settings, compiler, providers)
+		return instance(settings, providers)
 	}
 
+	fun instance(settings: ProcessingSettings, vararg providers: RuleSetProvider): Detektor {
+		return instance(settings, providers.toList())
+	}
+
+	fun instance(settings: ProcessingSettings, providers: List<RuleSetProvider>): Detektor {
+		val compiler = KtTreeCompiler.instance(settings)
+		return Detektor(settings, compiler, providers)
+	}
 }
