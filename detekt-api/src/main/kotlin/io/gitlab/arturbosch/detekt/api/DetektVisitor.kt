a169c5071e7e1144a30e3006f437be1973aadce5
abosch <arturbosch@gmx.de>
2017-05-27 21:56:32 +0200

    Implement visit leaf psi element methods

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/DetektVisitor.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/DetektVisitor.kt
@@ -1,10 +1,37 @@
 package io.gitlab.arturbosch.detekt.api
 
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
 
 /**
  * Base visitor for detekt rules.
+ * Adds additional rules for psi leaf elements.
  *
  * @author Artur Bosch
  */
-open class DetektVisitor : KtTreeVisitorVoid()
+@Suppress("EmptyFunctionBlock")
+open class DetektVisitor : KtTreeVisitorVoid() {
+
+	override fun visitElement(element: PsiElement) {
+		if (element is LeafPsiElement) visitLeaf(element)
+		super.visitElement(element)
+	}
+
+	open fun visitLeaf(element: LeafPsiElement) {
+		when (element.text) {
+			"}" -> visitLeftBrace(element)
+			"{" -> visitRightBrace(element)
+			":" -> visitColon(element)
+		}
+	}
+
+	open fun visitColon(element: LeafPsiElement) {
+	}
+
+	open fun visitLeftBrace(element: LeafPsiElement) {
+	}
+
+	open fun visitRightBrace(element: LeafPsiElement) {
+	}
+}
