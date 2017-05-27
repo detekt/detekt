ba9b2a58a6e81aa4a91ad34ec836f44abe3183c2
abosch <arturbosch@gmx.de>
2017-05-27 21:07:10 +0200

    Reimplement spacing around colon

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/SpacingAroundColon.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/SpacingAroundColon.kt
@@ -1,54 +1,39 @@
 package io.gitlab.arturbosch.detekt.formatting
 
-import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
 import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Entity
-import io.gitlab.arturbosch.detekt.api.TokenRule
-import org.jetbrains.kotlin.psi.KtAnnotation
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
-import org.jetbrains.kotlin.psi.KtClassOrObject
+import io.gitlab.arturbosch.detekt.api.Rule
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtAnnotatedExpression
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtObjectDeclaration
+import org.jetbrains.kotlin.psi.KtVariableDeclaration
 
 /**
- * Based on KtLint.
- *
- * @author Shyiko
+ * @author Artur Bosch
  */
-class SpacingAroundColon(config: Config) : TokenRule("SpacingAroundColon", Severity.Style, config) {
+class SpacingAroundColon(config: Config) : Rule("SpacingAroundColon", Severity.Style, config) {
 
-	override fun procedure(node: ASTNode) {
-		if (node is LeafPsiElement && node.textMatches(":") && !node.isPartOfString()) {
-			if (node.isPartOf(KtAnnotation::class) || node.isPartOf(KtAnnotationEntry::class)) {
-				// todo: enforce "no spacing"
-				return
+	override fun visitElement(element: PsiElement) {
+		val type = element.node.elementType
+		if (type == KtTokens.COLON && element is LeafPsiElement) {
+			val parent = element.parent
+			val modified = when {
+				parent is KtAnnotatedExpression -> false
+				parent is KtVariableDeclaration -> element.trimSpacesAfter(autoCorrect)
+				parent is KtObjectDeclaration && parent.name == null -> element.trimSpacesAfter(autoCorrect)
+				parent is KtClass -> element.trimSpacesAround(autoCorrect)
+				else -> false
 			}
-			val missingSpacingBefore = node.prevSibling !is PsiWhiteSpace && node.parent is KtClassOrObject
-			val missingSpacingAfter = node.nextSibling !is PsiWhiteSpace
-			when {
-				missingSpacingBefore && missingSpacingAfter -> {
-					addFindings(CodeSmell(id, Entity.from(node), "Missing spacing around \":\""))
-					withAutoCorrect {
-						node.rawInsertBeforeMe(PsiWhiteSpaceImpl(" "))
-						node.rawInsertAfterMe(PsiWhiteSpaceImpl(" "))
-					}
-				}
-				missingSpacingBefore -> {
-					addFindings(CodeSmell(id, Entity.from(node), "Missing spacing before \":\""))
-					withAutoCorrect {
-						node.rawInsertBeforeMe(PsiWhiteSpaceImpl(" "))
-					}
-				}
-				missingSpacingAfter -> {
-					addFindings(CodeSmell(id, Entity.from(node, offset = 1), "Missing spacing after \":\""))
-					withAutoCorrect {
-						node.rawInsertAfterMe(PsiWhiteSpaceImpl(" "))
-					}
-				}
+			if (modified) {
+				addFindings(CodeSmell(id, Entity.from(element)))
 			}
 		}
+
+		super.visitElement(element)
 	}
 
 }
