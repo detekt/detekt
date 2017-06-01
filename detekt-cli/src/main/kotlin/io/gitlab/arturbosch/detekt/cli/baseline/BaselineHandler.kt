5a65d0e76981315cd34eb46edf5f47dfb73405de
abosch <arturbosch@gmx.de>
2017-06-01 15:41:16 +0200

    Use proper handling of escaped sequences in ID tags - #57

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/BaselineHandler.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/BaselineHandler.kt
@@ -10,13 +10,13 @@ import java.time.Instant
 class BaselineHandler : DefaultHandler() {
 
 	private var current: String? = null
-	private var content: String? = null
+	private var content: String = ""
 	private var whitestamp: String? = null
 	private var blackstamp: String? = null
 	private val whiteIds = mutableListOf<String>()
 	private val blackIds = mutableListOf<String>()
 
-	fun createBaseline() = Baseline(Blacklist(blackIds, blackstamp ?: now()), Whitelist(whiteIds, whitestamp ?: now()))
+	internal fun createBaseline() = Baseline(Blacklist(blackIds, blackstamp ?: now()), Whitelist(whiteIds, whitestamp ?: now()))
 	private fun now() = Instant.now().toEpochMilli().toString()
 
 	override fun startElement(uri: String, localName: String, qName: String, attributes: Attributes) {
@@ -29,22 +29,24 @@ class BaselineHandler : DefaultHandler() {
 				current = WHITELIST
 				whitestamp = attributes.getValue(TIMESTAMP)
 			}
+			ID -> content = ""
 		}
 	}
 
 	override fun endElement(uri: String, localName: String, qName: String) {
 		when (qName) {
-			ID -> if (content != null) {
+			ID -> if (content.isNotBlank()) {
 				when (current) {
-					BLACKLIST -> blackIds.add(content!!)
-					WHITELIST -> whiteIds.add(content!!)
+					BLACKLIST -> blackIds.add(content)
+					WHITELIST -> whiteIds.add(content)
 				}
+				content = ""
 			}
 			BLACKLIST, WHITELIST -> current == null
 		}
 	}
 
 	override fun characters(ch: CharArray, start: Int, length: Int) {
-		if (current != null) content = String(ch, start, length)
+		if (current != null) content += String(ch, start, length)
 	}
 }
