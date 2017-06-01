994c6548f9faf4c53f3fde460adcc116ce2711c4
abosch <arturbosch@gmx.de>
2017-06-01 14:29:04 +0200

    Move BaselineHandler to own class - #57

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/Baseline.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/Baseline.kt
@@ -1,9 +1,5 @@
 package io.gitlab.arturbosch.detekt.cli.baseline
 
-import org.xml.sax.Attributes
-import org.xml.sax.helpers.DefaultHandler
-import java.time.Instant
-
 /**
  * @author Artur Bosch
  */
@@ -21,44 +17,3 @@ val TIMESTAMP = "timestamp"
 val ID = "ID"
 
 class InvalidBaselineState(msg: String, error: Throwable) : IllegalStateException(msg, error)
-
-class BaselineHandler : DefaultHandler() {
-
-	private var current: String? = null
-	private var content: String? = null
-	private var whitestamp: String? = null
-	private var blackstamp: String? = null
-	private val whiteIds = mutableListOf<String>()
-	private val blackIds = mutableListOf<String>()
-
-	fun createBaseline() = Baseline(Blacklist(blackIds, blackstamp ?: now()), Whitelist(whiteIds, whitestamp ?: now()))
-	private fun now() = Instant.now().toEpochMilli().toString()
-
-	override fun startElement(uri: String, localName: String, qName: String, attributes: Attributes) {
-		when (qName) {
-			BLACKLIST -> {
-				current = BLACKLIST
-				blackstamp = attributes.getValue(TIMESTAMP)
-			}
-			WHITELIST -> {
-				current = WHITELIST
-				whitestamp = attributes.getValue(TIMESTAMP)
-			}
-		}
-	}
-
-	override fun endElement(uri: String, localName: String, qName: String) {
-		when (qName) {
-			ID -> if (content != null) {
-				when (current) {
-					BLACKLIST -> blackIds.add(content!!)
-					WHITELIST -> whiteIds.add(content!!)
-				}
-			}
-		}
-	}
-
-	override fun characters(ch: CharArray, start: Int, length: Int) {
-		content = String(ch, start, length)
-	}
-}
