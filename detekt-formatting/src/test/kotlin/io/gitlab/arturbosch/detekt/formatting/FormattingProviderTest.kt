e4b54a90abc47efaa1d6e353c7de438c2bfe6e60
abosch <arturbosch@gmx.de>
2017-06-11 14:40:16 +0200

    Fix FormattingProviderTest to expect 8+ findings as SpacingAroundBraces is fixed now

--- detekt-formatting/src/test/kotlin/io/gitlab/arturbosch/detekt/formatting/FormattingProviderTest.kt
+++ detekt-formatting/src/test/kotlin/io/gitlab/arturbosch/detekt/formatting/FormattingProviderTest.kt
@@ -1,7 +1,6 @@
 package io.gitlab.arturbosch.detekt.formatting
 
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Unstable
 import io.gitlab.arturbosch.detekt.test.compileForTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -11,7 +10,6 @@ import java.nio.file.Paths
 /**
  * @author Artur Bosch
  */
-@Unstable("GreaterThanOrEqualTo must be 8 - see #47")
 class FormattingProviderTest {
 
 	val path: Path = Paths.get(FormattingProviderTest::class.java.getResource("/cases/Formatting.kt").path)
@@ -20,6 +18,6 @@ class FormattingProviderTest {
 	fun test() {
 		val ruleSet = FormattingProvider().instance(Config.empty)
 		val (_, findings) = ruleSet.acceptAll(listOf(compileForTest(path)))
-		assertThat(findings.groupBy { it.id }.values.size).isGreaterThanOrEqualTo(7)
+		assertThat(findings.groupBy { it.id }.values.size).isGreaterThanOrEqualTo(8)
 	}
 }
