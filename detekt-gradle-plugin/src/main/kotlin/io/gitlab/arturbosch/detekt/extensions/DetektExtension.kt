c940fdb9cab9578824a1828f3c7c026738ebd83c
abosch <arturbosch@gmx.de>
2017-06-17 14:10:13 +0200

    Restructure extension classes

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/DetektExtension.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/DetektExtension.kt
@@ -12,9 +12,10 @@ open class DetektExtension(open var version: String = "1.0.0.M11",
 						   open var ideaExtension: IdeaExtension = IdeaExtension()) {
 
 	val profiles: MutableList<ProfileExtension> = mutableListOf()
+
 	val defaultProfile get() = profiles.find { it.name == profile }
-	val systemOrDefaultProfile get() = profiles.find { it.name == specifiedProfileNameThroughSystemProperty() }
-			?: defaultProfile
+	val systemProfile get() = profiles.find { it.name == specifiedProfileNameThroughSystemProperty() }
+	val systemOrDefaultProfile get() = systemProfile ?: defaultProfile
 
 	val ideaFormatArgs get() = ideaExtension.formatArgs(this)
 	val ideaInspectArgs get() = ideaExtension.inspectArgs(this)
@@ -31,33 +32,30 @@ open class DetektExtension(open var version: String = "1.0.0.M11",
 	}
 
 	fun profileArgumentsOrDefault(project: Project): List<String> {
-		val systemProfile = specifiedProfileNameThroughSystemProperty()
-		val argumentsForProfile = createArgumentsForProfile(systemProfile)
-		return if (argumentsForProfile.isNotEmpty()) argumentsForProfile else {
-			listOf("--project", project.projectDir.absolutePath, "--config-resource", "/default-detekt-config.yml")
+		val arguments = createArgumentsForProfile()
+		return if (arguments.isNotEmpty()) arguments else project.fallbackArguments()
+	}
+
+	private fun createArgumentsForProfile(): List<String> {
+		val allArguments = defaultProfile?.arguments() ?: mutableMapOf<String, String>()
+		val overriddenArguments = systemProfile?.arguments() ?: mutableMapOf<String, String>()
+		overriddenArguments.forEach { allArguments.merge(it.key, it.value) { _, v2 -> v2 } }
+		return allArguments.flatMap { filterBooleans(it.key, it.value) }.apply {
+			if (debug) {
+				val name = systemOrDefaultProfile?.name ?: "_fallback_"
+				println("detekt version: $version - usedProfile: $name")
+				println("Arguments: $this")
+			}
 		}
 	}
 
-	fun createArgumentsForProfile(name: String): List<String> {
-		debugProfileArguments(name)
-		val arguments = profiles.find { it.name == profile }?.arguments() ?: mutableMapOf<String, String>()
-		val actualProfileArgs = profiles.find { it.name == name }?.arguments()
-		actualProfileArgs?.let { it.forEach { arguments.merge(it.key, it.value) { _, v2 -> v2 } } }
-		return arguments.flatMap { filterBooleans(it.key, it.value) }.apply {
-			if (debug) println("Arguments: $this")
-		}
-	}
+	private fun Project.fallbackArguments() = listOf(
+			"--project", projectDir.absolutePath,
+			"--config-resource", "/default-detekt-config.yml",
+			"--filters", ".*/test/.*,.*/resources/.*,.*/build/.*,.*/target/.*")
 
 	private fun filterBooleans(key: String, value: String)
 			= if (value == "true" || value == "false") listOf(key) else listOf(key, value)
-
-	private fun debugProfileArguments(name: String) {
-		if (debug) {
-			val systemProfile = specifiedProfileNameThroughSystemProperty()
-			println("detekt version: $version - requestedProfile: $name - usedProfile: $systemProfile" +
-					"\n\t${profiles.find { it.name == systemProfile }}")
-		}
-	}
 
 	private fun specifiedProfileNameThroughSystemProperty(): String = System.getProperty("detekt.profile") ?: profile
 
