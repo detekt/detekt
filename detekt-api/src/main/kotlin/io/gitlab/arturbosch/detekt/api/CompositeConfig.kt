2d3d94382b8a7aa161570fd0ed80ca08dbd6967f
abosch <arturbosch@gmx.de>
2017-06-17 16:15:40 +0200

    Implement composable configurations - closes #92

---
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/CompositeConfig.kt
@@ -0,0 +1,16 @@
+package io.gitlab.arturbosch.detekt.api
+
+/**
+ * @author Artur Bosch
+ */
+class CompositeConfig(private val lookFirst: Config, private val lookSecond: Config) : Config() {
+
+	override fun subConfig(key: String): Config
+			= CompositeConfig(lookFirst.subConfig(key), lookSecond.subConfig(key))
+
+	override fun <T : Any> valueOrDefault(key: String, default: T): T {
+		val firstResult = lookFirst.valueOrDefault(key, default)
+		return if (firstResult != default) firstResult else lookSecond.valueOrDefault(key, default)
+	}
+
+}
