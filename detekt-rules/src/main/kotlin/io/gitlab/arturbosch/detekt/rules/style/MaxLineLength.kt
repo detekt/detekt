dcedccd7774bc25d876141d8a1d3b0e1aa6a46ff
abosch <arturbosch@gmx.de>
2017-06-18 08:14:28 +0200

    Do not override core visit(root: ktFile) method of rule as it performs isActive check - #56

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxLineLength.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxLineLength.kt
@@ -8,22 +8,23 @@ import org.jetbrains.kotlin.psi.KtFile
 
 class MaxLineLength(config: Config = Config.empty) : Rule("MaxLineLength", Severity.Style, config) {
 
-	private val maxLineLength: Int = withConfig { valueOrDefault(MAX_LINE_LENGTH, 120) }
+	private val maxLineLength: Int = withConfig { valueOrDefault(MAX_LINE_LENGTH, DEFAULT_IDEA_LINE_LENGTH) }
 
-	override fun visit(root: KtFile) {
+	override fun visitKtFile(file: KtFile) {
 		var offset = 0
-		root.text.splitToSequence("\n")
+		file.text.splitToSequence("\n")
 				.map { it.length }
 				.forEach {
 					offset += it
 					if (it > maxLineLength) {
-						addFindings(CodeSmell(id, severity, Entity.Companion.from(root, offset)))
+						addFindings(CodeSmell(id, severity, Entity.Companion.from(file, offset)))
 					}
 				}
 	}
 
 	companion object {
 		val MAX_LINE_LENGTH = "maxLineLength"
+		val DEFAULT_IDEA_LINE_LENGTH = 120
 	}
 }
 
