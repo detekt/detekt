65ddf218109a17c6a8b480c111de7805ffbbb007
Artur Bosch <arturbosch@gmx.de>
2017-06-21 11:04:21 +0200

    Revert formatting changes in test cases classes - #97

--- detekt-formatting/src/test/resources/cases/Formatting.kt
+++ detekt-formatting/src/test/resources/cases/Formatting.kt
@@ -1,19 +1,17 @@
-@file:Suppress("UNUSED_EXPRESSION", "UNUSED")
+@file:Suppress("UNUSED_EXPRESSION","UNUSED")
 
 package cases
 
 
-fun main() {
+fun main( )  {
 
-}
+    }
 
-fun fn() {}
-fun call() {
-	try {
-		1 + 1
-	} catch(e: Exception) {
-	}
-	if (true) true else false
+fun fn (){}
+fun  call()  {
+	try{1+1}
+	catch(e:Exception){}
+	if(true)true else false
 }
 
 
