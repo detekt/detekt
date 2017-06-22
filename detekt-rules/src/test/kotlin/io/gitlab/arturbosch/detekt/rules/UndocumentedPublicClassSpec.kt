7bb80891430aa2af0146a4f14da3c2706074593b
Artur Bosch <arturbosch@gmx.de>
2017-06-22 13:34:04 +0200

    Rename NoDocOverX rules to UndocumentedX - closes #109

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/UndocumentedPublicClassSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/UndocumentedPublicClassSpec.kt
@@ -1,6 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules
 
-import io.gitlab.arturbosch.detekt.rules.documentation.NoDocOverPublicClass
+import io.gitlab.arturbosch.detekt.rules.documentation.UndocumentedPublicClass
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.it
@@ -9,8 +9,8 @@ import org.jetbrains.spek.subject.SubjectSpek
 /**
  * @author Artur Bosch
  */
-class NoDocOverPublicClassSpec : SubjectSpek<NoDocOverPublicClass>({
-	subject { NoDocOverPublicClass() }
+class UndocumentedPublicClassSpec : SubjectSpek<UndocumentedPublicClass>({
+	subject { UndocumentedPublicClass() }
 
 	it("finds two undocumented classes") {
 		subject.lint(Case.Comments.path())
