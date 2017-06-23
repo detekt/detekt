c0e39ae42e831696946cdbd7917b47df8d52f632
Artur Bosch <arturbosch@gmx.de>
2017-06-23 12:07:01 +0200

    Use os independent paths

--- detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/KT.kt
+++ detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/KT.kt
@@ -3,6 +3,7 @@ package io.gitlab.arturbosch.detekt.core
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+import io.gitlab.arturbosch.detekt.test.resource
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -10,7 +11,7 @@ import java.nio.file.Paths
  * @author Artur Bosch
  */
 
-val path: Path = Paths.get(KtTreeCompilerSpec::class.java.getResource("/cases").path)
+val path: Path = Paths.get(resource("/cases"))
 
 class TestProvider(override val ruleSetId: String = "Test") : RuleSetProvider {
 	override fun instance(config: Config): RuleSet {
