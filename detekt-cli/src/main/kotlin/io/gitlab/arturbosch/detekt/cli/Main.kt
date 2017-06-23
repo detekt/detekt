abdebfed01b7a20772248ed50eee39f656ea7822
Artur Bosch <arturbosch@gmx.de>
2017-06-23 11:22:01 +0200

    Fix style violations - closes #100

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/Main.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/Main.kt
@@ -16,36 +16,43 @@ class Main {
 			converter = ExistingPathConverter::class, description = "Project path to analyze (path/to/project).")
 	lateinit var project: Path
 
-	@Parameter(names = arrayOf("--filters", "-f"), description = "Path filters defined through regex with separator ';' (\".*test.*\").")
+	@Parameter(names = arrayOf("--filters", "-f"),
+			description = "Path filters defined through regex with separator ';' (\".*test.*\").")
 	var filters: String? = null // Using a converter for List<PathFilter> resulted in a ClassCastException
 
 	@Parameter(names = arrayOf("--config", "-c"), description = "Path to the config file (path/to/config.yml).")
 	var config: String? = null
 
-	@Parameter(names = arrayOf("--config-resource", "-cr"), description = "Path to the config resource on detekt's classpath (path/to/config.yml).",
+	@Parameter(names = arrayOf("--config-resource", "-cr"),
+			description = "Path to the config resource on detekt's classpath (path/to/config.yml).",
 			converter = MultipleClasspathResourceConverter::class)
 	var configResource: String? = null
 
-	@Parameter(names = arrayOf("--generate-config", "-gc"), description = "Export default config to default-detekt-config.yml.")
+	@Parameter(names = arrayOf("--generate-config", "-gc"),
+			description = "Export default config to default-detekt-config.yml.")
 	var generateConfig: Boolean = false
 
 	@Parameter(names = arrayOf("--rules", "-r"), description = "Extra paths to ruleset jars separated by ';'.")
 	var rules: String? = null
 
-	@Parameter(names = arrayOf("--format"), description = "Enables formatting of source code. Cannot be used together with --config.")
+	@Parameter(names = arrayOf("--format"),
+			description = "Enables formatting of source code. Cannot be used together with --config.")
 	var formatting: Boolean = false
 
-	@Parameter(names = arrayOf("--parallel"), description = "Enables parallel compilation of source files. Should only be used if the analyzing project has more than ~200 kotlin files.")
+	@Parameter(names = arrayOf("--parallel"), description = "Enables parallel compilation of source files." +
+			" Should only be used if the analyzing project has more than ~200 kotlin files.")
 	var parallel: Boolean = false
 
 	@Parameter(names = arrayOf("--useTabs"), description = "Tells the formatter that indentation with tabs are valid.")
 	var useTabs: Boolean = false
 
-	@Parameter(names = arrayOf("--baseline", "-b"), description = "If a baseline xml file is passed in, only new code smells not in the baseline are printed in the console.",
+	@Parameter(names = arrayOf("--baseline", "-b"), description = "If a baseline xml file is passed in," +
+			" only new code smells not in the baseline are printed in the console.",
 			converter = PathConverter::class)
 	var baseline: Path? = null
 
-	@Parameter(names = arrayOf("--create-baseline", "-cb"), description = "Treats current analysis findings as a smell baseline for further detekt runs.")
+	@Parameter(names = arrayOf("--create-baseline", "-cb"),
+			description = "Treats current analysis findings as a smell baseline for further detekt runs.")
 	var createBaseline: Boolean = false
 
 	@Parameter(names = arrayOf("--output", "-o"), description = "Specify the file to output to.",
