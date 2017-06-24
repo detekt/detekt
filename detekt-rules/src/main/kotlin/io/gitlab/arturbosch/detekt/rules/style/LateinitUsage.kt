4390f2143a0f7b085c60cddc57423a1c9b3c063f
Marvin Ramin <marvin.ramin@gmail.com>
2017-06-24 16:15:31 +0200

    add rule to report lateinit usages

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/LateinitUsage.kt
@@ -0,0 +1,21 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.psiUtil.allChildren
+
+class LateinitUsage(config: Config = Config.empty) : Rule(config) {
+
+	override val issue = Issue(javaClass.simpleName, Severity.Style, "Usage of lateinit")
+
+	override fun visitProperty(property: KtProperty) {
+		val lateinitModifiers = property.modifierList
+				?.allChildren
+				?.filter { it.text == "lateinit" }
+				?.count() ?: 0
+
+		if (lateinitModifiers > 0) {
+			report(CodeSmell(issue, Entity.from(property)))
+		}
+	}
+}
