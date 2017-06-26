bba3a095e7b9a602ab828872425a0dadeef4f5a8
Marvin Ramin <marvin.ramin@gmail.com>
2017-06-26 21:40:09 +0200

    adds excluding annotations from LateinitUsage rule

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/LateinitUsage.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/LateinitUsage.kt
@@ -1,21 +1,39 @@
 package io.gitlab.arturbosch.detekt.rules.bugs
 
 import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtProperty
-import org.jetbrains.kotlin.psi.psiUtil.allChildren
+import org.jetbrains.kotlin.psi.KtPsiUtil
+import org.jetbrains.kotlin.utils.ifEmpty
 
 class LateinitUsage(config: Config = Config.empty) : Rule(config) {
 
 	override val issue = Issue(javaClass.simpleName, Severity.Style, "Usage of lateinit. Using lateinit for property initialization is error prone, try using constructor injection or delegation.")
 
-	override fun visitProperty(property: KtProperty) {
-		val lateinitModifiers = property.modifierList
-				?.allChildren
-				?.filter { it.text == "lateinit" }
-				?.count() ?: 0
+	private val excludeAnnotatedProperties: List<String> = valueOrDefault(EXCLUDE_ANNOTATED_PROPERTIES, "").split("|")
 
-		if (lateinitModifiers > 0) {
-			report(CodeSmell(issue, Entity.from(property)))
+	lateinit var test: String
+
+	override fun visitProperty(property: KtProperty) {
+		val isLateinitProperty = property.modifierList?.hasModifier(KtTokens.LATEINIT_KEYWORD) ?: false
+		if (!isLateinitProperty) {
+			return
 		}
+
+		val isExcluded = property.annotationEntries
+				.map { KtPsiUtil.getShortName(it).toString() }
+				.filter { excludeAnnotatedProperties.contains(it) }
+				.count() != 0
+		if (isExcluded) {
+			return
+		}
+
+		report(CodeSmell(issue, Entity.from(property)))
+	}
+
+	companion object {
+		const val EXCLUDE_ANNOTATED_PROPERTIES = "excludeAnnotatedProperties"
 	}
 }
--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/LateinitUsageSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/LateinitUsageSpec.kt
@@ -1,5 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.bugs
 
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions
 import org.jetbrains.spek.api.Spek
@@ -10,15 +11,26 @@ class LateinitUsageSpec : Spek({
 
 	given("a kt file with lateinit usages") {
 		val code = """
+			package foo
+
 			class Test {
-				lateinit var first: String
-				lateinit var second: Int
+				@Ignore lateinit var test: Int
 			}
 		"""
 
 		it("should report lateinit usages") {
 			val findings = LateinitUsage().lint(code)
-			Assertions.assertThat(findings).hasSize(2)
+			Assertions.assertThat(findings).hasSize(1)
+		}
+
+		it("should not report lateinit properties annotated @Ignore") {
+			val findings = LateinitUsage(TestConfig(mapOf(LateinitUsage.EXCLUDE_ANNOTATED_PROPERTIES to "Ignore"))).lint(code)
+			Assertions.assertThat(findings).hasSize(0)
+		}
+
+		it("should not exclude lateinit properties not matching the exclude pattern") {
+			val findings = LateinitUsage(TestConfig(mapOf(LateinitUsage.EXCLUDE_ANNOTATED_PROPERTIES to "IgnoreThis"))).lint(code)
+			Assertions.assertThat(findings).hasSize(1)
 		}
 	}
 })
