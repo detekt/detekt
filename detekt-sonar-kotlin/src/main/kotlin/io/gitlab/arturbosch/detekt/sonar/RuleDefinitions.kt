494ad73eb42e7c427058dc30c9ad2752e297919d
Artur Bosch <arturbosch@gmx.de>
2017-06-28 14:35:23 +0200

    Migrate to new Issue - Severity classes

--- detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/RuleDefinitions.kt
+++ detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/RuleDefinitions.kt
@@ -1,20 +1,20 @@
 package io.gitlab.arturbosch.detekt.sonar
 
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.formatting.FormattingProvider
-import io.gitlab.arturbosch.detekt.rules.complexity.ComplexityProvider
-import io.gitlab.arturbosch.detekt.rules.documentation.CommentSmellProvider
 import io.gitlab.arturbosch.detekt.rules.providers.CodeSmellProvider
+import io.gitlab.arturbosch.detekt.rules.providers.CommentSmellProvider
+import io.gitlab.arturbosch.detekt.rules.providers.ComplexityProvider
 import io.gitlab.arturbosch.detekt.rules.providers.EmptyCodeProvider
 import io.gitlab.arturbosch.detekt.rules.providers.ExceptionsProvider
 import io.gitlab.arturbosch.detekt.rules.providers.PotentialBugProvider
-import io.gitlab.arturbosch.detekt.rules.style.StyleGuideProvider
+import io.gitlab.arturbosch.detekt.rules.providers.StyleGuideProvider
 import org.sonar.api.rule.RuleKey
 import org.sonar.api.rule.RuleStatus
-import org.sonar.api.rule.Severity as SonarSeverity
 import org.sonar.api.server.rule.RulesDefinition
+import org.sonar.api.rule.Severity as SonarSeverity
 
 private val CONFIG = Config.empty
 
@@ -41,23 +41,24 @@ fun findKey(id: String) = RULE_KEYS.find { it.rule() == id }
 private fun defineRuleKey(id: String): RuleKey = RuleKey.of(DETEKT_REPOSITORY, id)
 
 fun RulesDefinition.NewRepository.createRules() = this.apply {
-	COMPLEXITY_RULES.map { defineRule(it, "20min") } +
-			STYLES_RULES.map { defineRule(it, "5min") } +
-			CODE_SMELL_RULES.map { defineRule(it, "20min") } +
-			COMMENTS_RULES.map { defineRule(it, "10min") } +
-			EXCEPTIONS_RULES.map { defineRule(it, "10min") } +
-			EMPTY_RULES.map { defineRule(it, "5min") } +
-			POTENTIAL_BUGS_RULES.map { defineRule(it, "10min") } +
-			FORMATTING_RULES.map { defineRule(it, "1min") }
+	COMPLEXITY_RULES.map { defineRule(it) } +
+			STYLES_RULES.map { defineRule(it) } +
+			CODE_SMELL_RULES.map { defineRule(it) } +
+			COMMENTS_RULES.map { defineRule(it) } +
+			EXCEPTIONS_RULES.map { defineRule(it) } +
+			EMPTY_RULES.map { defineRule(it) } +
+			POTENTIAL_BUGS_RULES.map { defineRule(it) } +
+			FORMATTING_RULES.map { defineRule(it) }
 }
 
-private fun RulesDefinition.NewRepository.defineRule(rule: Rule, dept: String) {
+private fun RulesDefinition.NewRepository.defineRule(rule: Rule) {
 	val newRule = createRule(rule.id).setName(rule.id)
 			.setHtmlDescription("No description yet for ${rule.id}!")
 			.setTags(rule.issue.severity.name.toLowerCase())
 			.setStatus(RuleStatus.READY)
 			.setSeverity(severityMap[rule.issue.severity])
-	newRule.setDebtRemediationFunction(newRule.debtRemediationFunctions().linear(dept))
+	newRule.setDebtRemediationFunction(
+			newRule.debtRemediationFunctions().linear(rule.issue.dept.toString()))
 }
 
 private val severityMap = mapOf(
