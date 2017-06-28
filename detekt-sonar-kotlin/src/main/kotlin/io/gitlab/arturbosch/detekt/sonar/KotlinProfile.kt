38c393dad8efb1095f95dfa531a1a8572b6689f5
Artur Bosch <arturbosch@gmx.de>
2017-06-28 14:37:59 +0200

    Activate detekt way profile

--- detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/KotlinProfile.kt
+++ detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/KotlinProfile.kt
@@ -2,6 +2,7 @@ package io.gitlab.arturbosch.detekt.sonar
 
 import org.sonar.api.profiles.ProfileDefinition
 import org.sonar.api.profiles.RulesProfile
+import org.sonar.api.rules.Rule
 import org.sonar.api.utils.ValidationMessages
 
 /**
@@ -9,7 +10,12 @@ import org.sonar.api.utils.ValidationMessages
  */
 class KotlinProfile : ProfileDefinition() {
 
-	override fun createProfile(validation: ValidationMessages): RulesProfile
-			= RulesProfile.create(KOTLIN_NAME, KOTLIN_NAME)
+	override fun createProfile(validation: ValidationMessages): RulesProfile {
+		val profile = RulesProfile.create(DETEKT_WAY, KOTLIN_KEY)
+		RULE_KEYS.forEach {
+			profile.activateRule(Rule.create(it.repository(), it.rule()), null)
+		}
+		return profile
+	}
 
 }
