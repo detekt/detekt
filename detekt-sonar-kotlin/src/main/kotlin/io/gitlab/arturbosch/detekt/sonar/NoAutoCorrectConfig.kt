446a9ba00b3770ca8908d2bf89c98da7b77eb7b4
Artur Bosch <arturbosch@gmx.de>
2017-07-04 14:56:51 +0200

    Indent with tabs an use == instead of equals

--- detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/NoAutoCorrectConfig.kt
+++ detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/NoAutoCorrectConfig.kt
@@ -6,15 +6,15 @@ import io.gitlab.arturbosch.detekt.api.Config
  * Config wrapper for disabling automatic correction
  */
 @Suppress("UNCHECKED_CAST")
-class NoAutoCorrectConfig(private val config: Config): Config {
+class NoAutoCorrectConfig(private val config: Config) : Config {
 
-    override fun subConfig(key: String): Config = config.subConfig(key)
+	override fun subConfig(key: String): Config = config.subConfig(key)
 
-    override fun <T : Any> valueOrDefault(key: String, default: T): T {
-        if ("autoCorrect".equals(key)) {
-            return false as T
-        }
-        return config.valueOrDefault(key, default)
-    }
+	override fun <T : Any> valueOrDefault(key: String, default: T): T {
+		if ("autoCorrect" == key) {
+			return false as T
+		}
+		return config.valueOrDefault(key, default)
+	}
 
 }
