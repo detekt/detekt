7e3388deb1e2f663838a8650790e0ccbae0a23c2
Artur Bosch <arturbosch@gmx.de>
2017-07-05 13:11:32 +0200

    Remove quality profile property - closes #157

--- detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/DetektProperties.kt
+++ detekt-sonar-kotlin/src/main/kotlin/io/gitlab/arturbosch/detekt/sonar/foundation/DetektProperties.kt
@@ -23,23 +23,11 @@ const val PATH_FILTERS_DESCRIPTIONS = "Regex based path filters eg. '.*/test/.*'
 		" defaults are: '.*/test/.*,.*/resources/.*,.*/build/.*,.*/target/.*'"
 const val PATH_FILTERS_DEFAULTS = ".*/test/.*,.*/resources/.*,.*/build/.*,.*/target/.*"
 
-const val QUALITY_PROFILES_KEY = "detekt.sonar.kotlin.quality.profile.paths"
-const val QUALITY_PROFILES_DESCRIPTION = "Use absolute paths to detekt yaml configurations separated by ',' or ';'" +
-		" to create quality profiles based on these configs. The 'Detekt way'-profile is always created and set to" +
-		" default. It may be used as a reference configuration."
-const val QUALITY_PROFILES_DEFAULT = ""
-
-const val QUALITY_PROFILES_NAME = "Custom quality profile based on detekt configurations"
 const val CONFIG_PATH_NAME = "Detekt yaml config path"
 const val CONFIG_RESOURCE_NAME = "Detekt yaml resource config"
 const val PATH_FILTERS_NAME = "Detekt path filters"
 
 val PROPERTIES = listOf<PropertyDefinition>(
-		PropertyDefinition.builder(QUALITY_PROFILES_KEY)
-				.name(QUALITY_PROFILES_NAME)
-				.defaultValue(QUALITY_PROFILES_DEFAULT)
-				.description(QUALITY_PROFILES_DESCRIPTION)
-				.build(),
 		PropertyDefinition.builder(CONFIG_PATH_KEY)
 				.name(CONFIG_PATH_NAME)
 				.defaultValue(CONFIG_PATH_DEFAULT)
