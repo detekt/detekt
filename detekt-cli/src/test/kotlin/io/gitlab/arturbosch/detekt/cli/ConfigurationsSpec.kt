99a01c18ea0ed3ce88a5df0bbfbdd8dfedefc027
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-07-09 21:26:21 +0200

    Add failFast option to the configuration.

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/ConfigurationsSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/ConfigurationsSpec.kt
@@ -1,6 +1,7 @@
 package io.gitlab.arturbosch.detekt.cli
 
 import io.gitlab.arturbosch.detekt.test.resource
+import jdk.nashorn.internal.runtime.regexp.joni.encoding.CharacterType.W
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -77,4 +78,45 @@ internal class ConfigurationsSpec : Spek({
 			assertFails { Main().apply { configResource = "./i.do.not.exist.yml" }.loadConfiguration() }
 		}
 	}
+
+	describe("fail fast only") {
+		val config = Main().apply { configResource = "/configs/fail-fast-only.yml" }.loadConfiguration()
+
+		it("should override active to true by default") {
+			assertThat(config.subConfig("comments").subConfig("UndocumentedPublicClass").valueOrDefault("active", false)).isEqualTo(true)
+		}
+
+		it("should override warningThreshold to 0 by default") {
+			assertThat(config.subConfig("build").valueOrDefault("warningThreshold", -1)).isEqualTo(0)
+		}
+
+		it("should override failThreshold to 0 by default") {
+			assertThat(config.subConfig("build").valueOrDefault("failThreshold", -1)).isEqualTo(0)
+		}
+
+		it("should keep config from default") {
+			assertThat(config.subConfig("style").subConfig("MaxLineLength").valueOrDefault("maxLineLength", -1)).isEqualTo(120)
+		}
+	}
+
+	describe("fail fast override") {
+		val config = Main().apply { configResource = "/configs/fail-fast-override.yml" }.loadConfiguration()
+
+		it("should override config when specified") {
+			assertThat(config.subConfig("style").subConfig("MaxLineLength").valueOrDefault("maxLineLength", -1)).isEqualTo(100)
+		}
+
+		it("should override active when specified") {
+			assertThat(config.subConfig("comments").subConfig("CommentOverPrivateMethod").valueOrDefault("active", true)).isEqualTo(false)
+		}
+
+		it("should override warningThreshold when specified") {
+			assertThat(config.subConfig("build").valueOrDefault("warningThreshold", -1)).isEqualTo(1)
+		}
+
+		it("should override failThreshold when specified") {
+			assertThat(config.subConfig("build").valueOrDefault("failThreshold", -1)).isEqualTo(1)
+		}
+	}
+
 })
