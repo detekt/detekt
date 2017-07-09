f1deedce18dfd29b37e85d0f40c4f6b7904ef724
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-07-09 20:48:42 +0200

    Add Rule NewlineAtEndOfFile.

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/NewLineAtEndOfFile.kt
@@ -0,0 +1,26 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Dept
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.KtFile
+
+class NewLineAtEndOfFile(config: Config = Config.empty) : Rule(config) {
+
+	override val issue = Issue(javaClass.simpleName,
+			Severity.Style,
+			"Checks whether files end with a line separator.",
+			Dept.FIVE_MINS)
+
+	override fun visitKtFile(file: KtFile) {
+		val text = file.text
+		if (text.lastOrNull() != '\n') {
+			report(CodeSmell(issue, Entity.from(file, text.length - 1)))
+		}
+	}
+
+}
