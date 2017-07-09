729d68edb419fae5a10e35dcf93f597abb91989b
Ivan Balaksha <tagantroy@gmail.com>
2017-07-09 19:33:16 +0700

    implemented spread operator check

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/performance/SpreadOperator.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/performance/SpreadOperator.kt
@@ -1,10 +1,26 @@
 package io.gitlab.arturbosch.detekt.rules.performance
 
+import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtValueArgumentList
 
+/**
+ * @author Ivan Balaksha
+ */
 class SpreadOperator(config: Config = Config.empty) : Rule() {
 	override val issue: Issue = Issue("SpreadOperator", Severity.Performance, "")
+
+	override fun visitValueArgumentList(list: KtValueArgumentList) {
+		super.visitValueArgumentList(list)
+		list.arguments
+				.filter { it.firstChild.textMatches(KtTokens.MUL.value) }
+				.forEach {
+					report(CodeSmell(issue, Entity.from(list)))
+				}
+	}
 }
