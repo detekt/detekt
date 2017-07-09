705223a495732a3171a04b4e615762fb7f9924c4
Ivan Balaksha <tagantroy@gmail.com>
2017-07-09 12:40:18 +0700

    implemented rule

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LabeledExpression.kt
@@ -0,0 +1,20 @@
+package io.gitlab.arturbosch.detekt.rules.complexity
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.*
+
+class LabeledExpression(config: Config = Config.empty) : Rule(config) {
+    override val issue: Issue = Issue("LabeledExpression", Severity.Maintainability, "")
+
+    override fun visitExpressionWithLabel(expression: KtExpressionWithLabel) {
+        super.visitExpressionWithLabel(expression)
+        expression.getLabelName()?.let {
+            report(CodeSmell(issue, Entity.from(expression)))
+        }
+    }
+}
