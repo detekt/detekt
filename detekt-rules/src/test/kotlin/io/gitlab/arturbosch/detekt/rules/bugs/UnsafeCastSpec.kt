8ff40f9bcdbecbb965613d92d55aa4d500bd67d7
Ivan Balaksha <tagantroy@gmail.com>
2017-07-09 21:12:40 +0700

    Add UnsafeCast rule spec

---
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnsafeCastSpec.kt
@@ -0,0 +1,30 @@
+package io.gitlab.arturbosch.detekt.rules.bugs
+
+import io.gitlab.arturbosch.detekt.test.lint
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.subject.SubjectSpek
+import org.assertj.core.api.Assertions.assertThat
+
+class UnsafeCastSpec : SubjectSpek<UnsafeCast>({
+	subject { UnsafeCast() }
+
+	describe("check safe and unsafe casts") {
+
+		it("test unsafe cast") {
+			val code = """
+				fun test(s: Any) {
+					println(s as Int)
+				}"""
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("test safe cast") {
+			val code = """
+				fun test(s: Any) {
+					println((s as? Int) ?: 0)
+				}"""
+			assertThat(subject.lint(code)).isEmpty()
+		}
+	}
+})
