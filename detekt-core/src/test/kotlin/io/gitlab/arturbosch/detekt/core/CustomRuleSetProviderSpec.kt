aed506d7fbdd13191c08ab197e74fb796cd08425
Artur Bosch <arturbosch@gmx.de>
2017-07-17 21:58:03 +0200

    Provide a test case for custom rule sets - #214

---
+++ detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/CustomRuleSetProviderSpec.kt
@@ -0,0 +1,27 @@
+package io.gitlab.arturbosch.detekt.core
+
+import io.gitlab.arturbosch.detekt.test.resource
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import java.nio.file.Paths
+
+/**
+ * @author Artur Bosch
+ */
+class CustomRuleSetProviderSpec : Spek({
+
+	describe("custom rule sets should be loadable through jars") {
+
+		val sampleRuleSet = Paths.get(resource("sample-rule-set.jar"))
+
+		it("should load the sample provider") {
+			val settings = ProcessingSettings(path, excludeDefaultRuleSets = true, ruleSets = listOf(sampleRuleSet))
+			val detekt = DetektFacade.instance(settings)
+			val result = detekt.run()
+
+			assertThat(result.findings.keys).contains("sample")
+		}
+	}
+})
