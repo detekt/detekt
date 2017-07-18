0ad2de9a09b63055cbb0ba897f28228b208e8394
Marvin Ramin <marvin.ramin@gmail.com>
2017-07-18 21:28:36 +0200

    extract excluding parameters into Excludes class

---
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Excludes.kt
@@ -0,0 +1,12 @@
+package io.gitlab.arturbosch.detekt.api
+
+class Excludes(excludeParameter: String) {
+    private val excludes = excludeParameter
+            .split(",")
+            .map { it.trim() }
+            .filter { it.isNotBlank() }
+            .map { it.removeSuffix("*") }
+
+    fun contains(value: String) = excludes.filter { value.contains(it) }.isNotEmpty()
+    fun none(value: String) = !contains(value)
+}
