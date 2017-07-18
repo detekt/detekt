0f25372824e18759f01040388d2c084041c244a1
Marvin Ramin <marvin.ramin@gmail.com>
2017-07-18 20:56:14 +0200

    check if rules are active before applying

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/RuleExtensions.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/RuleExtensions.kt
@@ -4,6 +4,7 @@ import io.gitlab.arturbosch.detekt.api.Context
 
 inline fun <T> T.reportFindings(context: Context, rules: () -> List<SubRule<T>>) {
 	rules.invoke()
+			.filter { it.isActive() }
 			.onEach { it.apply(this) }
 			.flatMap { it.findings }
 			.apply { context.report(this) }
