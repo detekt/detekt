7f018ebbe44721f625219cec78c861851f8000e4
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-07-19 11:36:21 +0200

    Implement ForbiddenComment Rule.

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenComment.kt
@@ -0,0 +1,40 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.com.intellij.psi.PsiComment
+
+class ForbiddenComment(config: Config = Config.empty) : Rule(config) {
+
+	override val issue = Issue(javaClass.simpleName,
+			Severity.Style,
+			"Flags a forbidden comment. Defaults values are TODO:, FIXME: or STHOPSHIP:",
+			Debt.TEN_MINS)
+
+	private val values: List<String>
+			= valueOrDefault(VALUES, "TODO:,FIXME:,STOPSHIP:")
+			.split(",")
+			.filter { it.isNotBlank() }
+
+	override fun visitComment(comment: PsiComment) {
+		super.visitComment(comment)
+
+		val text = comment.text
+
+		values.forEach {
+			if (text.contains(it, ignoreCase = true)) {
+				report(CodeSmell(issue, Entity.from(comment)))
+			}
+		}
+	}
+
+	companion object {
+		const val VALUES = "values"
+	}
+}
+
---
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenCommentSpec.kt
@@ -0,0 +1,84 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.test.TestConfig
+import io.gitlab.arturbosch.detekt.test.lint
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+
+class ForbiddenCommentSpec : Spek({
+
+	val todoColon = "// TODO: I need to fix this."
+	val todo = "// TODO I need to fix this."
+
+	val fixmeColon = "// FIXME: I need to fix this."
+	val fixme = "// FIXME I need to fix this."
+
+	val stopShipColon = "// STOPSHIP: I need to fix this."
+	val stopShip = "// STOPSHIP I need to fix this."
+
+	given("the default values are configured") {
+
+		it("should report TODO: usages") {
+			val findings = ForbiddenComment().lint(todoColon)
+			assertThat(findings).hasSize(1)
+		}
+
+		it("should not report TODO usages") {
+			val findings = ForbiddenComment().lint(todo)
+			assertThat(findings).hasSize(0)
+		}
+
+		it("should report FIXME: usages") {
+			val findings = ForbiddenComment().lint(fixmeColon)
+			assertThat(findings).hasSize(1)
+		}
+
+		it("should not report FIXME usages") {
+			val findings = ForbiddenComment().lint(fixme)
+			assertThat(findings).hasSize(0)
+		}
+
+		it("should report STOPSHIP: usages") {
+			val findings = ForbiddenComment().lint(stopShipColon)
+			assertThat(findings).hasSize(1)
+		}
+
+		it("should not report STOPSHIP usages") {
+			val findings = ForbiddenComment().lint(stopShip)
+			assertThat(findings).hasSize(0)
+		}
+
+	}
+
+	given("custom default values are configured") {
+		val banana = "// Banana."
+
+		it("should not report TODO: usages") {
+			val findings = ForbiddenComment(TestConfig(mapOf(ForbiddenComment.VALUES to "Banana"))).lint(todoColon)
+			assertThat(findings).hasSize(0)
+		}
+
+		it("should not report FIXME: usages") {
+			val findings = ForbiddenComment(TestConfig(mapOf(ForbiddenComment.VALUES to "Banana"))).lint(fixmeColon)
+			assertThat(findings).hasSize(0)
+		}
+
+		it("should not report STOPME: usages") {
+			val findings = ForbiddenComment(TestConfig(mapOf(ForbiddenComment.VALUES to "Banana"))).lint(stopShipColon)
+			assertThat(findings).hasSize(0)
+		}
+
+		it("should report Banana usages") {
+			val findings = ForbiddenComment(TestConfig(mapOf(ForbiddenComment.VALUES to "Banana"))).lint(banana)
+			assertThat(findings).hasSize(1)
+		}
+
+		it("should report Banana usages regardless of case sensitive") {
+			val findings = ForbiddenComment(TestConfig(mapOf(ForbiddenComment.VALUES to "bAnAnA"))).lint(banana)
+			assertThat(findings).hasSize(1)
+		}
+
+	}
+})
