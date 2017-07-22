e72a8c73fd3e07d59450876bc074bc4f328116b2
schalkms <schalkms@icloud.com>
2017-07-22 23:33:55 +0200

    Added negative test cases for safe cast rule

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/SafeCastTest.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/SafeCastTest.kt
@@ -1,5 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
+import io.gitlab.arturbosch.detekt.api.Finding
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
@@ -30,8 +31,41 @@ class SafeCastTest {
 		findOne(code)
 	}
 
+	@Test
+	fun wrongCondition() {
+		val code = "fun test() {\n" +
+				"        if (element == other) {\n" +
+				"            element\n" +
+				"        } else {\n" +
+				"            null\n" +
+				"        }\n" +
+				"    }"
+		findNone(code)
+	}
+
+	@Test
+	fun wrongElseClause() {
+		val code = "fun test() {\n" +
+				"        if (element is KtElement) {\n" +
+				"            element\n" +
+				"        } else {\n" +
+				"            KtElement()\n" +
+				"        }\n" +
+				"    }"
+		findNone(code)
+	}
+
 	private fun findOne(code: String) {
-		val findings = SafeCast().lint(code)
+		val findings = retrieveFindings(code)
 		Assertions.assertThat(findings.size).isEqualTo(1)
+	}
+
+	private fun findNone(code: String) {
+		val findings = retrieveFindings(code)
+		Assertions.assertThat(findings.size).isEqualTo(0)
+	}
+
+	private fun retrieveFindings(code: String): List<Finding> {
+		return SafeCast().lint(code)
 	}
 }
