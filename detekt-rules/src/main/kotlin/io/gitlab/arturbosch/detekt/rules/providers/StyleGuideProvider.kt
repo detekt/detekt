95c7d6ac3b4f0a37f0254039345dea5b23df0ab7
schalkms <schalkms@icloud.com>
2017-07-23 11:51:59 +0200

    Applied code style for safe cast rule

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/StyleGuideProvider.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/StyleGuideProvider.kt
@@ -3,7 +3,12 @@ package io.gitlab.arturbosch.detekt.rules.providers
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
-import io.gitlab.arturbosch.detekt.rules.style.*
+import io.gitlab.arturbosch.detekt.rules.style.ForbiddenComment
+import io.gitlab.arturbosch.detekt.rules.style.MaxLineLength
+import io.gitlab.arturbosch.detekt.rules.style.NamingConventionViolation
+import io.gitlab.arturbosch.detekt.rules.style.NewLineAtEndOfFile
+import io.gitlab.arturbosch.detekt.rules.style.SafeCast
+import io.gitlab.arturbosch.detekt.rules.style.WildcardImport
 
 /**
  * @author Artur Bosch
--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/SafeCastTest.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/SafeCastTest.kt
@@ -1,71 +1,78 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
 import io.gitlab.arturbosch.detekt.api.Finding
+import io.gitlab.arturbosch.detekt.test.RuleTest
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
-class SafeCastTest {
+class SafeCastTest : RuleTest {
+
+	override val rule = SafeCast()
 
 	@Test
 	fun findNegated() {
-		val code = "fun test() {\n" +
-				"        if (element !is KtElement) {\n" +
-				"            null\n" +
-				"        } else {\n" +
-				"            element\n" +
-				"        }\n" +
-				"    }"
+		val code = """
+				fun test() {
+					if (element !is KtElement) {
+						null
+					} else {
+						element
+					}
+				}
+		"""
 		findOne(code)
 	}
 
 	@Test
 	fun find() {
-		val code = "fun test() {\n" +
-				"        if (element is KtElement) {\n" +
-				"            element\n" +
-				"        } else {\n" +
-				"            null\n" +
-				"        }\n" +
-				"    }"
+		val code = """
+				fun test() {
+					if (element is KtElement) {
+						element
+					} else {
+						null
+					}
+				}
+		"""
 		findOne(code)
 	}
 
 	@Test
 	fun wrongCondition() {
-		val code = "fun test() {\n" +
-				"        if (element == other) {\n" +
-				"            element\n" +
-				"        } else {\n" +
-				"            null\n" +
-				"        }\n" +
-				"    }"
+		val code = """
+				fun test() {
+					if (element == other) {
+						element
+					} else {
+						null
+					}
+				}
+		"""
 		findNone(code)
 	}
 
 	@Test
 	fun wrongElseClause() {
-		val code = "fun test() {\n" +
-				"        if (element is KtElement) {\n" +
-				"            element\n" +
-				"        } else {\n" +
-				"            KtElement()\n" +
-				"        }\n" +
-				"    }"
+		val code = """
+				fun test() {
+					if (element is KtElement) {
+						element
+					} else {
+						KtElement()
+					}
+				}
+		"""
 		findNone(code)
 	}
 
 	private fun findOne(code: String) {
-		val findings = retrieveFindings(code)
+		val findings = rule.lint(code)
 		Assertions.assertThat(findings.size).isEqualTo(1)
 	}
 
 	private fun findNone(code: String) {
-		val findings = retrieveFindings(code)
+		val findings = rule.lint(code)
 		Assertions.assertThat(findings.size).isEqualTo(0)
-	}
-
-	private fun retrieveFindings(code: String): List<Finding> {
-		return SafeCast().lint(code)
 	}
 }
