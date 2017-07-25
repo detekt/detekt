d19dc81f4be87e18c6b46677cc8ba6f50996216a
Artur Bosch <arturbosch@gmx.de>
2017-07-25 15:50:28 +0200

    Move output format to api module

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/out/format/OutputFormat.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/out/format/OutputFormat.kt
@@ -1,28 +1,10 @@
 package io.gitlab.arturbosch.detekt.cli.out.format
 
-import io.gitlab.arturbosch.detekt.api.Finding
-import java.nio.file.Files
-import java.nio.file.Path
+import io.gitlab.arturbosch.detekt.api.OutputFormat
 
 /**
  * @author Artur Bosch
  */
-abstract class OutputFormat {
-
-	open val id: String = javaClass.simpleName
-	open val priority: Int = -1
-
-	fun write(report: Path, smells: List<Finding>) {
-		val smellData = render(smells)
-		smellData?.let {
-			report.parent?.let { Files.createDirectories(it) }
-			Files.write(report, it.toByteArray())
-		}
-	}
-
-	abstract fun render(smells: List<Finding>): String?
-}
-
 enum class Formatter {
 	PLAIN,
 	XML;
