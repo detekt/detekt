7aca3aed64c0e21225750de0c042b6bb8b74e98f
Artur Bosch <arturbosch@gmx.de>
2017-07-28 14:06:16 +0200

    Notify the user about writing output reports

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/OutputFacade.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/OutputFacade.kt
@@ -13,6 +13,7 @@ class OutputFacade(private val arguments: Args,
 				   private val detektion: Detektion,
 				   private val settings: ProcessingSettings) {
 
+	private val printStream = System.out
 	private val config = settings.config
 	private val baselineFacade = arguments.baseline?.let { BaselineFacade(it) }
 	private val createBaseline = arguments.createBaseline
@@ -31,10 +32,21 @@ class OutputFacade(private val arguments: Args,
 		reports.sortedBy { it.priority }.asReversed().forEach { report ->
 			report.init(config)
 			when (report) {
-				is ConsoleReport -> report.print(System.out, result)
-				is OutputReport -> arguments.output?.apply { report.write(this, result) }
+				is ConsoleReport -> handleConsoleReport(report, result)
+				is OutputReport -> handleOutputReport(report, result)
 			}
 		}
+	}
+
+	private fun handleOutputReport(report: OutputReport, result: Detektion) {
+		arguments.output?.let {
+			report.write(it, result)
+			printStream.println("Successfully generated ${report.id}.")
+		}
+	}
+
+	private fun handleConsoleReport(report: ConsoleReport, result: Detektion) {
+		report.print(printStream, result)
 	}
 }
 
