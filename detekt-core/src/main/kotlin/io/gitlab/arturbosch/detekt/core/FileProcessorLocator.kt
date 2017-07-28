9828713892cb1c30f95653a97433c724a2a7efc9
Artur Bosch <arturbosch@gmx.de>
2017-07-28 13:28:31 +0200

    Allow to disable processors

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/FileProcessorLocator.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/FileProcessorLocator.kt
@@ -13,11 +13,16 @@ class FileProcessorLocator(settings: ProcessingSettings) {
 
 	private val plugins: Array<URL> = settings.pluginUrls
 	private val config: Config = settings.config
+	private val processorsActive = config.subConfig("processors")
+			.valueOrDefault("active", true)
 
 	fun load(): List<FileProcessListener> {
 		val detektLoader = URLClassLoader(plugins, javaClass.classLoader)
-		return ServiceLoader.load(FileProcessListener::class.java, detektLoader)
-				.onEach { it.init(config) }
-				.toList()
+		return if (processorsActive)
+			ServiceLoader.load(FileProcessListener::class.java, detektLoader)
+					.onEach { it.init(config) }
+					.toList()
+		else
+			emptyList()
 	}
 }
