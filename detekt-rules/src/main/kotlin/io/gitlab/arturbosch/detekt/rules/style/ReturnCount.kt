4ab7806884b0bccb5109314a4962091ba43c1df4
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-07-28 17:27:25 +0200

    Add ReturnCountRule.

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ReturnCount.kt
@@ -0,0 +1,35 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.rules.collectByType
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtReturnExpression
+
+class ReturnCount(config: Config = Config.empty) : Rule(config) {
+
+	override val issue = Issue(javaClass.simpleName, Severity.Style,
+			"Restricts the number of return statements in methods.", Debt.TEN_MINS)
+
+	private val max = valueOrDefault(MAX, 2)
+
+	override fun visitNamedFunction(function: KtNamedFunction) {
+		super.visitNamedFunction(function)
+
+		val numberOfReturns = function.collectByType<KtReturnExpression>()
+				.count { it is KtReturnExpression }
+
+		if (numberOfReturns > max) {
+			report(CodeSmell(issue, Entity.from(function)))
+		}
+	}
+
+	companion object {
+	  const val MAX = "max"
+	}
+}
