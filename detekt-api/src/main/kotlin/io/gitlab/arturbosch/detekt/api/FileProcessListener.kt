6fbc796dfd5e721c76dae17906188b06b4de1a26
Artur Bosch <arturbosch@gmx.de>
2017-07-28 13:33:52 +0200

    Allow to exclude processors with id

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/FileProcessListener.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/FileProcessListener.kt
@@ -3,11 +3,28 @@ package io.gitlab.arturbosch.detekt.api
 import org.jetbrains.kotlin.psi.KtFile
 
 /**
+ * Gather additional metrics about the analyzed kotlin file.
+ * Pay attention to the thread policy of each function!
+ *
  * @author Artur Bosch
  */
 @Suppress("EmptyFunctionBlock")
 interface FileProcessListener : Extension {
+
+	/**
+	 * Use this to gather some additional information for the real onProcess function.
+	 * This calculation should be lightweight as this method is called from the main thread.
+	 */
 	fun onStart(files: List<KtFile>) {}
+
+	/**
+	 * This method is called from a thread pool thread. Heavy computations allowed.
+	 */
 	fun onProcess(file: KtFile) {}
+
+	/**
+	 * Mainly use this method to save computed metrics from KtFile's to the {@link Detektion} container.
+	 * Do not do heavy computations here as this method is called from the main thread.
+	 */
 	fun onFinish(files: List<KtFile>, result: Detektion) {}
 }
