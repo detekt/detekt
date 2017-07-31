163513de874aa110bb194a2291355a5a831b2343
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-07-31 16:38:56 +0200

    Better description.

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenImport.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenImport.kt
@@ -13,7 +13,8 @@ import org.jetbrains.kotlin.psi.KtImportList
 class ForbiddenImport(config: Config = Config.empty) : Rule(config) {
 
 	override val issue = Issue(javaClass.simpleName, Severity.Style,
-			"Mark forbidden imports.", Debt.TEN_MINS)
+			"Mark forbidden imports. A forbidden import could be an import for an unstable / experimental api" +
+					"and hence you might want to mark it as forbidden in order to get warned about the usage.", Debt.TEN_MINS)
 
 	private val forbiddenImports = Excludes(valueOrDefault(IMPORTS, ""))
 
