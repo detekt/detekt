4ccf2659d16567513cb9b22b69b0be987686d338
schalkms <schalkms@icloud.com>
2017-07-31 16:20:25 +0200

    Added test cases with return labels

--- detekt-rules/src/test/resources/cases/UnreachableCode.kt
+++ detekt-rules/src/test/resources/cases/UnreachableCode.kt
@@ -18,6 +18,23 @@ class UnreachableCode {
 		return false
 	}
 
+	fun returnLabel1(ints: List<Int>): List<Int> {
+		return ints.map f@{
+			if (it == 0) {
+				return@f 0
+				println()
+			}
+			return@f 1
+		}
+	}
+
+	fun returnLabel2(ints: List<Int>) {
+		ints.forEach {
+			if (it == 0) return@forEach
+			println()
+		}
+	}
+
 	fun throw1(p: Int): Int {
 		if (p == 0) {
 			throw IllegalArgumentException()
