45eaf3edc3ce44f4a13e0844afbe93214dfec47f
Sebastiano Poggi <poggos@gmail.com>
2017-08-05 17:22:26 +0100

    Address bugs in MagicNumber check:  * flagging non-numeric constant expressions (e.g., "true")  * inability to ignore floating point values correctly  * incorrect hex number parsing  * silently ignores invalid configuration parameters  * ignores negative values when the corresponding positive number is    ignored (e.g., -2 is ignored when 2 is in ignoredNumbers)  * floating point numbers are not correctly parsed in some cases  * numeric literals that contain underscores are not correctly parsed

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/MagicNumberSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/MagicNumberSpec.kt
@@ -6,18 +6,19 @@ import org.assertj.core.api.Java6Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertFailsWith
 
 class MagicNumberSpec : Spek({
 
 	given("a float of 1") {
 		val code = "val myFloat = 1.0f"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should be reported when ignoreNumbers are overridden") {
+		it("should be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(1)
 		}
@@ -26,12 +27,12 @@ class MagicNumberSpec : Spek({
 	given("a const float of 1") {
 		val code = "const val MY_FLOAT = 1.0f"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should not be reported when ignoreNumbers are overridden") {
+		it("should not be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(0)
 		}
@@ -40,12 +41,12 @@ class MagicNumberSpec : Spek({
 	given("an integer of 1") {
 		val code = "val myInt = 1"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should be reported when ignoreNumbers are overridden") {
+		it("should be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(1)
 		}
@@ -54,12 +55,12 @@ class MagicNumberSpec : Spek({
 	given("a const integer of 1") {
 		val code = "const val MY_INT = 1"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should not be reported when ignoreNumbers are overridden") {
+		it("should not be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(0)
 		}
@@ -68,13 +69,51 @@ class MagicNumberSpec : Spek({
 	given("a long of 1") {
 		val code = "val myLong = 1L"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should be reported when ignoreNumbers are overridden") {
+		it("should be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
+			assertThat(findings).hasSize(1)
+		}
+	}
+
+	given("a long of -1") {
+		val code = "val myLong = -1L"
+
+		it("should not be reported by default") {
+			val findings = MagicNumber().lint(code)
+			assertThat(findings).hasSize(0)
+		}
+
+		it("should be reported when ignoredNumbers is empty") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
+			assertThat(findings).hasSize(1)
+		}
+	}
+
+	given("a long of -2") {
+		val code = "val myLong = -2L"
+
+		it("should be reported by default") {
+			val findings = MagicNumber().lint(code)
+			assertThat(findings).hasSize(1)
+		}
+
+		it("should be ignored when ignoredNumbers contains it verbatim") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "-2L"))).lint(code)
+			assertThat(findings).isEmpty()
+		}
+
+		it("should be ignored when ignoredNumbers contains it as floating point") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "-2f"))).lint(code)
+			assertThat(findings).isEmpty()
+		}
+
+		it("should not be ignored when ignoredNumbers contains 2 but not -2") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "1,2,3,-1,0"))).lint(code)
 			assertThat(findings).hasSize(1)
 		}
 	}
@@ -82,12 +121,12 @@ class MagicNumberSpec : Spek({
 	given("a const long of 1") {
 		val code = "const val MY_LONG = 1L"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should not be reported when ignoreNumbers are overridden") {
+		it("should not be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(0)
 		}
@@ -96,12 +135,12 @@ class MagicNumberSpec : Spek({
 	given("a double of 1") {
 		val code = "val myDouble = 1.0"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should be reported when ignoreNumbers are overridden") {
+		it("should be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(1)
 		}
@@ -110,12 +149,12 @@ class MagicNumberSpec : Spek({
 	given("a const double of 1") {
 		val code = "const val MY_DOUBLE = 1.0"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should not be reported when ignoreNumbers are overridden") {
+		it("should not be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(0)
 		}
@@ -124,12 +163,12 @@ class MagicNumberSpec : Spek({
 	given("a hex of 1") {
 		val code = "val myHex = 0x1"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should be reported when ignoreNumbers are overridden") {
+		it("should be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(1)
 		}
@@ -138,12 +177,12 @@ class MagicNumberSpec : Spek({
 	given("a const hex of 1") {
 		val code = "const val MY_HEX = 0x1"
 
-		it("it should not be reported by default") {
+		it("should not be reported by default") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(0)
 		}
 
-		it("it should not be reported when ignoreNumbers are overridden") {
+		it("should not be reported when ignoredNumbers is empty") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ""))).lint(code)
 			assertThat(findings).hasSize(0)
 		}
@@ -152,16 +191,54 @@ class MagicNumberSpec : Spek({
 	given("an integer of 300") {
 		val code = "val myInt = 300"
 
-		it("it should not be reported when ignoreNumbers contains 300") {
+		it("should not be reported when ignoredNumbers contains 300") {
 			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "300"))).lint(code)
 			assertThat(findings).hasSize(0)
+		}
+
+		it("should not be reported when ignoredNumbers contains a floating point 300") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "300.0"))).lint(code)
+			assertThat(findings).hasSize(0)
+		}
+	}
+
+	given("a binary literal") {
+		val code = "val myBinary = 0b01001"
+
+		it("should not be reported") {
+			val findings = MagicNumber().lint(code)
+			assertThat(findings).hasSize(0)
+		}
+	}
+
+	given("an integer literal with underscores") {
+		val code = "val myInt = 100_000"
+
+		it("should be reported by default") {
+			val findings = MagicNumber().lint(code)
+			assertThat(findings).hasSize(1)
+		}
+
+		it("should not be reported when ignored verbatim") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "100_000"))).lint(code)
+			assertThat(findings).isEmpty()
+		}
+
+		it("should not be reported when ignored with different underscores") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "10_00_00"))).lint(code)
+			assertThat(findings).isEmpty()
+		}
+
+		it("should not be reported when ignored without underscores") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "100000"))).lint(code)
+			assertThat(findings).isEmpty()
 		}
 	}
 
 	given("an if statement with magic numbers") {
 		val code = "val myInt = if (5 < 6) 7 else 8"
 
-		it("it should be reported") {
+		it("should be reported") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(4)
 		}
@@ -178,7 +255,7 @@ class MagicNumberSpec : Spek({
 			}
 		"""
 
-		it("it should be reported") {
+		it("should be reported") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(6)
 		}
@@ -191,9 +268,61 @@ class MagicNumberSpec : Spek({
 			}
 		"""
 
-		it("it should be reported") {
+		it("should be reported") {
 			val findings = MagicNumber().lint(code)
 			assertThat(findings).hasSize(1)
+		}
+	}
+
+	given("a boolean value") {
+		val code = """
+			fun test() : Boolean {
+				return true;
+			}
+		"""
+
+		it("should not be reported") {
+			val findings = MagicNumber().lint(code)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a non-numeric constant expression") {
+		val code = "val surprise = true"
+
+		it("should not be reported") {
+			val findings = MagicNumber().lint(code)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a float of 0.5") {
+		val code = "val test = 0.5f"
+
+		it("should be reported by default") {
+			val findings = MagicNumber().lint(code)
+			assertThat(findings).hasSize(1)
+		}
+
+		it("should not be reported when ignoredNumbers contains it") {
+			val findings = MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to ".5"))).lint(code)
+			assertThat(findings).hasSize(0)
+		}
+	}
+
+	given("an invalid ignoredNumber") {
+
+		it("throws a NumberFormatException") {
+			assertFailsWith(NumberFormatException::class) {
+				MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "banana")))
+			}
+		}
+	}
+
+	given("an empty ignoredNumber") {
+
+		it("doesn't throw an exception") {
+			MagicNumber(TestConfig(mapOf(MagicNumber.IGNORED_NUMBERS to "")))
 		}
 	}
 })
