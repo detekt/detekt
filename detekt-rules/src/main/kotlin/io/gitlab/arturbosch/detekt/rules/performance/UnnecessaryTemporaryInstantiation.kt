6030b1333e29b64d034f080ca0e86e47bcf82f1c
schalkms <schalkms@icloud.com>
2017-08-07 21:57:26 +0200

    Refactored TemporaryInstantiation rule and test

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/performance/UnnecessaryTemporaryInstantiation.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/performance/UnnecessaryTemporaryInstantiation.kt
@@ -6,18 +6,25 @@ import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtExpression
 
-class UnnecessaryConversionTemporary(config: Config = Config.empty) : Rule(config) {
+class UnnecessaryTemporaryInstantiation(config: Config = Config.empty) : Rule(config) {
 
-	override val issue: Issue = Issue("UnnecessaryConversionTemporary", Severity.Performance,
+	override val issue: Issue = Issue("UnnecessaryTemporaryInstantiation", Severity.Performance,
 			"Avoid temporary objects when converting primitive types to String")
 
 	private val types: Set<String> = hashSetOf("Boolean", "Byte", "Short", "Integer", "Long", "Float", "Double")
 
 	override fun visitCallExpression(expression: KtCallExpression) {
-		if (types.contains(expression.calleeExpression?.text) && expression.nextSibling?.nextSibling?.text == "toString()") {
+		if (isPrimitiveWrapperType(expression.calleeExpression)
+				&& isToStringMethod(expression.nextSibling?.nextSibling)) {
 			report(CodeSmell(issue, Entity.from(expression)))
 		}
 	}
+
+	private fun isPrimitiveWrapperType(expression: KtExpression?) = types.contains(expression?.text)
+
+	private fun isToStringMethod(element: PsiElement?) = element?.text == "toString()"
 }
