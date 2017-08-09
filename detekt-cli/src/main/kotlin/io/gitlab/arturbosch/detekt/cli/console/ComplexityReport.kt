46c762399b980192076ee58a117387788b35b2a0
schalkms <schalkms@icloud.com>
2017-08-09 22:03:39 +0200

    Added source comment ratio to report

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/ComplexityReport.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/ComplexityReport.kt
@@ -24,24 +24,28 @@ class ComplexityReport : ConsoleReport() {
 		val sloc = detektion.getData(SLOC_KEY)
 		val lloc = detektion.getData(LLOC_KEY)
 		val cloc = detektion.getData(NUMBER_OF_COMMENT_LINES_KEY)
-		if (mcc != null && lloc != null && lloc > 0) {
-			val numberOfSmells = findings.entries.sumBy { it.value.size }
-			val smellPerThousandLines = numberOfSmells * ONE_THOUSAND / lloc
-			val mccPerThousandLines = mcc * ONE_THOUSAND / lloc
-			return with(StringBuilder()) {
-				append("Complexity Report:".format())
-				append("$loc lines of code (loc)".format(PREFIX))
-				append("$sloc source lines of code (sloc)".format(PREFIX))
-				append("$lloc logical lines of code (lloc)".format(PREFIX))
-				append("$cloc comment lines of code (cloc)".format(PREFIX))
-				append("$mcc McCabe complexity (mcc)".format(PREFIX))
-				append("$numberOfSmells number of total code smells".format(PREFIX))
-				append("$mccPerThousandLines mcc per 1000 lloc".format(PREFIX))
-				append("$smellPerThousandLines code smells per 1000 lloc".format(PREFIX))
-				toString()
-			}
-		} else {
-			return null
+
+		if (mcc == null) return null
+		if (lloc == null || lloc == 0) return null
+		if (sloc == null || sloc == 0) return null
+		if (cloc == null) return null
+
+		val numberOfSmells = findings.entries.sumBy { it.value.size }
+		val smellPerThousandLines = numberOfSmells * 1000 / lloc
+		val mccPerThousandLines = mcc * 1000 / lloc
+		val commentSourceRatio = cloc * 100 / sloc
+		return with(StringBuilder()) {
+			append("Complexity Report:".format())
+			append("$loc lines of code (loc)".format(PREFIX))
+			append("$sloc source lines of code (sloc)".format(PREFIX))
+			append("$lloc logical lines of code (lloc)".format(PREFIX))
+			append("$cloc comment lines of code (cloc)".format(PREFIX))
+			append("$commentSourceRatio comment source ratio".format(PREFIX))
+			append("$mcc McCabe complexity (mcc)".format(PREFIX))
+			append("$numberOfSmells number of total code smells".format(PREFIX))
+			append("$mccPerThousandLines mcc per 1000 lloc".format(PREFIX))
+			append("$smellPerThousandLines code smells per 1000 lloc".format(PREFIX))
+			toString()
 		}
 	}
 
