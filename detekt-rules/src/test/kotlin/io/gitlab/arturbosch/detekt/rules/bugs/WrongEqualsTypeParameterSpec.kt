d5fca92daf03c2263e29af7739de1b5f82157fa9
schalkms <schalkms@icloud.com>
2017-08-12 20:52:06 +0200

    Added EqualsAlwaysReturnsTrueOrFalse rule

---
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/WrongEqualsTypeParameterSpec.kt
@@ -0,0 +1,35 @@
+package io.gitlab.arturbosch.detekt.rules.bugs
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.test.lint
+import org.assertj.core.api.Assertions
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.subject.SubjectSpek
+
+class WrongEqualsTypeParameterSpec  : SubjectSpek<WrongEqualsTypeParameter>({
+	subject { WrongEqualsTypeParameter(Config.empty) }
+
+	given("an equals method") {
+
+		it("uses Any? as parameter") {
+			val code = """
+				class A {
+					override fun equals(other: Any?): Boolean {
+						return super.equals(other)
+					}
+				}"""
+			Assertions.assertThat(subject.lint(code).size).isEqualTo(0)
+		}
+
+		it("uses String as parameter") {
+			val code = """
+				class A {
+					fun equals(other: String): Boolean {
+						return super.equals(other)
+					}
+				}"""
+			Assertions.assertThat(subject.lint(code).size).isEqualTo(1)
+		}
+	}
+})
