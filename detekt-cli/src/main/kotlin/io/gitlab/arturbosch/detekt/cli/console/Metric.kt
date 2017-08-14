e07855276477b72d7dc7b89597fac86a350fe684
schalkms <schalkms@icloud.com>
2017-08-14 15:04:51 +0200

    Refactored complexity report

---
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/Metric.kt
@@ -0,0 +1,18 @@
+package io.gitlab.arturbosch.detekt.cli.console
+
+import io.gitlab.arturbosch.detekt.api.Detektion
+import io.gitlab.arturbosch.detekt.core.processors.COMPLEXITY_KEY
+import io.gitlab.arturbosch.detekt.core.processors.LLOC_KEY
+import io.gitlab.arturbosch.detekt.core.processors.LOC_KEY
+import io.gitlab.arturbosch.detekt.core.processors.NUMBER_OF_COMMENT_LINES_KEY
+import io.gitlab.arturbosch.detekt.core.processors.SLOC_KEY
+
+internal class Metric(detektion: Detektion) {
+
+	val mcc = detektion.getData(COMPLEXITY_KEY)
+	val loc = detektion.getData(LOC_KEY)
+	val sloc = detektion.getData(SLOC_KEY)
+	val lloc = detektion.getData(LLOC_KEY)
+	val cloc = detektion.getData(NUMBER_OF_COMMENT_LINES_KEY)
+	val findings = detektion.findings.entries
+}
---
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/ReportGenerator.kt
@@ -0,0 +1,50 @@
+package io.gitlab.arturbosch.detekt.cli.console
+
+import io.gitlab.arturbosch.detekt.api.Detektion
+import io.gitlab.arturbosch.detekt.api.PREFIX
+import io.gitlab.arturbosch.detekt.api.format
+
+internal class ReportGenerator(private val metric: Metric) {
+
+	private var numberOfSmells = 0
+	private var smellPerThousandLines = 0
+	private var mccPerThousandLines = 0
+	private var commentSourceRatio = 0
+
+	companion object Factory {
+		fun create(detektion: Detektion): ReportGenerator = ReportGenerator(Metric(detektion))
+	}
+
+	fun generate(): String? {
+		if (cannotGenerate()) return null
+		return with(StringBuilder()) {
+			append("Complexity Report:".format())
+			append("${metric.loc} lines of code (loc)".format(PREFIX))
+			append("${metric.sloc} source lines of code (sloc)".format(PREFIX))
+			append("${metric.lloc} logical lines of code (lloc)".format(PREFIX))
+			append("${metric.cloc} comment lines of code (cloc)".format(PREFIX))
+			append("${metric.mcc} McCabe complexity (mcc)".format(PREFIX))
+			append("$numberOfSmells number of total code smells".format(PREFIX))
+			append("$commentSourceRatio comment source ratio".format(PREFIX))
+			append("$mccPerThousandLines mcc per 1000 lloc".format(PREFIX))
+			append("$smellPerThousandLines code smells per 1000 lloc".format(PREFIX))
+			toString()
+		}
+	}
+
+	private fun cannotGenerate(): Boolean {
+		return when {
+			metric.mcc == null -> true
+			metric.lloc == null || metric.lloc == 0 -> true
+			metric.sloc == null || metric.sloc == 0 -> true
+			metric.cloc == null -> true
+			else -> {
+				numberOfSmells = metric.findings.sumBy { it.value.size }
+				smellPerThousandLines = numberOfSmells * 1000 / metric.lloc
+				mccPerThousandLines = metric.mcc * 1000 / metric.lloc
+				commentSourceRatio = metric.cloc * 100 / metric.sloc
+				false
+			}
+		}
+	}
+}
