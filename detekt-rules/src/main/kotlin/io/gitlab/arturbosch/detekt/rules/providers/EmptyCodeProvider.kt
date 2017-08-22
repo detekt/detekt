562dbd85f0226dab7e0574473ba2f39e16ddf8c3
Artur Bosch <arturbosch@gmx.de>
2017-08-22 22:22:02 +0200

    Visit condition of empty block rules before running them - #95

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/EmptyCodeProvider.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/EmptyCodeProvider.kt
@@ -3,16 +3,7 @@ package io.gitlab.arturbosch.detekt.rules.providers
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyCatchBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyClassBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyDoWhileBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyElseBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyFinallyBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyForBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyFunctionBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyIfBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyWhenBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyWhileBlock
+import io.gitlab.arturbosch.detekt.rules.empty.EmptyBlocks
 
 /**
  * @author Artur Bosch
@@ -23,16 +14,7 @@ class EmptyCodeProvider : RuleSetProvider {
 
 	override fun instance(config: Config): RuleSet {
 		return RuleSet(ruleSetId, listOf(
-				EmptyCatchBlock(config),
-				EmptyFinallyBlock(config),
-				EmptyIfBlock(config),
-				EmptyDoWhileBlock(config),
-				EmptyWhileBlock(config),
-				EmptyForBlock(config),
-				EmptyElseBlock(config),
-				EmptyFunctionBlock(config),
-				EmptyClassBlock(config),
-				EmptyWhenBlock(config)
+				EmptyBlocks(config)
 		))
 	}
 
