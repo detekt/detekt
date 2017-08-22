d3d6c83b8056464b4df3c623b62d8df59e297925
Artur Bosch <arturbosch@gmx.de>
2017-08-22 12:30:50 +0200

    Add test case for deactivated empty block rules - #95

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyBlocksMultiRuleTest.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyBlocksMultiRuleTest.kt
@@ -1,8 +1,11 @@
 package io.gitlab.arturbosch.detekt.rules.empty
 
+import io.gitlab.arturbosch.detekt.api.YamlConfig
 import io.gitlab.arturbosch.detekt.rules.Case
+import io.gitlab.arturbosch.detekt.rules.providers.EmptyCodeProvider
 import io.gitlab.arturbosch.detekt.test.compileForTest
 import io.gitlab.arturbosch.detekt.test.lint
+import io.gitlab.arturbosch.detekt.test.resource
 import org.assertj.core.api.Assertions
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
@@ -22,7 +25,16 @@ class EmptyBlocksMultiRuleTest : SubjectSpek<EmptyBlocks>({
 		it("should report one finding per rule") {
 			val findings = subject.lint(file)
 			val rulesSize = subject.rules.size - 1 // no primary constructor
-			Assertions.assertThat(findings.size).isEqualTo(rulesSize)
+			Assertions.assertThat(findings).hasSize(rulesSize)
+		}
+
+		it("should not report as all empty block rules are deactivated") {
+			val config = YamlConfig.loadResource(resource("deactive-empty-blocks.yml").toURL())
+			val ruleSet = EmptyCodeProvider().buildRuleset(config)
+
+			val findings = ruleSet?.accept(file)
+
+			Assertions.assertThat(findings).hasSize(0)
 		}
 	}
 })
