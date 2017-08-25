ededb68920602263e252be4c3554b8e69d1607fc
Artur Bosch <arturbosch@gmx.de>
2017-08-25 17:21:58 +0200

    Remove some empty rules from style provider which do belong to the empty-blocks rule set

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/StyleGuideProvider.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/StyleGuideProvider.kt
@@ -3,16 +3,12 @@ package io.gitlab.arturbosch.detekt.rules.providers
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyDefaultConstructor
-import io.gitlab.arturbosch.detekt.rules.empty.EmptyInitBlock
-import io.gitlab.arturbosch.detekt.rules.empty.EmptySecondaryConstructor
 import io.gitlab.arturbosch.detekt.rules.style.EqualsNullCall
 import io.gitlab.arturbosch.detekt.rules.style.FileParsingRule
 import io.gitlab.arturbosch.detekt.rules.style.ForbiddenComment
 import io.gitlab.arturbosch.detekt.rules.style.ForbiddenImport
 import io.gitlab.arturbosch.detekt.rules.style.MagicNumber
 import io.gitlab.arturbosch.detekt.rules.style.ModifierOrder
-import io.gitlab.arturbosch.detekt.rules.style.naming.NamingRules
 import io.gitlab.arturbosch.detekt.rules.style.NewLineAtEndOfFile
 import io.gitlab.arturbosch.detekt.rules.style.OptionalAbstractKeyword
 import io.gitlab.arturbosch.detekt.rules.style.OptionalWhenBraces
@@ -21,6 +17,7 @@ import io.gitlab.arturbosch.detekt.rules.style.ReturnCount
 import io.gitlab.arturbosch.detekt.rules.style.SafeCast
 import io.gitlab.arturbosch.detekt.rules.style.UnnecessaryParentheses
 import io.gitlab.arturbosch.detekt.rules.style.WildcardImport
+import io.gitlab.arturbosch.detekt.rules.style.naming.NamingRules
 
 /**
  * @author Artur Bosch
@@ -45,10 +42,7 @@ class StyleGuideProvider : RuleSetProvider {
 				OptionalWhenBraces(config),
 				ProtectedMemberInFinalClass(config),
 				MagicNumber(config),
-				ModifierOrder(config),
-				EmptyInitBlock(config),
-				EmptyDefaultConstructor(config),
-				EmptySecondaryConstructor(config)
+				ModifierOrder(config)
 		))
 	}
 }
