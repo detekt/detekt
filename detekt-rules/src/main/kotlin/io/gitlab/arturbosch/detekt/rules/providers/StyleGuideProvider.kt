4c4cb4d06f5b138f605a053512d117af7abbfb0b
Ivan Balaksha <ivan.balaksha@agoda.com>
2017-08-31 11:14:36 +0700

    fix codestyle

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/StyleGuideProvider.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/StyleGuideProvider.kt
@@ -3,7 +3,21 @@ package io.gitlab.arturbosch.detekt.rules.providers
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
-import io.gitlab.arturbosch.detekt.rules.style.*
+import io.gitlab.arturbosch.detekt.rules.style.DataClassContainsFunctionsRule
+import io.gitlab.arturbosch.detekt.rules.style.EqualsNullCall
+import io.gitlab.arturbosch.detekt.rules.style.FileParsingRule
+import io.gitlab.arturbosch.detekt.rules.style.ForbiddenComment
+import io.gitlab.arturbosch.detekt.rules.style.ForbiddenImport
+import io.gitlab.arturbosch.detekt.rules.style.MagicNumber
+import io.gitlab.arturbosch.detekt.rules.style.ModifierOrder
+import io.gitlab.arturbosch.detekt.rules.style.NewLineAtEndOfFile
+import io.gitlab.arturbosch.detekt.rules.style.OptionalAbstractKeyword
+import io.gitlab.arturbosch.detekt.rules.style.OptionalWhenBraces
+import io.gitlab.arturbosch.detekt.rules.style.ProtectedMemberInFinalClass
+import io.gitlab.arturbosch.detekt.rules.style.ReturnCount
+import io.gitlab.arturbosch.detekt.rules.style.SafeCast
+import io.gitlab.arturbosch.detekt.rules.style.UnnecessaryParentheses
+import io.gitlab.arturbosch.detekt.rules.style.WildcardImport
 import io.gitlab.arturbosch.detekt.rules.style.naming.NamingRules
 
 /**
