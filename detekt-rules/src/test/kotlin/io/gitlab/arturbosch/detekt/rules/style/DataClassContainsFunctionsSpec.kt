099cd7c200a3f270081cb1069ada148f54e128b9
Ivan Balaksha <ivan.balaksha@agoda.com>
2017-08-31 11:14:36 +0700

    fix codestyle

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/DataClassContainsFunctionsSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/DataClassContainsFunctionsSpec.kt
@@ -11,27 +11,27 @@ import org.jetbrains.spek.subject.SubjectSpek
  */
 
 class DataClassContainsFunctionsSpec : SubjectSpek<DataClassContainsFunctionsRule>({
-    subject { DataClassContainsFunctionsRule() }
+	subject { DataClassContainsFunctionsRule() }
 
-    describe("") {
+	describe("") {
 
-        it("valid data class") {
-            val code = """data class Test(val id : String)"""
-            Assertions.assertThat(subject.lint(code)).hasSize(0)
-        }
+		it("valid data class") {
+			val code = """data class Test(val id : String)"""
+			Assertions.assertThat(subject.lint(code)).hasSize(0)
+		}
 
-        it("data class contains printTest function") {
-            val code = """
+		it("data class contains printTest function") {
+			val code = """
 				data class Test(val test : String){
                     fun printTest(){
                         println(test)
                     }
                 }"""
-            Assertions.assertThat(subject.lint(code)).hasSize(1)
-        }
+			Assertions.assertThat(subject.lint(code)).hasSize(1)
+		}
 
-        it("data class contains two function") {
-            val code = """
+		it("data class contains two function") {
+			val code = """
 				data class Test(val test : String){
                     fun printTest(){
                         println(test)
@@ -41,36 +41,36 @@ class DataClassContainsFunctionsSpec : SubjectSpek<DataClassContainsFunctionsRul
                         println(test)
                     }
                 }"""
-            Assertions.assertThat(subject.lint(code)).hasSize(2)
-        }
-        it("data class use overridden equals") {
-            val code = """
+			Assertions.assertThat(subject.lint(code)).hasSize(2)
+		}
+		it("data class use overridden equals") {
+			val code = """
 				data class Test(val test : String){
                     override fun equals(other: Any?): Boolean {
                         return super.equals(other)
                     }
                 }"""
-            Assertions.assertThat(subject.lint(code)).hasSize(0)
-        }
+			Assertions.assertThat(subject.lint(code)).hasSize(0)
+		}
 
-        it("data class use overridden hashcode") {
-            val code = """
+		it("data class use overridden hashcode") {
+			val code = """
 				data class Test(val test : String){
                     override fun hashCode(): Int {
                         return super.hashCode()
                     }
                 }"""
-            Assertions.assertThat(subject.lint(code)).hasSize(0)
-        }
+			Assertions.assertThat(subject.lint(code)).hasSize(0)
+		}
 
-        it("data class use overridden toString") {
-            val code = """
+		it("data class use overridden toString") {
+			val code = """
 				data class Test(val test : String){
                     override fun toString(): String {
                         return super.toString()
                     }
                 }"""
-            Assertions.assertThat(subject.lint(code)).hasSize(0)
-        }
-    }
+			Assertions.assertThat(subject.lint(code)).hasSize(0)
+		}
+	}
 })
