96e024829bec6ceb59cb825ed16ab9cde24d97c3
schalkms <schalkms@icloud.com>
2017-09-03 12:51:38 +0200

    Refactored ExceptionRaisedInUnexpectedLocation rule

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/ExceptionRaisedInUnexpectedLocation.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/ExceptionRaisedInUnexpectedLocation.kt
@@ -7,8 +7,6 @@ import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.rules.collectByType
-import io.gitlab.arturbosch.detekt.rules.isPublicAndOverridden
-import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtClassInitializer
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtNamedFunction
@@ -33,27 +31,12 @@ class ExceptionRaisedInUnexpectedLocation(config: Config = Config.empty) : Rule(
 
 	private fun isPotentialMethod(function: KtNamedFunction): Boolean {
 		return when (function.name) {
-			"toString", "hashCode" -> isToStringOrHashCodeFunction(function)
-			"equals" -> isEqualsFunction(function)
-			"finalize" -> isFinalizeFunction(function)
+			"toString", "hashCode", "equals", "finalize" -> true
 			else -> false
 		}
 	}
 
 	private fun hasThrowExpression(declaration: KtExpression?): Boolean {
 		return declaration?.collectByType<KtThrowExpression>()?.any() == true
-	}
-
-	private fun isToStringOrHashCodeFunction(function: KtNamedFunction): Boolean {
-		return function.isPublicAndOverridden() && function.valueParameters.size == 0
-	}
-
-	private fun isEqualsFunction(function: KtNamedFunction): Boolean {
-		val parameters = function.valueParameters
-		return function.isPublicAndOverridden() && parameters.size == 1 && parameters.first().typeReference?.text == "Any?"
-	}
-
-	private fun isFinalizeFunction(function: KtNamedFunction): Boolean {
-		return function.hasModifier(KtTokens.PROTECTED_KEYWORD) && function.valueParameters.size == 0
 	}
 }
