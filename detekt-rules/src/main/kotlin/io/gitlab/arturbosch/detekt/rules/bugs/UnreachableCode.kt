f98e4b65fd4d3421cd77be201e870d34d3414198
schalkms <schalkms@icloud.com>
2017-09-04 17:39:05 +0200

    Added break and continue to unreachable code rule

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnreachableCode.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnreachableCode.kt
@@ -8,6 +8,8 @@ import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import org.jetbrains.kotlin.psi.KtBlockExpression
+import org.jetbrains.kotlin.psi.KtBreakExpression
+import org.jetbrains.kotlin.psi.KtContinueExpression
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtReturnExpression
 import org.jetbrains.kotlin.psi.KtThrowExpression
@@ -27,11 +29,18 @@ class UnreachableCode(config: Config = Config.empty) : Rule(config) {
 		super.visitThrowExpression(expression)
 	}
 
+	override fun visitContinueExpression(expression: KtContinueExpression) {
+		followedByUnreachableCode(expression)
+	}
+
+	override fun visitBreakExpression(expression: KtBreakExpression) {
+		followedByUnreachableCode(expression)
+	}
+
 	private fun followedByUnreachableCode(expression: KtExpression) {
 		val statements = (expression.parent as? KtBlockExpression)?.statements ?: return
 		val indexOfStatement = statements.indexOf(expression)
-		val size = statements.size
-		if (indexOfStatement < size - 1) {
+		if (indexOfStatement < statements.size - 1) {
 			report(CodeSmell(issue, Entity.from(expression)))
 		}
 	}
