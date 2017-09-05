3f7ab93d022ca1e29dbd55f79c9db5d6856efdd8
Ivan Balaksha <ivan.balaksha@agoda.com>
2017-09-05 17:35:13 +0700

    correct description, check is open,abstract,inherit something

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UseDataClassRule.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UseDataClassRule.kt
@@ -6,11 +6,13 @@ import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.psiUtil.isAbstract
 import org.jetbrains.kotlin.psi.psiUtil.isPropertyParameter
 
 /**
@@ -19,12 +21,16 @@ import org.jetbrains.kotlin.psi.psiUtil.isPropertyParameter
 class UseDataClassRule(config: Config = Config.empty) : Rule(config) {
 	override val issue: Issue = Issue("UseDataClass",
 			Severity.Style,
-			"If a class does nothing but hold data the developer can introduce a data class.")
+			"Classes that do nothing but hold data should be replaced with a data class.")
 
 	private val defaultFunctionNames = listOf("hashCode", "equals", "toString", "copy")
 
 	override fun visitClass(klass: KtClass) {
-		if (!klass.isData()) {
+		if (!klass.isData()
+				&& !klass.isAbstract()
+				&& !klass.hasModifier(KtTokens.OPEN_KEYWORD)
+				&& klass.superTypeListEntries.isEmpty()) {
+
 			val declarations = extractDeclarations(klass)
 			val properties = declarations.filterIsInstance<KtProperty>()
 			val functions = declarations.filterIsInstance<KtNamedFunction>()
@@ -42,7 +48,7 @@ class UseDataClassRule(config: Config = Config.empty) : Rule(config) {
 	}
 
 	private fun extractDeclarations(klass: KtClass): List<KtDeclaration> {
-		return klass.getBody()?.declarations ?: mutableListOf()
+		return klass.getBody()?.declarations ?: emptyList()
 	}
 
 	private fun extractConstructorPropertyParameters(klass: KtClass): List<KtParameter> {
