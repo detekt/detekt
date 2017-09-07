56e905810cc8304772c7230e3c4bfd67d16673c4
Marvin Ramin <marvin.ramin@gmail.com>
2017-09-07 07:39:24 +0200

    update filtering logic of forbidden classname rule and tests

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenNameSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenNameSpec.kt
@@ -3,39 +3,20 @@ package io.gitlab.arturbosch.detekt.rules.style
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.rules.style.naming.ForbiddenClassName
 import io.gitlab.arturbosch.detekt.rules.style.naming.NamingRules
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.lint
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 
 
-class ForbiddenNameSpec: Spek({
+class ForbiddenNameSpec : Spek({
 
 	given("forbidden naming rules") {
-		val configCustomRules =
-				object : Config {
-					override fun subConfig(key: String): Config = Config.empty
-
-					@Suppress("UNCHECKED_CAST")
-					override fun <T : Any> valueOrDefault(key: String, default: T): T =
-							when (key) {
-								ForbiddenClassName.FORBIDDEN_NAME -> "Manager ,  Provider" as T
-								else -> default
-							}
-				}
-		val config = object : Config {
-			override fun subConfig(key: String): Config =
-					when (key) {
-						ForbiddenClassName::class.simpleName -> configCustomRules
-						else -> Config.empty
-					}
-
-			override fun <T : Any> valueOrDefault(key: String, default: T): T = default
-		}
-
 		it("should report classes with forbidden names") {
-			Assertions.assertThat(NamingRules(config).lint("""
+			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "Manager ,  Provider")))
+					.lint("""
 			class TestManager {
 				fun getTest() {
 					return "Test"
@@ -45,7 +26,19 @@ class ForbiddenNameSpec: Spek({
 		}
 
 		it("should report another class with a forbidden name") {
-			Assertions.assertThat(NamingRules(config).lint("""
+			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "Manager ,  Provider")))
+					.lint("""
+			class TestProvider {
+				fun getTest() {
+					return "Test"
+				}
+			}
+			""")).hasSize(1)
+		}
+
+		it("should report a class that starts with a forbidden name") {
+			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "test")))
+					.lint("""
 			class TestProvider {
 				fun getTest() {
 					return "Test"
@@ -55,7 +48,8 @@ class ForbiddenNameSpec: Spek({
 		}
 
 		it("should not report classes that don't contain any forbidden names") {
-			Assertions.assertThat(NamingRules(config).lint("""
+			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "Manager ,  Provider")))
+					.lint("""
 			class TestHolder {
 				fun getTest() {
 					return "Test"
