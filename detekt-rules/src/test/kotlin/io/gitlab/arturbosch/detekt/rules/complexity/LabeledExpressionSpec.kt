380ae35b2fed60054ab0daf9cd576d53079b09db
schalkms <schalkms@icloud.com>
2017-09-09 23:05:26 +0200

    Moved test code in "" to kt files for rules
    The compiler checks the code which was moved to the kt files.
    The places where violations occur are marked

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LabeledExpressionSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LabeledExpressionSpec.kt
@@ -1,10 +1,11 @@
 package io.gitlab.arturbosch.detekt.rules.complexity
 
+import io.gitlab.arturbosch.detekt.rules.Case
 import io.gitlab.arturbosch.detekt.test.lint
-import org.jetbrains.spek.api.dsl.describe
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
-import org.assertj.core.api.Assertions.assertThat
 
 /**
  * @author Ivan Balaksha
@@ -12,52 +13,11 @@ import org.assertj.core.api.Assertions.assertThat
 class LabeledExpressionSpec : SubjectSpek<LabeledExpression>({
 	subject { LabeledExpression() }
 
-	describe("") {
+	given("several labeled expressions") {
 
-		it("break with label") {
-			val code = """
-				fun breakWithLabel() {
-					loop@ for (i in 1..100) {
-						for (j in 1..100) {
-							if (j == 5) break@loop
-						}
-					}
-				}"""
-			assertThat(subject.lint(code)).hasSize(2)
-		}
-
-		it("continue with label") {
-			val code = """
-				fun continueWithLabel() {
-					loop@ for (i in 1..100) {
-						for (j in 1..100) {
-							if (j == 5) continue@loop
-						}
-					}
-				}"""
-			assertThat(subject.lint(code)).hasSize(2)
-		}
-
-		it("implicit return with label") {
-			val code = """
-				fun implicitReturnWithLabel(range: IntRange) {
-					range.forEach {
-						if (it == 5) return@forEach
-						println(it)
-					}
-				}"""
-			assertThat(subject.lint(code)).hasSize(1)
-		}
-
-		it("return with label") {
-			val code = """
-				fun returnWithLabel(range: IntRange) {
-					range.forEach label@ {
-						if (it == 5) return@label
-						println(it)
-					}
-				}"""
-			assertThat(subject.lint(code)).hasSize(2)
+		it("reports these labels") {
+			subject.lint(Case.LabeledExpression.path())
+			assertThat(subject.findings).hasSize(7)
 		}
 	}
 })
--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenNameSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenNameSpec.kt
@@ -1,6 +1,5 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
-import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.rules.style.naming.ForbiddenClassName
 import io.gitlab.arturbosch.detekt.rules.style.naming.NamingRules
 import io.gitlab.arturbosch.detekt.test.TestConfig
@@ -10,52 +9,24 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 
-
 class ForbiddenNameSpec : Spek({
 
 	given("forbidden naming rules") {
 		it("should report classes with forbidden names") {
+			val code = """
+				class TestManager {} // violation
+				class TestProvider {} // violation
+				class TestHolder"""
 			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "Manager ,  Provider")))
-					.lint("""
-			class TestManager {
-				fun getTest() {
-					return "Test"
-				}
-			}
-			""")).hasSize(1)
-		}
-
-		it("should report another class with a forbidden name") {
-			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "Manager ,  Provider")))
-					.lint("""
-			class TestProvider {
-				fun getTest() {
-					return "Test"
-				}
-			}
-			""")).hasSize(1)
+					.lint(code))
+					.hasSize(2)
 		}
 
 		it("should report a class that starts with a forbidden name") {
+			val code = "class TestProvider {}"
 			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "test")))
-					.lint("""
-			class TestProvider {
-				fun getTest() {
-					return "Test"
-				}
-			}
-			""")).hasSize(1)
-		}
-
-		it("should not report classes that don't contain any forbidden names") {
-			assertThat(NamingRules(TestConfig(mapOf(ForbiddenClassName.FORBIDDEN_NAME to "Manager ,  Provider")))
-					.lint("""
-			class TestHolder {
-				fun getTest() {
-					return "Test"
-				}
-			}
-			""")).hasSize(0)
+					.lint(code))
+					.hasSize(1)
 		}
 	}
 })
