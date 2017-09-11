304e0deb88c515c56e6cef3749ed1b1aee81cf37
Artur Bosch <arturbosch@gmx.de>
2017-09-11 16:08:39 +0200

    Use case specific descriptions - #380

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnnecessarySuperTypeInheritanceSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnnecessarySuperTypeInheritanceSpec.kt
@@ -7,8 +7,8 @@ import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
 
-class UnnecessarySuperTypeDeclarationSpec : SubjectSpek<UnnecessarySuperTypeDeclaration>({
-	subject { UnnecessarySuperTypeDeclaration(Config.empty) }
+class UnnecessarySuperTypeInheritanceSpec : SubjectSpek<UnnecessarySuperTypeInheritance>({
+	subject { UnnecessarySuperTypeInheritance(Config.empty) }
 
 	describe("check inherit classes") {
 
