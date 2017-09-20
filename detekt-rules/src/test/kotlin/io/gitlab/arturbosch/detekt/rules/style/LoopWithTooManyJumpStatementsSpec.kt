fa850579d4a3c786e32467d81a15ce742a3d5817
schalkms <schalkms@icloud.com>
2017-09-20 18:59:45 +0200

    Added maxJumpCount configuration option

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/LoopWithTooManyJumpStatementsSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/LoopWithTooManyJumpStatementsSpec.kt
@@ -1,6 +1,7 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
 import io.gitlab.arturbosch.detekt.rules.Case
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.given
@@ -12,9 +13,16 @@ class LoopWithTooManyJumpStatementsSpec : SubjectSpek<LoopWithTooManyJumpStateme
 
 	given("loops with multiple break or continue statements") {
 
+		val path = Case.LoopWithTooManyJumpStatements.path()
+
 		it("reports loops with more than 1 break or continue statement") {
-			val path = Case.LoopWithTooManyJumpStatements.path()
 			assertThat(subject.lint(path)).hasSize(3)
+		}
+
+		it("does not report when max count configuration is set to 2") {
+			val config = TestConfig(mapOf(LoopWithTooManyJumpStatements.MAX_JUMP_COUNT to "2"))
+			val findings = LoopWithTooManyJumpStatements(config).lint(path)
+			assertThat(findings).hasSize(0)
 		}
 	}
 })
