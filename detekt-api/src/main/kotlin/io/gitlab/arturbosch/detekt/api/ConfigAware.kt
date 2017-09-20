904923f427c9000a0806f31b388cf1c4684adf2c
Artur Bosch <arturbosch@gmx.de>
2017-09-20 10:12:44 +0200

    Remove autoCorrect functions - #406

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/ConfigAware.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/ConfigAware.kt
@@ -16,22 +16,6 @@ interface ConfigAware : Config {
 	val config: Config
 
 	/**
-	 * If your rule supports to automatically correct the misbehaviour of underlying smell,
-	 * specify your code inside this method call, to allow the user of your rule to trigger auto correction
-	 * only when needed.
-	 */
-	fun withAutoCorrect(block: () -> Unit) {
-		if (autoCorrect) {
-			block()
-		}
-	}
-
-	/**
-	 * Does this rule have auto correct specified in configuration?
-	 */
-	val autoCorrect: Boolean get() = valueOrDefault("autoCorrect", true)
-
-	/**
 	 * Is this rule specified as active in configuration?
 	 */
 	val active get() = valueOrDefault("active", true)
