11e24e5e6fdd96daae61b4434313b22ded7b6d81
schalkms <schalkms@icloud.com>
2017-09-22 23:30:34 +0200

    Added IllegalArgument exception rule

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/InstantiateIllegalArgumentExceptionCorrectly.kt
@@ -0,0 +1,27 @@
+package io.gitlab.arturbosch.detekt.rules.exceptions
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.KtCallExpression
+
+class InstantiateIllegalArgumentExceptionCorrectly(config: Config = Config.empty) : Rule(config) {
+
+	override val issue = Issue("InstantiateIllegalArgumentExceptionCorrectly", Severity.Warning,
+			"A call to the default constructor of an IllegalArgumentException was detected. " +
+					"Instead one of the constructor overloads should be called. " +
+					"This allows to provide more meaningful exceptions.",
+			Debt.FIVE_MINS)
+
+	override fun visitCallExpression(expression: KtCallExpression) {
+		if (expression.calleeExpression?.text == "IllegalArgumentException" && expression.valueArguments.isEmpty()) {
+			report(CodeSmell(issue, Entity.from(expression)))
+		}
+		super.visitCallExpression(expression)
+	}
+}
+
