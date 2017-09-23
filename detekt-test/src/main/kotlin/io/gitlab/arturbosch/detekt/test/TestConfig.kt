7a34c7d7cbe1e31c81ed2dcbf54dc2ea8a6c3f5c
schalkms <schalkms@icloud.com>
2017-09-23 23:09:31 +0200

    Fixed the TestConfig parameter which caused red tests

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/TestConfig.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/TestConfig.kt
@@ -5,11 +5,18 @@ import io.gitlab.arturbosch.detekt.api.BaseConfig
 /**
  * @author Artur Bosch
  */
-class TestConfig(val values: Map<String, String>) : BaseConfig() {
+class TestConfig(private val values: Map<String, String>) : BaseConfig() {
 
 	override fun subConfig(key: String) = this
 
 	@Suppress("UNCHECKED_CAST")
-	override fun <T : Any> valueOrDefault(key: String, default: T)
-			= valueOrDefaultInternal(values[key], default) as T
+	override fun <T : Any> valueOrDefault(key: String, default: T) = when (key) {
+		"active" -> getActiveValue(default) as T
+		else -> valueOrDefaultInternal(values[key], default) as T
+	}
+
+	private fun <T : Any> getActiveValue(default: T): Any {
+		val active = values["active"]
+		return if (active != null) valueOrDefaultInternal(active, default) else true
+	}
 }
