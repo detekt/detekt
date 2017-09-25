502dfd8c91453154f964de6afd8dc8919da3e035
schalkms <schalkms@icloud.com>
2017-09-25 13:00:18 +0200

    Added RuleProviderTest
    This checks if all defined rules in a package are listed in the
    respective rule provider.

---
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/RuleProviderTest.kt
@@ -0,0 +1,126 @@
+package io.gitlab.arturbosch.detekt.rules
+
+import io.gitlab.arturbosch.detekt.api.BaseRule
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.MultiRule
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+import io.gitlab.arturbosch.detekt.rules.providers.CommentSmellProvider
+import io.gitlab.arturbosch.detekt.rules.providers.ComplexityProvider
+import io.gitlab.arturbosch.detekt.rules.providers.EmptyCodeProvider
+import io.gitlab.arturbosch.detekt.rules.providers.ExceptionsProvider
+import io.gitlab.arturbosch.detekt.rules.providers.PerformanceProvider
+import io.gitlab.arturbosch.detekt.rules.providers.PotentialBugProvider
+import io.gitlab.arturbosch.detekt.rules.providers.StyleGuideProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.reflections.Reflections
+import java.lang.reflect.Modifier
+
+
+class RuleProviderTest {
+
+	@Test
+	fun commentSmellProvider() {
+		RuleProviderAssert(
+				CommentSmellProvider(),
+				"io.gitlab.arturbosch.detekt.rules.documentation",
+				Rule::class.java)
+				.assert()
+	}
+
+	@Test
+	fun complexityProvider() {
+		RuleProviderAssert(
+				ComplexityProvider(),
+				"io.gitlab.arturbosch.detekt.rules.complexity",
+				Rule::class.java)
+				.assert()
+	}
+
+	@Test
+	fun emptyCodeProvider() {
+		RuleProviderAssert(
+				EmptyCodeProvider(),
+				"io.gitlab.arturbosch.detekt.rules.empty",
+				MultiRule::class.java)
+				.assert()
+	}
+
+	@Test
+	fun exceptionsProvider() {
+		RuleProviderAssert(
+				ExceptionsProvider(),
+				"io.gitlab.arturbosch.detekt.rules.exceptions",
+				Rule::class.java)
+				.assert()
+	}
+
+	@Test
+	fun performanceProvider() {
+		RuleProviderAssert(
+				PerformanceProvider(),
+				"io.gitlab.arturbosch.detekt.rules.performance",
+				Rule::class.java)
+				.assert()
+	}
+
+	@Test
+	fun potentialBugProvider() {
+		RuleProviderAssert(
+				PotentialBugProvider(),
+				"io.gitlab.arturbosch.detekt.rules.bugs",
+				Rule::class.java)
+				.assert()
+	}
+
+	@Test
+	fun styleGuideProvider() {
+		RuleProviderAssert(
+				StyleGuideProvider(),
+				"io.gitlab.arturbosch.detekt.rules.style",
+				Rule::class.java)
+				.assert()
+	}
+
+	class RuleProviderAssert<T>
+					(private val provider: RuleSetProvider,
+					 private val packageName: String,
+					 private val clazz: Class<T>) {
+
+		fun assert() {
+			val rules = getRules(provider)
+			val classes = getClasses()
+			classes
+					.map { c -> rules.singleOrNull { it.javaClass.simpleName == c.simpleName } }
+					.forEach {
+						if (it == null) {
+							print(rules.size); print(" rules"); println()
+							print(classes.size); print(" classes")
+						}
+						assertThat(it).isNotNull()
+					}
+		}
+
+		private fun getRules(provider: RuleSetProvider): List<BaseRule> {
+			val ruleSet = provider.buildRuleset(Config.empty)
+			assertRuleSet(ruleSet!!)
+			return ruleSet.rules
+		}
+
+		private fun assertRuleSet(ruleSet: RuleSet) {
+			assertThat(ruleSet).isNotNull()
+			assertThat(ruleSet.rules).isNotNull
+			assertThat(ruleSet.rules).isNotEmpty
+		}
+
+		private fun getClasses(): List<Class<out T>> {
+			val reflections = Reflections(packageName)
+			val classes = reflections.getSubTypesOf(clazz)
+					.filter { !Modifier.isAbstract(it.modifiers) && it.superclass.simpleName != "SubRule" }
+			assertThat(classes).isNotEmpty
+			return classes
+		}
+	}
+}
