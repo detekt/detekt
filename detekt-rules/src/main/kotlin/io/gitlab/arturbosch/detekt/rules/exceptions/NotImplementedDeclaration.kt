cff3cbd3f795c1df7dc36d0e54102ef425d60698
schalkms <schalkms@icloud.com>
2017-09-27 14:46:43 +0200

    Fixed typo and unused fun in NotImplementedError

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/NotImplementedDeclaration.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/NotImplementedDeclaration.kt
@@ -17,11 +17,7 @@ class NotImplementedDeclaration(config: Config = Config.empty) : Rule(config) {
 			"The NotImplementedDeclaration should only be used when a method stub is necessary. " +
 					"This defers the development of the functionality of this function. " +
 					"Hence, the NotImplementedDeclaration should only serve as a temporary declaration. " +
-					"Before releasing this type of declaration should be removed.")
-
-	override fun visitNamedFunction(function: KtNamedFunction) {
-		super.visitNamedFunction(function)
-	}
+					"Before releasing, this type of declaration should be removed.")
 
 	override fun visitThrowExpression(expression: KtThrowExpression) {
 		val calleeExpression = expression.thrownExpression?.getCalleeExpressionIfAny()
