840f623845e8e3dc3a80f777bc5343e8c01f702e
schalkms <schalkms@icloud.com>
2017-10-07 20:45:06 +0200

    Fixed NestedClassVisibility violation detection
    This rule should not detect nested classes where the nesting depth is
    deeper than 1. An enum class should be excluded.
    The issue should be generic and not depend on the violated class name.

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/NestedClassesVisibility.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/NestedClassesVisibility.kt
@@ -2,46 +2,33 @@ package io.gitlab.arturbosch.detekt.rules.style
 
 import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.DetektVisitor
+import io.gitlab.arturbosch.detekt.api.Debt
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
-import io.gitlab.arturbosch.detekt.rules.isEnumEntry
 import io.gitlab.arturbosch.detekt.rules.isInternal
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.RecursiveTreeElementVisitor
+import io.gitlab.arturbosch.detekt.rules.isPublic
 import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtEnumEntry
-import org.jetbrains.kotlin.psi.psiUtil.isPrivate
 
 class NestedClassesVisibility(config: Config = Config.empty) : Rule(config) {
-	override val issue: Issue = Issue("NestedClassesVisibility",
-			severity = Severity.Security,
-			description = "")
 
-	private val visitor = VisibilityVisitor()
+	override val issue: Issue = Issue("NestedClassesVisibility", Severity.Style,
+			"Nested types are often used for implementing private functionality " +
+					"and therefore this should not be public.",
+					Debt.FIVE_MINS)
 
 	override fun visitClass(klass: KtClass) {
-		super.visitClass(klass)
-		if (klass.isInternal()) {
+		if (klass.isTopLevel() && klass.isInternal()) {
 			checkDeclarations(klass)
 		}
 	}
 
 	private fun checkDeclarations(klass: KtClass) {
-		klass.declarations.filterNot { it.isEnumEntry() }.forEach {
-			it.accept(visitor)
-		}
-	}
-
-	private inner class VisibilityVisitor : DetektVisitor() {
-		override fun visitClass(klass: KtClass) {
-			checkDeclarations(klass)
-			if (!klass.isInternal() && !klass.isPrivate()) {
-				report(CodeSmell(issue.copy(description = "Nested types are often used for implementing private functionality. " +
-						"However the visibility of ${klass.name} makes it visible externally."),
-						Entity.from(klass)))
-			}
+		klass.declarations
+				.filterIsInstance<KtClass>()
+				.filter { !it.isEnum() && it.isPublic() }
+				.forEach { report(CodeSmell(issue, Entity.from(it)))
 		}
 	}
 }
