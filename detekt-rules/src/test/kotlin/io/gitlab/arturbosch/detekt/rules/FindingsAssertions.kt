0bdbd4fba9ed62558e53e1f20061f77a25a824c5
Sebastiano Poggi <poggos@gmail.com>
2017-10-18 14:35:48 +0100

    Improve testing by creating FindingsAssertions

---
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/FindingsAssertions.kt
@@ -0,0 +1,28 @@
+package io.gitlab.arturbosch.detekt.rules
+
+import io.gitlab.arturbosch.detekt.api.Finding
+import org.assertj.core.api.AbstractAssert
+import org.assertj.core.api.AbstractListAssert
+import org.assertj.core.internal.Objects
+
+fun assertThat(findings: List<Finding>) = FindingsAssert(findings)
+class FindingsAssert(actual: List<Finding>) :
+		AbstractListAssert<FindingsAssert, List<Finding>,
+				Finding, FindingAssert>(actual, FindingsAssert::class.java) {
+
+	override fun toAssert(value: Finding?, description: String?): FindingAssert =
+			FindingAssert(value).`as`(description)
+
+	fun hasLocationStrings(vararg expected: String) {
+		isNotNull
+		val actualLocationStrings = actual.map { it.entity.location.locationString }
+		areEqual(actualLocationStrings, expected)
+	}
+
+	private fun areEqual(actualLocationStrings: List<String>, expectedLocationStrings: Array<out String>) {
+		Objects.instance()
+				.assertEqual(writableAssertionInfo, actualLocationStrings, expectedLocationStrings.toList())
+	}
+}
+
+class FindingAssert(actual: Finding?) : AbstractAssert<FindingAssert, Finding>(actual, FindingAssert::class.java)
