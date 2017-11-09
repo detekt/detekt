4be66d38a4ffd9ebe2050d4642824191e3ee16bb
schalkms <schalkms@icloud.com>
2017-11-09 21:37:18 +0100

    Refactored rules/documentation test cases
    This separates positive and negative test cases for rules. (#527)

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/CommentOverPrivatePropertiesSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/CommentOverPrivatePropertiesSpec.kt
@@ -1,13 +1,35 @@
 package io.gitlab.arturbosch.detekt.rules.documentation
 
-import io.gitlab.arturbosch.detekt.rules.CommonSpec
+import io.gitlab.arturbosch.detekt.test.lint
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
-import org.jetbrains.spek.subject.itBehavesLike
 
 /**
  * @author Artur Bosch
  */
 class CommentOverPrivatePropertiesSpec : SubjectSpek<CommentOverPrivateProperty>({
 	subject { CommentOverPrivateProperty() }
-	itBehavesLike(CommonSpec())
+
+	given("some properties with comments") {
+
+		it("reports private property with a comment") {
+			val code = """
+				/**
+				 * asdf
+				 */
+				private val v = 1"""
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("does not report public property with a comment") {
+			val code = """
+				/**
+				 * asdf
+				 */
+				val v = 1"""
+			assertThat(subject.lint(code)).hasSize(0)
+		}
+	}
 })
