29d772587df44620b7dabdaf13487395975034d5
schalkms <schalkms@icloud.com>
2017-11-09 20:41:21 +0100

    Refactored Iterator and rules/bugs test cases
    This separates positive and negative test cases for rules. (#527)

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/InvalidLoopConditionSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/InvalidLoopConditionSpec.kt
@@ -1,8 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.bugs
 
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.test.compileForTest
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.describe
@@ -12,11 +10,37 @@ import org.jetbrains.spek.subject.SubjectSpek
 class InvalidLoopConditionSpec : SubjectSpek<InvalidLoopCondition>({
 	subject { InvalidLoopCondition(Config.empty) }
 
-	describe("check loop conditions") {
+	describe("check for loop conditions") {
 
-		it("reports invalid loop conditions") {
-			val file = compileForTest(Case.InvalidLoopCondition.path())
-			assertThat(subject.lint(file.text)).hasSize(3)
+		it("does not report correct bounds in for loop conditions") {
+			val code = """
+				fun f() {
+					for (i in 2..2) {}
+					for (i in 2 downTo 2) {}
+					for (i in 2 until 2) {}
+					for (i in (1+1)..3) { }
+				}"""
+			assertThat(subject.lint(code)).hasSize(0)
+		}
+
+		it("reports incorrect bounds in for loop conditions") {
+			val code = """
+				fun f() {
+					for (i in 2..1) { }
+					for (i in 1 downTo 2) { }
+					for (i in 2 until 1) { }
+				}"""
+			assertThat(subject.lint(code)).hasSize(3)
+		}
+
+		it("reports nested loops with incorrect bounds in for loop conditions") {
+			val code = """
+				fun f() {
+					for (i in 2..2) {
+						for (i in 2..1) { }
+					}
+				}"""
+			assertThat(subject.lint(code)).hasSize(1)
 		}
 	}
 })
