0458eabb8c2185832c047bee8d50d83390c89909
schalkms <schalkms@icloud.com>
2017-11-09 19:51:48 +0100

    Fixed bug in EqualsAlwaysReturnsTrueOrFalse
    A violation was reported when an equals function was used as a global
    function.
    The test cases for this rule were refactored.
    This separates positive and negative test cases for rules. (#527)

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/EqualsAlwaysReturnsTrueOrFalseSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/EqualsAlwaysReturnsTrueOrFalseSpec.kt
@@ -1,10 +1,8 @@
 package io.gitlab.arturbosch.detekt.rules.bugs
 
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.test.compileForTest
 import io.gitlab.arturbosch.detekt.test.lint
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
@@ -14,9 +12,62 @@ class EqualsAlwaysReturnsTrueOrFalseSpec : SubjectSpek<EqualsAlwaysReturnsTrueOr
 
 	describe("check if equals() method always returns true or false") {
 
-		it("returns constant boolean") {
-			val file = compileForTest(Case.EqualsAlwaysReturnsConstant.path())
-			Assertions.assertThat(subject.lint(file.text)).hasSize(2)
+		it("reports returning 'true'") {
+			val code = """
+				class A {
+					override fun equals(other: Any?): Boolean {
+						return true
+					}
+				}"""
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("reports returning 'false'") {
+			val code = """
+				class A {
+					override fun equals(other: Any?): Boolean {
+						return false
+					}
+				}"""
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("does not report returning no constant'") {
+			val code = """
+				class A {
+					override fun equals(other: Any?): Boolean {
+						return this == other
+					}
+				}"""
+			assertThat(subject.lint(code)).hasSize(0)
+		}
+
+		it("does not report global equals function") {
+			val code = """
+				fun equals(other: Any?): Boolean {
+					return false;
+				}"""
+			assertThat(subject.lint(code)).hasSize(0);
+		}
+
+		it("does not report equals function with wrong name") {
+			val code = """
+				class A {
+					fun equal(other: Any?): Boolean {
+						return true
+					}
+				}"""
+			assertThat(subject.lint(code)).hasSize(0)
+		}
+
+		it("does not report equals function with wrong parameter type") {
+			val code = """
+				class A {
+					fun equals(other: Any): Boolean {
+						return true
+					}
+				}"""
+			assertThat(subject.lint(code)).hasSize(0)
 		}
 	}
 })
