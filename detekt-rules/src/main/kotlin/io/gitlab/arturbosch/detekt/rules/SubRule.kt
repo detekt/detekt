134c907d6bf5701732f7c2f4fc66121c6b0dc1f9
Marvin Ramin <marvin.ramin@gmail.com>
2017-11-19 12:15:22 +0100

    remove SubRule and replace last usage with standard Rule

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/SubRule.kt
+++
@@ -1,8 +0,0 @@
-package io.gitlab.arturbosch.detekt.rules
-
-import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Rule
-
-abstract class SubRule<in Element>(config: Config) : Rule(config) {
-	abstract fun apply(element: Element)
-}
