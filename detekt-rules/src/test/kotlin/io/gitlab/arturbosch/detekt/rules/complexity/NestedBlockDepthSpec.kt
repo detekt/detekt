06b6982848b5ab1f50ac6a697ec36c860927f9b7
schalkms <schalkms@icloud.com>
2017-11-24 19:19:47 +0100

    Moved test cases from Default.kt to the corresponding rule specs
    The Default.kt file was refactored because there were to many different
    rule violations in it.

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/NestedBlockDepthSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/NestedBlockDepthSpec.kt
@@ -2,13 +2,11 @@ package io.gitlab.arturbosch.detekt.rules.complexity
 
 import io.gitlab.arturbosch.detekt.api.ThresholdedCodeSmell
 import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.rules.CommonSpec
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
-import org.jetbrains.spek.subject.itBehavesLike
 import kotlin.test.assertEquals
 
 /**
@@ -16,13 +14,40 @@ import kotlin.test.assertEquals
  */
 class NestedBlockDepthSpec : SubjectSpek<NestedBlockDepth>({
 	subject { NestedBlockDepth(threshold = 3) }
-	itBehavesLike(CommonSpec())
 
 	describe("nested classes are also considered") {
 		it("should detect only the nested large class") {
 			subject.lint(Case.NestedClasses.path())
 			assertEquals(subject.findings.size, 1)
 			assertEquals((subject.findings[0] as ThresholdedCodeSmell).value, 5)
+		}
+
+		it("should detect too nested block depth") {
+			val code = """
+				fun f() {
+					if (true) {
+						if (true) {
+							if (true) {
+								if (true) {
+								}
+							}
+						}
+					}
+				}"""
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("should not detect valid nested block depth") {
+			val code = """
+				fun f() {
+					if (true) {
+						if (true) {
+							if (true) {
+							}
+						}
+					}
+				}"""
+			assertThat(subject.lint(code)).isEmpty()
 		}
 
 		it("should not count else if as two") {
