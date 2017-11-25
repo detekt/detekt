c867bce7f2e720a4f700f9c2efec63d461af93c8
Marvin Ramin <marvin.ramin@gmail.com>
2017-11-25 22:39:53 +0100

    add YAML DSL

---
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/out/Yaml.kt
@@ -0,0 +1,52 @@
+package io.gitlab.arturbosch.detekt.generator.out
+
+/**
+ * @author Marvin Ramin
+ */
+sealed class YML(open val indent: Int = 0, open var content: String = "") {
+	fun append(value: String) {
+		content = if (content.isEmpty()) {
+			"${getIndent()}$value"
+		} else {
+			"$content\n${getIndent()}$value"
+		}
+	}
+
+	fun emptyLine() {
+		content = "$content\n"
+	}
+
+	fun getIndent(): String {
+		var spaces = ""
+		indent times {
+			spaces += "  "
+		}
+		return spaces
+	}
+}
+data class YamlNode(override val indent: Int = 0, override var content: String = ""): YML()
+
+infix fun Int.times(function: () -> Unit) {
+	var i = this
+	while (i > 0) {
+		function()
+		i--
+	}
+}
+inline fun yaml(content: YamlNode.() -> Unit): String {
+	return YamlNode().let { yaml ->
+		content(yaml)
+		yaml.content
+	}
+
+}
+fun YamlNode.node(name: String, node: YamlNode.() -> Unit) {
+	val yamlNode = YamlNode(indent = indent + 1, content = "$name:")
+	node(yamlNode)
+	append(yamlNode.content)
+}
+inline fun YamlNode.keyValue(keyValue: () -> Pair<String, String>) {
+	val (key, value) = keyValue()
+	append("$key: $value")
+}
+inline fun YamlNode.yaml(yaml: () -> String) = append(yaml())
