ed5d1866ec743d8843635c31833c9e1b019cdd86
Marvin Ramin <marvin.ramin@gmail.com>
2017-11-27 20:29:32 +0100

    remove JCommander and Args from detekt-generator module

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/Main.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/Main.kt
@@ -2,11 +2,22 @@
 
 package io.gitlab.arturbosch.detekt.generator
 
+import io.gitlab.arturbosch.detekt.core.exists
+import java.nio.file.Path
+import java.nio.file.Paths
+
 /**
  * @author Marvin Ramin
  */
 fun main(args: Array<String>) {
-	val arguments = parseArguments(args)
-	val executable = Runner(arguments)
+	val path = getPath()
+	val executable = Runner(path)
 	executable.execute()
+}
+
+private const val RULES_SOURCES_PATH = "./detekt-rules/src/main/kotlin"
+fun getPath(): Path {
+	val path = Paths.get(RULES_SOURCES_PATH)
+	require(path.exists()) { "Path to detekt-rules module does not exist." }
+	return path
 }
