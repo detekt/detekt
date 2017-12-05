0be6d5eba2e9b5f53d0003f1b264a38f10410778
Marvin Ramin <marvin.ramin@gmail.com>
2017-12-05 20:55:49 +0100

    split documentation and config output paths

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/DetektPrinter.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/DetektPrinter.kt
@@ -1,20 +1,20 @@
 package io.gitlab.arturbosch.detekt.generator.printer
 
+import io.gitlab.arturbosch.detekt.generator.Args
 import io.gitlab.arturbosch.detekt.generator.out.markdownFile
 import io.gitlab.arturbosch.detekt.generator.out.yamlFile
 import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.ConfigPrinter
 import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.RuleSetPage
 import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.RuleSetPagePrinter
-import java.nio.file.Path
 
-class DetektPrinter {
+class DetektPrinter(private val arguments: Args) {
 
-	fun print(path: Path, pages: List<RuleSetPage>) {
+	fun print(pages: List<RuleSetPage>) {
 		pages.forEach {
-			markdownFile(path, it.ruleSet.name) { RuleSetPagePrinter.print(it) }
+			markdownFile(arguments.documentationPath, it.ruleSet.name) { RuleSetPagePrinter.print(it) }
 		}
 
-		yamlFile(path, "default-detekt-config") { ConfigPrinter.print(pages) }
+		yamlFile(arguments.configPath, "default-detekt-config") { ConfigPrinter.print(pages) }
 	}
 
 }
