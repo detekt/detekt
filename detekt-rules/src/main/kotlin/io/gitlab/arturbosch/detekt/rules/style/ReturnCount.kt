6db10715e0bb60b6041825eafb6e7148be2cd248
Patrick Pilch <patrickpilch@users.noreply.github.com>
2017-12-09 19:50:35 -0600

    ReturnCount augmented to ignore specified function names
    Due to scenarios like IntelliJ's automatic code-generation of `equals()`
    with multiple return statements, having the ability to ignore them
    globally would be preferable over many suppression annotations.

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ReturnCount.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ReturnCount.kt
@@ -7,6 +7,7 @@ import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.api.SplitPattern
 import io.gitlab.arturbosch.detekt.rules.collectByType
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtReturnExpression
@@ -19,7 +20,10 @@ import org.jetbrains.kotlin.psi.KtReturnExpression
  *
  * @configuration max - define the maximum number of return statements allowed per function
  * (default: 2)
+ * @configuration ignoredFunctionNames - define functions to be ignored by this check
+ * (default: none)
  * @active since v1.0.0
+ * @author Patrick Pilch
  */
 class ReturnCount(config: Config = Config.empty) : Rule(config) {
 
@@ -27,18 +31,24 @@ class ReturnCount(config: Config = Config.empty) : Rule(config) {
 			"Restrict the number of return statements in methods.", Debt.TEN_MINS)
 
 	private val max = valueOrDefault(MAX, 2)
+	private val ignoredFunctions = SplitPattern(valueOrDefault(IGNORED_FUNCTION_NAMES, ""))
 
 	override fun visitNamedFunction(function: KtNamedFunction) {
 		super.visitNamedFunction(function)
 
-		val numberOfReturns = function.collectByType<KtReturnExpression>().count()
+		if (!isIgnoredFunction(function)) {
+			val numberOfReturns = function.collectByType<KtReturnExpression>().count()
 
-		if (numberOfReturns > max) {
-			report(CodeSmell(issue, Entity.from(function), message = ""))
+			if (numberOfReturns > max) {
+				report(CodeSmell(issue, Entity.from(function), message = ""))
+			}
 		}
 	}
 
+	private fun isIgnoredFunction(function: KtNamedFunction) = ignoredFunctions.contains(function.name)
+
 	companion object {
 		const val MAX = "max"
+		const val IGNORED_FUNCTION_NAMES = "ignoredFunctionNames"
 	}
 }
