2d10b3d96337eae4149a510ef6a6862d39ef9a79
schalkms <schalkms@icloud.com>
2017-12-15 23:16:06 +0100

    Added more descriptive message to exception in detekt-generator

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/collection/InvalidCodeExampleDocumentationException.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/collection/InvalidCodeExampleDocumentationException.kt
@@ -1,4 +1,4 @@
 package io.gitlab.arturbosch.detekt.generator.collection
 
-class InvalidCodeExampleDocumentationException
-	: RuntimeException("Invalid rule documentation for noncompliant and compliant code examples.")
+class InvalidCodeExampleDocumentationException(ruleName: String)
+	: RuntimeException("Invalid rule documentation for noncompliant and compliant code examples in $ruleName.")
