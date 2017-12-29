416c6e22f2dfc229f3d976d2c79b27ad9b571297
schalkms <schalkms@icloud.com>
2017-12-29 20:29:41 +0100

    Added ConfigPrinter test

---
+++ detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/ConfigPrinterSpec.kt
@@ -0,0 +1,23 @@
+package io.gitlab.arturbosch.detekt.generator.printer
+
+import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.ConfigPrinter
+import io.gitlab.arturbosch.detekt.generator.util.createRuleSetPage
+import io.gitlab.arturbosch.detekt.test.resource
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import java.io.File
+
+class ConfigPrinterSpec : Spek({
+
+	given("a RuleSetPage config to print") {
+
+		it("prints the correct yaml format") {
+			val ruleSetList = listOf(createRuleSetPage())
+			val yamlString = ConfigPrinter.print(ruleSetList)
+			val expectedYamlString = File(resource("/RuleSetConfig.yaml")).readText()
+			assertThat(yamlString).contains(expectedYamlString)
+		}
+	}
+})
