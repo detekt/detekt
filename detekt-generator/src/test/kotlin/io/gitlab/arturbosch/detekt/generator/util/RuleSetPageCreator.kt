066171808671300c798d222ecfa46c7892c98623
schalkms <schalkms@icloud.com>
2017-12-29 20:27:12 +0100

    Added RuleSetPagePrinter test

---
+++ detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/util/RuleSetPageCreator.kt
@@ -0,0 +1,19 @@
+package io.gitlab.arturbosch.detekt.generator.util
+
+import io.gitlab.arturbosch.detekt.generator.collection.Configuration
+import io.gitlab.arturbosch.detekt.generator.collection.Rule
+import io.gitlab.arturbosch.detekt.generator.collection.RuleSetProvider
+import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.RuleSetPage
+
+internal fun createRuleSetPage(): RuleSetPage {
+	val rules = createRules()
+	val ruleSetProvider = RuleSetProvider("style", "style rule set", true, rules.map { it.name })
+	return RuleSetPage(ruleSetProvider, rules)
+}
+
+internal fun createRules(): List<Rule> {
+	val rule1 = Rule("WildcardImport", "a wildcard import", "import foo.*", "import foo.bar", true,
+			listOf(Configuration("conf1", "a config option", "foo")))
+	val rule2 = Rule("EqualsNull", "equals null", "", "", false, emptyList())
+	return listOf(rule1, rule2)
+}
