fc147226afa7994924769bb7fef96dd0aa7d073f
schalkms <schalkms@icloud.com>
2017-12-30 18:20:08 +0100

    Removed code duplication in detekt-generator/out

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/DetektPrinter.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/DetektPrinter.kt
@@ -1,20 +1,22 @@
 package io.gitlab.arturbosch.detekt.generator.printer
 
 import io.gitlab.arturbosch.detekt.generator.Args
-import io.gitlab.arturbosch.detekt.generator.out.markdownFile
-import io.gitlab.arturbosch.detekt.generator.out.yamlFile
+import io.gitlab.arturbosch.detekt.generator.out.MarkdownWriter
+import io.gitlab.arturbosch.detekt.generator.out.YamlWriter
 import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.ConfigPrinter
 import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.RuleSetPage
 import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.RuleSetPagePrinter
 
 class DetektPrinter(private val arguments: Args) {
 
+	private val markdownWriter = MarkdownWriter()
+	private val yamlWriter = YamlWriter()
+
 	fun print(pages: List<RuleSetPage>) {
 		pages.forEach {
-			markdownFile(arguments.documentationPath, it.ruleSet.name) { RuleSetPagePrinter.print(it) }
+			markdownWriter.write(arguments.documentationPath, it.ruleSet.name) { RuleSetPagePrinter.print(it) }
 		}
-
-		yamlFile(arguments.configPath, "default-detekt-config") { ConfigPrinter.print(pages) }
+		yamlWriter.write(arguments.configPath, "default-detekt-config") { ConfigPrinter.print(pages) }
 	}
 
 }
