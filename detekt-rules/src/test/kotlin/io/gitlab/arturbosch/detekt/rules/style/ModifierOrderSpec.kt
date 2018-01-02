5d0b0c063f9feba47f612903bd8e0c75f0d3794f
Matthew Haughton <3flex@users.noreply.github.com>
2018-01-02 13:19:04 +1100

    Add more tests for ModifierOrder rule

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ModifierOrderSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ModifierOrderSpec.kt
@@ -12,24 +12,31 @@ class ModifierOrderSpec : SubjectSpek<ModifierOrder>({
 	subject { ModifierOrder(Config.empty) }
 
 	given("kt classes with modifiers") {
-		val code = "data internal class Test(val test: String)"
+		val bad1 = "data internal class Test(val test: String)"
+		val bad2 = "actual private class Test(val test: String)"
+		val bad3 = "annotation expect class Test"
 
 		it("should report incorrectly ordered modifiers") {
-			assertThat(subject.lint(code)).hasSize(1)
+			assertThat(subject.lint(bad1)).hasSize(1)
+			assertThat(subject.lint(bad2)).hasSize(1)
+			assertThat(subject.lint(bad3)).hasSize(1)
 		}
 
 		it("does not report correctly ordered modifiers") {
 			assertThat(subject.lint("internal data class Test")).isEmpty()
+			assertThat(subject.lint("private actual class Test(val test: String)")).isEmpty()
+			assertThat(subject.lint("expect annotation class Test")).isEmpty()
 		}
 
 		it("should not report issues if inactive") {
 			val rule = ModifierOrder(TestConfig(mapOf("active" to "false")))
-			assertThat(rule.lint(code)).isEmpty()
+			assertThat(rule.lint(bad1)).isEmpty()
+			assertThat(rule.lint(bad2)).isEmpty()
+			assertThat(rule.lint(bad3)).isEmpty()
 		}
 	}
 
 	given("a kt parameter with modifiers") {
-
 
 		it("should report wrongly ordered modifiers") {
 			val code = "lateinit internal private val test: String"
@@ -40,6 +47,7 @@ class ModifierOrderSpec : SubjectSpek<ModifierOrder>({
 			val code = "private internal lateinit val test: String"
 			assertThat(subject.lint(code)).isEmpty()
 		}
+
 	}
 
 	given("an overridden function") {
@@ -61,4 +69,30 @@ class ModifierOrderSpec : SubjectSpek<ModifierOrder>({
 		}
 	}
 
+	given("a tailrec function") {
+
+		it("should report incorrectly ordered modifiers") {
+			val code = "tailrec private fun findFixPoint(x: Double = 1.0): Double"
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("should not report correctly ordered modifiers") {
+			val code = "private tailrec fun findFixPoint(x: Double = 1.0): Double"
+			assertThat(subject.lint(code)).isEmpty()
+		}
+	}
+
+	given("a vararg argument") {
+
+		it("should report incorrectly ordered modifiers") {
+			val code = "fun foo(vararg private val strings: String) {}"
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("should not report correctly ordered modifiers") {
+			val code = "fun foo(private vararg val strings: String) {}"
+			assertThat(subject.lint(code)).isEmpty()
+		}
+	}
+	
 })
