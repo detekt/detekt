e4e4b83f7bfaa949d673ddc0aa8b901e1a268528
Sebastiano Poggi <poggos@gmail.com>
2018-01-13 22:24:59 +0000

    Use tabs for indenting (as per project conventions)

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/FindingsAssertions.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/FindingsAssertions.kt
@@ -8,26 +8,26 @@ import org.assertj.core.internal.Objects
 fun assertThat(findings: List<Finding>) = FindingsAssert(findings)
 
 class FindingsAssert(actual: List<Finding>) :
-        AbstractListAssert<FindingsAssert, List<Finding>,
-                Finding, FindingAssert>(actual, FindingsAssert::class.java) {
+		AbstractListAssert<FindingsAssert, List<Finding>,
+				Finding, FindingAssert>(actual, FindingsAssert::class.java) {
 
-    override fun toAssert(value: Finding?, description: String?): FindingAssert =
-            FindingAssert(value).`as`(description)
+	override fun toAssert(value: Finding?, description: String?): FindingAssert =
+			FindingAssert(value).`as`(description)
 
-    fun hasLocationStrings(vararg expected: String, trimIndent: Boolean = false) {
-        isNotNull
-        val locationStrings = actual.map { it.locationAsString }
-        if (trimIndent) {
-            areEqual(locationStrings.map { it.trimIndent() }, expected.map { it.trimIndent() })
-        } else {
-            areEqual(locationStrings, expected.toList())
-        }
-    }
+	fun hasLocationStrings(vararg expected: String, trimIndent: Boolean = false) {
+		isNotNull
+		val locationStrings = actual.map { it.locationAsString }
+		if (trimIndent) {
+			areEqual(locationStrings.map { it.trimIndent() }, expected.map { it.trimIndent() })
+		} else {
+			areEqual(locationStrings, expected.toList())
+		}
+	}
 
-    private fun areEqual(actualLocationStrings: List<String>, expectedLocationStrings: List<String>) {
-        Objects.instance()
-                .assertEqual(writableAssertionInfo, actualLocationStrings, expectedLocationStrings.toList())
-    }
+	private fun areEqual(actualLocationStrings: List<String>, expectedLocationStrings: List<String>) {
+		Objects.instance()
+				.assertEqual(writableAssertionInfo, actualLocationStrings, expectedLocationStrings.toList())
+	}
 }
 
 class FindingAssert(actual: Finding?) : AbstractAssert<FindingAssert, Finding>(actual, FindingAssert::class.java)
