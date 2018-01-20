74c0eed453db225813a136f7b3e668388b05ca5f
Marvin Ramin <marvin.ramin@gmail.com>
2018-01-20 21:02:38 +0100

    introduce kdoc style rule starting with end of first sentence format

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/KDocStyle.kt
@@ -0,0 +1,56 @@
+package io.gitlab.arturbosch.detekt.rules.documentation
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.MultiRule
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.KtDeclaration
+
+class KDocStyle(config: Config = Config.empty) : MultiRule() {
+
+	private val endOfSentenceFormat = EndOfSentenceFormat()
+
+	override val rules = listOf(
+			endOfSentenceFormat
+	)
+
+	override fun visitDeclaration(dcl: KtDeclaration) {
+		super.visitDeclaration(dcl)
+		endOfSentenceFormat.verify(dcl)
+	}
+}
+
+/**
+ * This rule validates the end of the first sentence of a KDoc comment. It should end with proper punctuation.
+ *
+ * @configuration endOfSentenceFormat - regular expression which should match the end of the first sentence in the KDoc
+ * (default: ([.?!][ \t\n\r\f<])|([.?!]$))
+ *
+ *  @author Marvin Ramin
+ */
+class EndOfSentenceFormat(config: Config = Config.empty) : Rule(config) {
+
+	override val issue = Issue(javaClass.simpleName,
+			Severity.Maintainability,
+			"The first sentence in a KDoc comment should end with correct punctuation.")
+
+	private val endOfSentenceFormat =
+			Regex(valueOrDefault(END_OF_SENTENCE_FORMAT, "([.?!][ \\t\\n\\r\\f<])|([.?!]\$)"))
+
+	fun verify(declaration: KtDeclaration) {
+		declaration.docComment?.let {
+			val text = it.getDefaultSection().getContent()
+			if (!endOfSentenceFormat.containsMatchIn(text)) {
+				report(CodeSmell(issue, Entity.from(declaration), "The first sentence of this KDoc does not" +
+						"end with the correct punctuation."))
+			}
+		}
+	}
+
+	companion object {
+		const val END_OF_SENTENCE_FORMAT = "endOfSentenceFormat"
+	}
+}
--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/CommentSmellProvider.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/CommentSmellProvider.kt
@@ -5,6 +5,7 @@ import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 import io.gitlab.arturbosch.detekt.rules.documentation.CommentOverPrivateFunction
 import io.gitlab.arturbosch.detekt.rules.documentation.CommentOverPrivateProperty
+import io.gitlab.arturbosch.detekt.rules.documentation.KDocStyle
 import io.gitlab.arturbosch.detekt.rules.documentation.UndocumentedPublicClass
 import io.gitlab.arturbosch.detekt.rules.documentation.UndocumentedPublicFunction
 
@@ -23,6 +24,7 @@ class CommentSmellProvider : RuleSetProvider {
 		return RuleSet(ruleSetId, listOf(
 				CommentOverPrivateFunction(config),
 				CommentOverPrivateProperty(config),
+				KDocStyle(config),
 				UndocumentedPublicClass(config),
 				UndocumentedPublicFunction(config)
 		))
