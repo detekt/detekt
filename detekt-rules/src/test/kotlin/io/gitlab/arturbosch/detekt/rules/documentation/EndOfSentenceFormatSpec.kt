460e3f9888f793477978bed32971f490c21c8363
Marvin Ramin <marvin.ramin@gmail.com>
2018-01-20 21:02:38 +0100

    introduce kdoc style rule starting with end of first sentence format

---
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/EndOfSentenceFormatSpec.kt
@@ -0,0 +1,126 @@
+package io.gitlab.arturbosch.detekt.rules.documentation
+
+import io.gitlab.arturbosch.detekt.test.lint
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.subject.SubjectSpek
+
+/**
+ * @author Marvin Ramin
+ */
+class EndOfSentenceFormatSpec : SubjectSpek<KDocStyle>({
+	subject { KDocStyle() }
+
+	it("reports invalid KDoc endings on classes") {
+		val code = """
+			/** Some doc */
+			class Test {
+			}
+			"""
+		assertThat(subject.lint(code)).hasSize(1)
+	}
+
+	it("reports invalid KDoc endings on objects") {
+		val code = """
+			/** Some doc */
+			object Test {
+			}
+			"""
+		assertThat(subject.lint(code)).hasSize(1)
+	}
+
+	it("reports invalid KDoc endings on properties") {
+		val code = """
+			class Test {
+			 	/** Some doc */
+				val test = 3
+			}
+			"""
+		assertThat(subject.lint(code)).hasSize(1)
+	}
+
+	it("reports invalid KDoc endings on top-level functions") {
+		val code = """
+			/** Some doc */
+			fun test() = 3
+			"""
+		assertThat(subject.lint(code)).hasSize(1)
+	}
+
+	it("reports invalid KDoc endings on functions") {
+		val code = """
+			class Test {
+			 	/** Some doc */
+				fun test() = 3
+			}
+			"""
+		assertThat(subject.lint(code)).hasSize(1)
+	}
+
+	it("reports invalid KDoc endings") {
+		val code = """
+			class Test {
+			 	/** Some doc-- */
+				fun test() = 3
+			}
+			"""
+		assertThat(subject.lint(code)).hasSize(1)
+	}
+
+	it("reports invalid KDoc endings in block") {
+		val code = """
+			/**
+			 * Something off abc@@
+			 */
+			class Test {
+			}
+			"""
+		assertThat(subject.lint(code)).hasSize(1)
+	}
+
+	it("does not valid first sentence KDoc endings in a multi sentence comment") {
+		val code = """
+			/**
+			 * This sentence is correct.
+			 *
+			 * This sentence doesn't matter
+			 */
+			class Test {
+			}
+			"""
+		assertThat(subject.lint(code)).isEmpty()
+	}
+
+	it("does not report KDoc ending with periods") {
+		val code = """
+			/**
+			 * Something correct.
+			 */
+			class Test {
+			}
+			"""
+		assertThat(subject.lint(code)).isEmpty()
+	}
+
+	it("does not report KDoc ending with questionmarks") {
+		val code = """
+			/**
+			 * Something correct?
+			 */
+			class Test {
+			}
+			"""
+		assertThat(subject.lint(code)).isEmpty()
+	}
+
+	it("does not report KDoc ending with exclamation marks") {
+		val code = """
+			/**
+			 * Something correct!
+			 */
+			class Test {
+			}
+			"""
+		assertThat(subject.lint(code)).isEmpty()
+	}
+})
