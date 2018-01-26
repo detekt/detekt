8385059272d8540cf66638e3c1085966b51a793a
Artur Bosch <arturbosch@gmx.de>
2018-01-26 16:44:50 +0100

    Improve documentation of sample project - #438

--- detekt-sample-extensions/src/main/kotlin/io/gitlab/arturbosch/detekt/sample/extensions/rules/TooManyFunctions.kt
+++ detekt-sample-extensions/src/main/kotlin/io/gitlab/arturbosch/detekt/sample/extensions/rules/TooManyFunctions.kt
@@ -9,18 +9,23 @@ import org.jetbrains.kotlin.com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
 /**
+ * This is a sample rule reporting too many functions inside a file.
+ *
  * @author Artur Bosch
  */
 class TooManyFunctions : Rule() {
 
-	override val issue = Issue(javaClass.simpleName, Severity.CodeSmell, "")
+	override val issue = Issue(javaClass.simpleName, Severity.CodeSmell,
+			"This rule reports a file with an excessive function count.")
 
 	private var amount: Int = 0
 
 	override fun visitFile(file: PsiFile) {
 		super.visitFile(file)
 		if (amount > THRESHOLD) {
-			report(CodeSmell(issue, Entity.from(file), message = ""))
+			report(CodeSmell(issue, Entity.from(file),
+					message = "The file ${file.name} has $amount function declarations. " +
+							"Threshold is specified with $THRESHOLD."))
 		}
 	}
 
