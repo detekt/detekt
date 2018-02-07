b9d3435c15b4f85589ede3f64007e39c970ce207
Mike Kobit <mkobit@gmail.com>
2018-02-07 11:33:43 -0600

    Simplify `SingleAssign` by using `lateinit` and add tests for it
    Kotlin `1.2` added the ability to check if a `lateinit` variable has been assigned.
    This simplifies the implementation.
    This also changes the `SingleAssign` to be non-null, which may be changed later.
    There are no uses of a nullable `SingleAssign` type previously, so there is no internal breakage.

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/SingleAssign.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/SingleAssign.kt
@@ -5,28 +5,17 @@ import kotlin.reflect.KProperty
 /**
  * @author Artur Bosch
  */
-class SingleAssign<T> {
+class SingleAssign<T : Any> {
 
-	private var initialized = false
-	private var _value: Any? = UNINITIALIZED_VALUE
+	private lateinit var _value: T
 
 	operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
-		if (!initialized) {
-			throw IllegalStateException("Property ${property.name} has not been assigned yet!")
-		}
-		@Suppress("UNCHECKED_CAST")
-		return _value as T
+		check(this::_value.isInitialized) { "Property ${property.name} has not been assigned yet!" }
+		return _value
 	}
 
 	operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-		if (initialized) {
-			throw IllegalStateException("Property ${property.name} has already been assigned!")
-		}
+		check(!this::_value.isInitialized) { "Property ${property.name} has already been assigned!" }
 		_value = value
-		initialized = true
-	}
-
-	companion object {
-		private val UNINITIALIZED_VALUE = Any()
 	}
 }
