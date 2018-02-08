73be74597cf42446a014939ef96b8a5927d02a4e
schalkms <schalkms@icloud.com>
2018-02-08 20:10:02 +0100

    Added allowedExceptionNameRegex config for EmptyCatchBlock
    reference #734

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyCatchBlock.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyCatchBlock.kt
@@ -6,14 +6,25 @@ import org.jetbrains.kotlin.psi.KtCatchClause
 /**
  * Reports empty `catch` blocks. Empty blocks of code serve no purpose and should be removed.
  *
+ * @configuration allowedExceptionNameRegex - ignores exception types which match this regex (default: "")
  * @active since v1.0.0
  * @author Artur Bosch
  * @author Marvin Ramin
  */
 class EmptyCatchBlock(config: Config) : EmptyRule(config = config) {
 
+	private val allowedExceptionNameRegex = Regex(valueOrDefault(ALLOWED_EXCEPTION_NAME_REGEX, ""))
+
 	override fun visitCatchSection(catchClause: KtCatchClause) {
+		val text = catchClause.catchParameter?.typeReference?.text
+		if (text != null && text.matches(allowedExceptionNameRegex)) {
+			return
+		}
 		catchClause.catchBody?.addFindingIfBlockExprIsEmpty()
+	}
+
+	companion object {
+		const val ALLOWED_EXCEPTION_NAME_REGEX = "allowedExceptionNameRegex"
 	}
 
 }
