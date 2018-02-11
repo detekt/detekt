c060d6db78fcda99076e605bd093f8c50c178e72
Marvin Ramin <marvin.ramin@gmail.com>
2018-02-11 16:12:44 +0100

    add task inputs/outputs to detektCheck task

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektCheckTask.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektCheckTask.kt
@@ -1,18 +1,37 @@
 package io.gitlab.arturbosch.detekt
 
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import io.gitlab.arturbosch.detekt.extensions.INPUT_PARAMETER
+import io.gitlab.arturbosch.detekt.extensions.OUTPUT_PARAMETER
 import org.gradle.api.DefaultTask
 import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 /**
  * @author Artur Bosch
  */
 open class DetektCheckTask : DefaultTask() {
 
+	@InputDirectory
+	val input: File?
+
+	@OutputDirectory
+	val output: File?
+
 	init {
 		description = "Analyze your kotlin code with detekt."
 		group = "verification"
+
+		val detektExtension = project.extensions.getByName("detekt") as DetektExtension
+		val arguments = detektExtension.resolveArguments(project)
+		val inputIndex = arguments.indexOf(INPUT_PARAMETER)
+		input = File(arguments[inputIndex + 1])
+
+		val outputIndex = arguments.indexOf(OUTPUT_PARAMETER)
+		output = File(arguments[outputIndex + 1])
 	}
 
 	@TaskAction
