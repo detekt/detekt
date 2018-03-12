d4036dc93235d48ec2047ce6cf198abc11665c6f
Dmytro Primshyts <dprimshyts@gmail.com>
2018-03-12 19:25:50 +0200

    Detekt IntelliJ Plugin UI

---
+++ detekt-intellij-plugin/src/main/java/io/gitlab/arturbosch/detekt/config/DetektConfigurationForm.java
@@ -0,0 +1,11 @@
+package io.gitlab.arturbosch.detekt.config;
+
+import javax.swing.*;
+
+/**
+ * @author Dmytro Troynikov
+ */
+public class DetektConfigurationForm {
+    private JCheckBox enableDetekt;
+    private JCheckBox checkTestSources;
+}
