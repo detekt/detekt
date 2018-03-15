1d9c167df6a9b64888c88d50b86fb78dfca632f6
schalkms <schalkms@icloud.com>
2018-03-15 22:32:18 +0100

    Updated debt report

---
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/DebtSummingSpec.kt
@@ -0,0 +1,36 @@
+package io.gitlab.arturbosch.detekt.cli.console
+
+import io.gitlab.arturbosch.detekt.api.Debt
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.subject.SubjectSpek
+
+internal class DebtSummingSpec : SubjectSpek<DebtSumming>({
+
+	given("debt minutes, hours and days") {
+
+		it("outputs correct minutes, hours and days") {
+			assertThat(createDebtSumming(1, 23, 62).toString()).isEqualTo("2d 2min")
+		}
+
+		it("outputs correct minutes and hours") {
+			assertThat(createDebtSumming(hours = 0, mins = 62).toString()).isEqualTo("1h 2min")
+		}
+
+		it("outputs correct minutes") {
+			assertThat(createDebtSumming(mins = 42).toString()).isEqualTo("42min")
+		}
+
+		it("outputs no debt") {
+			assertThat(DebtSumming().calculateDebt()).isNull()
+		}
+	}
+})
+
+private fun createDebtSumming(days: Int = 0, hours: Int = 0, mins: Int): Debt? {
+	val debt = Debt(days, hours, mins)
+	val debtReport = DebtSumming()
+	debtReport.add(debt)
+	return debtReport.calculateDebt()
+}
