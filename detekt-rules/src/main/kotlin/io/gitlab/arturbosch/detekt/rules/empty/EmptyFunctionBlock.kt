d5459623cb3b74a42fae1121e891791a94c4dd2a
schalkms <schalkms@icloud.com>
2018-03-15 23:36:06 +0100

    Updated EmptyFunctionBlock rule
    * Fixed false negative
    * Added ignoreOverriddenFunctions config option
    
    reference #800

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyFunctionBlock.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyFunctionBlock.kt
@@ -9,17 +9,34 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
  * Reports empty functions. Empty blocks of code serve no purpose and should be removed.
  * This rule will not report functions overriding others.
  *
+ * @configuration ignoreOverriddenFunctions - excludes overridden functions with an empty body (default: false)
+ *
  * @active since v1.0.0
  * @author Artur Bosch
  * @author Marvin Ramin
+ * @author schalkms
  */
 class EmptyFunctionBlock(config: Config) : EmptyRule(config) {
 
+	private val ignoreOverriddenFunctions = valueOrDefault(IGNORE_OVERRIDDEN_FUNCTIONS, false)
+
 	override fun visitNamedFunction(function: KtNamedFunction) {
-		if (!function.isOverridden() && function.notMeantForOverriding()) {
-			function.bodyExpression?.addFindingIfBlockExprIsEmpty()
+		if (function.isOpen()) {
+			return
+		}
+		val bodyExpression = function.bodyExpression
+		if (!ignoreOverriddenFunctions) {
+			if (function.isOverridden()) {
+				bodyExpression?.addFindingIfBlockExprIsEmptyAndNotCommented()
+			} else {
+				bodyExpression?.addFindingIfBlockExprIsEmpty()
+			}
+		} else if (!function.isOverridden()) {
+			bodyExpression?.addFindingIfBlockExprIsEmpty()
 		}
 	}
 
-	private fun KtNamedFunction.notMeantForOverriding() = !isOpen()
+	companion object {
+		const val IGNORE_OVERRIDDEN_FUNCTIONS = "ignoreOverriddenFunctions"
+	}
 }
--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyFunctionBlockSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyFunctionBlockSpec.kt
@@ -1,9 +1,10 @@
 package io.gitlab.arturbosch.detekt.rules.empty
 
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
-import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
 
@@ -14,16 +15,39 @@ class EmptyFunctionBlockSpec : SubjectSpek<EmptyFunctionBlock>({
 
 	subject { EmptyFunctionBlock(Config.empty) }
 
-	describe("should not flag functions meant to be overridden") {
+	given("some functions") {
 
 		it("should flag function with protected modifier") {
-			val findings = subject.lint("protected fun stuff() {}")
-			assertThat(findings).hasSize(1)
+			val code = "protected fun stuff() {}"
+			assertThat(subject.lint(code)).hasSize(1)
 		}
 
 		it("should not flag function with open modifier") {
-			val findings = subject.lint("open fun stuff() {}")
-			assertThat(findings).isEmpty()
+			val code = "open fun stuff() {}"
+			assertThat(subject.lint(code)).isEmpty()
+		}
+	}
+
+	given("some overridden functions") {
+
+		val code = """
+				override fun stuff1() {}
+
+				override fun stuff2() {
+					TODO("Implement this")
+				}
+
+				override fun stuff3() {
+					// this is necessary...
+				}"""
+
+		it("should flag empty block in overridden function") {
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+
+		it("should not flag overridden functions") {
+			val config = TestConfig(mapOf(EmptyFunctionBlock.IGNORE_OVERRIDDEN_FUNCTIONS to "true"))
+			assertThat(EmptyFunctionBlock(config).lint(code)).isEmpty()
 		}
 	}
 
