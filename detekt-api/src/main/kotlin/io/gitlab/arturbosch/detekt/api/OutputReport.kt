23a240b7ba3eab609b2de66d73e7444d64a046a1
schalkms <schalkms@icloud.com>
2018-03-16 00:53:52 +0100

    Added filepath to console output when generating reports

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/OutputReport.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/OutputReport.kt
@@ -11,10 +11,9 @@ abstract class OutputReport : Extension {
 	abstract var fileName: String
 	abstract val ending: String
 
-	fun write(report: Path, detektion: Detektion) {
+	fun write(filePath: Path, detektion: Detektion) {
 		val smellData = render(detektion)
 		smellData?.let {
-			val filePath = report.resolve("$fileName.$ending")
 			filePath.parent?.let { Files.createDirectories(it) }
 			Files.write(filePath, it.toByteArray())
 		}
