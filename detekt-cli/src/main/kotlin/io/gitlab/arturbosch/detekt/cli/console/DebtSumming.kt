7d7de673f787055b4d697bb8363017b0703f4b7d
schalkms <schalkms@icloud.com>
2018-03-15 22:32:18 +0100

    Updated debt report

---
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/DebtSumming.kt
@@ -0,0 +1,41 @@
+package io.gitlab.arturbosch.detekt.cli.console
+
+import io.gitlab.arturbosch.detekt.api.Debt
+
+internal class DebtSumming {
+
+	private val debtList = mutableListOf<Debt>()
+
+	fun add(debt: Debt) {
+		debtList.add(debt)
+	}
+
+	fun calculateDebt(): Debt? {
+		if (debtList.isEmpty())
+		{
+			return null
+		}
+		return calculate()
+	}
+
+	private fun calculate(): Debt {
+		var minutes = 0
+		var hours = 0
+		var days = 0
+		debtList.forEach {
+			minutes += it.mins
+			hours += it.hours
+			days += it.days
+		}
+		hours += minutes / MINUTES_PER_HOUR
+		minutes %= MINUTES_PER_HOUR
+		days += hours / HOURS_PER_DAY
+		hours %= HOURS_PER_DAY
+		return Debt(days, hours, minutes)
+	}
+
+	companion object {
+		private const val HOURS_PER_DAY = 24
+		private const val MINUTES_PER_HOUR = 60
+	}
+}
--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/FindingsReport.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/FindingsReport.kt
@@ -13,12 +13,25 @@ class FindingsReport : ConsoleReport() {
 
 	override fun render(detektion: Detektion): String? {
 		val findings = detektion.findings
+		val totalDebt = DebtSumming()
 		return with(StringBuilder()) {
 			findings.forEach {
 				append(it.key.format("Ruleset: "))
+				val index = length
+				val debtSumming = DebtSumming()
 				it.value.forEach {
+					debtSumming.add(it.issue.debt)
 					append(it.compact().format("\t"))
 				}
+				val debt = debtSumming.calculateDebt()
+				if (debt != null) {
+					insert(index, " - $debt debt")
+					totalDebt.add(debt)
+				}
+			}
+			val debt = totalDebt.calculateDebt()
+			if (debt != null) {
+				append("\nOverall debt: $debt\n")
 			}
 			toString()
 		}
