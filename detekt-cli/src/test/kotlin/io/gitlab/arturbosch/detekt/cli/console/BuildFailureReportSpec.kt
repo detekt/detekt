5314c7ce34bbef59808bd2fd0f03ce559d731050
schalkms <schalkms@icloud.com>
2018-04-10 21:57:03 +0200

    Added tests to console/out package

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/BuildFailureReportSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/BuildFailureReportSpec.kt
@@ -1,13 +1,10 @@
 package io.gitlab.arturbosch.detekt.cli.console
 
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Detektion
-import io.gitlab.arturbosch.detekt.api.Finding
-import io.gitlab.arturbosch.detekt.api.Notification
-import io.gitlab.arturbosch.detekt.api.ProjectMetric
+import io.gitlab.arturbosch.detekt.cli.TestDetektion
+import io.gitlab.arturbosch.detekt.cli.createFinding
 import io.gitlab.arturbosch.detekt.test.TestConfig
 import org.assertj.core.api.Assertions.assertThat
-import org.jetbrains.kotlin.com.intellij.openapi.util.Key
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
@@ -23,15 +20,7 @@ internal class BuildFailureReportSpec : SubjectSpek<BuildFailureReport>({
 	describe("build failure threshold is configurable by configuration") {
 
 		describe("empty code smell result") {
-			val detektion = object : Detektion {
-				override val metrics: Collection<ProjectMetric> = listOf()
-				override val findings: Map<String, List<Finding>> = mapOf()
-				override val notifications: List<Notification> = listOf()
-				override fun add(notification: Notification) = throw UnsupportedOperationException("not implemented")
-				override fun add(projectMetric: ProjectMetric) = throw UnsupportedOperationException("not implemented")
-				override fun <V> getData(key: Key<V>) = throw UnsupportedOperationException("not implemented")
-				override fun <V> addData(key: Key<V>, value: V) = throw UnsupportedOperationException("not implemented")
-			}
+			val detektion = TestDetektion(createFinding())
 
 			it("should fail because no config is provided for configurable console reporter") {
 				assertFails { subject.render(detektion) }
@@ -44,10 +33,10 @@ internal class BuildFailureReportSpec : SubjectSpek<BuildFailureReport>({
 			}
 
 			it("should print a warning if threshold met") {
-				subject.init(TestConfig(mapOf("warningThreshold" to "-2")))
+				subject.init(TestConfig(mapOf("warningThreshold" to "1")))
 				val report = subject.render(detektion)
-				assertThat(report).isEqualTo("Warning: 0 weighted code smells found." +
-						" Warning threshold is -2 and fail threshold is -1!")
+				assertThat(report).isEqualTo("Warning: 1 weighted code smells found." +
+						" Warning threshold is 1 and fail threshold is -1!")
 			}
 
 			it("should throw a build failure error") {
