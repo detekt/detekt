a917751933cb734f606e22fb9a40f0432419d09f
schalkms <schalkms@icloud.com>
2018-04-10 21:14:12 +0200

    Added tests to cli/out package

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/TestFactory.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/TestFactory.kt
@@ -5,6 +5,7 @@ import io.gitlab.arturbosch.detekt.api.Debt
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Location
+import io.gitlab.arturbosch.detekt.api.ProjectMetric
 import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.SourceLocation
 import io.gitlab.arturbosch.detekt.api.TextLocation
@@ -13,6 +14,8 @@ fun createFinding() = CodeSmell(createIssue(), createEntity(), "TestMessage")
 
 fun createIssue()= Issue("TestSmell", Severity.CodeSmell, "For Test", Debt.FIVE_MINS)
 
-fun createEntity() = Entity("TestEntity", "TestEntity", "", createLocation())
+fun createEntity() = Entity("TestEntity", "TestEntity", "S1", createLocation())
 
 fun createLocation() = Location(SourceLocation(1, 1), TextLocation(1, 1), "", "TestFile.kt")
+
+fun createMetric() = ProjectMetric("TestMetric", 1)
