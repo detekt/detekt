6e37fe7644f34bca3f6150fdd5b3026feea9eec6
schalkms <schalkms@icloud.com>
2018-04-18 21:26:26 +0200

    Splitted positive and negative test cases in ObjectPropertyNaming

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/ObjectPropertyNamingSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/ObjectPropertyNamingSpec.kt
@@ -12,79 +12,101 @@ class ObjectPropertyNamingSpec : SubjectSpek<ObjectPropertyNaming>({
 
 	subject { ObjectPropertyNaming() }
 
-	describe("constants in object declarations") {
-		val code = compileContentForTest("""
-			object O {
-				const val MY_NAME_8 = "Artur"
-				const val MYNAME = "Artur"
-				const val MyNAME = "Artur"
-				const val name = "Artur"
-				const val nAme = "Artur"
-				const val _nAme = "Artur" // invalid
-				const val serialVersionUID = 42L
-			}
-		""")
-
-		it("should detect one constant not matching [A-Za-z][_A-Za-z\\d]*") {
-			val findings = subject.lint(code)
-			assertThat(findings).hasSize(1)
-		}
-	}
-
-	describe("constants in companion object") {
-		val code = compileContentForTest("""
-			class C {
-				companion object {
+	val negative = """
 					const val MY_NAME_8 = "Artur"
 					const val MYNAME = "Artur"
 					const val MyNAME = "Artur"
 					const val name = "Artur"
 					const val nAme = "Artur"
-					const val _nAme = "Artur" // invalid
-					const val serialVersionUID = 42L
-				}
-			}
-		""")
+					const val serialVersionUID = 42L"""
+	val positive = """const val _nAme = "Artur""""
 
-		it("should detect one constant not matching [A-Za-z][_A-Za-z\\d]*") {
-			val findings = subject.lint(code)
-			assertThat(findings).hasSize(1)
+	describe("constants in object declarations") {
+
+		it("should not detect any constants not complying to the naming rules") {
+			val code = compileContentForTest("""
+				object O {
+					$negative
+				}
+			""")
+			assertThat(subject.lint(code)).hasSize(0)
+		}
+
+		it("should detect constants not complying to the naming rules") {
+			val code = compileContentForTest("""
+				object O {
+					$positive
+				}
+			""")
+			assertThat(subject.lint(code)).hasSize(1)
+		}
+	}
+
+	describe("constants in companion object") {
+
+		it("should not detect any constants not complying to the naming rules") {
+			val code = compileContentForTest("""
+				class C {
+					companion object {
+						$negative
+					}
+				}
+			""")
+			assertThat(subject.lint(code)).hasSize(0)
+		}
+
+		it("should detect constants not complying to the naming rules") {
+			val code = compileContentForTest("""
+				class C {
+					companion object {
+						$positive
+					}
+				}
+			""")
+			assertThat(subject.lint(code)).hasSize(1)
 		}
 	}
 
 	describe("variables in objects") {
-		val code = compileContentForTest("""
-			object O {
-				val MY_NAME = "Artur"
-				val MYNAME = "Artur"
-				val MyNAME = "Artur"
-				val name = "Artur"
-				val nAme8 = "Artur"
-				val _nAme = "Artur" // invalid
-				private val _name = "Artur" // invalid
-				val serialVersionUID = 42L
-			}
-		""")
 
-		it("should detect two object properties not matching [A-Za-z][_A-Za-z\\d]*") {
-			val findings = subject.lint(code)
-			assertThat(findings).hasSize(2)
+		it("should not detect any constants not complying to the naming rules") {
+			val code = compileContentForTest("""
+				object O {
+					val MY_NAME = "Artur"
+					val MYNAME = "Artur"
+					val MyNAME = "Artur"
+					val name = "Artur"
+					val nAme8 = "Artur"
+					val serialVersionUID = 42L
+				}
+			""")
+			assertThat(subject.lint(code)).hasSize(0)
+		}
+
+		it("should detect constants not complying to the naming rules") {
+			val code = compileContentForTest("""
+				object O {
+					val _nAme = "Artur"
+					private val _name = "Artur"
+				}
+			""")
+			assertThat(subject.lint(code)).hasSize(2)
 		}
 	}
 
 	describe("variables and constants in objects with custom config") {
-		val code = compileContentForTest("""
-			object O {
-				val _name = "Artur"
-				val NAME = "Artur" // invalid
-				const val MYNAME = "Artur"
-				const val myname = "Artur" // invalid
-			}
-		""")
 
-		it("should detect invalid variables and constants in object") {
-			val config = TestConfig(mapOf(ObjectPropertyNaming.CONSTANT_PATTERN to "[A-Z][_A-Z0-9]*"))
-			assertThat(ObjectPropertyNaming(config).lint(code)).hasSize(2)
+		val config = TestConfig(mapOf(ObjectPropertyNaming.CONSTANT_PATTERN to "_[A-Za-z]*"))
+		val subject = ObjectPropertyNaming(config)
+
+		it("should not detect constants in object with underscores") {
+			val code = compileContentForTest("""
+				object O {
+					const val _NAME = "Artur"
+					const val _name = "Artur"
+				}
+			""")
+			assertThat(subject.lint(code)).hasSize(0)
 		}
 	}
 })
