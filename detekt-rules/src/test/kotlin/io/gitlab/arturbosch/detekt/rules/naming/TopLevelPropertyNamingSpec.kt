ea579ed6b5bff5003f60ce2b7af3c43ec8b666f4
Artur Bosch <arturbosch@gmx.de>
2018-04-19 19:11:20 +0200

    Adjust top level property naming to be on pair with intellij - #866

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/TopLevelPropertyNamingSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/TopLevelPropertyNamingSpec.kt
@@ -35,25 +35,26 @@ class TopLevelPropertyNamingSpec : SubjectSpek<TopLevelPropertyNaming>({
 
 	describe("variables on top level") {
 
-		it("should not detect any constants not complying to the naming rules") {
+		it("should not report any") {
 			val code = compileContentForTest("""
 				val name = "Artur"
 				val nAme8 = "Artur"
 				private val _name = "Artur"
             	val serialVersionUID = 42L
+                val MY_NAME = "Artur"
+        	    val MYNAME = "Artur"
+        	    val MyNAME = "Artur"
+				private val NAME = "Artur"
+				val s_d_d_1 = listOf("")
 			""")
 			assertThat(subject.lint(code)).hasSize(0)
 		}
 
-		it("should detect five constants not complying to the naming rules") {
+		it("should report non private property using underscore") {
 			val code = compileContentForTest("""
-				val MY_NAME = "Artur"
-        	    val MYNAME = "Artur"
-        	    val MyNAME = "Artur"
 				val _nAme = "Artur"
-				private val NAME = "Artur"
 			""")
-			assertThat(subject.lint(code)).hasSize(5)
+			assertThat(subject.lint(code)).hasSize(1)
 		}
 	}
 })
