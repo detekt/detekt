72d756cc3cfef0fa632c4a0afa53a871669083bd
Artur Bosch <arturbosch@gmx.de>
2018-04-28 11:40:53 +0200

    Make executor in detekt configurable - #862

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/ProcessingSettings.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/ProcessingSettings.kt
@@ -3,8 +3,12 @@ package io.gitlab.arturbosch.detekt.core
 import io.gitlab.arturbosch.detekt.api.Config
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.concurrent.ExecutorService
 
 /**
+ * Settings to be used by detekt.
+ * If using a custom executor service be aware that detekt won't shutdown it after use!
+ *
  * @author Artur Bosch
  */
 @Suppress("LongParameterList")
@@ -13,15 +17,18 @@ data class ProcessingSettings(val project: List<Path>,
 							  val pathFilters: List<PathFilter> = listOf(),
 							  val parallelCompilation: Boolean = false,
 							  val excludeDefaultRuleSets: Boolean = false,
-							  val pluginPaths: List<Path> = emptyList()) {
+							  val pluginPaths: List<Path> = emptyList(),
+							  val executorService: ExecutorService? = null) {
 
 	constructor(project: Path,
 				config: Config = Config.empty,
 				pathFilters: List<PathFilter> = listOf(),
 				parallelCompilation: Boolean = false,
 				excludeDefaultRuleSets: Boolean = false,
-				pluginPaths: List<Path> = emptyList()) :
-			this(listOf(project), config, pathFilters, parallelCompilation, excludeDefaultRuleSets, pluginPaths)
+				pluginPaths: List<Path> = emptyList(),
+				executorService: ExecutorService? = null) :
+			this(listOf(project), config, pathFilters, parallelCompilation,
+					excludeDefaultRuleSets, pluginPaths, executorService)
 
 	init {
 		pluginPaths.forEach {
