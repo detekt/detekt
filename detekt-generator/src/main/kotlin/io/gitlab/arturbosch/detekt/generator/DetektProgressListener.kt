497aa120164c60808f41beed253ab4e5ca1174c3
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-05-12 23:36:53 +0200

    Adjust terminal output in case there's only one file.

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/DetektProgressListener.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/DetektProgressListener.kt
@@ -9,7 +9,8 @@ import org.jetbrains.kotlin.psi.KtFile
 class DetektProgressListener : FileProcessListener {
 
 	override fun onStart(files: List<KtFile>) {
-		print("Analyzing ${files.size} kotlin files: ")
+		val name = if (files.size == 1) "file" else "files"
+		print("Analyzing ${files.size} kotlin $name: ")
 	}
 
 	override fun onProcess(file: KtFile) {
