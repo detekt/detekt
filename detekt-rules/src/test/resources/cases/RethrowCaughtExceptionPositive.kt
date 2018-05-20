a690241fd901fcd8c3d7479ba308d8b2943f1109
schalkms <schalkms@icloud.com>
2018-05-20 22:27:30 +0200

    Made RethrowException violation reporting stricter
    The rule checks if the rethrown caught exception is used before.

--- detekt-rules/src/test/resources/cases/RethrowCaughtExceptionPositive.kt
+++ detekt-rules/src/test/resources/cases/RethrowCaughtExceptionPositive.kt
@@ -1,6 +1,6 @@
 package cases
 
-@Suppress("unused", "UNREACHABLE_CODE")
+@Suppress("unused", "UNREACHABLE_CODE", "UNUSED_EXPRESSION")
 fun rethrowCaughtExceptionPositive() {
 	try {
 	} catch (e: IllegalStateException) {
@@ -10,5 +10,15 @@ fun rethrowCaughtExceptionPositive() {
 	} catch (e: IllegalStateException) {
 		throw e // reports 1 - dead code after the same exception is rethrown
 		print("log")
+	}
+	try {
+	} catch (e: IllegalStateException) {
+		print("log") // reports 1 - exception is not logged
+		throw e
+	}
+	try {
+	} catch (e: IllegalStateException) {
+		print(e.message) // reports 1 - logs only the exception message, exception is lost
+		throw e
 	}
 }
