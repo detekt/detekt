b26cf2bdc574489e1fbf72cb593d44b911d4ba07
Joey Kaan <jkaan@users.noreply.github.com>
2018-05-20 09:56:28 +0200

    Resolve PR comments

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/optional/MandatoryBracesIfStatements.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/optional/MandatoryBracesIfStatements.kt
@@ -7,13 +7,13 @@ import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
-import org.jetbrains.kotlin.KtNodeTypes
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
+import org.jetbrains.kotlin.psi.KtBlockExpression
 import org.jetbrains.kotlin.psi.KtIfExpression
 import org.jetbrains.kotlin.psi.psiUtil.siblings
 
 /**
- * This rule detects multi-line `if` statements which does have braces.
+ * This rule detects multi-line `if` statements which do not have braces.
  * Adding braces would improve readability and avoid possible errors.
  *
  * <noncompliant>
@@ -37,16 +37,18 @@ class MandatoryBracesIfStatements(config: Config = Config.empty) : Rule(config) 
 
 	override fun visitIfExpression(expression: KtIfExpression) {
 		if (isNotBlockExpression(expression) && hasNewLine(expression)) {
-			report(CodeSmell(issue, Entity.from(expression), message = ""))
+			report(CodeSmell(issue, Entity.from(expression),
+					message = "Multi-line if statements were found that do not have braces. " +
+							"These can be added to improve readability and prevent possible errors."))
 		}
 		super.visitIfExpression(expression)
 	}
 
 	private fun hasNewLine(expression: KtIfExpression): Boolean =
 			expression.rightParenthesis?.siblings(true, false)
-					?.filter { it is PsiWhiteSpace }
-					?.firstOrNull { (it as PsiWhiteSpace).textContains('\n') } != null
+					?.filterIsInstance<PsiWhiteSpace>()
+					?.firstOrNull { it.textContains('\n') } != null
 
 	private fun isNotBlockExpression(expression: KtIfExpression): Boolean =
-			expression.then?.node?.elementType != KtNodeTypes.BLOCK
+			expression.then !is KtBlockExpression
 }
