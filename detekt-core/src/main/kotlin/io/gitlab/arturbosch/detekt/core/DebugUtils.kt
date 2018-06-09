8b2e6631138210cdc9fe9a6719b8f88ca6ecdf5a
Artur Bosch <arturbosch@gmx.de>
2018-06-10 00:06:35 +0200

    Close manifest streams

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/DebugUtils.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/DebugUtils.kt
@@ -1,6 +1,7 @@
 package io.gitlab.arturbosch.detekt.core
 
 import java.io.IOException
+import java.net.URL
 import java.util.jar.Manifest
 
 /**
@@ -12,16 +13,21 @@ fun whichOS(): String = System.getProperty("os.name")
 fun whichJava(): String = System.getProperty("java.runtime.version")
 
 fun whichDetekt(): String {
-	for (resource in Detektor::class.java.classLoader
-			.getResources("META-INF/MANIFEST.MF")) {
+	for (resource in Detektor::class.java.classLoader.getResources("META-INF/MANIFEST.MF")) {
 		try {
-			Manifest(resource.openStream())
-					.mainAttributes
-					.getValue("DetektVersion")
-					?.let { return it }
+			val version = readDetektVersionInManifest(resource)
+			if (version != null) {
+				return version
+			}
 		} catch (_: IOException) {
 			// we search for the manifest with the detekt version
 		}
 	}
 	return "unknown"
 }
+
+private fun readDetektVersionInManifest(resource: URL) =
+		resource.openStream().use {
+			Manifest().mainAttributes
+					.getValue("DetektVersion")
+		}
