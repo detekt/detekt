ad09b0accbb25c134d11f46e6553b557cafc7c30
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-06-14 16:57:38 +0200

    WildcardImport: Remove one force unwrap and reuse it from let function

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/WildcardImport.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/WildcardImport.kt
@@ -68,7 +68,7 @@ class WildcardImport(config: Config = Config.empty) : Rule(config) {
 			if (excludedImports.contains(import)) {
 				return
 			}
-				report(CodeSmell(issue, Entity.from(importDirective), "${importDirective.importPath!!.pathStr} " +
+				report(CodeSmell(issue, Entity.from(importDirective), "$it " +
 						"is a wildcard import. Replace it with fully qualified imports."))
 		}
 	}
