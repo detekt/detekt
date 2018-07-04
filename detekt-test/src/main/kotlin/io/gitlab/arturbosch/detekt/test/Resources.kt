6c82994d4bf6daf9bd12c13599cac601b6e5c920
schalkms <schalkms@icloud.com>
2018-07-04 19:39:28 +0200

    Cleanup

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/Resources.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/Resources.kt
@@ -1,7 +1,6 @@
 package io.gitlab.arturbosch.detekt.test
 
 import io.gitlab.arturbosch.detekt.api.YamlConfig
-import java.io.File
 import java.net.URI
 
 internal object Resources
@@ -12,7 +11,5 @@ fun resource(name: String): URI {
 	requireNotNull(resource) { "Make sure the resource '$name' exists!" }
 	return resource.toURI()
 }
-
-fun resourceAsString(name: String): String = File(resource(name)).readText()
 
 fun yamlConfig(resource: String) = YamlConfig.loadResource(Resources::class.java.getResource("/$resource"))
