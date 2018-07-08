e671dd9f5408e22ab020054cba8566ac9053c419
Artur Bosch <arturbosch@gmx.de>
2018-07-08 16:06:07 +0200

    Use new loadRuleSet function

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/RuleSetExtensions.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/RuleSetExtensions.kt
@@ -4,9 +4,8 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 
 /**
- * @author Artur Bosch
+ * Creates a RuleSet instance of given RuleSetProvider.
  */
-
 inline fun <reified T : RuleSetProvider> loadRuleSet(config: Config = Config.empty) =
 		(T::class.java.constructors[0].newInstance() as T).buildRuleset(config)
 				?: throw IllegalStateException("Could not load RuleSet for '${T::class.java}'")
