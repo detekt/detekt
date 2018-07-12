024d2f0303ef6bded6a99cfa8e2140fda55ed856
shunsuke maeda <duck8823@gmail.com>
2018-07-12 19:37:48 +0900

    Fix to work on multi module in pom.xml

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/JCommander.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/JCommander.kt
@@ -3,11 +3,11 @@ package io.gitlab.arturbosch.detekt.cli
 import com.beust.jcommander.JCommander
 import com.beust.jcommander.ParameterException
 
-val jCommander = JCommander()
-
 inline fun <reified T : Args> parseArguments(args: Array<String>): T {
 	val cli = T::class.java.declaredConstructors.firstOrNull()?.newInstance() as? T
 			?: throw IllegalStateException("Could not create Args object for class ${T::class.java}")
+
+	val jCommander = JCommander()
 
 	jCommander.addObject(cli)
 	jCommander.programName = "detekt"
@@ -36,6 +36,6 @@ fun failWithErrorMessages(messages: Iterable<String?>) {
 		println(it)
 	}
 	println()
-	jCommander.usage()
+	JCommander().usage()
 	System.exit(-1)
 }
