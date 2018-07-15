839ad12a0288ea1e3b1c06053c98f7777e5d9899
Mikhail Levchenko <themishkun@yandex-team.ru>
2018-07-15 16:34:07 +0300

    Added the `unnecessary let` rule

---
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnnecessaryLetSpec.kt
@@ -0,0 +1,25 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.test.lint
+import org.assertj.core.api.Assertions
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.subject.SubjectSpek
+
+class UnnecessaryLetSpec : SubjectSpek<UnnecessaryLet>({
+	subject { UnnecessaryLet() }
+
+	describe("check lets"){
+		it("has unnecessary lets"){
+			val findings = subject.lint("""
+				val a : Int? = null
+				val b = a.let { it.plus(3) }
+				val c = b
+				b.let { it?.plus(c) }
+				a?.let { it.plus(b) }""")
+			println(findings.joinToString { it.message })
+			Assertions.assertThat(findings).hasSize(1)
+		}
+	}
+})
+
