2e740025611836587a20d1505cfa942dbc509b18
Mikhail Levchenko <themishkun@yandex-team.ru>
2018-07-15 16:34:07 +0300

    Added the `unnecessary let` rule

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnnecessaryLet.kt
@@ -0,0 +1,32 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.getCallNameExpression
+import org.jetbrains.kotlin.psi.psiUtil.getQualifiedExpressionForReceiver
+
+/**
+ * @author mishkun
+ */
+class UnnecessaryLet : Rule() {
+	override val issue = Issue(javaClass.simpleName, Severity.Style,
+			"The `let` usage is unnecessary", Debt.FIVE_MINS)
+
+	// catches the `let { it.foo() }` and `let { it.baz }` cases
+	private val letItRegex = """let\s*\{\s*it\??\.\w+(?:\(.*\))?\s*}""".toRegex()
+	// catches the `let { a -> a.foo() }` and `let { a -> a.baz }` cases
+	private val letParamRegex = """let\s*\{\s*(\w*)\s*->\s*\1\??\.\w*(?:\(.*\))?\s*}""".toRegex()
+
+	override fun visitCallExpression(expression: KtCallExpression) {
+		val isLetIt = expression.text matches letItRegex
+		val isLetParam = expression.text matches letParamRegex
+		if	(isLetIt || isLetParam){
+			report(CodeSmell(
+					issue, Entity.from(expression.parent ?: expression),
+					"let expression can be omitted"
+			))
+		}
+	}
+
+}
