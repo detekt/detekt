9832da88d40c3ede89d314afec3df8aa07afce7f
Markus Schwarz <post@markus-schwarz.net>
2018-07-22 23:43:03 +0200

    removes unused constants

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/Constants.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/Constants.kt
@@ -5,26 +5,7 @@ package io.gitlab.arturbosch.detekt.extensions
  * @author Marvin Ramin
  */
 
-const val DETEKT_PROFILE = "detekt.profile"
 const val DEFAULT_DEBUG_VALUE = false
 const val DEFAULT_PARALLEL_VALUE = false
 const val DEFAULT_DISABLE_RULESETS_VALUE = false
 const val DEFAULT_REPORT_ENABLED_VALUE = true
-
-const val DEFAULT_PROFILE_NAME = "main"
-const val DEFAULT_TRUE = "true"
-const val DEFAULT_DETEKT_CONFIG_RESOURCE = "/default-detekt-config.yml"
-const val DEFAULT_PATH_EXCLUDES = ".*/resources/.*,.*/build/.*,.*/target/.*"
-
-const val DEBUG_PARAMETER = "--debug"
-const val FILTERS_PARAMETER = "--filters"
-const val INPUT_PARAMETER = "--input"
-const val CONFIG_PARAMETER = "--config"
-const val BASELINE_PARAMETER = "--baseline"
-const val PARALLEL_PARAMETER = "--parallel"
-const val DISABLE_DEFAULT_RULESETS_PARAMETER = "--disable-default-rulesets"
-const val PLUGINS_PARAMETER = "--plugins"
-const val REPORT_XML_PARAMETER = "--report-xml"
-const val REPORT_HTML_PARAMETER = "--report-html"
-const val GENERATE_CONFIG_PARAMETER = "--generate-config"
-const val CREATE_BASELINE_PARAMETER = "--create-baseline"
