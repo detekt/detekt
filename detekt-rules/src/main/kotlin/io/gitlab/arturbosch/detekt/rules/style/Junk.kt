adb12ba1623c04150392bae6bd396ba1f76a31a5
schalkms <schalkms@icloud.com>
2018-08-02 10:36:39 +0200

    Fixed #1030 - incorrect linenr in TrailingWhitespace
    The TrailingWhitespace now reports the right line number in the xml
    report.

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/Junk.kt
@@ -0,0 +1,13 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import org.jetbrains.kotlin.com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.KtAnnotated
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.psiUtil.elementsInRange
+
+internal fun findAnnotatedStatementInLine(file: KtFile, offset: Int, line: String): PsiElement? {
+	return file.elementsInRange(TextRange.create(offset - line.length, offset))
+			.mapNotNull { it as? KtAnnotated ?: if (it.parent is KtAnnotated) it.parent else null }
+			.firstOrNull()
+}
--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxLineLength.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxLineLength.kt
@@ -8,11 +8,7 @@ import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.MultiRule
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
-import org.jetbrains.kotlin.com.intellij.openapi.util.TextRange
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.psi.KtAnnotated
 import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.psi.psiUtil.elementsInRange
 
 class FileParsingRule(val config: Config = Config.empty) : MultiRule() {
 
@@ -83,12 +79,6 @@ class MaxLineLength(config: Config = Config.empty) : Rule(config) {
 
 			offset += 1 /* '\n' */
 		}
-	}
-
-	private fun findAnnotatedStatementInLine(file: KtFile, offset: Int, line: String): PsiElement? {
-		return file.elementsInRange(TextRange.create(offset - line.length, offset))
-				.mapNotNull { it as? KtAnnotated ?: if (it.parent is KtAnnotated) it.parent else null }
-				.firstOrNull()
 	}
 
 	private fun isValidLine(line: String): Boolean {
