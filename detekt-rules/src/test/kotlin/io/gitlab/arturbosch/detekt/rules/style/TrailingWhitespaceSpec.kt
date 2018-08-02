c52dd694035d277672e2504e15125130bedc8e06
schalkms <schalkms@icloud.com>
2018-08-02 10:36:39 +0200

    Fixed #1030 - incorrect linenr in TrailingWhitespace
    The TrailingWhitespace now reports the right line number in the xml
    report.

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/TrailingWhitespaceSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/TrailingWhitespaceSpec.kt
@@ -1,7 +1,8 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
 import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.test.assertThat
+import io.gitlab.arturbosch.detekt.test.compileForTest
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -9,14 +10,24 @@ import org.jetbrains.spek.api.dsl.it
 class TrailingWhitespaceSpec : Spek({
 
 	given("a kt file that contains lines that end with a whitespace") {
+		val rule = TrailingWhitespace()
+		val file = compileForTest(Case.TrailingWhitespacePositive.path())
+		val lines = file.text.splitToSequence("\n")
+		rule.visit(KtFileContent(file, lines))
+
 		it("should flag it") {
-			val rule = TrailingWhitespace()
-			rule.visit(Case.TrailingWhitespacePositive.getKtFileContent())
 			assertThat(rule.findings).hasSize(7)
+		}
+
+		it("should report the correct source location") {
+			val findingSource = rule.findings[1].location.source
+			assertThat(findingSource.line).isEqualTo(4)
+			assertThat(findingSource.column).isEqualTo(1)
 		}
 	}
 
 	given("a kt file that does not contain lines that end with a whitespace") {
+
 		it("should not flag it") {
 			val rule = TrailingWhitespace()
 			rule.visit(Case.TrailingWhitespaceNegative.getKtFileContent())
