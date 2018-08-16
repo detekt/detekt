5ec52120b1462bbfe0992b307d891e10a3432714
schalkms <schalkms@icloud.com>
2018-08-16 22:52:46 +0200

    Updated NoTabs documentation

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/NoTabs.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/NoTabs.kt
@@ -17,7 +17,7 @@ import org.jetbrains.kotlin.psi.KtFile
  * According to
  * [Google's Kotlin style guide](https://android.github.io/kotlin-guides/style.html#whitespace-characters)
  * the only whitespace chars that are allowed in a source file are the line terminator sequence
- * and the ASCII horizontal space character (0x20).
+ * and the ASCII horizontal space character (0x20). Strings containing tabs are allowed.
  *
  * @author Misa Torres
  * @author schalkms
