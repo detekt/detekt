1c3fc9376757919d1cf29d2edc2f6af4ee0678d2
schalkms <schalkms@icloud.com>
2018-08-16 22:41:03 +0200

    Reworked NoTabs rule
    This rule now finds whitespaces with the corresponding PSI element
    instead of iterating over the characters of each file.

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxLineLength.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxLineLength.kt
@@ -5,27 +5,9 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Debt
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
-import io.gitlab.arturbosch.detekt.api.MultiRule
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import org.jetbrains.kotlin.psi.KtFile
-
-class FileParsingRule(val config: Config = Config.empty) : MultiRule() {
-
-	private val maxLineLength = MaxLineLength(config)
-	private val trailingWhitespace = TrailingWhitespace(config)
-	private val noTabs = NoTabs(config)
-	override val rules = listOf(maxLineLength, trailingWhitespace, noTabs)
-
-	override fun visitKtFile(file: KtFile) {
-		val lines = file.text.splitToSequence("\n")
-		val fileContents = KtFileContent(file, lines)
-
-		maxLineLength.runIfActive { visit(fileContents) }
-		trailingWhitespace.runIfActive { visit(fileContents) }
-		noTabs.runIfActive { visit(fileContents) }
-	}
-}
 
 data class KtFileContent(val file: KtFile, val content: Sequence<String>)
 
--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/NoTabs.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/NoTabs.kt
@@ -3,10 +3,14 @@ package io.gitlab.arturbosch.detekt.rules.style
 import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.DetektVisitor
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.api.isPartOfString
+import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
+import org.jetbrains.kotlin.psi.KtFile
 
 /**
  * This rule reports if tabs are used in Kotlin files.
@@ -16,6 +20,7 @@ import io.gitlab.arturbosch.detekt.api.Severity
  * and the ASCII horizontal space character (0x20).
  *
  * @author Misa Torres
+ * @author schalkms
  */
 class NoTabs(config: Config = Config.empty) : Rule(config) {
 
@@ -24,12 +29,22 @@ class NoTabs(config: Config = Config.empty) : Rule(config) {
 			"Checks if tabs are used in Kotlin files.",
 			Debt.FIVE_MINS)
 
-	fun visit(ktFileContent: KtFileContent) {
-		ktFileContent.content.forEachIndexed { index, line ->
-			if (line.contains('\t')) {
-				report(CodeSmell(issue, Entity.from(ktFileContent.file),
-						"Line ${index + 1} uses the tab character."))
+	fun findTabs(file: KtFile) {
+		file.collectWhitespaces()
+				.filter { !it.isPartOfString() && it.text.contains('\t') }
+				.forEach { report(CodeSmell(issue, Entity.from(it), "Tab character is in use.")) }
+	}
+
+	private fun KtFile.collectWhitespaces(): List<PsiWhiteSpace> {
+		val list = mutableListOf<PsiWhiteSpace>()
+		this.accept(object : DetektVisitor() {
+			override fun visitWhiteSpace(space: PsiWhiteSpace?) {
+				if (space != null) {
+					list.add(space)
+				}
+				super.visitWhiteSpace(space)
 			}
-		}
+		})
+		return list
 	}
 }
