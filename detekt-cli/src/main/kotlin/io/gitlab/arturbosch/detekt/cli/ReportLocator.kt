95c1d01d3cf32f1428b09127aef8733b2ac6f05f
Artur Bosch <arturbosch@gmx.de>
2018-09-08 14:25:11 +0200

    Reimplement custom output report support

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/ReportLocator.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/ReportLocator.kt
@@ -2,6 +2,7 @@ package io.gitlab.arturbosch.detekt.cli
 
 import io.gitlab.arturbosch.detekt.api.ConsoleReport
 import io.gitlab.arturbosch.detekt.api.Extension
+import io.gitlab.arturbosch.detekt.api.OutputReport
 import io.gitlab.arturbosch.detekt.core.ProcessingSettings
 import java.net.URLClassLoader
 import java.util.ServiceLoader
@@ -13,15 +14,31 @@ class ReportLocator(private val settings: ProcessingSettings) {
 
 	private val consoleSubConfig = settings.config.subConfig("console-reports")
 	private val consoleActive = consoleSubConfig.valueOrDefault(ACTIVE, true)
-	private val consoleExcludes = consoleSubConfig.valueOrDefault(EXCLUDE, emptyList<String>())
+	private val consoleExcludes = consoleSubConfig.valueOrDefault(EXCLUDE, emptyList<String>()).toSet()
+
+	private val outputSubConfig = settings.config.subConfig("output-reports")
+	private val outputActive = outputSubConfig.valueOrDefault(ACTIVE, true)
+	private val outputExcludes = outputSubConfig.valueOrDefault(EXCLUDE, emptyList<String>()).toSet()
 
 	fun load(): List<Extension> {
 		LOG.debug("console-report=$consoleActive")
+		LOG.debug("output-report=$outputActive")
 		val detektLoader = URLClassLoader(settings.pluginUrls, javaClass.classLoader)
 		val consoleReports = loadConsoleReports(detektLoader)
 		LOG.debug { "ConsoleReports: $consoleReports" }
-		return consoleReports
+		val outputReports = loadOutputReports(detektLoader)
+		LOG.debug { "OutputReports: $outputReports" }
+		return consoleReports.plus(outputReports)
 	}
+
+	private fun loadOutputReports(detektLoader: URLClassLoader) =
+			if (outputActive) {
+				ServiceLoader.load(OutputReport::class.java, detektLoader)
+						.filter { it.id !in outputExcludes }
+						.toList()
+			} else {
+				emptyList<OutputReport>()
+			}
 
 	private fun loadConsoleReports(detektLoader: URLClassLoader) =
 			if (consoleActive) {
---
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/out/ReportsSpec.kt
@@ -0,0 +1,62 @@
+package io.gitlab.arturbosch.detekt.cli.out
+
+import io.gitlab.arturbosch.detekt.api.Detektion
+import io.gitlab.arturbosch.detekt.api.OutputReport
+import io.gitlab.arturbosch.detekt.cli.CliArgs
+import io.gitlab.arturbosch.detekt.cli.ReportLocator
+import io.gitlab.arturbosch.detekt.cli.parseArguments
+import io.gitlab.arturbosch.detekt.core.ProcessingSettings
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Condition
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import java.util.function.Predicate
+
+/**
+ * @author Artur Bosch
+ */
+internal class ReportsSpec : Spek({
+
+	given("arguments for jcommander") {
+
+		val reportUnderTest = TestOutputReport::class.java.simpleName
+		val args = arrayOf(
+				"--input", "/tmp/must/be/given",
+				"--report", "xml:/tmp/path1",
+				"--report", "plain:/tmp/path2",
+				"--report", "$reportUnderTest:/tmp/path3"
+		)
+		val (cli, _) = parseArguments<CliArgs>(args)
+
+		val reports = cli.reportPaths
+
+		it("should parse multiple report entries") {
+			assertThat(reports).hasSize(3)
+		}
+
+		val extensions = ReportLocator(ProcessingSettings(listOf())).load()
+		val extensionsIds = extensions.mapTo(HashSet()) { it.id }
+
+		it("should be able to convert to output reports") {
+			assertThat(reports).allMatch { it.kind in extensionsIds }
+		}
+
+		it("should recognize custom output format") {
+			assertThat(reports).haveExactly(1,
+					Condition(Predicate { it.kind == reportUnderTest },
+							"Corresponds exactly to the test output report."))
+
+			assertThat(extensions).haveExactly(1,
+					Condition(Predicate { it is TestOutputReport && it.ending == "yml" },
+							"Is exactly the test output report."))
+		}
+	}
+})
+
+internal class TestOutputReport : OutputReport() {
+	override val ending: String = "yml"
+	override fun render(detektion: Detektion): String? {
+		throw UnsupportedOperationException("not implemented")
+	}
+}
