5d13461cc3eb9ac6b267eb92097d3c6688da4d42
Sebastiano Poggi <poggos@gmail.com>
2018-09-17 11:28:05 +0100

    Weight braceless when entries .5 in ComplexMethod, add config for weight

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/ComplexMethodSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/ComplexMethodSpec.kt
@@ -1,10 +1,11 @@
 package io.gitlab.arturbosch.detekt.rules.complexity
 
-import io.gitlab.arturbosch.detekt.api.ThresholdedCodeSmell
+import io.gitlab.arturbosch.detekt.api.SourceLocation
 import io.gitlab.arturbosch.detekt.rules.Case
 import io.gitlab.arturbosch.detekt.test.TestConfig
+import io.gitlab.arturbosch.detekt.test.assertThat
+import io.gitlab.arturbosch.detekt.test.isThresholded
 import io.gitlab.arturbosch.detekt.test.lint
-import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -19,9 +20,13 @@ class ComplexMethodSpec : Spek({
 		it("finds one complex method") {
 			val subject = ComplexMethod()
 			subject.lint(Case.ComplexClass.path())
-			assertThat(subject.findings).hasSize(1)
-			assertThat((subject.findings[0] as ThresholdedCodeSmell).value).isEqualTo(20)
-			assertThat((subject.findings[0] as ThresholdedCodeSmell).threshold).isEqualTo(10)
+
+			assertThat(subject.findings).hasSourceLocations(SourceLocation(3, 1))
+
+			assertThat(subject.findings.first())
+					.isThresholded()
+					.withValue(19)
+					.withThreshold(10)
 		}
 	}
 
@@ -30,14 +35,56 @@ class ComplexMethodSpec : Spek({
 		val path = Case.ComplexMethods.path()
 
 		it("does not report complex methods with a single when expression") {
-			val config = TestConfig(mapOf(ComplexMethod.IGNORE_SINGLE_WHEN_EXPRESSION to "true"))
+			val config = TestConfig(mapOf(
+					ComplexMethod.SIMPLE_WHEN_ENTRY_WEIGHT to "1.0",
+					ComplexMethod.IGNORE_SINGLE_WHEN_EXPRESSION to "true"))
 			val subject = ComplexMethod(config, threshold = 4)
-			assertThat(subject.lint(path)).hasSize(1)
+
+			assertThat(subject.lint(path)).hasSourceLocations(SourceLocation(42, 1))
 		}
 
 		it("reports all complex methods") {
-			val subject = ComplexMethod(threshold = 4)
-			assertThat(subject.lint(path)).hasSize(5)
+			val config = TestConfig(mapOf(ComplexMethod.SIMPLE_WHEN_ENTRY_WEIGHT to "1.0"))
+			val subject = ComplexMethod(config, threshold = 4)
+
+			assertThat(subject.lint(path)).hasSourceLocations(
+					SourceLocation(5, 1),
+					SourceLocation(14, 1),
+					SourceLocation(24, 1),
+					SourceLocation(34, 1),
+					SourceLocation(42, 1)
+			)
+		}
+
+		it("does not trip for a reasonable amount of simple when entries") {
+			val config = TestConfig(mapOf(ComplexMethod.SIMPLE_WHEN_ENTRY_WEIGHT to "0.5"))
+			val subject = ComplexMethod(config)
+			val code = """
+				internal fun Map<String, Any>.asBundle(): Bundle {
+					val bundle = Bundle(size)
+
+					for ((key, value) in this) {
+						val transformedKey = key.asFcmId()
+
+						when (value) {
+							is Int -> bundle.putInt(transformedKey, value)
+							is String -> bundle.putString(transformedKey, value)
+							is Float -> bundle.putFloat(transformedKey, value)
+							is Double -> bundle.putDouble(transformedKey, value)
+							is Byte -> bundle.putByte(transformedKey, value)
+							is Short -> bundle.putShort(transformedKey, value)
+							is Long -> bundle.putLong(transformedKey, value)
+							is Boolean -> bundle.putBoolean(transformedKey, value)
+							else -> throw IllegalArgumentException("Unexpected type value")
+						}
+					}
+
+					return bundle
+				}
+			""".trimIndent()
+
+			val findings = subject.lint(code)
+			assertThat(findings).isEmpty()
 		}
 	}
 })
