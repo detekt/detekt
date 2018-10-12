a16d9baead23adc3ec6b826cb88ddcd3350de365
Alistair Sykes <alistairsykes@users.noreply.github.com>
2018-10-12 09:25:40 +0100

    Update CliArgs.kt
    Specified that you can use multiple report arguments, and gave an example

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
@@ -57,7 +57,9 @@ class CliArgs : Args {
 	@Parameter(names = ["--report", "-r"],
 			description = "Generates a report for given 'report-id' and stores it on given 'path'. " +
 					"Entry should consist of: [report-id:path]. " +
-		   			"Available 'report-id' values: 'txt', 'xml', 'html'")
+		   			"Available 'report-id' values: 'txt', 'xml', 'html'. " +
+		  			"These can also be used in combination with each other " + 
+					"e.g. '-r txt:reports/detekt.txt -r xml:reports/detekt.xml'")
 	private var reports: List<String>? = null
 
 	@Parameter(names = ["--disable-default-rulesets", "-dd"],
