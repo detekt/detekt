400ea90b40690f3908c709a0928c9e2d87c5297c
Mikhail Levchenko <themishkun@yandex-team.ru>
2018-10-14 16:40:14 +0300

    Add static import for Assertions.assertThat
    Improves style and code aesthetic

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ExplicitItLambdaParameterSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ExplicitItLambdaParameterSpec.kt
@@ -2,7 +2,7 @@ package io.gitlab.arturbosch.detekt.rules.style
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.test.lint
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
@@ -18,7 +18,7 @@ class ExplicitItLambdaParameterSpec : SubjectSpek<ExplicitItLambdaParameter>({
 					val digits = 1234.let { it -> lambda(it) }.toList()
 					val flat = listOf(listOf(1), listOf(2)).flatMap { it -> it }
 				}""")
-			Assertions.assertThat(findings).hasSize(3)
+			assertThat(findings).hasSize(3)
 		}
 	}
 	given("some code using lambdas with (slightly) better style guidelines"){
@@ -28,7 +28,7 @@ class ExplicitItLambdaParameterSpec : SubjectSpek<ExplicitItLambdaParameter>({
 					val lambda = { it: Int, that: String -> it.toString() + that }
 					val flat = listOf(listOf(1), listOf(2)).mapIndexed { index, it -> it + index }
 				}""")
-			Assertions.assertThat(findings).isEmpty()
+			assertThat(findings).isEmpty()
 		}
 		it("does not report compliant (implicit) it parameters") {
 			val findings = subject.lint("""
@@ -37,7 +37,7 @@ class ExplicitItLambdaParameterSpec : SubjectSpek<ExplicitItLambdaParameter>({
 					val digits = 1234.let { lambda(it) }.toList()
 					val flat = listOf(listOf(1), listOf(2)).flatMap { it }
 				}""")
-			Assertions.assertThat(findings).isEmpty()
+			assertThat(findings).isEmpty()
 		}
 	}
 })
