4347fc99f2561df2d39adc13b4a74e16146bb6d2
Mikhail Levchenko <themishkun@yandex-team.ru>
2018-10-17 23:35:21 +0300

    Make tests more sound by better separating test cases in groups
    Rearrange tests by tree structure:
    ├ one parameter lambdas
    │ ├ explicit `it` parameter
    │ │ ├ with parameter type
    │ │ └ without parameter type
    │ └ implicit `it` parameter
    └ multiple parameter lambdas
      ├ with parameter type
      └ without parameter type

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ExplicitItLambdaParameterSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ExplicitItLambdaParameterSpec.kt
@@ -5,39 +5,57 @@ import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import org.jetbrains.spek.subject.SubjectSpek
 
 class ExplicitItLambdaParameterSpec : SubjectSpek<ExplicitItLambdaParameter>({
 	subject { ExplicitItLambdaParameter(Config.empty) }
 
-	given("some code using single parameter lambdas extensively with poor style guidelines") {
-		it("reports every single it parameter declared explicitly") {
-			val findings = subject.lint("""
+	given("lambda with single parameter") {
+		on("single parameter with name `it` declared explicitly") {
+			it("reports when parameter type is not declared") {
+				val findings = subject.lint("""
+				fun f() {
+					val digits = 1234.let { it -> listOf(it) }
+				}""")
+				assertThat(findings).hasSize(1)
+			}
+			it("reports when parameter type is declared explicitly") {
+				val findings = subject.lint("""
 				fun f() {
 					val lambda = { it: Int -> it.toString() }
-					val digits = 1234.let { it -> lambda(it) }.toList()
-					val flat = listOf(listOf(1), listOf(2)).flatMap { it -> it }
 				}""")
-			assertThat(findings).hasSize(3)
+				assertThat(findings).hasSize(1)
+			}
 		}
-	}
-	given("some code using lambdas with (slightly) better style guidelines"){
-		it("does not report explicit it parameter when there are multiple parameters") {
-			val findings = subject.lint("""
-				fun f() {
-					val lambda = { it: Int, that: String -> it.toString() + that }
-					val flat = listOf(listOf(1), listOf(2)).mapIndexed { index, it -> it + index }
-				}""")
-			assertThat(findings).isEmpty()
-		}
-		it("does not report compliant (implicit) it parameters") {
-			val findings = subject.lint("""
+		on("no parameter declared explicitly") {
+			it("does not report implicit `it` parameter usage") {
+				val findings = subject.lint("""
 				fun f() {
 					val lambda = { it.toString() }
 					val digits = 1234.let { lambda(it) }.toList()
 					val flat = listOf(listOf(1), listOf(2)).flatMap { it }
 				}""")
-			assertThat(findings).isEmpty()
+				assertThat(findings).isEmpty()
+			}
+		}
+	}
+	given("some code using lambdas with (slightly) better style guidelines") {
+		on("multiple parameters one of which with name `it` declared explicitly") {
+			it("does not report when parameter types are not declared") {
+				val findings = subject.lint("""
+				fun f() {
+					val flat = listOf(listOf(1), listOf(2)).mapIndexed { index, it -> it + index }
+				}""")
+				assertThat(findings).isEmpty()
+			}
+			it("does not report when parameter types are declared explicitly"){
+				val findings = subject.lint("""
+				fun f() {
+					val lambda = { it: Int, that: String -> it.toString() + that }
+				}""")
+				assertThat(findings).isEmpty()
+			}
 		}
 	}
 })
