b093ce722acc57ae391aa559a1c5f526c02a7c8b
Matthew Haughton <3flex@users.noreply.github.com>
2018-10-19 15:19:57 +1100

    Run detekt-formatting plugin on detekt itself (#1255)
    * Run detekt-formatting plugin on detekt itself
    
    * nit: remove trailing space
    
    * nit: remove consecutive blank line
    
    * nit: remove superfluous blank lines

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
@@ -59,7 +59,7 @@ class CliArgs : Args {
 			description = "Generates a report for given 'report-id' and stores it on given 'path'. " +
 					"Entry should consist of: [report-id:path]. " +
 		   			"Available 'report-id' values: 'txt', 'xml', 'html'. " +
-		  			"These can also be used in combination with each other " + 
+		  			"These can also be used in combination with each other " +
 					"e.g. '-r txt:reports/detekt.txt -r xml:reports/detekt.xml'")
 	private var reports: List<String>? = null
 
