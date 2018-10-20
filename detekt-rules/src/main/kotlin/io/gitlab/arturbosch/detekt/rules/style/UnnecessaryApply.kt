ea1f94ac29cbd10d1cc1ae878dd258c4235482d7
Arjan <arjankleene@gmail.com>
2018-10-20 08:31:47 +0200

    Add unnecessary apply rule (#1229)
    * Add rule to report unnecessary apply statements
    
    Adds a rule, some tests for it, and also updates the StyleGuideProvider
    to include this new rule.
    
    * Add Arjan to the readme file

---
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnnecessaryApply.kt
@@ -0,0 +1,61 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.KtBlockExpression
+import org.jetbrains.kotlin.psi.KtCallExpression
+
+/**
+ * `apply` expressions are used frequently, but sometimes their usage should be replaced with
+ * an ordinary method/extension function call to reduce visual complexity
+ *
+ * <noncompliant>
+ * config.apply { version = "1.2" } // can be replaced with `config.version = "1.2"`
+ * config?.apply { environment = "test" } // can be replaced with `config?.environment = "test"`
+ * </noncompliant>
+ *
+ * <compliant>
+ * config.apply {
+ *     version = "1.2"
+ *     environment = "test"
+ * }
+ * </compliant>
+ *
+ * @author arjank
+ */
+class UnnecessaryApply(config: Config) : Rule(config) {
+
+	override val issue = Issue(javaClass.simpleName, Severity.Style,
+			"The `apply` usage is unnecessary", Debt.FIVE_MINS)
+
+	override fun visitCallExpression(expression: KtCallExpression) {
+		super.visitCallExpression(expression)
+
+		if (!expression.isApplyExpr()) return
+
+		val lambdaExpr = expression.firstLambdaArg
+		val lambdaBody = lambdaExpr?.bodyExpression
+
+		if (lambdaBody.hasOnlyOneStatement()) {
+			report(CodeSmell(
+					issue, Entity.from(expression),
+					"apply expression can be omitted"
+			))
+		}
+	}
+
+}
+
+
+private const val APPLY_LITERAL = "apply"
+
+private fun KtCallExpression.isApplyExpr() = calleeExpression?.textMatches(APPLY_LITERAL) == true
+
+private val KtCallExpression.firstLambdaArg get() = lambdaArguments.firstOrNull()?.getLambdaExpression()
+
+private fun KtBlockExpression?.hasOnlyOneStatement() = this?.children?.size == 1
