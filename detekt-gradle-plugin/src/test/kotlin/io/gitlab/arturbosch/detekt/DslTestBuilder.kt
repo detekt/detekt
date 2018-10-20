96cd5b2a251c1755f08f3a4ea3279548cca66afa
marschwar <marschwar@users.noreply.github.com>
2018-10-20 09:38:37 +0200

    [gradle plugin] test code refactoring (#1205)
    * Decrease amount of boilerplate code in tests.
    
    - Make tests more expressive.
    - Use the same test code for groovy and kotlin dsl.
    
    * Move manipulation before second run outside of check block.
    
    * Change wording and indentation
    
    * Increase the number of properties configured in a custom task config.

---
+++ detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/DslTestBuilder.kt
@@ -0,0 +1,91 @@
+package io.gitlab.arturbosch.detekt
+
+/**
+ * @author Markus Schwarz
+ */
+abstract class DslTestBuilder {
+
+	abstract val gradleBuildConfig: String
+	abstract val gradleBuildName: String
+
+	private var detektConfig: String = ""
+	private var projectLayout: ProjectLayout = ProjectLayout(1)
+	private var baselineFile: String? = null
+	private var configFile: String? = null
+
+	fun withDetektConfig(config: String): DslTestBuilder {
+		detektConfig = config
+		return this
+	}
+
+	fun withProjectLayout(layout: ProjectLayout): DslTestBuilder {
+		projectLayout = layout
+		return this
+	}
+
+	fun withBaseline(filename: String): DslTestBuilder {
+		baselineFile = filename
+		return this
+	}
+
+	fun withConfigFile(filename: String): DslTestBuilder {
+		configFile = filename
+		return this
+	}
+
+	fun build(): DslGradleRunner {
+		val mainBuildFileContent = """
+			| $gradleBuildConfig
+			| $detektConfig
+		""".trimMargin()
+		val runner = DslGradleRunner(
+				projectLayout,
+				gradleBuildName,
+				mainBuildFileContent,
+				configFile,
+				baselineFile)
+		runner.setupProject()
+		return runner
+	}
+
+	private class GroovyBuilder : DslTestBuilder() {
+		override val gradleBuildName: String = "build.gradle"
+		override val gradleBuildConfig: String = """
+				|import io.gitlab.arturbosch.detekt.DetektPlugin
+				|
+				|plugins {
+				|   id "java-library"
+				|   id "io.gitlab.arturbosch.detekt"
+				|}
+				|
+				|repositories {
+				|	jcenter()
+				|	mavenLocal()
+				|}
+				""".trimMargin()
+
+	}
+
+	private class KotlinBuilder : DslTestBuilder() {
+		override val gradleBuildName: String = "build.gradle.kts"
+		override val gradleBuildConfig: String = """
+				|import io.gitlab.arturbosch.detekt.detekt
+				|
+				|plugins {
+				|   `java-library`
+				|	id("io.gitlab.arturbosch.detekt")
+				|}
+				|
+				|repositories {
+				|	jcenter()
+				|	mavenLocal()
+				|}
+				""".trimMargin()
+	}
+
+	companion object {
+		fun kotlin(): DslTestBuilder = KotlinBuilder()
+		fun groovy(): DslTestBuilder = GroovyBuilder()
+	}
+}
+
