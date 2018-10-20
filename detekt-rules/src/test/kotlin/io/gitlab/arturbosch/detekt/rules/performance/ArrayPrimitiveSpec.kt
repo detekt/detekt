dd3fed102d628724c472e5e2e8c1df7c444e63ec
Nadine Kost <nadine@door2door.io>
2018-10-20 15:20:27 +0200

    Make tests run
    Co-authored-by: inytar <pieter@door2door.io>

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/performance/ArrayPrimitiveSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/performance/ArrayPrimitiveSpec.kt
@@ -3,26 +3,29 @@ package io.gitlab.arturbosch.detekt.rules.performance
 import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.subject.SubjectSpek
 
 class ArrayPrimitiveSpec : SubjectSpek<ArrayPrimitive>({
 	subject { ArrayPrimitive() }
 
-	describe("function with Array<Primitive> parameter") {
-		val code = "fun function(array: Array<Int>) {}"
-		subject.lint(code)
-		assertThat(subject.findings.size).isEqualTo(1)
-	}
+	describe("function parameter") {
+		it("is Array<Primitive>") {
+			val code = "fun function(array: Array<Int>) {}"
+			subject.lint(code)
+			assertThat(subject.findings.size).isEqualTo(1)
+		}
 
-	describe("function with non-array parameter") {
-		val code = "fun function(i: Int) {}"
-		subject.lint(code)
-		assertThat(subject.findings.size).isZero()
-	}
+		it("is not an array") {
+			val code = "fun function(i: Int) {}"
+			subject.lint(code)
+			assertThat(subject.findings.size).isZero()
+		}
 
-	describe("function with PrimitiveArray") {
-		val code = "fun function(array: IntArray) {}"
-		subject.lint(code)
-		assertThat(subject.findings.size).isZero()
+		it("is a specialized array") {
+			val code = "fun function(array: IntArray) {}"
+			subject.lint(code)
+			assertThat(subject.findings.size).isZero()
+		}
 	}
 })
