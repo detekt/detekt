6833be71d236b32173bfbd52c08e4a8b31348121
Markus Schwarz <post@markus-schwarz.net>
2018-11-05 19:55:06 +0100

    Avoid multiple conversions in test.

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgsSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgsSpec.kt
@@ -7,40 +7,40 @@ import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 
 internal class CliArgsSpec : Spek({
 
-	val projectDir = Paths.get(resource("/empty.txt")).parent.parent.parent.parent.toFile()
+	val projectPath = Paths.get(resource("/empty.txt")).parent.parent.parent.parent.toAbsolutePath()
 
 	describe("Parsing the input path") {
 
 		it("the current working directory is used if parameter is not set") {
 			val (cli, _) = parseArguments<CliArgs>(emptyArray())
 			assertThat(cli.inputPaths).hasSize(1)
-			assertThat(cli.inputPaths.first().toFile().absolutePath).isEqualTo(System.getProperty("user.dir"))
+			assertThat(cli.inputPaths.first()).isEqualTo(Paths.get(System.getProperty("user.dir")))
 		}
 
 		it("a single value is converted to a path") {
-			val (cli, _) = parseArguments<CliArgs>(arrayOf("--input", projectDir.absolutePath))
+			val (cli, _) = parseArguments<CliArgs>(arrayOf("--input", "${projectPath}"))
 			assertThat(cli.inputPaths).hasSize(1)
-			assertThat(cli.inputPaths.first().toAbsolutePath().toFile()).isEqualTo(projectDir)
+			assertThat(cli.inputPaths.first().toAbsolutePath()).isEqualTo(projectPath)
 		}
 
 		it("multiple imput paths can be separated by comma") {
-			val mainPath = Paths.get(File(projectDir, "src/main").absolutePath)
-			val testPath = Paths.get(File(projectDir, "src/test").absolutePath)
+			val mainPath = projectPath.resolve("src/main").toAbsolutePath()
+			val testPath = projectPath.resolve("src/test").toAbsolutePath()
 			val (cli, _) = parseArguments<CliArgs>(arrayOf(
-					"--input", "${mainPath.toFile().absolutePath},${testPath.toFile().absolutePath}")
+					"--input", "$mainPath,$testPath")
 			)
 			assertThat(cli.inputPaths).hasSize(2)
 			assertThat(cli.inputPaths.map(Path::toAbsolutePath)).containsExactlyInAnyOrder(mainPath, testPath)
 		}
 
 		it("reports an error if the input path does not exist") {
-			val params = arrayOf("--input", File(projectDir, "nonExistent").absolutePath)
+			val pathToNonExistentDirectory = projectPath.resolve("nonExistent")
+			val params = arrayOf("--input", "$pathToNonExistentDirectory")
 
 			assertThatExceptionOfType(ParameterException::class.java)
 					.isThrownBy { parseArguments<CliArgs>(params).first.inputPaths }
