d5b929eb871b7824ce6f80659b3f7fe746e8ac5d
Markus Schwarz <post@markus-schwarz.net>
2018-11-11 10:59:04 +0100

    Fix formatting issue.

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/Blacklist.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/Blacklist.kt
@@ -3,8 +3,4 @@ package io.gitlab.arturbosch.detekt.cli.baseline
 /**
  * @author Artur Bosch
  */
-internal data class Blacklist(
-		override val ids: Set<String>,
-		override val timestamp: String
-) : Listing<Blacklist>
-
+internal data class Blacklist(override val ids: Set<String>, override val timestamp: String) : Listing<Blacklist>
