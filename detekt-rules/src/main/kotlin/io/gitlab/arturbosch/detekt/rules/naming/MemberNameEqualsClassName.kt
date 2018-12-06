ca1ccb82e26a02e16289b883dc9ff371655b0337
Matthew Haughton <3flex@users.noreply.github.com>
2018-12-06 13:57:44 +1100

    fix typos & stop claiming an object is a class

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/naming/MemberNameEqualsClassName.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/naming/MemberNameEqualsClassName.kt
@@ -55,12 +55,12 @@ import org.jetbrains.kotlin.util.collectionUtils.concat
 class MemberNameEqualsClassName(config: Config = Config.empty) : Rule(config) {
 
 	override val issue = Issue(javaClass.simpleName, Severity.Style,
-			"A member should not given the same name as its parent class or object.",
+			"A member should not be given the same name as its parent class or object.",
 			Debt.FIVE_MINS)
 
 	private val classMessage = "A member is named after the class. This might result in confusion. " +
 			"Either rename the member or change it to a constructor."
-	private val objectMessage = "A member is named after the class object. " +
+	private val objectMessage = "A member is named after the object. " +
 			"This might result in confusion. Please rename the member."
 
 	private val ignoreOverriddenFunction = valueOrDefault(IGNORE_OVERRIDDEN_FUNCTION, true)
