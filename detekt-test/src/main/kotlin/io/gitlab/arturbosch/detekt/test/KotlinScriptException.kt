0213ab4d4d753025e0af4fe2a0bc9da2c76409d0
schalkms <schalkms@icloud.com>
2018-12-08 11:16:06 +0100

    Added KotlinScriptEngine recovery
    If a compilation error occurs the script engine is recovered.

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/KotlinScriptException.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/KotlinScriptException.kt
@@ -3,6 +3,10 @@ package io.gitlab.arturbosch.detekt.test
 import javax.script.ScriptException
 
 /**
+ * The generic exception class for the Kotlin script engine APIs.
+ * Exception types thrown by the underlying scripting implementations are wrapped in instances of
+ * KotlinScriptException.
+ *
  * @author schalkms
  */
 class KotlinScriptException(e: ScriptException) : RuntimeException("Given Kotlin code is invalid.", e)
