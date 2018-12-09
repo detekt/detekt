b4e621cfa54311ad17293db64bc8bd505f18cffc
Matthew Haughton <3flex@users.noreply.github.com>
2018-12-09 22:05:36 +1100

    Simplify PrivateElementUtil

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/PrivateElementUtil.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/PrivateElementUtil.kt
@@ -1,9 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.documentation
 
-import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.psiUtil.isPrivate
 
-internal fun KtDeclaration.hasCommentInPrivateMember(): Boolean {
-	val modifiers = this.modifierList
-	return modifiers != null && docComment != null && modifiers.hasModifier(KtTokens.PRIVATE_KEYWORD)
-}
+internal fun KtDeclaration.hasCommentInPrivateMember() = docComment != null && isPrivate()
