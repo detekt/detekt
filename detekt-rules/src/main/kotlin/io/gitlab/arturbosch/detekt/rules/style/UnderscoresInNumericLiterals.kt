cdaf3511946078ca9a0342f9c20ec481dbebfe74
Tyler Wong <tbwong3@gmail.com>
2019-02-09 21:10:19 -0800

    Fix file indentation

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnderscoresInNumericLiterals.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnderscoresInNumericLiterals.kt
@@ -37,85 +37,85 @@ import org.jetbrains.kotlin.psi.KtProperty
  */
 class UnderscoresInNumericLiterals(config: Config = Config.empty) : Rule(config) {
 
-	override val issue = Issue(javaClass.simpleName, Severity.Style,
-			"Report missing or invalid underscores in decimal base 10 numeric literals. Numeric literals " +
+    override val issue = Issue(javaClass.simpleName, Severity.Style,
+            "Report missing or invalid underscores in decimal base 10 numeric literals. Numeric literals " +
                     "should be underscore separated to increase readability. Underscores that do not make groups of " +
                     "3 digits are also reported.", Debt.FIVE_MINS)
 
-	private val underscoreNumberRegex = Regex("^[0-9]{1,3}(_[0-9]{3})*\$")
+    private val underscoreNumberRegex = Regex("^[0-9]{1,3}(_[0-9]{3})*\$")
 
-	private val minAcceptableLength = valueOrDefault(MIN_ACCEPTABLE_LENGTH, DEFAULT_MIN_ACCEPTABLE_LENGTH_VALUE)
-	private val ignoredFieldNames = valueOrDefault(IGNORED_NAMES, "")
-			.splitToSequence(",")
-			.filterNot { it.isEmpty() }
-			.toList()
+    private val minAcceptableLength = valueOrDefault(MIN_ACCEPTABLE_LENGTH, DEFAULT_MIN_ACCEPTABLE_LENGTH_VALUE)
+    private val ignoredFieldNames = valueOrDefault(IGNORED_NAMES, "")
+            .splitToSequence(",")
+            .filterNot { it.isEmpty() }
+            .toList()
 
-	private val KtConstantExpression.associatedName: String?
-		get() {
-			var associatedName: String? = null
-			var element: PsiElement? = parent
+    private val KtConstantExpression.associatedName: String?
+        get() {
+            var associatedName: String? = null
+            var element: PsiElement? = parent
 
-			while (associatedName == null || element == null) {
-				associatedName = when (element) {
-					is KtProperty -> element.name
-					is KtParameter -> element.name
-					else -> null
-				}
-				element = element?.parent
-			}
+            while (associatedName == null || element == null) {
+                associatedName = when (element) {
+                    is KtProperty -> element.name
+                    is KtParameter -> element.name
+                    else -> null
+                }
+                element = element?.parent
+            }
 
-			return associatedName
-		}
+            return associatedName
+        }
 
-	override fun visitConstantExpression(expression: KtConstantExpression) {
-		if (propertyNameIsExcluded(expression) || isNotDecimal(expression)) {
-			return
-		}
+    override fun visitConstantExpression(expression: KtConstantExpression) {
+        if (propertyNameIsExcluded(expression) || isNotDecimal(expression)) {
+            return
+        }
 
-		val numberStringParts = normalizeForMatching(expression.text).split('.')
+        val numberStringParts = normalizeForMatching(expression.text).split('.')
 
-		if (numberStringParts.sumBy { it.length } < minAcceptableLength) {
-			if (numberStringParts.any { it.contains('_') }) {
-				reportIfInvalid(expression, numberStringParts)
-			}
-			return
-		}
+        if (numberStringParts.sumBy { it.length } < minAcceptableLength) {
+            if (numberStringParts.any { it.contains('_') }) {
+                reportIfInvalid(expression, numberStringParts)
+            }
+            return
+        }
 
-		reportIfInvalid(expression, numberStringParts)
-	}
+        reportIfInvalid(expression, numberStringParts)
+    }
 
-	private fun reportIfInvalid(expression: KtConstantExpression, numberStringParts: List<String>) {
-		for (part in numberStringParts) {
-			if (!part.matches(underscoreNumberRegex)) {
-				report(CodeSmell(issue, Entity.from(expression), "This numeric literal should be separated " +
-						"by underscores in order to increase readability."))
-				break
-			}
-		}
-	}
+    private fun reportIfInvalid(expression: KtConstantExpression, numberStringParts: List<String>) {
+        for (part in numberStringParts) {
+            if (!part.matches(underscoreNumberRegex)) {
+                report(CodeSmell(issue, Entity.from(expression), "This numeric literal should be separated " +
+                        "by underscores in order to increase readability."))
+                break
+            }
+        }
+    }
 
     private fun isNotDecimal(expression: KtConstantExpression): Boolean {
         val rawText = expression.text.toLowerCase(Locale.US)
         return rawText.startsWith("0x") || rawText.startsWith("0b")
     }
 
-	private fun propertyNameIsExcluded(expression: KtConstantExpression): Boolean {
-		val propertyName = expression.associatedName
-		return ignoredFieldNames.contains(propertyName)
-	}
+    private fun propertyNameIsExcluded(expression: KtConstantExpression): Boolean {
+        val propertyName = expression.associatedName
+        return ignoredFieldNames.contains(propertyName)
+    }
 
-	private fun normalizeForMatching(text: String): String {
-		return text.trim()
-				.toLowerCase(Locale.US)
-				.removeSuffix("l")
-				.removeSuffix("d")
-				.removeSuffix("f")
-	}
+    private fun normalizeForMatching(text: String): String {
+        return text.trim()
+                .toLowerCase(Locale.US)
+                .removeSuffix("l")
+                .removeSuffix("d")
+                .removeSuffix("f")
+    }
 
-	companion object {
-		const val MIN_ACCEPTABLE_LENGTH = "minAcceptableLength"
-		const val IGNORED_NAMES = "ignoredNames"
+    companion object {
+        const val MIN_ACCEPTABLE_LENGTH = "minAcceptableLength"
+        const val IGNORED_NAMES = "ignoredNames"
 
-		private const val DEFAULT_MIN_ACCEPTABLE_LENGTH_VALUE = 4
-	}
+        private const val DEFAULT_MIN_ACCEPTABLE_LENGTH_VALUE = 4
+    }
 }
