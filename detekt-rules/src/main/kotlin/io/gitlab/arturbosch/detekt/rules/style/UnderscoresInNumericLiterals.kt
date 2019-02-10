304f79d5ea834b5ae72d091e37704958c3627fe6
Tyler Wong <tbwong3@gmail.com>
2019-02-09 19:55:40 -0800

    Fix some edge cases and add more test cases

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnderscoresInNumericLiterals.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnderscoresInNumericLiterals.kt
@@ -8,10 +8,10 @@ import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtConstantExpression
-import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
-import org.jetbrains.kotlin.psi.KtPrefixExpression
+import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.KtProperty
+import java.util.Locale
 
 /**
  * This rule detects and reports base 10 numeric literals above a certain length that should be underscore separated for
@@ -31,44 +31,85 @@ import org.jetbrains.kotlin.psi.KtPrefixExpression
  *
  * @configuration minAcceptableLength - Length under which base 10 literals are not required to have underscores
  * (default: 4)
+ * @configuration ignoredNames - Names that are not to be reported on (default: "")
  *
  * @author Tyler Wong
  */
 class UnderscoresInNumericLiterals(config: Config = Config.empty) : Rule(config) {
 
 	override val issue = Issue(javaClass.simpleName, Severity.Style,
-			"Report missing underscores in numeric literals. Numeric literals should be underscore " +
-					"separated for increase readability. Underscores that do not make groups of 3 digits are also " +
-					"reported. Currently, only base 10 numeric literals are supported", Debt.FIVE_MINS)
+			"Report missing or invalid underscores in base 10 numeric literals. Numeric literals should " +
+					"be underscore separated to increase readability. Underscores that do not make groups of 3 " +
+					"digits are also reported.", Debt.FIVE_MINS)
 
 	private val underscoreNumberRegex = Regex("^[0-9]{1,3}(_[0-9]{3})*\$")
 
 	private val minAcceptableLength = valueOrDefault(MIN_ACCEPTABLE_LENGTH, DEFAULT_MIN_ACCEPTABLE_LENGTH_VALUE)
+	private val ignoredFieldNames = valueOrDefault(IGNORED_NAMES, "")
+			.splitToSequence(",")
+			.filterNot { it.isEmpty() }
+			.toList()
 
-	override fun visitConstantExpression(expression: KtConstantExpression) {
-		val parent = expression.parent
+	private val KtConstantExpression.associatedName: String?
+		get() {
+			var propertyName: String? = null
+			var element: PsiElement? = parent
 
-		val numberString = if (parent.hasUnaryMinusPrefix()) {
-			parent.text
-		} else {
-			expression.text
+			while (propertyName == null || element == null) {
+				propertyName = when (element) {
+					is KtProperty -> element.name
+					is KtParameter -> element.name
+					else -> null
+				}
+				element = element?.parent
+			}
+
+			return propertyName
 		}
 
-		if (numberString.replace("_", "").length < minAcceptableLength) {
+	override fun visitConstantExpression(expression: KtConstantExpression) {
+		if (propertyNameIsExcluded(expression)) {
 			return
 		}
 
-		if (!numberString.matches(underscoreNumberRegex)) {
-			report(CodeSmell(issue, Entity.from(expression), "This numeric literal should be underscore " +
-					"separated for readability."))
+		val numberStringParts = normalizeForMatching(expression.text).split('.')
+
+		if (numberStringParts.sumBy { it.length } < minAcceptableLength) {
+			if (numberStringParts.any { it.contains('_') }) {
+				reportIfInvalid(expression, numberStringParts)
+			}
+			return
+		}
+
+		reportIfInvalid(expression, numberStringParts)
+	}
+
+	private fun reportIfInvalid(expression: KtConstantExpression, numberStringParts: List<String>) {
+		for (part in numberStringParts) {
+			if (!part.matches(underscoreNumberRegex)) {
+				report(CodeSmell(issue, Entity.from(expression), "This numeric literal should be separated " +
+						"by underscores in order to increase readability."))
+				break
+			}
 		}
 	}
 
-	private fun PsiElement.hasUnaryMinusPrefix(): Boolean = this is KtPrefixExpression &&
-			(this.firstChild as? KtOperationReferenceExpression)?.operationSignTokenType == KtTokens.MINUS
+	private fun propertyNameIsExcluded(expression: KtConstantExpression): Boolean {
+		val propertyName = expression.associatedName
+		return ignoredFieldNames.contains(propertyName)
+	}
+
+	private fun normalizeForMatching(text: String): String {
+		return text.trim()
+				.toLowerCase(Locale.US)
+				.removeSuffix("l")
+				.removeSuffix("d")
+				.removeSuffix("f")
+	}
 
 	companion object {
 		const val MIN_ACCEPTABLE_LENGTH = "minAcceptableLength"
+		const val IGNORED_NAMES = "ignoredNames"
 
 		private const val DEFAULT_MIN_ACCEPTABLE_LENGTH_VALUE = 4
 	}
--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnderscoresInNumericLiteralsSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnderscoresInNumericLiteralsSpec.kt
@@ -9,6 +9,7 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 
 class UnderscoresInNumericLiteralsSpec : Spek({
+
 	given("an Int of 1000") {
 		val ktFile = compileContentForTest("val myInt = 1000")
 
@@ -23,6 +24,13 @@ class UnderscoresInNumericLiteralsSpec : Spek({
 			).lint(ktFile)
 			assertThat(findings).isEmpty()
 		}
+
+		it("should not be reported if ignoredPropertyNames contains myInt") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.IGNORED_NAMES to "myInt,myFloat"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
 	}
 
 	given("an Int of 1_000_000") {
@@ -31,6 +39,156 @@ class UnderscoresInNumericLiteralsSpec : Spek({
 		it("should not be reported") {
 			val findings = UnderscoresInNumericLiterals().lint(ktFile)
 			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a const Int of 1000000") {
+		val ktFile = compileContentForTest("const val myInt = 1000000")
+
+		it("should be reported by default") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isNotEmpty
+		}
+
+		it("should not be reported if minAcceptableLength is 8") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.MIN_ACCEPTABLE_LENGTH to "8"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a Float of 1000f") {
+		val ktFile = compileContentForTest("val myFloat = 1000f")
+
+		it("should be reported by default") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isNotEmpty
+		}
+
+		it("should not be reported if minAcceptableLength is 5") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.MIN_ACCEPTABLE_LENGTH to "5"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a Float of -1000f") {
+		val ktFile = compileContentForTest("val myFloat = -1000f")
+
+		it("should be reported by default") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isNotEmpty
+		}
+
+		it("should not be reported if minAcceptableLength is 5") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.MIN_ACCEPTABLE_LENGTH to "5"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a Float of -1_000f") {
+		val ktFile = compileContentForTest("const val myFloat = -1_000f")
+
+		it("should not be reported") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a Long of 1000000L") {
+		val ktFile = compileContentForTest("const val myLong = 1000000L")
+
+		it("should be reported by default") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isNotEmpty
+		}
+
+		it("should not be reported if ignoredPropertyNames contains myLong") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.IGNORED_NAMES to "myLong,myInt,myFloat"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+
+		it("should not be reported if ignored minAcceptableLength is 8") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.MIN_ACCEPTABLE_LENGTH to "8"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a Double of 1_000_000.00_000_000") {
+		val ktFile = compileContentForTest("val myDouble = 1_000_000.00_000_000")
+
+		it("should not be reported") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("a function with default Int parameter value 1000") {
+		val ktFile = compileContentForTest("fun testFunction(testParam: Int = 1000) {}")
+
+		it("should be reported by default") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isNotEmpty
+		}
+
+		it("should not be reported when ignoredNames contains testParam") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.IGNORED_NAMES to "testParam"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("an annotation with numeric literals 0 and 10") {
+		val ktFile = compileContentForTest(
+				"fun setCustomDimension(@IntRange(from = 0, to = 10) index: Int, value: String?) {}"
+		)
+
+		it("should not be reported") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("an annotation with numeric literals 0 and 1000000") {
+		val ktFile = compileContentForTest(
+				"fun setCustomDimension(@IntRange(from = 0, to = 1000000) index: Int, value: String?) {}"
+		)
+
+		it("should be reported by default") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isNotEmpty
+		}
+
+		it("should not be reported if ignoredNames contains index") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.IGNORED_NAMES to "index"))
+			).lint(ktFile)
+			assertThat(findings).isEmpty()
+		}
+	}
+
+	given("an Int of 10_00_00") {
+		val ktFile = compileContentForTest("const val myInt = 10_00_00")
+
+		it("should be reported by default") {
+			val findings = UnderscoresInNumericLiterals().lint(ktFile)
+			assertThat(findings).isNotEmpty
+		}
+
+		it("should still be reported even if minAcceptableLength is 7") {
+			val findings = UnderscoresInNumericLiterals(
+					TestConfig(mapOf(UnderscoresInNumericLiterals.MIN_ACCEPTABLE_LENGTH to "7"))
+			).lint(ktFile)
+			assertThat(findings).isNotEmpty
 		}
 	}
 })
