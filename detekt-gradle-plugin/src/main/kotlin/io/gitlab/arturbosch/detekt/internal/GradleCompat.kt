ab881e3479a730a733c281def3ed15ffe6e59ccd
Julian Wiersbitzki <julian@wiersbitzki.com>
2019-02-13 11:13:44 +0100

    turn around condition

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/GradleCompat.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/GradleCompat.kt
@@ -5,7 +5,7 @@ import org.gradle.api.file.RegularFileProperty
 import org.gradle.util.GradleVersion
 
 fun Project.fileProperty(): RegularFileProperty {
-    return if (GradleVersion.current() < GradleVersion.version("5.0")) {
+    return if (GradleVersion.current() >= GradleVersion.version("5.0")) {
         objects.fileProperty()
     } else {
         layout.fileProperty()
