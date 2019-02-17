a4d09aa985c8d0df5e318aa0cbf455aaeaee4cfd
Artur Bosch <arturbosch@gmx.de>
2019-02-17 23:14:59 +0100

    Update used detekt version in plugin

--- detekt-gradle-plugin/build.gradle.kts
+++ detekt-gradle-plugin/build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
     kotlin("jvm") version "1.3.21"
     id("org.jetbrains.dokka") version "0.9.17"
     id("com.github.ben-manes.versions") version "0.20.0"
-    id("io.gitlab.arturbosch.detekt") version "1.0.0-RC13"
+    id("io.gitlab.arturbosch.detekt") version "1.0.0-RC14"
 }
 
 group = "io.gitlab.arturbosch.detekt"
