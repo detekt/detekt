71bf594098b8e308564b7cf660afacb7a246e1c6
Nivaldo Bondanca <hick209@gmail.com>
2019-04-09 13:56:28 -0700

    Updated LOG file to be more flexible

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/LOG.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/LOG.kt
@@ -1,24 +1,61 @@
 package io.gitlab.arturbosch.detekt.cli
 
+import io.gitlab.arturbosch.detekt.cli.LogLevel.DEBUG
+import io.gitlab.arturbosch.detekt.cli.LogLevel.ERROR
+import io.gitlab.arturbosch.detekt.cli.LogLevel.INFO
+import io.gitlab.arturbosch.detekt.cli.LogLevel.VERBOSE
+import io.gitlab.arturbosch.detekt.cli.LogLevel.WARN
+import io.gitlab.arturbosch.detekt.cli.LogLevel.NONE
 import java.io.PrintStream
 
 /**
  * @author Artur Bosch
  */
+@Suppress("TooManyFunctions")
 object LOG {
-
+    @Deprecated("Use LOG.level instead", ReplaceWith("LOG.level", "io.gitlab.arturbosch.detekt.cli.LOG"))
+    var active: Boolean
+        get() = level != NONE
+        set(value) {
+            level = when {
+                value -> {
+                    // Only reset the level if log is not set
+                    if (level == NONE) INFO else level
+                }
+                else -> NONE
+            }
+        }
+    var level: LogLevel = INFO
     var printer: PrintStream = System.out
-    var active: Boolean = false
 
-    fun debug(message: String) {
-        if (active) {
-            printer.println(message)
-        }
-    }
+    fun verbose(message: Any?) = log(VERBOSE, message)
+    fun verbose(message: () -> Any?) = log(VERBOSE, message)
 
-    fun debug(message: () -> String) {
-        if (active) {
-            printer.println(message.invoke())
-        }
+    fun debug(message: Any?) = log(DEBUG, message)
+    fun debug(message: () -> Any?) = log(DEBUG, message)
+
+    fun info(message: Any?) = log(INFO, message)
+    fun info(message: () -> Any?) = log(INFO, message)
+
+    fun warn(message: Any?) = log(WARN, message)
+    fun warn(message: () -> Any?) = log(WARN, message)
+
+    fun error(message: Any?) = log(ERROR, message)
+    fun error(message: () -> Any?) = log(ERROR, message)
+
+    private fun log(level: LogLevel, message: Any?) {
+        if (level >= this.level) printer.println(message)
     }
+    private inline fun log(level: LogLevel, message: () -> Any?) {
+        if (level >= this.level) printer.println(message.invoke())
+    }
+}
+
+enum class LogLevel {
+    VERBOSE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    NONE
 }
---
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/LOGSpec.kt
@@ -0,0 +1,94 @@
+package io.gitlab.arturbosch.detekt.cli
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import org.assertj.core.api.Assertions.assertThat
+import java.io.PrintStream
+
+internal class LOGSpec : Spek({
+
+    describe("Logging messages") {
+
+        val defaultLogs: () -> Unit = {
+            LOG.verbose("verbose")
+            LOG.debug("debug")
+            LOG.info("info")
+            LOG.warn("warn")
+            LOG.error("error")
+        }
+
+        fun captureLog(action: () -> Unit): List<String> {
+            val tempFile = createTempFile()
+            PrintStream(tempFile).use {
+                LOG.printer = it
+                action()
+            }
+            return tempFile.readLines()
+        }
+
+        it ("all logs are suppressed when level = NONE") {
+            LOG.level = LogLevel.NONE
+
+            val logLines = captureLog(defaultLogs)
+
+            assertThat(logLines.size).isEqualTo(0)
+        }
+
+        it ("everything is logged when level = VERBOSE") {
+            LOG.level = LogLevel.VERBOSE
+
+            val logLines = captureLog(defaultLogs)
+
+            assertThat(logLines.size).isEqualTo(5)
+        }
+
+        it ("only the necessary log levels are output") {
+            LOG.level = LogLevel.WARN
+
+            val logLines = captureLog(defaultLogs)
+
+            assertThat(logLines.size).isEqualTo(2)
+            assertThat(logLines[0]).isEqualTo("warn")
+            assertThat(logLines[1]).isEqualTo("error")
+        }
+
+        it ("lambdas are not executed if not necessary") {
+            LOG.level = LogLevel.INFO
+
+            var verbose = false
+            var debug = false
+            var info = false
+            var warn = false
+            var error = false
+            val logLines = captureLog {
+                LOG.verbose {
+                    verbose = true
+                    "verbose"
+                }
+                LOG.debug {
+                    debug = true
+                    "debug"
+                }
+                LOG.info {
+                    info = true
+                    "info"
+                }
+                LOG.warn {
+                    warn = true
+                    "warn"
+                }
+                LOG.error {
+                    error = true
+                    "error"
+                }
+            }
+
+            assertThat(logLines.size).isEqualTo(3)
+            assertThat(verbose).isFalse()
+            assertThat(debug).isFalse()
+            assertThat(info).isTrue()
+            assertThat(warn).isTrue()
+            assertThat(error).isTrue()
+        }
+    }
+})
