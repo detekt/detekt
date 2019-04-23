076aeafb8223d1c26f130676dbcb849c488c7f25
Artur Bosch <arturbosch@gmx.de>
2019-04-23 07:01:13 +0200

    Reformat cli args according to kotlin style

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
@@ -14,12 +14,13 @@ interface Args {
 class CliArgs : Args {
 
     @Parameter(names = ["--input", "-i"],
-            description = "Input paths to analyze. Multiple paths are separated by comma. If not specified the " +
-                    "current working directory is used.")
+        description = "Input paths to analyze. Multiple paths are separated by comma. If not specified the " +
+            "current working directory is used.")
     private var input: String? = null
 
     @Parameter(names = ["--include", "-in"],
-        description = "Globing patterns describing paths to include in the analysis. Useful in combination with 'excludes' patterns.")
+        description = "Globing patterns describing paths to include in the analysis. " +
+            "Useful in combination with 'excludes' patterns.")
     var includes: String? = null
 
     @Parameter(names = ["--exclude", "-ex"],
@@ -34,76 +35,76 @@ class CliArgs : Args {
     var classpath: String? = null
 
     @Parameter(names = ["--config", "-c"],
-            description = "Path to the config file (path/to/config.yml). " +
-                    "Multiple configuration files can be specified with ',' or ';' as separator.")
+        description = "Path to the config file (path/to/config.yml). " +
+            "Multiple configuration files can be specified with ',' or ';' as separator.")
     var config: String? = null
 
     @Parameter(names = ["--config-resource", "-cr"],
-            description = "Path to the config resource on detekt's classpath (path/to/config.yml).")
+        description = "Path to the config resource on detekt's classpath (path/to/config.yml).")
     var configResource: String? = null
 
     @Parameter(names = ["--generate-config", "-gc"],
-            description = "Export default config to default-detekt-config.yml.")
+        description = "Export default config to default-detekt-config.yml.")
     var generateConfig: Boolean = false
 
     @Parameter(names = ["--plugins", "-p"],
-            description = "Extra paths to plugin jars separated by ',' or ';'.")
+        description = "Extra paths to plugin jars separated by ',' or ';'.")
     var plugins: String? = null
 
     @Parameter(names = ["--parallel"],
-            description = "Enables parallel compilation of source files." +
-                    " Should only be used if the analyzing project has more than ~200 kotlin files.")
+        description = "Enables parallel compilation of source files." +
+            " Should only be used if the analyzing project has more than ~200 kotlin files.")
     var parallel: Boolean = false
 
     @Parameter(names = ["--baseline", "-b"],
-            description = "If a baseline xml file is passed in," +
-                    " only new code smells not in the baseline are printed in the console.",
-            converter = PathConverter::class)
+        description = "If a baseline xml file is passed in," +
+            " only new code smells not in the baseline are printed in the console.",
+        converter = PathConverter::class)
     var baseline: Path? = null
 
     @Parameter(names = ["--create-baseline", "-cb"],
-            description = "Treats current analysis findings as a smell baseline for future detekt runs.")
+        description = "Treats current analysis findings as a smell baseline for future detekt runs.")
     var createBaseline: Boolean = false
 
     @Parameter(names = ["--report", "-r"],
-            description = "Generates a report for given 'report-id' and stores it on given 'path'. " +
-                    "Entry should consist of: [report-id:path]. " +
-                    "Available 'report-id' values: 'txt', 'xml', 'html'. " +
-                    "These can also be used in combination with each other " +
-                    "e.g. '-r txt:reports/detekt.txt -r xml:reports/detekt.xml'")
+        description = "Generates a report for given 'report-id' and stores it on given 'path'. " +
+            "Entry should consist of: [report-id:path]. " +
+            "Available 'report-id' values: 'txt', 'xml', 'html'. " +
+            "These can also be used in combination with each other " +
+            "e.g. '-r txt:reports/detekt.txt -r xml:reports/detekt.xml'")
     private var reports: List<String>? = null
 
     @Parameter(names = ["--disable-default-rulesets", "-dd"],
-            description = "Disables default rule sets.")
+        description = "Disables default rule sets.")
     var disableDefaultRuleSets: Boolean = false
 
     @Parameter(names = ["--build-upon-default-config"],
-            description = "Preconfigures detekt with a bunch of rules and some opinionated defaults for you. " +
-                    "Allows additional provided configurations to override the defaults.")
+        description = "Preconfigures detekt with a bunch of rules and some opinionated defaults for you. " +
+            "Allows additional provided configurations to override the defaults.")
     var buildUponDefaultConfig: Boolean = false
 
     @Parameter(names = ["--fail-fast"],
-            description = "Shortcut for 'build-upon-default-config' together with all available rules active and " +
-                    "exit code 0 only when no code smells are found. " +
-                    "Additional configuration files can override properties but not the 'active' one.")
+        description = "Shortcut for 'build-upon-default-config' together with all available rules active and " +
+            "exit code 0 only when no code smells are found. " +
+            "Additional configuration files can override properties but not the 'active' one.")
     var failFast: Boolean = false
 
     @Parameter(names = ["--debug"],
-            description = "Prints extra information about configurations and extensions.")
+        description = "Prints extra information about configurations and extensions.")
     var debug: Boolean = false
 
     @Parameter(names = ["--help", "-h"],
-            help = true, description = "Shows the usage.")
+        help = true, description = "Shows the usage.")
     override var help: Boolean = false
 
     @Parameter(names = ["--run-rule"],
-            description = "Specify a rule by [RuleSet:Rule] pattern and run it on input.",
-            hidden = true)
+        description = "Specify a rule by [RuleSet:Rule] pattern and run it on input.",
+        hidden = true)
     var runRule: String? = null
 
     @Parameter(names = ["--print-ast"],
-            description = "Prints the AST for given [input] file. Must be no directory.",
-            hidden = true)
+        description = "Prints the AST for given [input] file. Must be no directory.",
+        hidden = true)
     var printAst: Boolean = false
 
     val inputPaths: List<Path> by lazy {
