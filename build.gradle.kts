a8b67c29cb2936ac21fc3a67600b18a34b6d6598
Matthew Haughton <3flex@users.noreply.github.com>
2019-04-24 23:55:22 +1000

    Kotlin 1.3.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.Date
 
 plugins {
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.30"
     id("com.jfrog.bintray") version "1.8.4"
     id("com.github.ben-manes.versions") version "0.21.0"
     id("com.github.johnrengelman.shadow") version "5.0.0" apply false
