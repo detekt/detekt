100e0e153c96d3490a24b062c80ebffc829fbede
Matthew Haughton <3flex@users.noreply.github.com>
2019-04-28 22:00:38 +1000

    Link documentation to JDK 8

--- detekt-api/build.gradle.kts
+++ detekt-api/build.gradle.kts
@@ -24,9 +24,6 @@ dependencies {
 tasks.withType<DokkaTask> {
     // suppresses undocumented classes but not dokka warnings https://github.com/Kotlin/dokka/issues/90
     reportUndocumented = false
-    outputFormat = "javadoc"
-    outputDirectory = "$buildDir/javadoc"
-    // Java 8 is only version supported both by Oracle/OpenJDK and Dokka itself
-    // https://github.com/Kotlin/dokka/issues/294
-    enabled = JavaVersion.current().isJava8
+    @Suppress("MagicNumber")
+    jdkVersion = 8
 }
