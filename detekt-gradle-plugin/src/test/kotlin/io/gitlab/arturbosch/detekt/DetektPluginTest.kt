2332d1003750ba0e9c9b0dfa69380ec3c0fc1275
Matthew Haughton <3flex@users.noreply.github.com>
2019-05-07 23:32:43 +1000

    Lazily add `detekt` task as a dependency of the `check` task

---
+++ detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/DetektPluginTest.kt
@@ -0,0 +1,26 @@
+package io.gitlab.arturbosch.detekt
+
+import org.assertj.core.api.Assertions.assertThat
+import org.gradle.api.Task
+import org.gradle.language.base.plugins.LifecycleBasePlugin
+import org.gradle.testfixtures.ProjectBuilder
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object DetektPluginTest : Spek({
+    describe("detekt plugin") {
+
+        fun Task.dependencies() = taskDependencies.getDependencies(this)
+
+        it("lazily adds detekt as a dependency of the `check` task") {
+            val project = ProjectBuilder.builder().build()
+
+            /** Ordering here is important - to prove lazily adding the dependency works the LifecycleBasePlugin must be
+             * added to the project after the detekt plugin. */
+            project.pluginManager.apply(DetektPlugin::class.java)
+            project.pluginManager.apply(LifecycleBasePlugin::class.java)
+
+            assertThat(project.tasks.getAt("check").dependencies().map {it.name }).contains("detekt")
+        }
+    }
+})
