a0eb183957b55fc2d0a9d939f748d7800d8992b7
Artur Bosch <arturbosch@gmx.de>
2019-05-10 05:19:54 +0200

    Do not report when using shortcut 'return' on nullable types - Closes #842

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/Traversing.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/Traversing.kt
@@ -4,6 +4,7 @@ import io.gitlab.arturbosch.detekt.api.DetektVisitor
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFile
+import java.util.ArrayDeque
 
 /**
  * Returns a list of all parents of type [T] before first occurrence of [S].
@@ -19,7 +20,6 @@ inline fun <reified T : KtElement, reified S : KtElement> KtElement.parentsOfTyp
             }
         }
 
-@Suppress("UnsafeCast")
 inline fun <reified T : KtElement, reified S : KtElement> KtElement.parentsOfTypeUntil() = sequence<T> {
     var current: PsiElement? = this@parentsOfTypeUntil
     while (current != null && current !is S) {
@@ -45,3 +45,17 @@ inline fun <reified T : KtElement> KtElement.collectByType(): List<T> {
     })
     return list
 }
+
+@Suppress("USELESS_CAST", "UnsafeCast") // @BuilderInference is not smart enough
+inline fun <reified T : KtElement> KtElement.sequenceOfType(): Sequence<T> =
+    if (this.children.isEmpty()) emptySequence() else sequence {
+        val stack = ArrayDeque<PsiElement>(this@sequenceOfType.children.toList())
+        var current = stack.pollFirst()
+        while (current != null) {
+            if (current is T) {
+                yield(current as T)
+            }
+            stack.addAll(current.children.toList())
+            current = stack.pollFirst()
+        }
+    }
