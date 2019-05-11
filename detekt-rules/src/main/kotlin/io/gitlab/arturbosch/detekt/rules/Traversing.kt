efd35faec1b16b97975dbab7040cf2a70060a2cc
Artur Bosch <arturbosch@gmx.de>
2019-05-11 21:30:50 +0200

    Start at callee node to consider all nodes

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/Traversing.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/Traversing.kt
@@ -1,6 +1,5 @@
 package io.gitlab.arturbosch.detekt.rules
 
-import io.gitlab.arturbosch.detekt.api.DetektVisitor
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFile
@@ -23,29 +22,15 @@ inline fun <reified T : KtElement, reified S : KtElement> KtElement.parentsOfTyp
 inline fun <reified T : KtElement> KtElement.parentOfType(strict: Boolean = true) =
     parentsOfTypeUntil<T, KtFile>(strict).firstOrNull()
 
-inline fun <reified T : KtElement> KtElement.collectByType(): List<T> {
-    val list = mutableListOf<T>()
-    this.accept(object : DetektVisitor() {
-        override fun visitKtElement(element: KtElement) {
-            if (element is T) {
-                list.add(element)
-            }
-            element.children.forEach { it.accept(this) }
-        }
-    })
-    return list
-}
-
 @Suppress("USELESS_CAST", "UnsafeCast") // @BuilderInference is not smart enough
-inline fun <reified T : KtElement> KtElement.sequenceOfType(): Sequence<T> =
-    if (this.children.isEmpty()) emptySequence() else sequence {
-        val stack = ArrayDeque<PsiElement>(this@sequenceOfType.children.toList())
-        var current = stack.pollFirst()
-        while (current != null) {
-            if (current is T) {
-                yield(current as T)
-            }
-            stack.addAll(current.children.toList())
-            current = stack.pollFirst()
+inline fun <reified T : KtElement> KtElement.collectByType(): Sequence<T> = sequence {
+    val stack = ArrayDeque<PsiElement>()
+    var current: PsiElement? = this@collectByType
+    while (current != null) {
+        if (current is T) {
+            yield(current as T)
         }
+        stack.addAll(current.children.toList())
+        current = stack.pollFirst()
     }
+}
