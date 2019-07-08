b5844819d19e9beb4f7070033ea2e3e98823b76d
Nicola Corti <corti.nico@gmail.com>
2019-07-08 03:44:51 +0200

    Update Kotlin to 1.3.41 (#1759)

--- detekt-gradle-plugin/build.gradle.kts
+++ detekt-gradle-plugin/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     `maven-publish`
     id("com.gradle.plugin-publish") version "0.10.1"
     id("com.jfrog.bintray") version "1.8.4"
-    kotlin("jvm") version "1.3.40"
+    kotlin("jvm") version "1.3.41"
     id("org.jetbrains.dokka") version "0.9.18"
     id("com.github.ben-manes.versions") version "0.21.0"
     id("io.gitlab.arturbosch.detekt") version "1.0.0-RC15"
