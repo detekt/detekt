4dfc047cb3ff387ac7a736b7798541d7353ca608
Artur Bosch <arturbosch@gmx.de>
2019-07-28 21:12:34 +0200

    Update to KtLint 0.34.2

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/MultiLineIfElse.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/MultiLineIfElse.kt
@@ -1,6 +1,6 @@
 package io.gitlab.arturbosch.detekt.formatting.wrappers
 
-import com.pinterest.ktlint.ruleset.standard.MultiLineIfElseRule
+import com.pinterest.ktlint.ruleset.experimental.MultiLineIfElseRule
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
