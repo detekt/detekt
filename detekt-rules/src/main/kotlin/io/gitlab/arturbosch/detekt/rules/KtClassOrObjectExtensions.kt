3d46099d22c0b711db523525de5ace9eebe00575
M Schalk <30376729+schalkms@users.noreply.github.com>
2019-07-28 13:00:52 +0200

    Fix UseDataClass false positive (delegation) (#1783)
    * Fix UseDataClass false positive (delegation)
    
    A class containing delegated properties is no data class candidate.
    This closes #1585
    
    * Fix detekt.TrailingWhitespace warning

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtClassOrObjectExtensions.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtClassOrObjectExtensions.kt
@@ -2,7 +2,15 @@ package io.gitlab.arturbosch.detekt.rules
 
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.psiUtil.isAbstract
 
 fun KtClassOrObject.isInterface(): Boolean {
     return (this as? KtClass)?.isInterface() == true
 }
+
+fun KtClass.doesNotExtendAnything() = superTypeListEntries.isEmpty()
+
+fun KtClass.isClosedForExtension() = !isAbstract() && !isOpen()
+
+fun KtClass.extractDeclarations(): List<KtDeclaration> = body?.declarations.orEmpty()
