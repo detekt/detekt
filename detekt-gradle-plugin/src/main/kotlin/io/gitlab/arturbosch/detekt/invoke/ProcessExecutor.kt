e3d28af14b267fab6fdf0924a7ed9735bae64e3d
Matthew Haughton <3flex@users.noreply.github.com>
2019-08-30 22:16:33 +1000

    Use Gradle's logger to print debug output

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/invoke/ProcessExecutor.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/invoke/ProcessExecutor.kt
@@ -1,17 +1,18 @@
 package io.gitlab.arturbosch.detekt.invoke
 
 import io.gitlab.arturbosch.detekt.BuildFailure
+import org.gradle.api.logging.Logger
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 object ProcessExecutor {
-    fun startProcess(args: Array<String>, debug: Boolean = false) {
+    fun startProcess(args: Array<String>, logger: Logger) {
         val process = Runtime.getRuntime().exec(args)
 
         BufferedReader(InputStreamReader(BufferedInputStream(process.inputStream))).use {
             val inputs = it.readLines().joinToString("\n")
-            if (debug) println(inputs)
+            logger.debug(inputs)
         }
 
         BufferedReader(InputStreamReader(BufferedInputStream(process.errorStream))).use {
