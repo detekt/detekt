b5ebb308637a29186f77f8ed7e12e73e7fefccfa
Artur Bosch <arturbosch@gmx.de>
2019-08-30 17:11:22 +0200

    Revert "Use Gradle's logger to print debug output (#1869)" (#1872)
    This reverts commit dc9b9e8228709d581464c31b0be14aa744bff72c.

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/invoke/ProcessExecutor.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/invoke/ProcessExecutor.kt
@@ -1,19 +1,17 @@
 package io.gitlab.arturbosch.detekt.invoke
 
 import io.gitlab.arturbosch.detekt.BuildFailure
-import org.gradle.api.logging.Logging
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 object ProcessExecutor {
-    fun startProcess(args: Array<String>) {
+    fun startProcess(args: Array<String>, debug: Boolean = false) {
         val process = Runtime.getRuntime().exec(args)
-        val logger = Logging.getLogger(this::class.java)
 
         BufferedReader(InputStreamReader(BufferedInputStream(process.inputStream))).use {
             val inputs = it.readLines().joinToString("\n")
-            logger.debug(inputs)
+            if (debug) println(inputs)
         }
 
         BufferedReader(InputStreamReader(BufferedInputStream(process.errorStream))).use {
