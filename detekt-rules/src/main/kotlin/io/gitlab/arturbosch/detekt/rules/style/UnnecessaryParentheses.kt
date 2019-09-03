61f1734fdebfad31c9becd117cb30af4a4c370ae
Matthew Haughton <3flex@users.noreply.github.com>
2019-09-03 20:44:56 +1000

    Don't check for unnecessary parentheses around lambdas (#1890)

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnnecessaryParentheses.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/UnnecessaryParentheses.kt
@@ -7,14 +7,8 @@ import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
-import org.jetbrains.kotlin.psi.KtConstructorDelegationCall
-import org.jetbrains.kotlin.psi.KtLambdaArgument
-import org.jetbrains.kotlin.psi.KtLambdaExpression
 import org.jetbrains.kotlin.psi.KtParenthesizedExpression
 import org.jetbrains.kotlin.psi.KtPsiUtil
-import org.jetbrains.kotlin.psi.KtSuperTypeCallEntry
-import org.jetbrains.kotlin.psi.KtValueArgument
-import org.jetbrains.kotlin.psi.KtValueArgumentList
 
 /**
  * This rule reports unnecessary parentheses around expressions.
@@ -51,42 +45,12 @@ class UnnecessaryParentheses(config: Config = Config.empty) : Rule(config) {
     override fun visitParenthesizedExpression(expression: KtParenthesizedExpression) {
         super.visitParenthesizedExpression(expression)
 
+        if (expression.expression == null) return
+
         if (KtPsiUtil.areParenthesesUseless(expression)) {
             val message = "Parentheses in ${expression.text} are unnecessary and can be replaced with: " +
                     "${KtPsiUtil.deparenthesize(expression)?.text}"
             report(CodeSmell(issue, Entity.from(expression), message))
         }
-    }
-
-    override fun visitArgument(argument: KtValueArgument) {
-        super.visitArgument(argument)
-        if (argument.children.any { it is KtLambdaExpression } &&
-                isSingleLambdaInArgumentList(argument) &&
-                !isArgumentInFunctionCallWithTwoLambdas(argument)) {
-            val parent = argument.parent
-            val message = "Parentheses around the lambda ${parent.text} are unnecessary and can be removed."
-            report(CodeSmell(issue, Entity.from(parent), message))
-        }
-    }
-
-    private fun isSingleLambdaInArgumentList(argument: KtValueArgument): Boolean {
-        val parent = argument.parent
-        val isOnlyArgument = parent.children.size == 1
-        val nodeBeforeArgumentList = parent.parent
-        val isSuperTypeCallEntry = nodeBeforeArgumentList is KtSuperTypeCallEntry ||
-                nodeBeforeArgumentList is KtConstructorDelegationCall
-
-        return isOnlyArgument &&
-                !isSuperTypeCallEntry &&
-                argument.equalsToken == null
-    }
-
-    private fun isArgumentInFunctionCallWithTwoLambdas(argument: KtValueArgument): Boolean {
-        val parent = argument.parent
-        val grandParent = parent.parent
-
-        return parent is KtValueArgumentList &&
-                grandParent.children.size >= 2 &&
-                grandParent.children.last() is KtLambdaArgument
     }
 }
