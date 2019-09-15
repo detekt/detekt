03a37661b56e515f433265d6aaf0f47ffb26ac0e
Matthew Haughton <3flex@users.noreply.github.com>
2019-09-15 18:07:18 +1000

    Remove unnecessary extension

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/KotlinExtension.kt
+++
@@ -1,7 +0,0 @@
-package io.gitlab.arturbosch.detekt
-
-import io.gitlab.arturbosch.detekt.extensions.DetektExtension
-import org.gradle.api.Project
-
-fun Project.detekt(configure: DetektExtension.() -> Unit) =
-    extensions.configure(DetektExtension::class.java, configure)
