dc659f69ec1b7e50d0421094850a315399dfc02f
schalkms <schalkms@icloud.com>
2019-09-17 22:55:45 +0200

    Replace tabs with spaces in test snippets

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/UndocumentedPublicPropertySpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/UndocumentedPublicPropertySpec.kt
@@ -12,90 +12,90 @@ class UndocumentedPublicPropertySpec : Spek({
 
         it("reports undocumented public property") {
             val code = """
-    			val a = 1
-			"""
+                val a = 1
+            """
             assertThat(subject.compileAndLint(code)).hasSize(1)
         }
 
         it("reports undocumented public properties in companion object") {
             val code = """
-    			class Test {
-    				companion object {
-    					val a = 1
-    					public val b = 1
-    				}
-    			}
-			"""
+                class Test {
+                    companion object {
+                        val a = 1
+                        public val b = 1
+                    }
+                }
+            """
             assertThat(subject.compileAndLint(code)).hasSize(2)
         }
 
         it("does not report documented public property") {
             val code = """
-    			/**
-			     * Comment
-				 */
-				val a = 1
-			"""
+                /**
+                 * Comment
+                 */
+                val a = 1
+            """
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("does not report documented public property in class") {
             val code = """
-				class Test {
-					/**
-					*
-					*/
-					val a = 1
-				}
-			"""
+                class Test {
+                    /**
+                    *
+                    */
+                    val a = 1
+                }
+            """
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("does not report undocumented internal and private property") {
             val code = """
-    			class Test {
-    				internal val a = 1
-    				private val b = 1
-    			}
-			"""
+                class Test {
+                    internal val a = 1
+                    private val b = 1
+                }
+            """
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("does not report local variables") {
             val code = """
-				fun commented(x: Int) {
-					var a = x
-				}
-			"""
+                fun commented(x: Int) {
+                    var a = x
+                }
+            """
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("does not report public properties in internal class") {
             val code = """
-    			internal class NoComments {
+                internal class NoComments {
                     public val a = 1
-    				val b = 1
-				}
-			"""
+                    val b = 1
+                }
+            """
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("does not report public properties in private class") {
             val code = """
-    			private class NoComments {
+                private class NoComments {
                     public val a = 1
-    				val b = 1
-				}
-			"""
+                    val b = 1
+                }
+            """
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("does not report properties in constructor") {
             val code = """
-    			class Test(public val a: Int) {
+                class Test(public val a: Int) {
                     constructor(a: Int, b: Int) : this(a)
-				}
-			"""
+                }
+            """
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
