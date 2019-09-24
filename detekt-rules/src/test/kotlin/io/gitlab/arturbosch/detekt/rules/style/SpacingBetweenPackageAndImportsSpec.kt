77c40ff2916f41d25e493647625a40eea545c3ba
schalkms <schalkms@icloud.com>
2019-09-24 21:01:57 +0200

    Change lint assert - hasSize(0) to isEmpty()

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/SpacingBetweenPackageAndImportsSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/SpacingBetweenPackageAndImportsSpec.kt
@@ -15,36 +15,36 @@ class SpacingBetweenPackageAndImportsSpec : Spek({
 
         it("has no blank lines violation") {
             val code = "package test\n\nimport a.b\n\nclass A {}"
-            assertThat(subject.lint(code)).hasSize(0)
+            assertThat(subject.lint(code)).isEmpty()
         }
 
         it("has a package and import declaration") {
             val code = "package test\n\nimport a.b"
-            assertThat(subject.lint(code)).hasSize(0)
+            assertThat(subject.lint(code)).isEmpty()
         }
 
         it("has no import declaration") {
             val code = "package test\n\nclass A {}"
-            assertThat(subject.lint(code)).hasSize(0)
+            assertThat(subject.lint(code)).isEmpty()
         }
 
         it("has no package declaration") {
             val code = "import a.b\n\nclass A {}"
-            assertThat(subject.lint(code)).hasSize(0)
+            assertThat(subject.lint(code)).isEmpty()
         }
 
         it("has no package and import declaration") {
             val code = "class A {}"
-            assertThat(subject.lint(code)).hasSize(0)
+            assertThat(subject.lint(code)).isEmpty()
         }
 
         it("has a comment declaration") {
             val code = "import a.b\n\n// a comment"
-            assertThat(subject.lint(code)).hasSize(0)
+            assertThat(subject.lint(code)).isEmpty()
         }
 
         it("is an empty kt file") {
-            assertThat(subject.lint("")).hasSize(0)
+            assertThat(subject.lint("")).isEmpty()
         }
 
         it("has code on new line") {
@@ -70,7 +70,7 @@ class SpacingBetweenPackageAndImportsSpec : Spek({
         it("does not report for Kotlin script file") {
             val code = "package test\nimport a.b\nclass A {}"
             val ktsFile = KtTestCompiler.compileFromContent(code, "Test.kts")
-            assertThat(subject.lint(ktsFile)).hasSize(0)
+            assertThat(subject.lint(ktsFile)).isEmpty()
         }
 
         it("has multiple imports in file") {
@@ -82,7 +82,7 @@ class SpacingBetweenPackageAndImportsSpec : Spek({
 
 				class A { }
 				"""
-            assertThat(subject.compileAndLint(code)).hasSize(0)
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("has no class") {
@@ -92,7 +92,7 @@ class SpacingBetweenPackageAndImportsSpec : Spek({
 				import kotlin.collections.List
 				import kotlin.collections.Set
 				"""
-            assertThat(subject.compileAndLint(code)).hasSize(0)
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
 })
