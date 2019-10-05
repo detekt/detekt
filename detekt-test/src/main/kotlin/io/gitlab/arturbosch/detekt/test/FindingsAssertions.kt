3766ce763f847e927131a4144b959e61da9c971c
Brais Gabín <braisgabin@gmail.com>
2019-10-05 12:57:32 +0200

    Add hasTextLocation in FindingsAssertions

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/FindingsAssertions.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/FindingsAssertions.kt
@@ -2,6 +2,7 @@ package io.gitlab.arturbosch.detekt.test
 
 import io.gitlab.arturbosch.detekt.api.Finding
 import io.gitlab.arturbosch.detekt.api.SourceLocation
+import io.gitlab.arturbosch.detekt.api.TextLocation
 import org.assertj.core.api.AbstractAssert
 import org.assertj.core.api.AbstractListAssert
 import org.assertj.core.util.Objects.areEqual
@@ -54,6 +55,21 @@ class FindingsAssert(actual: List<Finding>) :
 
         if (!areEqual(actualSources.toList(), expectedSources.toList())) {
             failWithMessage("Expected source locations to be ${expectedSources.toList()} but was ${actualSources.toList()}")
+        }
+    }
+
+    fun hasTextLocation(vararg expected: TextLocation) = apply {
+        isNotNull
+
+        val actualSources = actual.asSequence()
+            .map { it.location.text }
+            .sortedWith(compareBy({ it.start }, { it.end }))
+
+        val expectedSources = expected.asSequence()
+            .sortedWith(compareBy({ it.start }, { it.end }))
+
+        if (!areEqual(actualSources.toList(), expectedSources.toList())) {
+            failWithMessage("Expected text locations to be ${expectedSources.toList()} but was ${actualSources.toList()}")
         }
     }
 }
