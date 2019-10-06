cb6ec602d3c2d0f3544efadb53c5b01fccc2f24b
M Schalk <30376729+schalkms@users.noreply.github.com>
2019-10-06 17:46:28 +0200

    Use inline code snippets instead of case files (#1976)
    This commit inlines the code snippets for data class related rules in
    the style ruleset.

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/DataClassShouldBeImmutableSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/DataClassShouldBeImmutableSpec.kt
@@ -1,7 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
-import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.test.lint
+import io.gitlab.arturbosch.detekt.test.compileAndLint
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -11,14 +10,69 @@ class DataClassShouldBeImmutableSpec : Spek({
 
     describe("DataClassShouldBeImmutable rule") {
 
-        it("reports positive cases") {
-            val path = Case.DataClassShouldBeImmutablePositive.path()
-            assertThat(subject.lint(path)).hasSize(4)
+        it("reports mutable variable in primary constructor") {
+            val code = "data class C(var i: Int)"
+            assertThat(subject.compileAndLint(code)).hasSize(1)
         }
 
-        it("does not report negative cases") {
-            val path = Case.DataClassShouldBeImmutableNegative.path()
-            assertThat(subject.lint(path)).hasSize(0)
+        it("reports mutable property in class body") {
+            val code = """
+                data class C(val i: Int) {
+                    var s: String? = null
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("reports mutable private property in class body") {
+            val code = """
+                data class C(val i: Int) {
+                    var s: String = ""
+                        private set
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("reports lateinit property in class body") {
+            val code = """
+                data class C(val i: Int) {
+                    lateinit var s: String
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("does not report readonly variable in primary constructor") {
+            val code = "data class C(val i: Int)"
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report readonly property in class body") {
+            val code = """
+                data class C(val i: Int) {
+                    val s: String? = null
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report lazy property in class body") {
+            val code = """
+                data class C(val i: Int) {
+                    val s: String by lazy { "" }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report mutable variables in non-data classes") {
+            val code = """
+                class C(var i: Int) {
+                    val s: String by lazy { "" }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
 })
