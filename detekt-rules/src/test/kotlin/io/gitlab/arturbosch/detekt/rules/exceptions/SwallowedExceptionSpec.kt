f40cb865ceb9be3f5f6323fe8e8cb9dfeb8a35fe
M Schalk <30376729+schalkms@users.noreply.github.com>
2019-10-06 13:52:42 +0200

    Use inline code snippets instead of case files (#1989)
    This PR inlines code snippets for rules in the exception ruleset.

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/SwallowedExceptionSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/SwallowedExceptionSpec.kt
@@ -1,8 +1,7 @@
 package io.gitlab.arturbosch.detekt.rules.exceptions
 
-import io.gitlab.arturbosch.detekt.rules.Case
+import io.gitlab.arturbosch.detekt.test.compileAndLint
 import io.gitlab.arturbosch.detekt.test.TestConfig
-import io.gitlab.arturbosch.detekt.test.lint
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -12,24 +11,126 @@ class SwallowedExceptionSpec : Spek({
 
     describe("SwallowedException rule") {
 
-        it("reports swallowed exceptions") {
-            assertThat(subject.lint(Case.SwallowedExceptionPositive.path())).hasSize(5)
+        it("reports a swallowed exception") {
+            val code = """
+                fun f() {
+                    try {
+                    } catch (e: Exception) {
+                        throw IllegalArgumentException()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
         }
 
-        it("ignores given exception types in configuration") {
-            val config = TestConfig(mapOf(SwallowedException.IGNORED_EXCEPTION_TYPES to "IOException"))
+        it("reports swallowed exceptions only using exception strings") {
+            val code = """
+                fun f() {
+                    try {
+                    } catch (e: IllegalStateException) {
+                        throw IllegalArgumentException(e.message)
+                    } catch (f: Exception) {
+                        throw Exception(IllegalArgumentException(f.toString()))
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(2)
+        }
+
+        it("reports a swallowed exception that is not logged") {
+            val code = """
+                fun f() {
+                    try {
+                    } catch (e: Exception) {
+                        println()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        context("ignores given exception types config") {
+
+            val config = TestConfig(mapOf(SwallowedException.IGNORED_EXCEPTION_TYPES to "IllegalArgumentException"))
             val rule = SwallowedException(config)
-            assertThat(rule.lint(Case.SwallowedExceptionPositive.path())).hasSize(4)
+
+            it("ignores given exception type in configuration") {
+                val code = """
+                    fun f() {
+                        try {
+                        } catch (e: IllegalArgumentException) {
+                        }
+                    }
+                """
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
+
+            it("reports exception type that is missing in the configuration") {
+                val code = """
+                    fun f() {
+                        try {
+                        } catch (e: Exception) {
+                        }
+                    }
+                """
+                assertThat(rule.compileAndLint(code)).hasSize(1)
+            }
         }
 
-        it("ignores given exception name") {
+        context("ignores given exception name config") {
+
             val config = TestConfig(mapOf(SwallowedException.ALLOWED_EXCEPTION_NAME_REGEX to "myIgnore"))
             val rule = SwallowedException(config)
-            assertThat(rule.lint(Case.SwallowedExceptionPositive.path())).hasSize(4)
+
+            it("ignores given exception name") {
+                val code = """
+                    fun f() {
+                        try {
+                        } catch (myIgnore: IllegalArgumentException) {
+                        }
+                    }
+                """
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
+
+            it("reports exception name") {
+                val code = """
+                    fun f() {
+                        try {
+                        } catch (e: IllegalArgumentException) {
+                        }
+                    }
+                """
+                assertThat(rule.compileAndLint(code)).hasSize(1)
+            }
         }
 
-        it("does not report thrown catch blocks") {
-            assertThat(subject.lint(Case.SwallowedExceptionNegative.path())).hasSize(0)
+        it("does not report wrapped exceptions") {
+            val code = """
+                fun f() {
+                    try {
+                    } catch (e: IllegalStateException) {
+                        throw IllegalArgumentException(e.message, e)
+                    } catch (e: Exception) {
+                        throw IllegalArgumentException(e)
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report used exception variables") {
+            val code = """
+                fun f() {
+                    try {
+                    } catch (e: IllegalArgumentException) {
+                        print(e)
+                    } catch (e: Exception) {
+                        print(e.message)
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
 })
