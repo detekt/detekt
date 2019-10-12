6869d224c93ca6c9e439c203ef7ca80d61f508f0
Brais Gabín <braisgabin@gmail.com>
2019-10-12 23:40:44 +0200

    Simplify tests. All those cases are already checked

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/ClassNamingSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/ClassNamingSpec.kt
@@ -15,47 +15,21 @@ class ClassNamingSpec : Spek({
                     class MyClassWithNumbers5
 
                     class NamingConventions {
-
-                        private val _classVariable = 5
-                        val classVariable = 5
-
-                        fun classMethod() {}
-
-                        fun underscoreTestMethod() {
-                            val (_, status) = Pair(1, 2) // valid: _
-                        }
-
-                        companion object {
-                            const val stUff = "stuff"
-                            val SSS = "stuff"
-                            val ooo = Any()
-                            val OOO = Any()
-                        }
                     }
                 """
             )
             assertThat(findings).isEmpty()
         }
 
-        it("should find seven violations") {
+        it("should find two violations") {
             val findings = NamingRules().compileAndLint(
                     """
-                    class _NamingConventions {
+                    class _NamingConventions
 
-                        val C_lassVariable = 5
-                        val CLASSVARIABLE = 5
-
-                        fun _classmethod() {}
-                        fun Classmethod() {}
-
-                        companion object {
-                            val __bla = Any()
-                        }
-                    }
                     class namingConventions {}
                 """
             )
-            assertThat(findings).hasSize(7)
+            assertThat(findings).hasSize(2)
         }
     }
 })
