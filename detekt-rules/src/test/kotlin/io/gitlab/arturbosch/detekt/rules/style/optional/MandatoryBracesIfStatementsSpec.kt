e432864d11e253e6470a1024c0d86ee78c7a82e6
Brais Gabín <braisgabin@gmail.com>
2019-10-16 22:39:53 +0200

    Split MandatoryBracesIfStatementsSpec in smaller tests

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/optional/MandatoryBracesIfStatementsSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/optional/MandatoryBracesIfStatementsSpec.kt
@@ -2,20 +2,70 @@ package io.gitlab.arturbosch.detekt.rules.style.optional
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.rules.Case
+import io.gitlab.arturbosch.detekt.test.assertThat
 import io.gitlab.arturbosch.detekt.test.lint
-import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 class MandatoryBracesIfStatementsSpec : Spek({
     val subject by memoized { MandatoryBracesIfStatements(Config.empty) }
 
-    describe("MandatoryBracesIfStatements rule") {
+    describe("reports multi-line if statements should have braces") {
 
-        it("reports multi-line if statements should have braces") {
-            val path = Case.MandatoryBracesIfStatementsPositive.path()
-            assertThat(subject.lint(path)).hasSize(7)
+        it("simple if") {
+            val findings = subject.lint("""
+            fun f() {
+                if (true)
+                    println()
+            }
+            """.trimIndent())
+
+            assertThat(findings).hasSize(1)
         }
+
+        it("if-else") {
+            val findings = subject.lint("""
+            fun f() {
+                if (true)
+                    println()
+                else
+                    println()
+            }
+            """.trimIndent())
+
+            assertThat(findings).hasSize(2)
+        }
+
+        it("if-else with else-if") {
+            val findings = subject.lint("""
+            fun f() {
+                if (true)
+                    println()
+                else if (false)
+                    println()
+                else
+                    println()
+            }
+            """.trimIndent())
+
+            assertThat(findings).hasSize(3)
+        }
+
+        it("if with braces but else without") {
+            val findings = subject.lint("""
+            fun f() {
+                if (true) {
+                    println()
+                } else
+                    println()
+            }
+            """.trimIndent())
+
+            assertThat(findings).hasSize(1)
+        }
+    }
+
+    describe("MandatoryBracesIfStatements rule") {
 
         it("reports non multi-line if statements should have braces") {
             val path = Case.MandatoryBracesIfStatementsNegative.path()
