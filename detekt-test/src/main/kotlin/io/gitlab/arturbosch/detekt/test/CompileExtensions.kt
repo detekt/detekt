ea8cf75fe38ce59d3eef585a442ee4dc78dc1dc6
Brais Gabín <braisgabin@gmail.com>
2019-10-28 18:48:51 +0100

    Allow to pass the Filename directly to compileContentForTest

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/CompileExtensions.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/CompileExtensions.kt
@@ -5,7 +5,8 @@ import java.nio.file.Path
 /**
  * Use this method if you define a kt file/class as a plain string in your test.
  */
-fun compileContentForTest(content: String) = KtTestCompiler.compileFromContent(content)
+fun compileContentForTest(content: String, filename: String = TEST_FILENAME) =
+    KtTestCompiler.compileFromContent(content, filename)
 
 /**
  * Use this method if you test a kt file/class in the test resources.
