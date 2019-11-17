1562f50b0a41e716bb9d5651500407b592ea535a
Brais Gabín <braisgabin@gmail.com>
2019-11-17 10:48:32 +0100

    Overload textLocation(String) (#2091)

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/FindingsAssertions.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/FindingsAssertions.kt
@@ -58,6 +58,37 @@ class FindingsAssert(actual: List<Finding>) :
             failWithMessage("Expected text locations to be ${expectedSources.toList()} but was ${actualSources.toList()}")
         }
     }
+
+    fun hasTextLocations(vararg expected: String): FindingsAssert {
+        isNotNull
+
+        val finding = actual.firstOrNull()
+        if (finding == null) {
+            if (expected.isEmpty()) {
+                return this
+            } else {
+                failWithMessage("Expected ${expected.size} findings but was 0")
+            }
+        }
+        val code = finding!!.entity.ktElement?.containingKtFile?.text
+        if (code == null) {
+            failWithMessage("Expected ${expected.size} findings but was 0")
+        }
+
+        val textLocations = expected.map { snippet ->
+            val index = code!!.indexOf(snippet)
+            if (index < 0) {
+                failWithMessage("The snippet \"$snippet\" doesn't exist in the code")
+            } else {
+                if (code.indexOf(snippet, index + 1) >= 0) {
+                    failWithMessage("The snippet \"$snippet\" appears multiple times in the code")
+                }
+            }
+            index to index + snippet.length
+        }.toTypedArray()
+
+        return hasTextLocations(*textLocations)
+    }
 }
 
 class FindingAssert(val actual: Finding?) : AbstractAssert<FindingAssert, Finding>(actual, FindingAssert::class.java)
