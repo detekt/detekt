1c8766aae01fa30d7fae024db5d55936ed98dfa6
schalkms <schalkms@icloud.com>
2019-11-21 20:00:28 +0100

    Filter auto corrected issues in FileBasedFindingsReport

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/FileBasedFindingsReport.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/console/FileBasedFindingsReport.kt
@@ -1,17 +1,26 @@
 package io.gitlab.arturbosch.detekt.cli.console
 
+import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.ConsoleReport
 import io.gitlab.arturbosch.detekt.api.Detektion
 import io.gitlab.arturbosch.detekt.api.Finding
 import io.gitlab.arturbosch.detekt.api.RuleSetId
+import io.gitlab.arturbosch.detekt.api.SingleAssign
+import io.gitlab.arturbosch.detekt.cli.filterAutoCorrectedIssues
 
 class FileBasedFindingsReport : ConsoleReport() {
 
+    private var config: Config by SingleAssign()
+
     override val priority: Int = 40
+
+    override fun init(config: Config) {
+        this.config = config
+    }
 
     override fun render(detektion: Detektion): String? {
         val findings = detektion
-            .findings
+            .filterAutoCorrectedIssues(config)
             .filter { it.value.isNotEmpty() }
         if (findings.isEmpty()) {
             return null
