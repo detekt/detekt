6cd5ce13380b07c2261d2ae8928a79a49218889a
schalkms <schalkms@icloud.com>
2019-11-21 22:10:12 +0100

    Create FindingsReport test for correctable code smells

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/FindingsReportSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/FindingsReportSpec.kt
@@ -2,7 +2,9 @@ package io.gitlab.arturbosch.detekt.cli.console
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Finding
+import io.gitlab.arturbosch.detekt.cli.createCorrectableFinding
 import io.gitlab.arturbosch.detekt.cli.createFinding
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.TestDetektion
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
@@ -51,6 +53,14 @@ class FindingsReportSpec : Spek({
                     Pair("EmptySmells", emptyList()))
             }
             assertThat(subject.render(detektion)).isNull()
+        }
+
+        it("should not add auto corrected issues to report") {
+            val report = FindingsReport()
+            report.init(TestConfig("excludeCorrectable" to "true"))
+            val correctableCodeSmell = createCorrectableFinding()
+            val detektionWithCorrectableSmell = TestDetektion(correctableCodeSmell)
+            assertThat(report.render(detektionWithCorrectableSmell)).isNull()
         }
     }
 })
