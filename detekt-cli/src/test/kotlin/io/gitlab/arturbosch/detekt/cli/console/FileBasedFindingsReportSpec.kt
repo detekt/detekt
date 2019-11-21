a68e3f19c43bcba177938f7a90e2751cbca9578e
schalkms <schalkms@icloud.com>
2019-11-21 22:09:08 +0100

    Create FileBasedFindingsReport test for correctable code smells

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/FileBasedFindingsReportSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/FileBasedFindingsReportSpec.kt
@@ -2,8 +2,10 @@ package io.gitlab.arturbosch.detekt.cli.console
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Finding
+import io.gitlab.arturbosch.detekt.cli.createCorrectableFinding
 import io.gitlab.arturbosch.detekt.test.TestDetektion
 import io.gitlab.arturbosch.detekt.cli.createFinding
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -55,6 +57,14 @@ class FileBasedFindingsReportSpec : Spek({
                 )
             }
             assertThat(subject.render(detektion)).isNull()
+        }
+
+        it("should not add auto corrected issues to report") {
+            val report = FileBasedFindingsReport()
+            report.init(TestConfig("excludeCorrectable" to "true"))
+            val correctableCodeSmell = createCorrectableFinding()
+            val detektionWithCorrectableSmell = TestDetektion(correctableCodeSmell)
+            assertThat(report.render(detektionWithCorrectableSmell)).isNull()
         }
     }
 })
