71b532e00bcc99a0814053d0d5b99aa5cf2d9903
schalkms <schalkms@icloud.com>
2019-11-21 22:03:16 +0100

    Create BuildFailureReport test for correctable code smells

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/BuildFailureReportSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/console/BuildFailureReportSpec.kt
@@ -1,6 +1,7 @@
 package io.gitlab.arturbosch.detekt.cli.console
 
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.cli.createCorrectableFinding
 import io.gitlab.arturbosch.detekt.test.TestDetektion
 import io.gitlab.arturbosch.detekt.cli.createFinding
 import io.gitlab.arturbosch.detekt.test.TestConfig
@@ -16,7 +17,8 @@ internal class BuildFailureReportSpec : Spek({
     describe("build failure threshold is configurable by configuration") {
 
         describe("empty code smell result") {
-            val detektion = TestDetektion(createFinding())
+            val codeSmell = createFinding()
+            val detektion = TestDetektion(codeSmell)
 
             it("should fail because no config is provided for configurable console reporter") {
                 assertThatIllegalStateException().isThrownBy { subject.render(detektion) }
@@ -41,8 +43,16 @@ internal class BuildFailureReportSpec : Spek({
             }
 
             it("should not print a warning if weighted issues are zero") {
-                subject.init(TestConfig(mapOf("maxIssues" to "10")))
+                subject.init(TestConfig(mapOf("maxIssues" to "0")))
                 val report = subject.render(TestDetektion())
+                assertThat(report).isNull()
+            }
+
+            it("should not report auto corrected issues as build failure") {
+                subject.init(TestConfig(mapOf("maxIssues" to "0", "excludeCorrectable" to "true")))
+                val correctableCodeSmell = createCorrectableFinding()
+                val detektionWithCorrectableSmell = TestDetektion(correctableCodeSmell)
+                val report = subject.render(detektionWithCorrectableSmell)
                 assertThat(report).isNull()
             }
         }
