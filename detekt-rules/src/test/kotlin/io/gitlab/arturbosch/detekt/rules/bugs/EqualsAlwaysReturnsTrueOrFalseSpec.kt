5dd88d3d59e165f8d0215cccf388a47e5d5e7873
schalkms <schalkms@icloud.com>
2019-11-24 11:33:08 +0100

    Fix NPE for EqualsAlwaysReturnsTrueOrFalse
    Fixes an exception that occurs when the equals method contains no
    dedicated return expression.
    Closes #2103

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/EqualsAlwaysReturnsTrueOrFalseSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/EqualsAlwaysReturnsTrueOrFalseSpec.kt
@@ -23,14 +23,26 @@ class EqualsAlwaysReturnsTrueOrFalseSpec : Spek({
 
         it("detects and doesn't crash when return expression is annotated - #2021") {
             val code = """
-            class C {
-                override fun equals(other: Any?): Boolean {
-                    @Suppress("UnsafeCallOnNullableType")
-                    return true
+                class C {
+                    override fun equals(other: Any?): Boolean {
+                        @Suppress("UnsafeCallOnNullableType")
+                        return true
+                    }
                 }
-            }
             """
             assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("detects and doesn't crash when the equals method contains no return expression - #2103") {
+            val code = """
+                open class SuperClass
+                
+                data class Item(val text: String) : SuperClass() {
+                    override fun equals(other: Any?): Boolean = (other as? Item)?.text == this.text
+                    override fun hashCode(): Int = text.hashCode()
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
 })
