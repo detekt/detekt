c590af9b32ed1d2fc129e43e92bb9cc932e47361
Matthew Haughton <3flex@users.noreply.github.com>
2019-11-26 17:52:11 +1100

    Kotlin 1.3.60 with fix for "Unable to load JNA library" warning (#2129)
    * Update KotlinEnvironmentUtils.kt
    
    * Kotlin 1.3.60
    
    * Kotlin 1.3.60
    
    * Update KotlinEnvironmentUtils.kt

--- detekt-gradle-plugin/build.gradle.kts
+++ detekt-gradle-plugin/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     `maven-publish`
     id("com.gradle.plugin-publish") version "0.10.1"
     id("com.jfrog.bintray") version "1.8.4"
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.60"
     id("org.jetbrains.dokka") version "0.10.0"
     id("com.github.ben-manes.versions") version "0.27.0"
     id("io.gitlab.arturbosch.detekt") version "1.2.0"
