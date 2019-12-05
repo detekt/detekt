ba7aa049effbeaf46500767a55b798e59a3e0403
Brais Gabín <braisgabin@gmail.com>
2019-12-05 08:05:31 +0100

    Create UnnecessaryAnnotationUseSiteTargetRule (#2099)
    * Create UnnecessaryAnnotationUseSiteTargetRule
    
    * Add code to allow to escape `@property`
    
    * Fix exception comment to no show doble point at the end
    
    * Create documentation

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/collection/RuleVisitor.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/collection/RuleVisitor.kt
@@ -78,7 +78,7 @@ internal class RuleVisitor : DetektVisitor() {
         active = classOrObject.kDocSection()?.findTagByName(TAG_ACTIVE) != null
         autoCorrect = classOrObject.kDocSection()?.findTagByName(TAG_AUTO_CORRECT) != null
 
-        val comment = classOrObject.kDocSection()?.getContent()?.trim() ?: return
+        val comment = classOrObject.kDocSection()?.getContent()?.trim()?.replace("@@", "@") ?: return
         extractRuleDocumentation(comment)
         configuration.addAll(classOrObject.parseConfigurationTags())
     }
@@ -92,7 +92,7 @@ internal class RuleVisitor : DetektVisitor() {
                 extractCompliantDocumentation(comment, compliantIndex)
             }
             compliantIndex != -1 -> throw InvalidCodeExampleDocumentationException(
-                    "Rule $name contains a compliant without a noncompliant code definition.")
+                    "Rule $name contains a compliant without a noncompliant code definition")
             else -> description = comment
         }
     }
@@ -101,7 +101,7 @@ internal class RuleVisitor : DetektVisitor() {
         val nonCompliantEndIndex = comment.indexOf(ENDTAG_NONCOMPLIANT)
         if (nonCompliantEndIndex == -1) {
             throw InvalidCodeExampleDocumentationException(
-                    "Rule $name contains an incorrect noncompliant code definition.")
+                    "Rule $name contains an incorrect noncompliant code definition")
         }
         description = comment.substring(0, nonCompliantIndex).trim()
         nonCompliant = comment.substring(nonCompliantIndex + TAG_NONCOMPLIANT.length, nonCompliantEndIndex)
@@ -114,7 +114,7 @@ internal class RuleVisitor : DetektVisitor() {
         if (compliantIndex != -1) {
             if (compliantEndIndex == -1) {
                 throw InvalidCodeExampleDocumentationException(
-                        "Rule $name contains an incorrect compliant code definition.")
+                        "Rule $name contains an incorrect compliant code definition")
             }
             compliant = comment.substring(compliantIndex + TAG_COMPLIANT.length, compliantEndIndex)
                     .trimStartingLineBreaks()
