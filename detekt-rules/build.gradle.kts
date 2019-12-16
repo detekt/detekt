e81eb1289bbb66d5102a156fb897ac4f9453fe36
Alexandre Abita <aabita@bloomberg.net>
2019-12-16 12:18:44 +0000

    Remove coroutines dependency from gradle

--- detekt-rules/build.gradle.kts
+++ detekt-rules/build.gradle.kts
@@ -5,7 +5,6 @@ tasks.build { finalizedBy(":detekt-generator:generateDocumentation") }
 val junitPlatformVersion: String by project
 val spekVersion: String by project
 val reflectionsVersion: String by project
-val coroutinesVersion: String by project
 
 dependencies {
     implementation(project(":detekt-api"))
@@ -13,7 +12,6 @@ dependencies {
     testImplementation("org.reflections:reflections:$reflectionsVersion")
     testImplementation(project(":detekt-test"))
     testImplementation(kotlin("reflect"))
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:$junitPlatformVersion")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
 }
