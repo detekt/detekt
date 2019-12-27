323a0cfc84be879b49acca58cc726c3c6be11dd3
M Schalk <30376729+schalkms@users.noreply.github.com>
2019-12-27 14:49:53 +0100

    Use shorthand syntax for assertThat() (#2196)

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/BuildFailureSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/BuildFailureSpec.kt
@@ -1,19 +1,19 @@
 package io.gitlab.arturbosch.detekt.cli
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 class BuildFailureSpec : Spek({
 
     describe("isValidAndSmallerOrEqual extension function tests") {
-        Assertions.assertThat(0.isValidAndSmallerOrEqual(0)).isEqualTo(false)
-        Assertions.assertThat((-1).isValidAndSmallerOrEqual(0)).isEqualTo(false)
-        Assertions.assertThat(1.isValidAndSmallerOrEqual(0)).isEqualTo(false)
-        Assertions.assertThat(1.isValidAndSmallerOrEqual(1)).isEqualTo(true)
-        Assertions.assertThat(1.isValidAndSmallerOrEqual(2)).isEqualTo(true)
-        Assertions.assertThat(12.isValidAndSmallerOrEqual(11)).isEqualTo(false)
-        Assertions.assertThat(12.isValidAndSmallerOrEqual(12)).isEqualTo(true)
-        Assertions.assertThat(12.isValidAndSmallerOrEqual(13)).isEqualTo(true)
+        assertThat(0.isValidAndSmallerOrEqual(0)).isEqualTo(false)
+        assertThat((-1).isValidAndSmallerOrEqual(0)).isEqualTo(false)
+        assertThat(1.isValidAndSmallerOrEqual(0)).isEqualTo(false)
+        assertThat(1.isValidAndSmallerOrEqual(1)).isEqualTo(true)
+        assertThat(1.isValidAndSmallerOrEqual(2)).isEqualTo(true)
+        assertThat(12.isValidAndSmallerOrEqual(11)).isEqualTo(false)
+        assertThat(12.isValidAndSmallerOrEqual(12)).isEqualTo(true)
+        assertThat(12.isValidAndSmallerOrEqual(13)).isEqualTo(true)
     }
 })
