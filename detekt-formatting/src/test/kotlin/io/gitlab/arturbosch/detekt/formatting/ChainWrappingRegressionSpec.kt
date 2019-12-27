9d18cf69aedd9d1bf9dd9e4b3027c73fbddcdff8
schalkms <schalkms@icloud.com>
2019-12-27 10:36:55 +0100

    Use shorthand syntax for assertThat()

--- detekt-formatting/src/test/kotlin/io/gitlab/arturbosch/detekt/formatting/ChainWrappingRegressionSpec.kt
+++ detekt-formatting/src/test/kotlin/io/gitlab/arturbosch/detekt/formatting/ChainWrappingRegressionSpec.kt
@@ -3,7 +3,7 @@ package io.gitlab.arturbosch.detekt.formatting
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.formatting.wrappers.ChainWrapping
 import io.gitlab.arturbosch.detekt.test.format
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
@@ -16,8 +16,8 @@ class ChainWrappingRegressionSpec : Spek({
 
             val findings = ChainWrapping(Config.empty).format(subject.text)
 
-            Assertions.assertThat(findings).isNotEmpty()
-            Assertions.assertThat(subject.text).isEqualTo(expected)
+            assertThat(findings).isNotEmpty()
+            assertThat(subject.text).isEqualTo(expected)
         }
     }
 })
