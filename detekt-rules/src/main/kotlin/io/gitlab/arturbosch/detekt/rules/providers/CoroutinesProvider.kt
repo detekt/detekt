1f03d2902c9ecdd2be47149105f52be7a6f9042c
Alexandre Abita <aabita@bloomberg.net>
2020-01-02 09:44:20 +0100

    Correct coroutines provider comments

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/CoroutinesProvider.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/providers/CoroutinesProvider.kt
@@ -5,9 +5,10 @@ import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 import io.gitlab.arturbosch.detekt.rules.coroutines.GlobalScopeUsage
 
-/** The coroutines rule set analyzes code for potential coroutines problems.
+/**
+ * The coroutines rule set analyzes code for potential coroutines problems.
  *
- * @active since v1.3.0
+ * @active since v1.4.0
  */
 class CoroutinesProvider : RuleSetProvider {
     override val ruleSetId: String = "coroutines"
