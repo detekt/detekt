8410a850db01af7060afa95dcf3693c48c533a69
schalkms <schalkms@icloud.com>
2020-01-11 13:32:37 +0100

    Refactor duplicated condition in isMainFunction()
    This commit also fixes a codefactor warning.

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/MethodSignature.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/MethodSignature.kt
@@ -14,18 +14,16 @@ private val knownAnys = setOf("Any?", "kotlin.Any?")
 fun KtFunction.hasCorrectEqualsParameter() =
         this.valueParameters.firstOrNull()?.typeReference?.text in knownAnys
 
-fun KtNamedFunction.isMainFunction() =
-    (this.isTopLevelMain() || this.isMainInsideObject()) && this.hasMainParameter()
+fun KtNamedFunction.isMainFunction() = hasMainSignature() && (this.isTopLevel || isMainInsideObject())
 
-private fun KtNamedFunction.isTopLevelMain() =
-    this.name == "main" && this.isPublicNotOverridden() && this.isTopLevel
+private fun KtNamedFunction.hasMainSignature() =
+    this.name == "main" && this.isPublicNotOverridden() && this.hasMainParameter()
+
+private fun KtNamedFunction.hasMainParameter() =
+    valueParameters.isEmpty() || valueParameters.size == 1 && valueParameters[0].typeReference?.text == "Array<String>"
 
 private fun KtNamedFunction.isMainInsideObject() =
         this.name == "main" &&
                 this.isPublicNotOverridden() &&
                 this.parent?.parent is KtObjectDeclaration &&
                 this.hasAnnotation("JvmStatic", "kotlin.jvm.JvmStatic")
-
-private fun KtNamedFunction.hasMainParameter() =
-    valueParameters.isEmpty() ||
-            (valueParameters.size == 1 && valueParameters[0].typeReference?.text == "Array<String>")
