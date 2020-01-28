52d0f7c484401a1c01bdd2de473cc47ab46ef59b
Artur Bosch <arturbosch@gmx.de>
2020-01-28 18:50:39 +0100

    Use 1.5.0 for self analysis

--- detekt-gradle-plugin/build.gradle.kts
+++ detekt-gradle-plugin/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     kotlin("jvm") version "1.3.61"
     id("org.jetbrains.dokka") version "0.10.0"
     id("com.github.ben-manes.versions") version "0.27.0"
-    id("io.gitlab.arturbosch.detekt") version "1.4.0"
+    id("io.gitlab.arturbosch.detekt") version "1.5.0"
 }
 
 group = "io.gitlab.arturbosch.detekt"
