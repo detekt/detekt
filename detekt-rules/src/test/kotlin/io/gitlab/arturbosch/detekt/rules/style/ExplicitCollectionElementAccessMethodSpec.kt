8710f993f609e95ca0b44676e47b2327ba96258d
M Schalk <30376729+schalkms@users.noreply.github.com>
2020-02-03 07:40:52 +0100

    Fix ExplicitCollectionElementAccessMethod crash (#2302)
    * Fix ExplicitCollectionElementAccessMethod crash
    
    The rule shouldn't crash for getter and setter that have no parent expression.
    Fixes #2301
    
    * Add a test case for getters
    
    * Fix test snippet to be compilable

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ExplicitCollectionElementAccessMethodSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ExplicitCollectionElementAccessMethodSpec.kt
@@ -207,4 +207,16 @@ class ExplicitCollectionElementAccessMethodSpec : Spek({
         }
     }
 
+    describe("getters") {
+
+        it("does not crash for getter") {
+            val code = """
+                class A {
+                    val i: Int get() = 1 + 2
+                    val c: Char? get() = "".first() ?: throw IllegalArgumentException("getter")
+                }
+            """
+            assertThat(subject.compileAndLintWithContext(wrapper.env, code)).isEmpty()
+        }
+    }
 })
