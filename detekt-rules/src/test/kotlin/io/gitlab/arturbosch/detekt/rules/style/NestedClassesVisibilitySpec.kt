80019f835ce082688c77afc64345a170900d2d38
schalkms <schalkms@icloud.com>
2020-02-16 12:14:28 +0100

    Inline test cases of NestedClassesVisibility

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/NestedClassesVisibilitySpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/NestedClassesVisibilitySpec.kt
@@ -1,7 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
-import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.test.lint
+import io.gitlab.arturbosch.detekt.test.compileAndLint
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -11,12 +10,102 @@ class NestedClassesVisibilitySpec : Spek({
 
     describe("NestedClassesVisibility rule") {
 
-        it("reports public nested classes") {
-            assertThat(subject.lint(Case.NestedClassVisibilityPositive.path())).hasSize(4)
+        it("reports explicit public visibility in nested objects/classes/interfaces") {
+            val code = """
+                internal class Outer {
+                    public interface A
+                    public object B
+                    public class C
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(3)
         }
 
-        it("does not report internal and (package) private nested classes") {
-            assertThat(subject.lint(Case.NestedClassVisibilityNegative.path())).isEmpty()
+        it("reports explicit public visibility in nested classes inside an enum") {
+            val code = """
+                internal enum class Enum {
+                    A;
+                    public class C
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("does not report nested internal classes and interfaces") {
+            val code = """
+                internal class Outer {
+                     class A
+                     internal class B
+                     enum class Enum { One }
+                     internal interface I
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report nested private classes") {
+            val code = """
+                internal class Outer {
+                    private class A
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report nested public enums") {
+            val code = """
+                internal class Outer {
+                    public enum class Enum { One }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report companion object that is explicitly public") {
+            val code = """
+                internal class Outer {
+                    public companion object C
+                } 
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report companion object") {
+            val code = """
+                internal class Outer {
+                    companion object C
+                } 
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report nested classes inside a private class") {
+            val code = """
+                private class Outer {
+                     class A
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report nested internal classes inside an interface") {
+            val code = """
+                internal interface Outer {
+                     class A
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report nested classes with a nesting depth higher than 1") {
+            val code = """
+                internal class Outer {
+                    class C1 {
+                        public class C2
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
 })
