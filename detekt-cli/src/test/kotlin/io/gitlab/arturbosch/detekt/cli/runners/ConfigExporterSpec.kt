302f4708d469212df04fe1051f46d96aadd2f607
schalkms <schalkms@icloud.com>
2020-03-01 22:05:45 +0100

    Add test cases for cli/runners package

---
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/runners/ConfigExporterSpec.kt
@@ -0,0 +1,24 @@
+package io.gitlab.arturbosch.detekt.cli.runners
+
+import io.gitlab.arturbosch.detekt.cli.CliArgs
+import org.assertj.core.api.Assertions.assertThat
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import java.nio.file.Files
+
+class ConfigExporterSpec : Spek({
+
+    describe("config exporter") {
+
+        it("should print the ast as string") {
+            val tmpConfig = Files.createTempFile("ConfigPrinterSpec", ".yml")
+            val cliArgs = CliArgs.parse(arrayOf(
+                "--config", tmpConfig.toString()
+            ))
+
+            ConfigExporter(cliArgs).execute()
+
+            assertThat(Files.readAllLines(tmpConfig)).isNotEmpty
+        }
+    }
+})
