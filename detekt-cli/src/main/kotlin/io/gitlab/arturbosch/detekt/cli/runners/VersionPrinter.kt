d5801553e71c88614faa9c97a2a170b2518660cf
Brais Gabín <braisgabin@gmail.com>
2020-03-02 19:30:49 +0100

    Add --version to cli

---
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/runners/VersionPrinter.kt
@@ -0,0 +1,16 @@
+package io.gitlab.arturbosch.detekt.cli.runners
+
+import io.gitlab.arturbosch.detekt.core.whichDetekt
+import java.io.PrintStream
+
+class VersionPrinter(private val outputPrinter: PrintStream) : Executable {
+
+    override fun execute() {
+        val version = whichDetekt()
+        if (version != null) {
+            outputPrinter.println(version)
+        } else {
+            throw IllegalStateException("Can't find the detekt version")
+        }
+    }
+}
