b9de4898d10746d0e0291b32e828ad5ba66661ce
Brais Gabín <braisgabin@gmail.com>
2020-03-09 23:08:32 +0100

    Add more tests to Main

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/MainSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/MainSpec.kt
@@ -5,6 +5,7 @@ import io.gitlab.arturbosch.detekt.cli.runners.ConfigExporter
 import io.gitlab.arturbosch.detekt.cli.runners.Runner
 import io.gitlab.arturbosch.detekt.cli.runners.SingleRuleRunner
 import io.gitlab.arturbosch.detekt.cli.runners.VersionPrinter
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -42,6 +43,39 @@ class MainSpec : Spek({
                     }
                 }
             }
+        }
+    }
+
+    describe("check arguments") {
+
+        it("fails with --create-baseline but without --baseline") {
+            val out = ByteArrayOutputStream()
+            val err = ByteArrayOutputStream()
+
+            try {
+                val args = arrayOf("--create-baseline")
+
+                buildRunner(args, PrintStream(out), PrintStream(err))
+                Assertions.fail("This should throw an exception.")
+            } catch (_: HandledArgumentViolation) {
+                assertThat(String(err.toByteArray()).trim())
+                    .isEqualTo("Creating a baseline.xml requires the --baseline parameter to specify a path.")
+            }
+        }
+
+        it("succeed --create-baseline and --baseline") {
+            val out = ByteArrayOutputStream()
+            val err = ByteArrayOutputStream()
+
+            val args = arrayOf(
+                "--create-baseline",
+                "--baseline",
+                "baseline.xml"
+            )
+
+            buildRunner(args, PrintStream(out), PrintStream(err))
+
+            assertThat(String(err.toByteArray())).isEmpty()
         }
     }
 })
