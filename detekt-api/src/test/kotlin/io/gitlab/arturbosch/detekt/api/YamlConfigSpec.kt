ac0fbed235e5bcd54787ca6f40d848acf12b890e
schalkms <schalkms@icloud.com>
2020-03-09 20:25:23 +0100

    Add tests for YamlConfig
    subsequent task of #2417

---
+++ detekt-api/src/test/kotlin/io/gitlab/arturbosch/detekt/api/YamlConfigSpec.kt
@@ -0,0 +1,40 @@
+package io.gitlab.arturbosch.detekt.api
+
+import io.gitlab.arturbosch.detekt.test.resource
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import java.nio.file.Paths
+
+class YamlConfigSpec : Spek({
+
+    describe("yaml config loader") {
+
+        it("loads the config from a given yaml file") {
+            val path = Paths.get(resource("detekt.yml"))
+            val config = YamlConfig.load(path)
+            assertThat(config).isNotNull
+        }
+
+        it("loads the config from a given text file") {
+            val path = Paths.get(resource("detekt.txt"))
+            val config = YamlConfig.load(path)
+            assertThat(config).isNotNull
+        }
+
+        it("throws an exception on an non-existing file") {
+            val path = Paths.get(resource("/pathFilters/").toString() + "doesNotExist.yml")
+            assertThatIllegalArgumentException()
+                .isThrownBy { YamlConfig.load(path) }
+                .withMessageStartingWith("Configuration does not exist")
+        }
+
+        it("throws an exception on a directory") {
+            val path = Paths.get(resource("/config_validation"))
+            assertThatIllegalArgumentException()
+                .isThrownBy { YamlConfig.load(path) }
+                .withMessageStartingWith("Configuration must be a file")
+        }
+    }
+})
