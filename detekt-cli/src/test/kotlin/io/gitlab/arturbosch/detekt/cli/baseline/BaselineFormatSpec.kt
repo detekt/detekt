978ce2374f71e5e6a7e584b6fee811337424fcab
schalkms <schalkms@icloud.com>
2020-03-18 23:18:28 +0100

    Refactor BaselineFormatSpec.kt

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/BaselineFormatSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/BaselineFormatSpec.kt
@@ -13,50 +13,51 @@ class BaselineFormatSpec : Spek({
 
     describe("baseline format") {
 
-        it("loadBaseline") {
-            val path = Paths.get(resource("/smell-baseline.xml"))
-            val (blacklist, whitelist) = BaselineFormat().read(path)
+        context("read a baseline file") {
 
-            assertThat(blacklist.ids).hasSize(2)
-            assertThat(blacklist.ids).anySatisfy { it.startsWith("LongParameterList") }
-            assertThat(blacklist.ids).anySatisfy { it.startsWith("LongMethod") }
-            assertThat(whitelist.ids).hasSize(1)
-            assertThat(whitelist.ids).anySatisfy { it.startsWith("FeatureEnvy") }
+            it("loads the baseline file") {
+                val path = Paths.get(resource("/smell-baseline.xml"))
+                val (blacklist, whitelist) = BaselineFormat().read(path)
+
+                assertThat(blacklist.ids).hasSize(2)
+                assertThat(blacklist.ids).anySatisfy { it.startsWith("LongParameterList") }
+                assertThat(blacklist.ids).anySatisfy { it.startsWith("LongMethod") }
+                assertThat(whitelist.ids).hasSize(1)
+                assertThat(whitelist.ids).anySatisfy { it.startsWith("FeatureEnvy") }
+            }
+
+            it("throws on an invalid baseline file") {
+                val path = Paths.get(resource("/invalid-smell-baseline.txt"))
+                assertThatThrownBy { BaselineFormat().read(path) }.isInstanceOf(InvalidBaselineState::class.java)
+            }
         }
 
-        it("savedAndLoadedXmlAreEqual") {
-            val now = Instant.now().toEpochMilli().toString()
-            val tempFile = Files.createTempFile("baseline", now)
-
-            val savedBaseline = Baseline(
-                    Blacklist(setOf("4", "2", "2")),
-                    Whitelist(setOf("1", "2", "3")))
-
-            val format = BaselineFormat()
-            format.write(savedBaseline, tempFile)
-            val loadedBaseline = format.read(tempFile)
-
-            assertThat(loadedBaseline).isEqualTo(savedBaseline)
-        }
-
-        it("loadInvalidBaseline") {
-            val path = Paths.get(resource("/invalid-smell-baseline.txt"))
-            assertThatThrownBy { BaselineFormat().read(path) }.isInstanceOf(InvalidBaselineState::class.java)
-        }
-
-        it("newlineAtTheEndOfFile") {
-            val now = Instant.now().toEpochMilli().toString()
-            val tempFile = Files.createTempFile("baseline", now)
+        context("writes a baseline file") {
 
             val savedBaseline = Baseline(
                 Blacklist(setOf("4", "2", "2")),
                 Whitelist(setOf("1", "2", "3")))
 
-            val format = BaselineFormat()
-            format.write(savedBaseline, tempFile)
-            val bytes = Files.readAllBytes(tempFile)
-            val content = String(bytes, Charsets.UTF_8)
-            assertThat(content).endsWith(">\n")
+            it("has a new line at the end of the written baseline file") {
+                val tempFile = Files.createTempFile("baseline1", ".xml")
+
+                val format = BaselineFormat()
+                format.write(savedBaseline, tempFile)
+                val bytes = Files.readAllBytes(tempFile)
+                val content = String(bytes, Charsets.UTF_8)
+
+                assertThat(content).endsWith(">\n")
+            }
+
+            it("asserts that the saved and loaded baseline files are equal") {
+                val tempFile = Files.createTempFile("baseline-saved", ".xml")
+
+                val format = BaselineFormat()
+                format.write(savedBaseline, tempFile)
+                val loadedBaseline = format.read(tempFile)
+
+                assertThat(loadedBaseline).isEqualTo(savedBaseline)
+            }
         }
     }
 })
