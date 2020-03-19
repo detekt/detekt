ff837a9ba0b606b59512bd250214b92ca4df7b85
schalkms <schalkms@icloud.com>
2020-03-19 17:52:03 +0100

    Remove default implementations of toString()
    * Blacklist.kt
    * Whitelist.kt

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/Whitelist.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/baseline/Whitelist.kt
@@ -2,9 +2,4 @@ package io.gitlab.arturbosch.detekt.cli.baseline
 
 data class Whitelist(
     override val ids: Set<String>
-) : Listing<Whitelist> {
-
-    override fun toString(): String {
-        return "Blacklist(ids=$ids)"
-    }
-}
+) : Listing<Whitelist>
