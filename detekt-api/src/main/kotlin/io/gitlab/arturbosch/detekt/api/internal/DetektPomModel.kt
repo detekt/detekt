10eac1a6b43e9ca2d5f84dd4bc77af7be41acd85
Artur Bosch <arturbosch@gmx.de>
2020-03-21 20:49:43 +0100

    Synchronize use of root extension point - Closes #2433

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/DetektPomModel.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/DetektPomModel.kt
@@ -16,15 +16,16 @@ import sun.reflect.ReflectionFactory
  * Adapted from https://github.com/pinterest/ktlint/blob/master/ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/KtLint.kt
  * Licenced under the MIT licence - https://github.com/pinterest/ktlint/blob/master/LICENSE
  */
-@Suppress("UnsafeCallOnNullableType")
 class DetektPomModel(project: Project) : UserDataHolderBase(), PomModel {
 
     init {
-        val extensionPoint = "org.jetbrains.kotlin.com.intellij.treeCopyHandler"
-        val extensionClassName = TreeCopyHandler::class.java.name!!
-        arrayOf(project.extensionArea, Extensions.getRootArea())
-            .filter { !it.hasExtensionPoint(extensionPoint) }
-            .forEach { it.registerExtensionPoint(extensionPoint, extensionClassName, ExtensionPoint.Kind.INTERFACE) }
+        val extension = "org.jetbrains.kotlin.com.intellij.treeCopyHandler"
+        val extensionClass = TreeCopyHandler::class.java.name
+        synchronized(Extensions.getRootArea()) {
+            arrayOf(project.extensionArea, Extensions.getRootArea())
+                .filter { !it.hasExtensionPoint(extension) }
+                .forEach { it.registerExtensionPoint(extension, extensionClass, ExtensionPoint.Kind.INTERFACE) }
+        }
     }
 
     override fun runTransaction(transaction: PomTransaction) {
