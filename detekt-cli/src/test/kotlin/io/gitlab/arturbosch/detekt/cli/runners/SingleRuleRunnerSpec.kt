14f02e33e7d2af5648866f687e7c1a433029dcd9
Brais Gabín <braisgabin@gmail.com>
2020-03-22 16:44:07 +0100

    Send the PrintStreams to the ProcessingSettings in SingleRuleRunner

--- detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/runners/SingleRuleRunnerSpec.kt
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/runners/SingleRuleRunnerSpec.kt
@@ -1,6 +1,7 @@
 package io.gitlab.arturbosch.detekt.cli.runners
 
 import io.gitlab.arturbosch.detekt.cli.createCliArgs
+import io.gitlab.arturbosch.detekt.test.NullPrintStream
 import io.gitlab.arturbosch.detekt.test.resource
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
@@ -24,26 +25,25 @@ class SingleRuleRunnerSpec : Spek({
                 "--run-rule", "test:test"
             )
 
-            SingleRuleRunner(args).execute()
+            SingleRuleRunner(args, NullPrintStream(), NullPrintStream()).execute()
 
             assertThat(Files.readAllLines(tmp)).hasSize(1)
         }
 
         it("should throw on non existing rule") {
             val args = createCliArgs("--run-rule", "test:non_existing")
-            assertThatThrownBy { SingleRuleRunner(args).execute() }
+            assertThatThrownBy { SingleRuleRunner(args, NullPrintStream(), NullPrintStream()).execute() }
                 .isExactlyInstanceOf(IllegalArgumentException::class.java)
         }
 
         it("should throw on non existing rule set") {
             val args = createCliArgs("--run-rule", "non_existing:test")
-            assertThatThrownBy { SingleRuleRunner(args).execute() }
-                .isExactlyInstanceOf(IllegalArgumentException::class.java)
+            assertThatThrownBy { SingleRuleRunner(args, NullPrintStream(), NullPrintStream()).execute() }
         }
 
         it("should throw on non existing run-rule") {
             val args = createCliArgs()
-            assertThatThrownBy { SingleRuleRunner(args).execute() }
+            assertThatThrownBy { SingleRuleRunner(args, NullPrintStream(), NullPrintStream()).execute() }
                 .isExactlyInstanceOf(IllegalStateException::class.java)
                 .withFailMessage("Unexpected empty 'runRule' argument.")
         }
