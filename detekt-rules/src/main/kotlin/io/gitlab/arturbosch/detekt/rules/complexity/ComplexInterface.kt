2e40e53c71a0ce1f33d2b026262f234138086225
Nicola Corti <corti.nico@gmail.com>
2020-03-22 10:17:49 +0100

    Extend ComplexInterface to support exclusion of private member/functions (#2478)
    * Extend ComplexInterface to support exclusion of private member/functions
    
    Fixes #2446
    
    * Include @schalkms suggestions
    
    * Remove unnecessary parenthesis
    
    * Add a validation exclusion for detekt-gradle-plugin
    
    * Refactor calculateMembers

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/ComplexInterface.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/ComplexInterface.kt
@@ -9,11 +9,14 @@ import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.ThresholdRule
 import io.gitlab.arturbosch.detekt.api.ThresholdedCodeSmell
 import io.gitlab.arturbosch.detekt.rules.companionObject
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtClassBody
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtObjectDeclaration
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtTypeParameterListOwner
+import org.jetbrains.kotlin.psi.psiUtil.isPrivate
 
 /**
  * Complex interfaces which contain too many functions and/or properties indicate that this interface is handling too
@@ -25,6 +28,7 @@ import org.jetbrains.kotlin.psi.KtProperty
  *
  * @configuration threshold - the amount of definitions in an interface to trigger the rule (default: `10`)
  * @configuration includeStaticDeclarations - whether static declarations should be included (default: `false`)
+ * @configuration includePrivateDeclarations - whether private declarations should be included (default: `false`)
  */
 class ComplexInterface(
     config: Config = Config.empty,
@@ -39,6 +43,7 @@ class ComplexInterface(
             Debt.TWENTY_MINS)
 
     private val includeStaticDeclarations = valueOrDefault(INCLUDE_STATIC_DECLARATIONS, false)
+    private val includePrivateDeclarations = valueOrDefault(INCLUDE_PRIVATE_DECLARATIONS, false)
 
     override fun visitClass(klass: KtClass) {
         if (klass.isInterface()) {
@@ -48,10 +53,12 @@ class ComplexInterface(
                 size += countStaticDeclarations(klass.companionObject())
             }
             if (size >= threshold) {
-                report(ThresholdedCodeSmell(issue,
+                report(
+                    ThresholdedCodeSmell(issue,
                         Entity.from(klass),
                         Metric("SIZE: ", size, threshold),
-                        "The interface ${klass.name} is too complex. Consider splitting it up."))
+                        "The interface ${klass.name} is too complex. Consider splitting it up.")
+                )
             }
         }
         super.visitClass(klass)
@@ -62,10 +69,20 @@ class ComplexInterface(
         return if (body != null) calculateMembers(body) else 0
     }
 
-    private fun calculateMembers(body: KtClassBody) = body.children.count { it is KtNamedFunction || it is KtProperty }
+    private fun calculateMembers(body: KtClassBody): Int {
+        fun PsiElement.considerPrivate() = includePrivateDeclarations ||
+                this is KtTypeParameterListOwner && !this.isPrivate()
+
+        fun PsiElement.isMember() = this is KtNamedFunction || this is KtProperty
+
+        return body.children
+            .filter(PsiElement::considerPrivate)
+            .count(PsiElement::isMember)
+    }
 
     companion object {
         const val INCLUDE_STATIC_DECLARATIONS = "includeStaticDeclarations"
+        const val INCLUDE_PRIVATE_DECLARATIONS = "includePrivateDeclarations"
         const val DEFAULT_LARGE_INTERFACE_COUNT = 10
     }
 }
