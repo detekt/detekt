ccfc2ac1a7bef0cc3cf91c329381470fb7a9cf99
Brais Gabín <braisgabin@gmail.com>
2020-03-22 19:13:23 +0100

    Don't use println :detekt-generator (#2488)
    * Send the printers to the ProcessingSettings in :detet-generator
    
    * user outPrinter.println instead of println

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/Runner.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/Runner.kt
@@ -4,16 +4,23 @@ import io.gitlab.arturbosch.detekt.core.KtTreeCompiler
 import io.gitlab.arturbosch.detekt.core.ProcessingSettings
 import io.gitlab.arturbosch.detekt.generator.collection.DetektCollector
 import io.gitlab.arturbosch.detekt.generator.printer.DetektPrinter
+import java.io.PrintStream
 import java.nio.file.Path
 import kotlin.system.measureTimeMillis
 
-class Runner(private val arguments: GeneratorArgs) {
+class Runner(
+    private val arguments: GeneratorArgs,
+    private val outPrinter: PrintStream,
+    private val errPrinter: PrintStream
+) {
     private val listeners = listOf(DetektProgressListener())
     private val collector = DetektCollector()
     private val printer = DetektPrinter(arguments)
 
-    private fun createCompiler(path: Path) =
-        KtTreeCompiler.instance(ProcessingSettings(path))
+    private fun createCompiler(path: Path) = KtTreeCompiler.instance(ProcessingSettings(
+        path,
+        outPrinter = outPrinter,
+        errorPrinter = errPrinter))
 
     fun execute() {
         val time = measureTimeMillis {
@@ -29,6 +36,6 @@ class Runner(private val arguments: GeneratorArgs) {
             printer.print(collector.items)
         }
 
-        println("\nGenerated all detekt documentation in $time ms.")
+        outPrinter.println("\nGenerated all detekt documentation in $time ms.")
     }
 }
