f926c956bdbb74c774305ccfa393d7afe60bc9a9
Artur Bosch <arturbosch@gmx.de>
2020-03-24 18:40:00 +0100

    Add LicenceHeaderExtension test to verify resource path loading is supported - #2503

---
+++ detekt-cli/src/test/kotlin/io/gitlab/arturbosch/detekt/cli/ExtensionsSpec.kt
@@ -0,0 +1,30 @@
+package io.gitlab.arturbosch.detekt.cli
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.SetupContext
+import io.gitlab.arturbosch.detekt.api.UnstableApi
+import io.gitlab.arturbosch.detekt.rules.documentation.LicenceHeaderLoaderExtension
+import org.assertj.core.api.Assertions.assertThatCode
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import java.net.URI
+
+@UnstableApi
+class ExtensionsSpec : Spek({
+
+    describe("Licence header extension - #2503") {
+
+        it("should not crash when using resources") {
+            assertThatCode {
+                val uris = CliArgs {
+                    configResource = "extensions/config.yml"
+                }.extractUris()
+
+                LicenceHeaderLoaderExtension().init(object : SetupContext {
+                    override val configUris: Collection<URI> = uris
+                    override val config: Config = Config.empty
+                })
+            }.doesNotThrowAnyException()
+        }
+    }
+})
