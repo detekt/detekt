30f0441c7fa06f36e9e20aae9c15932f60ac5978
Nicola Corti <corti.nico@gmail.com>
2020-03-28 09:10:00 +0100

    Update Gradle plugin-publish to 0.11.0 (#2537)

--- detekt-gradle-plugin/build.gradle.kts
+++ detekt-gradle-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "0.10.1"
+    id("com.gradle.plugin-publish") version "0.11.0"
 }
 
 dependencies {
