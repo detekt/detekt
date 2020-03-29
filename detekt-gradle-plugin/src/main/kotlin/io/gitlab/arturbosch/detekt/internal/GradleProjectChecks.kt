d1e7b4b4e93fe82a2c37704691af4357c0535b5e
Sebastiano Poggi <poggos@gmail.com>
2020-03-29 13:54:31 +0200

    Only check for jCenter in afterEvaluate { }
    This is necessary because we need to make sure it's there _somehow_,
    and it may be added by other plugins, or blocks evaluated after this
    plugin is applied.

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/GradleProjectChecks.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/GradleProjectChecks.kt
@@ -5,18 +5,20 @@ import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 import org.gradle.api.internal.artifacts.dsl.DefaultRepositoryHandler
 import java.net.URI
 
+private val bintrayJCenterUri = URI.create(DefaultRepositoryHandler.BINTRAY_JCENTER_URL)
+
 internal fun checkRequiredRepositoriesAreConfiguredOn(project: Project) {
-    val bintrayJcenterUri = URI.create(DefaultRepositoryHandler.BINTRAY_JCENTER_URL)
+    project.afterEvaluate {
+        val missingJCenter = project.repositories.none {
+            it is MavenArtifactRepository && it.url == bintrayJCenterUri
+        }
 
-    val missingJCenter = project.repositories.none {
-        it is MavenArtifactRepository && it.url == bintrayJcenterUri
-    }
-
-    if (missingJCenter) {
-        project.logger.error(
-            "The project ${project.path} doesn't have the jcenter() repository in its repositories list; " +
-                    "this is required for Detekt to work correctly. Please add jcenter() to the project's " +
-                    "repositories { } closure and try again."
-        )
+        if (missingJCenter) {
+            project.logger.error(
+                "The project ${project.path} doesn't have the jcenter() repository in its repositories list; " +
+                        "this is required for Detekt to work correctly. Please add jcenter() to the project's " +
+                        "repositories { } closure and try again."
+            )
+        }
     }
 }
