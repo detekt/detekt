7a371e7e158fc116ebba48a6a4016979f1e2b82a
Toshiaki Kameyama <kmymtsak@gmail.com>
2020-03-30 19:46:08 +0900

    Fix

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtValueArgument.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtValueArgument.kt
@@ -2,18 +2,20 @@ package io.gitlab.arturbosch.detekt.rules
 
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.KtValueArgument
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
 
 fun KtValueArgument.isString(bindingContext: BindingContext): Boolean {
+    val argumentExpression = getArgumentExpression()
     return if (bindingContext != BindingContext.EMPTY) {
         val descriptor =
-            getArgumentExpression()?.getResolvedCall(bindingContext)?.resultingDescriptor as? ValueParameterDescriptor
+            argumentExpression?.getResolvedCall(bindingContext)?.resultingDescriptor as? ValueParameterDescriptor
         val type = descriptor?.type
         type != null && KotlinBuiltIns.isString(type)
     } else {
-        true
+        argumentExpression is KtStringTemplateExpression
     }
 }
 
