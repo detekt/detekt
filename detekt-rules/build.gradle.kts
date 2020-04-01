306af87485df68e9e79d54e62d23e72afe38012a
Zac Sweers <zsweers@slack-corp.com>
2020-04-01 19:59:38 -0400

    Add inject

--- detekt-rules/build.gradle.kts
+++ detekt-rules/build.gradle.kts
@@ -4,4 +4,7 @@ dependencies {
     implementation(project(":detekt-api"))
 
     testImplementation(project(":detekt-test"))
+
+    // For @javax.inject.Inject usage in LongParameterListSpec.kt
+    testImplementation("javax.inject:javax.inject:1")
 }
