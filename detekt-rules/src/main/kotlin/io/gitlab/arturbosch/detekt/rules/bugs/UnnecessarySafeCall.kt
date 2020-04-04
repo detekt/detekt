3da526d03d6ad54f035481f576efbfda9a50676c
Nicola Corti <corti.nico@gmail.com>
2020-04-04 02:52:59 +0200

    Update detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnnecessarySafeCall.kt
    Co-Authored-By: M Schalk <30376729+schalkms@users.noreply.github.com>

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnnecessarySafeCall.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnnecessarySafeCall.kt
@@ -15,7 +15,7 @@ import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
 import org.jetbrains.kotlin.resolve.BindingContext
 
 /**
- * Reports Unnecessary Safe call operators that can be removed by the user.
+ * Reports unnecessary safe call operators (`.?`) that can be removed by the user.
  *
  * <noncompliant>
  * fun foo(s: String) {
