ea6c8e9b80f7df59813cb94e46a69fd15ed012fe
Artur Bosch <arturbosch@gmx.de>
2020-04-28 14:15:33 +0200

    Create new metrics module

---
+++ detekt-metrics/src/main/kotlin/io/github/detekt/metrics/CognitiveComplexity.kt
@@ -0,0 +1,1 @@
+package io.github.detekt.metrics
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,15 @@
 rootProject.name = "detekt"
 include(
     "detekt-api",
-    "detekt-core",
-    "detekt-rules",
     "detekt-cli",
-    "detekt-test",
-    "detekt-sample-extensions",
-    "detekt-generator",
+    "detekt-core",
     "detekt-formatting",
-    "detekt-gradle-plugin"
+    "detekt-generator",
+    "detekt-gradle-plugin",
+    "detekt-metrics",
+    "detekt-rules",
+    "detekt-sample-extensions",
+    "detekt-test"
 )
 
 pluginManagement {
