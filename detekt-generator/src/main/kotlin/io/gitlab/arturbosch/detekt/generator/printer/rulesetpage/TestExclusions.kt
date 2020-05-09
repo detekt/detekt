49060231b5865587a2694df27fa08192516576b4
schalkms <schalkms@icloud.com>
2020-05-09 15:11:10 +0200

    Include Kotlin multiplatform test folders to default exclude config
    Closes #2608

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/rulesetpage/TestExclusions.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/rulesetpage/TestExclusions.kt
@@ -8,7 +8,8 @@ import io.gitlab.arturbosch.detekt.generator.collection.Rule
  */
 object TestExclusions {
 
-    const val pattern = "['**/test/**', '**/androidTest/**', '**/*.Test.kt', '**/*.Spec.kt', '**/*.Spek.kt']"
+    @Suppress("MaxLineLength")
+    const val pattern = "['**/test/**', '**/androidTest/**', '**/*.Test.kt', '**/*.Spec.kt', '**/*.Spek.kt', '**/commonTest/**', '**/jvmTest/**', '**/jsTest/**', '**/iosTest/**']"
     val ruleSets = setOf("comments")
     val rules = setOf(
         "NamingRules",
