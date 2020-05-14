83657d7f065f27c4141166dd97374ca98c4ffb70
M Schalk <30376729+schalkms@users.noreply.github.com>
2020-05-14 22:54:34 +0200

    Simplify LongParameterList tests (#2688)

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LongParameterListSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LongParameterListSpec.kt
@@ -1,7 +1,5 @@
 package io.gitlab.arturbosch.detekt.rules.complexity
 
-import io.gitlab.arturbosch.detekt.rules.complexity.LongParameterList.Companion.DEFAULT_CONSTRUCTOR_THRESHOLD
-import io.gitlab.arturbosch.detekt.rules.complexity.LongParameterList.Companion.DEFAULT_FUNCTION_THRESHOLD
 import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.compileAndLint
 import org.assertj.core.api.Assertions.assertThat
@@ -10,119 +8,135 @@ import org.spekframework.spek2.style.specification.describe
 
 class LongParameterListSpec : Spek({
 
-    val subject by memoized { LongParameterList() }
+    val defaultThreshold = 2
+    val defaultConfig = TestConfig(mapOf(
+        LongParameterList.FUNCTION_THRESHOLD to defaultThreshold,
+        LongParameterList.CONSTRUCTOR_THRESHOLD to defaultThreshold
+    ))
+
+    val subject by memoized { LongParameterList(defaultConfig) }
 
     describe("LongParameterList rule") {
 
-        val reportMessageForFunction =
-            "The function long(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int) has too many parameters. " +
-                    "The current threshold is set to $DEFAULT_FUNCTION_THRESHOLD."
-        val reportMessageForConstructor =
-            "The constructor(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) has too many parameters. " +
-                    "The current threshold is set to $DEFAULT_CONSTRUCTOR_THRESHOLD."
+        val reportMessageForFunction = "The function long(a: Int, b: Int) has too many parameters. " +
+                "The current threshold is set to $defaultThreshold."
+        val reportMessageForConstructor = "The constructor(a: Int, b: Int) has too many parameters. " +
+                "The current threshold is set to $defaultThreshold."
 
         it("reports too long parameter list") {
-            val code = "fun long(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int) {}"
+            val code = "fun long(a: Int, b: Int) {}"
             val findings = subject.compileAndLint(code)
             assertThat(findings).hasSize(1)
             assertThat(findings.first().message).isEqualTo(reportMessageForFunction)
         }
 
         it("does not report short parameter list") {
-            val code = "fun long(a: Int, b: Int, c: Int, d: Int, e: Int) {}"
+            val code = "fun long(a: Int) {}"
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("reports too long parameter list event for parameters with defaults") {
-            val code = "fun long(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int = 1) {}"
+            val code = "fun long(a: Int, b: Int = 1) {}"
             assertThat(subject.compileAndLint(code)).hasSize(1)
         }
 
         it("does not report long parameter list if parameters with defaults should be ignored") {
             val config = TestConfig(mapOf(LongParameterList.IGNORE_DEFAULT_PARAMETERS to "true"))
             val rule = LongParameterList(config)
-            val code = "fun long(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int = 1, g: Int = 2) {}"
+            val code = "fun long(a: Int, b: Int, c: Int = 2) {}"
             assertThat(rule.compileAndLint(code)).isEmpty()
         }
 
         it("reports too long parameter list for primary constructors") {
-            val code = "class LongCtor(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int)"
+            val code = "class LongCtor(a: Int, b: Int)"
             val findings = subject.compileAndLint(code)
             assertThat(findings).hasSize(1)
             assertThat(findings.first().message).isEqualTo(reportMessageForConstructor)
         }
 
         it("does not report short parameter list for primary constructors") {
-            val code = "class LongCtor(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int)"
+            val code = "class LongCtor(a: Int)"
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("reports too long parameter list for secondary constructors") {
-            val code = "class LongCtor() { constructor(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) : this() }"
+            val code = "class LongCtor() { constructor(a: Int, b: Int) : this() }"
             val findings = subject.compileAndLint(code)
             assertThat(findings).hasSize(1)
             assertThat(findings.first().message).isEqualTo(reportMessageForConstructor)
         }
 
         it("does not report short parameter list for secondary constructors") {
-            val code = "class LongCtor() { constructor(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int) : this() }"
+            val code = "class LongCtor() { constructor(a: Int) : this() }"
             assertThat(subject.compileAndLint(code)).isEmpty()
         }
 
         it("reports long parameter list if custom threshold is set") {
-            val config = TestConfig(mapOf(LongParameterList.CONSTRUCTOR_THRESHOLD to "2"))
+            val config = TestConfig(mapOf(LongParameterList.CONSTRUCTOR_THRESHOLD to "1"))
             val rule = LongParameterList(config)
-            val code = "class LongCtor(a: Int, b: Int, c: Int)"
+            val code = "class LongCtor(a: Int)"
             assertThat(rule.compileAndLint(code)).hasSize(1)
         }
 
         it("does not report long parameter list for constructors of data classes if asked") {
-            val config = TestConfig(mapOf(LongParameterList.IGNORE_DATA_CLASSES to "true"))
+            val config = TestConfig(mapOf(
+                LongParameterList.IGNORE_DATA_CLASSES to "true",
+                LongParameterList.CONSTRUCTOR_THRESHOLD to "1"
+            ))
             val rule = LongParameterList(config)
-            val code = "data class Data(val a: Int, val b: Int, val c: Int, val d: Int, val e: Int, val f: Int, val g: Int)"
+            val code = "data class Data(val a: Int)"
             assertThat(rule.compileAndLint(code)).isEmpty()
         }
 
-        it("does not report long parameter list for constructors if file is annotated with ignored annotation") {
-            val config = TestConfig(mapOf(LongParameterList.IGNORE_ANNOTATED to listOf("javax.annotation.Generated")))
-            val rule = LongParameterList(config)
-            val code = "@file:javax.annotation.Generated class Data(val a: Int, val b: Int, val c: Int, val d: Int, val e: Int, val f: Int, val g: Int)"
-            assertThat(rule.compileAndLint(code)).isEmpty()
-        }
+        describe("constructors and functions with ignored annotations") {
 
-        it("does not report long parameter list for functions if file is annotated with ignored annotation") {
-            val config = TestConfig(mapOf(LongParameterList.IGNORE_ANNOTATED to listOf("javax.annotation.Generated")))
+            val config = TestConfig(mapOf(
+                LongParameterList.IGNORE_ANNOTATED to listOf("javax.annotation.Generated", "kotlin.Deprecated"),
+                LongParameterList.FUNCTION_THRESHOLD to 1,
+                LongParameterList.CONSTRUCTOR_THRESHOLD to 1
+            ))
             val rule = LongParameterList(config)
-            val code = "@file:javax.annotation.Generated class Data { fun foo(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {} }"
-            assertThat(rule.compileAndLint(code)).isEmpty()
-        }
 
-        it("does not report long parameter list for constructors if class is annotated with ignored annotation") {
-            val config = TestConfig(mapOf(LongParameterList.IGNORE_ANNOTATED to listOf("javax.annotation.Generated")))
-            val rule = LongParameterList(config)
-            val code = "@javax.annotation.Generated class Data(val a: Int, val b: Int, val c: Int, val d: Int, val e: Int, val f: Int, val g: Int)"
-            assertThat(rule.compileAndLint(code)).isEmpty()
-        }
+            it("does not report long parameter list for constructors if file is annotated with ignored annotation") {
+                val code = "@file:javax.annotation.Generated class Data(val a: Int)"
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
 
-        it("does not report long parameter list for functions if class is annotated with ignored annotation") {
-            val config = TestConfig(mapOf(LongParameterList.IGNORE_ANNOTATED to listOf("javax.annotation.Generated")))
-            val rule = LongParameterList(config)
-            val code = "@javax.annotation.Generated class Data { fun foo(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {} }"
-            assertThat(rule.compileAndLint(code)).isEmpty()
-        }
+            it("does not report long parameter list for functions if file is annotated with ignored annotation") {
+                val code = """
+                    @file:javax.annotation.Generated 
+                    class Data { 
+                        fun foo(a: Int) {} 
+                    }
+                """
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
 
-        it("does not report long parameter list for constructors if constructor is annotated with ignored annotation") {
-            val config = TestConfig(mapOf(LongParameterList.IGNORE_ANNOTATED to listOf("kotlin.Deprecated")))
-            val rule = LongParameterList(config)
-            val code = "class Data @kotlin.Deprecated(message = \"\") constructor(val a: Int, val b: Int, val c: Int, val d: Int, val e: Int, val f: Int, val g: Int)"
-            assertThat(rule.compileAndLint(code)).isEmpty()
-        }
+            it("does not report long parameter list for constructors if class is annotated with ignored annotation") {
+                val code = "@javax.annotation.Generated class Data(val a: Int)"
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
 
-        it("does not report long parameter list for functions if function is annotated with ignored annotation") {
-            val config = TestConfig(mapOf(LongParameterList.IGNORE_ANNOTATED to listOf("kotlin.Deprecated")))
-            val rule = LongParameterList(config)
-            val code = "class Data { @kotlin.Deprecated(message = \"\") fun foo(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {} }"
-            assertThat(rule.compileAndLint(code)).isEmpty()
+            it("does not report long parameter list for functions if class is annotated with ignored annotation") {
+                val code = """
+                    @javax.annotation.Generated class Data { 
+                        fun foo(a: Int) {} 
+                    }
+                """
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
+
+            it("does not report long parameter list for constructors if constructor is annotated with ignored annotation") {
+                val code = "class Data @kotlin.Deprecated(message = \"\") constructor(val a: Int)"
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
+
+            it("does not report long parameter list for functions if function is annotated with ignored annotation") {
+                val code = """class Data {
+                    @kotlin.Deprecated(message = "") fun foo(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {} }
+                """
+                assertThat(rule.compileAndLint(code)).isEmpty()
+            }
         }
     }
 })
