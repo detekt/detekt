d27090d4306f4853122911edcfc4100d609d2907
M Schalk <30376729+schalkms@users.noreply.github.com>
2020-05-16 00:17:19 +0200

    Inline test cases of CollapsibleIfStatements (#2696)

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/CollapsibleIfStatementsSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/CollapsibleIfStatementsSpec.kt
@@ -1,8 +1,7 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.test.lint
+import io.gitlab.arturbosch.detekt.test.compileAndLint
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -13,13 +12,100 @@ class CollapsibleIfStatementsSpec : Spek({
     describe("CollapsibleIfStatements rule") {
 
         it("reports if statements which can be merged") {
-            val path = Case.CollapsibleIfsPositive.path()
-            assertThat(subject.lint(path)).hasSize(2)
+            val code = """
+                fun f() {
+                    if (true) {
+                        if (1 == 1) {}
+                        // a comment
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
         }
 
-        it("does not report if statements which can't be merged") {
-            val path = Case.CollapsibleIfsNegative.path()
-            assertThat(subject.lint(path)).isEmpty()
+        it("reports nested if statements which can be merged") {
+            val code = """
+                fun f() {
+                    if (true) {
+                        if (1 == 1) {
+                            if (2 == 2) {}
+                        }
+                        println()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("does not report else-if") {
+            val code = """
+                fun f() {
+                    if (true) {}
+                    else if (1 == 1) {
+                        if (true) {}
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report if-else") {
+            val code = """
+                fun f() {
+                    if (true) {
+                        if (1 == 1) {}
+                    } else {}
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report if-elseif-else") {
+            val code = """
+                fun f() {
+                    if (true) {
+                        if (1 == 1) {}
+                    } else if (false) {}
+                    else {}
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report if with statements in the if body") {
+            val code = """
+                fun f() {
+                    if (true) {
+                        if (1 == 1) ;
+                        println()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report nested if-else") {
+            val code = """
+                fun f() {
+                    if (true) {
+                        if (1 == 1) {
+                        } else {}
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("does not report nested if-elseif") {
+            val code = """
+                fun f() {
+                    if (true) {
+                        if (1 == 1) {
+                        } else if (2 == 2) {}
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
 })
