50aa3166d99412cec19a4fc40d463eb2f1718c08
M Schalk <30376729+schalkms@users.noreply.github.com>
2020-05-15 13:49:19 +0200

    Inline test snippets for the UnreachableCode rule (#2695)

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnreachableCodeSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnreachableCodeSpec.kt
@@ -1,8 +1,7 @@
 package io.gitlab.arturbosch.detekt.rules.bugs
 
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.rules.Case
-import io.gitlab.arturbosch.detekt.test.lint
+import io.gitlab.arturbosch.detekt.test.compileAndLint
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -12,9 +11,84 @@ class UnreachableCodeSpec : Spek({
 
     describe("UnreachableCode rule") {
 
-        it("reports unreachable code") {
-            val path = Case.UnreachableCode.path()
-            assertThat(subject.lint(path)).hasSize(6)
+        it("reports unreachable code after return") {
+            val code = """
+                fun f(i: Int) {
+                    if (i == 0) {
+                        return
+                        println()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("reports unreachable code after return in lambda") {
+            val code = """
+                fun f(s: String): Boolean {
+                    s.let {
+                        return it.length < 3
+                        println()
+                    }
+                    return false
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("reports unreachable code after return with label") {
+            val code = """
+                fun f(ints: List<Int>): List<Int> {
+                    return ints.map f@{
+                        if (it == 0) {
+                            return@f 0
+                            println()
+                        }
+                        return@f 1
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("reports unreachable code after throwing an exception") {
+            val code = """
+                fun f(i: Int) {
+                    if (i == 0) {
+                        throw IllegalArgumentException()
+                        println()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(1)
+        }
+
+        it("reports unreachable code after break and continue") {
+            val code = """
+                fun f() {
+                    for (i in 1..2) {
+                        break
+                        println()
+                    }
+                    for (i in 1..2) {
+                        continue
+                        println()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).hasSize(2)
+        }
+
+        it("does not report reachable code after conditional return with label") {
+            val code = """
+                fun f(ints: List<Int>) {
+                    ints.forEach {
+                        if (it == 0) return@forEach
+                        println()
+                    }
+                }
+            """
+            assertThat(subject.compileAndLint(code)).isEmpty()
         }
     }
 })
