72148703f1134cdfcbf3f81f0f4af769e2803ec5
Artur Bosch <arturbosch@gmx.de>
2020-06-05 09:17:30 +0200

    Update spek to 2.0.11 disabling timeouts (#2767)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ object Versions {
     const val DETEKT: String = "1.9.1"
     const val JVM_TARGET: String = "1.8"
     const val ASSERTJ: String = "3.16.1"
-    const val SPEK: String = "2.0.10"
+    const val SPEK: String = "2.0.11"
     const val REFLECTIONS: String = "0.9.12"
     const val MOCKK: String = "1.10.0"
     const val JUNIT: String = "1.6.2"
