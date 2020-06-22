e0407049e130b0e97bbc1906c8f8a94509286e98
Niraj Palecha <nirajftw@gmail.com>
2020-06-22 21:07:32 +0530

    LongMethod: disregard params in method definition (#2806)
    * failing tests for #2804 LongMethod with params
    
    * fix test
    
    * skipTrees for KtParameterList for linesOfCode
    
    * use test snippets that compile
    
    * [ci ckip] use fun body for LongMethod
    - revert changes in LinesOfCode.kt
    - still need to update nested methods
    
    * remove unused import
    
    * add functionToBodyLinesCache to track fun body length
    
    * remove spaces from test asserts

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LongMethodSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LongMethodSpec.kt
@@ -1,5 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.complexity
 
+import io.gitlab.arturbosch.detekt.api.ThresholdedCodeSmell
 import io.gitlab.arturbosch.detekt.test.assertThat
 import io.gitlab.arturbosch.detekt.test.compileAndLint
 import org.spekframework.spek2.Spek
@@ -43,6 +44,109 @@ class LongMethodSpec : Spek({
             """
 
             assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("should not find too long method with params on newlines") {
+            val code = """
+                fun methodWithParams(
+                    param1: String
+                ) { // 4 lines
+                    println()
+                    println()
+                }
+            """
+
+            assertThat(subject.compileAndLint(code)).isEmpty()
+        }
+
+        it("should find too long method with params on newlines") {
+            val code = """
+                fun longMethodWithParams(
+                    param1: String
+                ) { // 5 lines
+                    println()
+                    println()
+                    println()
+                }
+            """
+
+            val findings = subject.compileAndLint(code)
+
+            assertThat(findings).hasSize(1)
+            assertThat(findings[0] as ThresholdedCodeSmell).hasValue(5)
+        }
+
+        it("should find long method with method call with params on separate lines") {
+            val code = """
+                fun longMethod( 
+                    x1: Int,
+                    x2: Int,
+                    y1: Int,
+                    y2: Int
+                ) { // 8 lines
+                    listOf(
+                        x1,
+                        y1,
+                        x2,
+                        y2
+                    )
+                }
+            """
+
+            val findings = subject.compileAndLint(code)
+
+            assertThat(findings).hasSize(1)
+            assertThat(findings[0] as ThresholdedCodeSmell).hasValue(8)
+        }
+
+        it("should find two long methods with params on separate lines") {
+            val code = """
+                fun longMethod(
+                    param1: String
+                ) { // 5 lines
+                    println()
+                    println()
+                    println()
+            
+                    fun nestedLongMethod(
+                        param1: String
+                    ) { // 5 lines
+                        println()
+                        println()
+                        println()
+                    }
+                }
+            """
+
+            val findings = subject.compileAndLint(code)
+
+            assertThat(findings).hasSize(2)
+            assertThat(findings).hasTextLocations("longMethod", "nestedLongMethod")
+        }
+
+        it("should find nested long methods with params on separate lines") {
+            val code = """
+                fun longMethod(
+                    param1: String
+                ) { // 4 lines
+                    println()
+                    println()
+            
+                    fun nestedLongMethod(
+                        param1: String
+                    ) { // 5 lines
+                        println()
+                        println()
+                        println()
+                    }
+                }
+            """
+
+            val findings = subject.compileAndLint(code)
+
+            assertThat(findings).hasSize(1)
+            assertThat(findings).hasTextLocations("nestedLongMethod")
+            assertThat(findings[0] as ThresholdedCodeSmell).hasValue(5)
         }
     }
 })
