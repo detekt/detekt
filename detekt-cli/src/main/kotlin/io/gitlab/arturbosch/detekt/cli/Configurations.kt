37bccae2cc3ac5813105f1e0d8b55c612f029297
Artur Bosch <arturbosch@gmx.de>
2020-07-01 21:12:45 +0200

    Deprecate obsolete methods to retrieve the default configuration

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/Configurations.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/Configurations.kt
@@ -5,7 +5,19 @@ import io.gitlab.arturbosch.detekt.core.NotApiButProbablyUsedByUsers
 import io.gitlab.arturbosch.detekt.core.config.DefaultConfig
 
 @NotApiButProbablyUsedByUsers
+@Deprecated("""
+    Exposes internal resource name. There should not be a case were just the resource name is needed.
+    Please use the DefaultConfigurationProvider to get a default config instance.
+"""
+)
 const val DEFAULT_CONFIG: String = DefaultConfig.RESOURCE_NAME
 
 @NotApiButProbablyUsedByUsers
+@Deprecated(
+    "Use official api for the default config.",
+    ReplaceWith(
+        "DefaultConfigurationProvider().load().get()",
+        "io.github.detekt.tooling.api.DefaultConfigurationProvider"
+    )
+)
 fun loadDefaultConfig(): Config = DefaultConfig.newInstance()
