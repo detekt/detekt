0b4c81555870fc19b6e8515827d7eab39e51fe87
Artur Bosch <arturbosch@gmx.de>
2020-07-12 20:44:30 +0200

    Make DefaultConfig object internal

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/DefaultConfig.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/DefaultConfig.kt
@@ -3,7 +3,7 @@ package io.gitlab.arturbosch.detekt.core.config
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.internal.YamlConfig
 
-object DefaultConfig {
+internal object DefaultConfig {
 
     const val RESOURCE_NAME = "default-detekt-config.yml"
 
