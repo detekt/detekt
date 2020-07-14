98272d3b60ce529ddadab29960796fabd3b459d7
Artur Bosch <arturbosch@gmx.de>
2020-07-14 23:06:50 +0200

    Simplify parsing to KtFile's

--- detekt-parser/src/main/kotlin/io/github/detekt/parser/KtCompiler.kt
+++ detekt-parser/src/main/kotlin/io/github/detekt/parser/KtCompiler.kt
@@ -5,10 +5,8 @@ import io.github.detekt.psi.LINE_SEPARATOR
 import io.github.detekt.psi.RELATIVE_PATH
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.com.intellij.openapi.util.text.StringUtilRt
-import org.jetbrains.kotlin.com.intellij.psi.PsiFileFactory
-import org.jetbrains.kotlin.com.intellij.testFramework.LightVirtualFile
-import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtPsiFactory
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -16,7 +14,7 @@ open class KtCompiler(
     protected val environment: KotlinCoreEnvironment = createKotlinCoreEnvironment()
 ) {
 
-    protected val psiFileFactory: PsiFileFactory = PsiFileFactory.getInstance(environment.project)
+    protected val psiFileFactory = KtPsiFactory(environment.project)
 
     fun compile(basePath: Path, path: Path): KtFile {
         require(Files.isRegularFile(path)) { "Given sub path ($path) should be a regular file!" }
@@ -33,14 +31,12 @@ open class KtCompiler(
         val absolutePath = path.toAbsolutePath().normalize()
         val lineSeparator = content.determineLineSeparator()
 
-        val psiFile = psiFileFactory.createFileFromText(
+        val psiFile = psiFileFactory.createPhysicalFile(
             path.fileName.toString(),
-            KotlinLanguage.INSTANCE,
-            StringUtilRt.convertLineSeparators(content),
-            true, true, false,
-            LightVirtualFile(path.toString())
+            StringUtilRt.convertLineSeparators(content)
         )
-        return (psiFile as? KtFile ?: error("kotlin file expected")).apply {
+
+        return psiFile.apply {
             putUserData(LINE_SEPARATOR, lineSeparator)
             putUserData(RELATIVE_PATH, relativePath.toString())
             putUserData(ABSOLUTE_PATH, absolutePath.toString())
