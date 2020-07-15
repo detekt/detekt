920a8dc1c0b8eda1f8bf8d55e3ae3bf8c212f5d0
Amit Dash <amitdash291@gmail.com>
2020-07-16 00:01:52 +0530

    Format GuardClauses file

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/GuardClauses.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/GuardClauses.kt
@@ -8,26 +8,27 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.findDescendantOfType
 import org.jetbrains.kotlin.psi.psiUtil.lastBlockStatementOrThis
 
-internal inline fun <reified T:KtExpression> KtNamedFunction.yieldStatementsSkippingGuardClauses(): Sequence<KtExpression> = sequence {
-    var firstNonGuardFound = false
-    this@yieldStatementsSkippingGuardClauses.bodyBlockExpression?.statements?.forEach {
-        if (firstNonGuardFound) {
-            yield(it)
-        } else {
-            if (!it.isGuardClause<T>()) {
-                firstNonGuardFound = true
+internal inline fun <reified T : KtExpression> KtNamedFunction.yieldStatementsSkippingGuardClauses(): Sequence<KtExpression> =
+    sequence {
+        var firstNonGuardFound = false
+        this@yieldStatementsSkippingGuardClauses.bodyBlockExpression?.statements?.forEach {
+            if (firstNonGuardFound) {
                 yield(it)
+            } else {
+                if (!it.isGuardClause<T>()) {
+                    firstNonGuardFound = true
+                    yield(it)
+                }
             }
         }
     }
-}
 
-internal inline fun <reified T:KtExpression> KtExpression.isGuardClause(): Boolean {
+internal inline fun <reified T : KtExpression> KtExpression.isGuardClause(): Boolean {
     val descendantExpr = this.findDescendantOfType<T>() ?: return false
     return this.isIfConditionGuardClause(descendantExpr) || this.isElvisOperatorGuardClause()
 }
 
-internal fun <T:KtExpression> KtExpression.isIfConditionGuardClause(descendantExpr: T): Boolean {
+internal fun <T : KtExpression> KtExpression.isIfConditionGuardClause(descendantExpr: T): Boolean {
     val ifExpr = this as? KtIfExpression ?: return false
     return ifExpr.`else` == null &&
             descendantExpr === ifExpr.then?.lastBlockStatementOrThis()
