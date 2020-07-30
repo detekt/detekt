5001b863fb7c89ac56cdc7881c78a1cebc3b56ef
Artur Bosch <arturbosch@gmx.de>
2020-07-30 09:57:27 +0200

    Generate output report config parts for config validation to know the properties - Closes #2891

--- detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/ConfigPrinterSpec.kt
+++ detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/ConfigPrinterSpec.kt
@@ -1,12 +1,11 @@
 package io.gitlab.arturbosch.detekt.generator.printer
 
-import io.github.detekt.test.utils.resource
+import io.github.detekt.test.utils.readResourceContent
 import io.gitlab.arturbosch.detekt.generator.printer.rulesetpage.ConfigPrinter
 import io.gitlab.arturbosch.detekt.generator.util.createRuleSetPage
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import java.io.File
 
 class ConfigPrinterSpec : Spek({
 
@@ -14,9 +13,14 @@ class ConfigPrinterSpec : Spek({
 
         it("prints the correct yaml format") {
             val ruleSetList = listOf(createRuleSetPage())
+            val expectedRulePart = readResourceContent("RuleSetConfig.yml")
+
             val yamlString = ConfigPrinter.print(ruleSetList)
-            val expectedYamlString = File(resource("/RuleSetConfig.yml")).readText()
-            assertThat(yamlString).contains(expectedYamlString)
+
+            assertThat(yamlString).contains(expectedRulePart)
+            assertThat(yamlString).contains("output-reports:")
+            assertThat(yamlString).contains("console-reports:")
+            assertThat(yamlString).contains("processors:")
         }
     }
 })
