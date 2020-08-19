f3877b44307ba61c445e75b1022c981584054322
Artur Bosch <arturbosch@gmx.de>
2020-08-19 23:29:52 +0200

    Prepare 1.11.2 release (#2989)
    * Prepare 1.11.2 release
    
    * Use 1.11.2 for self analysis

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-    const val DETEKT: String = "1.11.1"
+    const val DETEKT: String = "1.11.2"
     const val JVM_TARGET: String = "1.8"
     const val JACOCO: String = "0.8.5"
 
