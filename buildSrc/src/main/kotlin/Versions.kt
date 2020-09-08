6550f37cecd334ea9521adf111729c1bcecc8130
Artur Bosch <arturbosch@gmx.de>
2020-09-08 14:17:59 +0200

    Prepare 1.13.0 release

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-    const val DETEKT: String = "1.12.0"
+    const val DETEKT: String = "1.13.0"
     const val JVM_TARGET: String = "1.8"
     const val JACOCO: String = "0.8.5"
 
