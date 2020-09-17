34b5108a59effd85f71c38287d25210f0237fc27
Peter Trifanov <peter.trifanov@mail.ru>
2020-09-17 17:37:46 +0300

    Use File.pathSeparator to split entries in classpath

--- detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/settings/EnvironmentFacadeSpec.kt
+++ detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/settings/EnvironmentFacadeSpec.kt
@@ -3,22 +3,23 @@ package io.gitlab.arturbosch.detekt.core.settings
 import io.gitlab.arturbosch.detekt.core.createNullLoggingSpec
 import io.gitlab.arturbosch.detekt.core.createProcessingSettings
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.kotlin.konan.file.File
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 internal class EnvironmentFacadeSpec : Spek({
 
-    describe("classpath entries can be separated by : or ;") {
+    describe("classpath entries should be separated by platform-specific separator") {
 
-        arrayOf(
-            "supports ;" to "file1;file2;file3",
-            "supports :" to "file1:file2:file3",
-            "supports mixing ; and :" to "file1;file2:file3"
-        ).forEach { (testName, classpath) ->
-            it(testName) {
-                testSettings(classpath).use {
-                    assertThat(it.classpath).hasSize(3)
-                }
+        val classpath = when (File.pathSeparator) {
+            ":" -> "/path/to/file1:/path/to/file2:/path/to/file3"
+            ";" -> """C:\path\to\file1;C:\path\to\file2;C:\path\to\file3"""
+            else -> ""
+        }
+
+        it("supports ${File.pathSeparator}") {
+            testSettings(classpath).use {
+                assertThat(it.classpath).hasSize(3)
             }
         }
     }
