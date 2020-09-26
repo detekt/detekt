931795d93cf603e2e66845d37e467284b74ff304
Artur Bosch <arturbosch@gmx.de>
2020-09-26 08:32:55 +0200

    Update detekt dependencies

--- detekt-bom/build.gradle.kts
+++ detekt-bom/build.gradle.kts
@@ -9,13 +9,13 @@ dependencies {
     }
 
     constraints {
-        api("org.assertj:assertj-core:3.16.1")
+        api("org.assertj:assertj-core:3.17.2")
         api("org.spekframework.spek2:spek-dsl-jvm:${version.spek}")
         api("org.spekframework.spek2:spek-runner-junit5:${version.spek}")
         api("org.reflections:reflections:0.9.12")
         api("io.mockk:mockk:1.10.0")
-        api("org.junit.platform:junit-platform-launcher:1.6.2")
-        api("org.yaml:snakeyaml:1.26")
+        api("org.junit.platform:junit-platform-launcher:1.7.0")
+        api("org.yaml:snakeyaml:1.27")
         api("com.beust:jcommander:1.78")
         api("com.pinterest.ktlint:ktlint-ruleset-standard:${version.ktlint}")
         api("com.pinterest.ktlint:ktlint-core:${version.ktlint}")
