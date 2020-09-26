6facdee24eb69c6772a7026fb61f454b8705110b
Artur Bosch <arturbosch@gmx.de>
2020-09-26 10:49:32 +0200

    Update jacoco with support of Java 15

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
 
     const val DETEKT: String = "1.13.1"
     const val JVM_TARGET: String = "1.8"
-    const val JACOCO: String = "0.8.5"
+    const val JACOCO: String = "0.8.6"
 
     fun currentOrSnapshot(): String {
         if (System.getProperty("snapshot")?.toBoolean() == true) {
