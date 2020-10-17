619cf13a818465821c24fa8705783e802596d73d
Artur Bosch <arturbosch@gmx.de>
2020-10-17 20:44:56 +0200

    Update Gradle to 6.7

--- detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/MultiVersionTest.kt
+++ detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/MultiVersionTest.kt
@@ -29,9 +29,9 @@ object MultiVersionTest : Spek({
 
 private fun getGradleVersionsUnderTest() =
     if (getJdkVersion() < 13) {
-        listOf("5.4", "6.6")
+        listOf("5.4", "6.7")
     } else {
-        listOf("6.6")
+        listOf("6.7")
     }
 
 private fun getJdkVersion(): Int {
