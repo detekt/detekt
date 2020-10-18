1d7bd33ac75cbe1d8d66e9549e5efc01278f9279
Artur Bosch <arturbosch@gmx.de>
2020-10-18 21:48:31 +0200

    Change spek and assertj to compile only dependencies for detekt-test consumers - Closes #3082 (#3152)

--- detekt-test/build.gradle.kts
+++ detekt-test/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     api(project(":detekt-api"))
     api(project(":detekt-test-utils"))
+    compileOnly("org.assertj:assertj-core")
     implementation(project(":detekt-parser"))
 }
