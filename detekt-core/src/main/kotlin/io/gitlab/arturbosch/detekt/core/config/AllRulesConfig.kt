299f3ca7d762283357b558057fded8f3bfcd199b
Artur Bosch <arturbosch@gmx.de>
2020-11-24 21:37:47 +0100

    Remove maxIssues property from AllRulesConfig as maxIssues=0 is the new default already

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/AllRulesConfig.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/AllRulesConfig.kt
@@ -14,7 +14,6 @@ internal data class AllRulesConfig(
     override fun <T : Any> valueOrDefault(key: String, default: T): T {
         return when (key) {
             "active" -> originalConfig.valueOrDefault(key, true) as T
-            "maxIssues" -> originalConfig.valueOrDefault(key, 0) as T
             else -> originalConfig.valueOrDefault(key, defaultConfig.valueOrDefault(key, default))
         }
     }
@@ -22,7 +21,6 @@ internal data class AllRulesConfig(
     override fun <T : Any> valueOrNull(key: String): T? {
         return when (key) {
             "active" -> originalConfig.valueOrNull(key) ?: true as? T
-            "maxIssues" -> originalConfig.valueOrNull(key) ?: 0 as? T
             else -> originalConfig.valueOrNull(key) ?: defaultConfig.valueOrNull(key)
         }
     }
