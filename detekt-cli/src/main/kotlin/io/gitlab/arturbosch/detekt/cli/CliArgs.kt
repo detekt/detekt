5e7246995fac6d183bb10edd2d20f8a7d30b6283
Artur Bosch <arturbosch@gmx.de>
2020-11-24 22:19:41 +0100

    Deprecate failFast cli flag

--- detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
+++ detekt-cli/src/main/kotlin/io/gitlab/arturbosch/detekt/cli/CliArgs.kt
@@ -100,10 +100,12 @@ class CliArgs {
 
     @Parameter(
         names = ["--fail-fast"],
-        description = "Same as 'build-upon-default-config' but explicitly running all available rules. " +
+        description = "DEPRECATED: please use '--build-upon-default-config' together with '--all-rules'. " +
+            "Same as 'build-upon-default-config' but explicitly running all available rules. " +
             "With this setting only exit code 0 is returned when the analysis does not find a single code smell. " +
             "Additional configuration files can override rule properties which includes turning off specific rules."
     )
+    @Deprecated("Please use the buildUponDefaultConfig and allRules flags instead.", ReplaceWith("allRules"))
     var failFast: Boolean = false
 
     @Parameter(
