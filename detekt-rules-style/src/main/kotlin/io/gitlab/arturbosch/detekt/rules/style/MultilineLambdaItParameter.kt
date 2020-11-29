23e958697192df89f9d2bd27da4d68f7508f4086
adamkobor <kobor.adam@gmail.com>
2020-11-29 18:39:34 +0100

    Change rule behavior to report only for multiple statements

--- detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MultilineLambdaItParameter.kt
+++ detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MultilineLambdaItParameter.kt
@@ -13,34 +13,49 @@ import org.jetbrains.kotlin.psi.KtLambdaExpression
 /**
  * Lambda expressions are are very useful in a lot of cases and they often include very small chunks of
  * code using only one parameter. In this cases Kotlin can supply the implicit `it` parameter
- * to make code more concise. However, when you are dealing with multiline lambdas, you might end up with a code
- * that is hard to read if you don't specify the parameter name explicitly.
+ * to make code more concise. However, when you are dealing with lambdas that contain multiple statements,
+ * you might end up with a code that is hard to read if you don't specify the parameter name explicitly.
  *
  * <noncompliant>
  * val digits = 1234.let {
+ *   println(it)
  *   listOf(it)
  * }
  *
  * val digits = 1234.let { it ->
+ *   println(it)
  *   listOf(it)
  * }
  *
  * val flat = listOf(listOf(1), listOf(2)).mapIndexed { index, it ->
+ *   println(it)
  *   it + index
  * }
  * </noncompliant>
  *
  * <compliant>
  * val digits = 1234.let { explicitParameterName ->
+ *   println(explicitParameterName)
  *   listOf(explicitParameterName)
  * }
+ *
  * val lambda = { item: Int, that: String ->
+ *   println(item)
  *   item.toString() + that
  * }
  *
  * val digits = 1234.let { listOf(it) }
+ * val digits = 1234.let {
+ *   listOf(it)
+ * }
  * val digits = 1234.let { it -> listOf(it) }
+ * val digits = 1234.let { it ->
+ *   listOf(it)
+ * }
  * val digits = 1234.let { explicit -> listOf(explicit) }
+ * val digits = 1234.let { explicit ->
+ *   listOf(explicit)
+ * }
  * </compliant>
  */
 class MultilineLambdaItParameter(val config: Config) : Rule(config) {
@@ -52,12 +67,13 @@ class MultilineLambdaItParameter(val config: Config) : Rule(config) {
     override fun visitLambdaExpression(lambdaExpression: KtLambdaExpression) {
         super.visitLambdaExpression(lambdaExpression)
         val parameterNames = lambdaExpression.valueParameters.map { it.name }
-        val isMultiline = lambdaExpression.text.contains("\n")
-        if (isMultiline && (parameterNames.isEmpty() || IT_LITERAL in parameterNames)) {
+        val statementCount = lambdaExpression.bodyExpression?.statements?.size ?: 0
+        if (statementCount > 1 && (parameterNames.isEmpty() || IT_LITERAL in parameterNames)) {
             report(
                 CodeSmell(
                     issue, Entity.from(lambdaExpression),
-                    "The parameter name in a multiline lambda should not be an implicit/explicit `it`. Consider giving your parameter a name"
+                    "The parameter name in a multiline lambda should not be an implicit/explicit `it`. " +
+                            "Consider giving your parameter a name"
                 )
             )
         }
--- detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/MultilineLambdaItParameterSpec.kt
+++ detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/MultilineLambdaItParameterSpec.kt
@@ -10,80 +10,123 @@ class MultilineLambdaItParameterSpec : Spek({
     val subject by memoized { MultilineLambdaItParameter(Config.empty) }
 
     describe("MultilineLambdaItParameter rule") {
-        context("single parameter, multiline lambda") {
+        context("single parameter, multiline lambda with multiple statements") {
             it("reports when parameter name is implicit `it`") {
-                val findings = subject.compileAndLint(
-                    """
+                val code = """
+                fun f() {
+                    val digits = 1234.let { 
+                        listOf(it)
+                        println(it)
+                    }
+                }"""
+                val findings = subject.compileAndLint(code)
+                assertThat(findings).hasSize(1)
+            }
+            it("reports when parameter name is explicit `it`") {
+                val code = """
+                fun f() {
+                    val digits = 1234.let { it ->
+                        listOf(it)
+                        println(it)
+                    }
+                }"""
+                val findings = subject.compileAndLint(code)
+                assertThat(findings).hasSize(1)
+            }
+            it("does not report when parameter name is explicit and not `it`") {
+                val code = """
+                fun f() {
+                    val digits = 1234.let { param ->
+                        listOf(param)
+                        println(param)
+                    }
+                }"""
+                val findings = subject.compileAndLint(code)
+                assertThat(findings).hasSize(0)
+            }
+        }
+
+        context("single parameter, multiline lambda with a single statement") {
+            it("does not report when parameter name is an implicit `it`") {
+                val code = """
                 fun f() {
                     val digits = 1234.let { 
                         listOf(it)
                     }
-                }""")
-                assertThat(findings).hasSize(1)
+                }"""
+                val findings = subject.compileAndLint(code)
+                assertThat(findings).hasSize(0)
             }
-            it("reports when parameter name is explicit `it`") {
-                val findings = subject.compileAndLint(
-                    """
+            it("does not report when parameter name is an explicit `it`") {
+                val code = """
                 fun f() {
                     val digits = 1234.let { it ->
                         listOf(it)
                     }
-                }""")
-                assertThat(findings).hasSize(1)
+                }"""
+                val findings = subject.compileAndLint(code)
+                assertThat(findings).hasSize(0)
             }
             it("does not report when parameter name is explicit and not `it`") {
-                val findings = subject.compileAndLint("""
+                val code = """
                 fun f() {
-                    val digits = 1234.let { explicitParameterName ->
-                        listOf(explicitParameterName)
+                    val digits = 1234.let { param ->
+                        listOf(param)
                     }
-                }""")
+                }"""
+                val findings = subject.compileAndLint(code)
                 assertThat(findings).hasSize(0)
             }
         }
 
         context("single parameter, single-line lambda") {
             it("does not report when parameter name is an implicit `it`") {
-                val findings = subject.compileAndLint(
-                    """
+                val code = """
                 fun f() {
                     val digits = 1234.let { listOf(it) }
-                }""")
+                }"""
+                val findings = subject.compileAndLint(code)
                 assertThat(findings).hasSize(0)
             }
             it("does not report when parameter name is an explicit `it`") {
-                val findings = subject.compileAndLint("""
+                val code = """
                 fun f() {
                     val digits = 1234.let { it -> listOf(it) }
-                }""")
+                }"""
+                val findings = subject.compileAndLint(code)
                 assertThat(findings).hasSize(0)
             }
             it("does not report when parameter name is explicit and not `it`") {
-                val findings = subject.compileAndLint("""
+                val code = """
                 fun f() {
-                    val digits = 1234.let { explicit -> listOf(explicit) }
-                }""")
+                    val digits = 1234.let { param -> listOf(param) }
+                }"""
+                val findings = subject.compileAndLint(code)
                 assertThat(findings).hasSize(0)
             }
         }
 
         context("multiple parameters, multiline lambda") {
             it("reports when one of the explicit parameters is an `it`") {
-                val findings = subject.compileAndLint("""
+                val code = """
                 fun f() {
-                    val flat = listOf(listOf(1), listOf(2)).mapIndexed { index, it -> 
+                    val flat = listOf(listOf(1), listOf(2)).mapIndexed { index, it ->
+                        println(it)
                         it + index 
                     }
-                }""")
+                }"""
+                val findings = subject.compileAndLint(code)
                 assertThat(findings).hasSize(1)
             }
             it("does not report when none of the explicit parameters is an `it`") {
-                val findings = subject.compileAndLint("""
+                val code = """
                 fun f() {
                     val lambda = { item: Int, that: String -> 
+                        println(item)
                         item.toString() + that 
                     }
-                }""")
+                }"""
+                val findings = subject.compileAndLint(code)
                 assertThat(findings).hasSize(0)
             }
         }
