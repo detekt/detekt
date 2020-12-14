8513ed6fb106dc9fe88a6e6c34e1594cb64c42bc
Artur Bosch <arturbosch@gmx.de>
2020-12-14 18:27:31 +0100

    Prepare 1.15.0 rc2 (#3286)
    * Prepare 1.15.0-RC2 release
    
    * Include exit guards when one release step finishes with exit code != 0
    
    * Update self analysis version
    
    * Upload formatting jar to github

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
-gradle build publishToMavenLocal -x detekt -x test
-gradle build
-gradle publishAllPublicationsToMavenCentralRepository --max-workers 1
-gradle publishPlugins -DautomatePublishing=true
-gradle githubRelease
-gradle applyDocVersion applySelfAnalysisVersion
+gradle build publishToMavenLocal -x detekt -x test || exit
+gradle build || exit
+gradle publishAllPublicationsToMavenCentralRepository --max-workers 1 || exit
+gradle publishPlugins -DautomatePublishing=true || exit
+gradle githubRelease || exit
+gradle applyDocVersion applySelfAnalysisVersion || exit
+gradle closeAndReleaseRepository || exit
