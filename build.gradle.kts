e31c46d81ae9b05c6f3fb8670b187624fddc1d22
Chao Zhang <cazhang@linkedin.com>
2020-12-25 19:34:54 -0800

    Remove TODO as forbidden comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,8 @@ buildScan {
 
 apiValidation {
     // We only need to perform api validation for :detekt-api.
-    // TODO: rootProject.name is a temporary workaround to exclude api validation of rootProject.
+    //
+    // rootProject.name is a temporary workaround to exclude api validation of rootProject.
     // We should refactoring our gradle setup to not apply `JavaBasePlugin`
     ignoredProjects.addAll(subprojects.filter { it.name != "detekt-api" }.map { it.name } + rootProject.name)
     ignoredPackages.add("io.gitlab.arturbosch.detekt.api.internal")
