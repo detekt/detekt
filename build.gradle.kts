ff655d2a4e9a1d7f72008a22b0626e7578cfecb4
Chao Zhang <cazhang@linkedin.com>
2020-12-30 13:03:45 -0800

    Apply binary compatibility plugin to Detekt (#3320)
    * Apply binary compatibility plugin to Detekt
    
    * Exclude internal api
    
    * Add an additional new line
    
    * Fix comment to point out the problem in gradle setup
    
    * Remove TODO as forbidden comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,19 @@ plugins {
     id("com.github.johnrengelman.shadow") apply false
     id("com.github.ben-manes.versions")
     id("org.sonarqube")
+    id("binary-compatibility-validator")
 }
 
 buildScan {
     termsOfServiceUrl = "https://gradle.com/terms-of-service"
     termsOfServiceAgree = "yes"
+}
+
+apiValidation {
+    // We only need to perform api validation for :detekt-api.
+    //
+    // rootProject.name is a temporary workaround to exclude api validation of rootProject.
+    // We should refactoring our gradle setup to not apply `JavaBasePlugin`
+    ignoredProjects.addAll(subprojects.filter { it.name != "detekt-api" }.map { it.name } + rootProject.name)
+    ignoredPackages.add("io.gitlab.arturbosch.detekt.api.internal")
 }
