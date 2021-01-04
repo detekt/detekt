ee727f59a5446bd101916f848b5e088f639aacc3
Chao Zhang <cazhang@linkedin.com>
2021-01-03 23:28:06 -0800

    Refactor integration test for detekt-gradle-plugin

--- detekt-gradle-plugin/src/intTest/kotlin/io/gitlab/arturbosch/detekt/DetektTaskMultiModuleIntegrationTest.kt
+++ detekt-gradle-plugin/src/intTest/kotlin/io/gitlab/arturbosch/detekt/DetektTaskMultiModuleIntegrationTest.kt
@@ -1,8 +1,8 @@
 package io.gitlab.arturbosch.detekt
 
+import io.gitlab.arturbosch.detekt.testkit.DslGradleRunner
 import io.gitlab.arturbosch.detekt.testkit.DslTestBuilder.Companion.groovy
 import io.gitlab.arturbosch.detekt.testkit.DslTestBuilder.Companion.kotlin
-import io.gitlab.arturbosch.detekt.testkit.DslGradleRunner
 import io.gitlab.arturbosch.detekt.testkit.ProjectLayout
 import org.assertj.core.api.Assertions.assertThat
 import org.gradle.testkit.runner.TaskOutcome
--- detekt-gradle-plugin/src/intTest/kotlin/io/gitlab/arturbosch/detekt/testkit/DslGradleRunner.kt
+++ detekt-gradle-plugin/src/intTest/kotlin/io/gitlab/arturbosch/detekt/testkit/DslGradleRunner.kt
@@ -6,6 +6,7 @@ import java.io.File
 import java.nio.file.Files
 import java.util.UUID
 
+@Suppress("TooManyFunctions")
 class DslGradleRunner @Suppress("LongParameterList") constructor(
     val projectLayout: ProjectLayout,
     val buildFileName: String,
