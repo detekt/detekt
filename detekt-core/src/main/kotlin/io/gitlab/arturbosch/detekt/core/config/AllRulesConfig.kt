221d7a3b8beec08c2d7ecb15162d767950a31570
Artur Bosch <arturbosch@gmx.de>
2021-01-18 17:03:22 +0100

    Introduce --all-rules flag (#3253)
    * Add --all-rules cli flag
    
    * Rename to allRules which must not be experimental per se
    
    * Rename FailFastConfig to AllRulesConfig
    
    * Remove maxIssues property from AllRulesConfig as maxIssues=0 is the new default already
    
    * Deprecate failFast cli flag

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/AllRulesConfig.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/AllRulesConfig.kt
@@ -3,16 +3,17 @@ package io.gitlab.arturbosch.detekt.core.config
 import io.gitlab.arturbosch.detekt.api.Config
 
 @Suppress("UNCHECKED_CAST")
-data class FailFastConfig(private val originalConfig: Config, private val defaultConfig: Config) :
-    Config, ValidatableConfiguration {
+internal data class AllRulesConfig(
+    private val originalConfig: Config,
+    private val defaultConfig: Config
+) : Config, ValidatableConfiguration {
 
     override fun subConfig(key: String) =
-        FailFastConfig(originalConfig.subConfig(key), defaultConfig.subConfig(key))
+        AllRulesConfig(originalConfig.subConfig(key), defaultConfig.subConfig(key))
 
     override fun <T : Any> valueOrDefault(key: String, default: T): T {
         return when (key) {
             Config.ACTIVE_KEY -> originalConfig.valueOrDefault(key, true) as T
-            MAX_ISSUES_KEY -> originalConfig.valueOrDefault(key, 0) as T
             else -> originalConfig.valueOrDefault(key, defaultConfig.valueOrDefault(key, default))
         }
     }
@@ -20,7 +21,6 @@ data class FailFastConfig(private val originalConfig: Config, private val defaul
     override fun <T : Any> valueOrNull(key: String): T? {
         return when (key) {
             Config.ACTIVE_KEY -> originalConfig.valueOrNull(key) ?: true as? T
-            MAX_ISSUES_KEY -> originalConfig.valueOrNull(key) ?: 0 as? T
             else -> originalConfig.valueOrNull(key) ?: defaultConfig.valueOrNull(key)
         }
     }
