67d206c41b54ccd8053525065db7456b06080af7
Brais Gabín <braisgabin@gmail.com>
2021-02-08 22:26:39 +0100

    Introduce ResolvedContext

---
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/v2/ResolvedContext.kt
@@ -0,0 +1,9 @@
+package io.gitlab.arturbosch.detekt.api.v2
+
+import io.gitlab.arturbosch.detekt.api.internal.CompilerResources
+import org.jetbrains.kotlin.resolve.BindingContext
+
+interface ResolvedContext {
+    val binding: BindingContext
+    val resources: CompilerResources
+}
