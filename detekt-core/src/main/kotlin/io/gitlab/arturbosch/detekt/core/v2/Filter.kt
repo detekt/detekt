72eba603a6da38d37b519fb99a2a93819e257c84
Brais Gabín <braisgabin@gmail.com>
2021-02-13 15:28:47 +0100

    Rename NewIssue to Issue

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/v2/Filter.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/v2/Filter.kt
@@ -1,10 +1,10 @@
 package io.gitlab.arturbosch.detekt.core.v2
 
-import io.gitlab.arturbosch.detekt.api.v2.NewIssue
+import io.gitlab.arturbosch.detekt.api.v2.Issue
 import java.nio.file.Path
 
 interface Filter {
     fun filter(path: Path): Boolean
 
-    fun filter(issue: NewIssue): Boolean
+    fun filter(issue: Issue): Boolean
 }
