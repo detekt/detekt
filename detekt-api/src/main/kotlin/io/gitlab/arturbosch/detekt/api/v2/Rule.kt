f48e645dbca9bbe93cbab9fed700b5e56843542f
Brais Gabín <braisgabin@gmail.com>
2021-02-13 15:27:40 +0100

    Don't use old issue

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/v2/Rule.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/v2/Rule.kt
@@ -2,14 +2,13 @@ package io.gitlab.arturbosch.detekt.api.v2
 
 import io.gitlab.arturbosch.detekt.api.Debt
 import io.gitlab.arturbosch.detekt.api.Entity
-import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.SeverityLevel
 import org.jetbrains.kotlin.psi.KtFile
 
 // This will be a sealed interface in 1.5
 interface Rule {
-    // This should not be here but the code is not ready yet to remove it
-    val issue: Issue
+    val id: String
+    val description: String
 }
 
 interface PlainRule : Rule, (KtFile) -> List<NewIssue> {
