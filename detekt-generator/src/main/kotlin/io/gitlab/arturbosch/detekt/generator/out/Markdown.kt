1e0aa411e7139e1346bcbfa6917e578417f408d9
Sebastian Schuberth <sschuberth@gmail.com>
2021-03-07 18:02:08 +0100

    Fix Markdown inline code blocks to use only single backticks

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/out/Markdown.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/out/Markdown.kt
@@ -39,7 +39,7 @@ inline fun MarkdownContent.orderedList(sectionList: () -> List<String>) {
     }
 }
 
-inline fun MarkdownContent.code(code: () -> String) = "``${code()}``"
+inline fun MarkdownContent.code(code: () -> String) = "`${code()}`"
 inline fun MarkdownContent.codeBlock(code: () -> String) = "```kotlin\n${code()}\n```"
 
 fun MarkdownContent.emptyLine() = append("")
