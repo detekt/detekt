ffa64b752cd43ef529b71fefd70e6483be93963b
Sebastian Schuberth <sschuberth@gmail.com>
2021-03-07 19:33:48 +0100

    Markdown: Make the syntax hint configurabale for code blocks

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/out/Markdown.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/out/Markdown.kt
@@ -40,7 +40,7 @@ inline fun MarkdownContent.orderedList(sectionList: () -> List<String>) {
 }
 
 inline fun MarkdownContent.code(code: () -> String) = "`${code()}`"
-inline fun MarkdownContent.codeBlock(code: () -> String) = "```kotlin\n${code()}\n```"
+inline fun MarkdownContent.codeBlock(syntax: String = "kotlin", code: () -> String) = "```$syntax\n${code()}\n```"
 
 fun MarkdownContent.emptyLine() = append("")
 
