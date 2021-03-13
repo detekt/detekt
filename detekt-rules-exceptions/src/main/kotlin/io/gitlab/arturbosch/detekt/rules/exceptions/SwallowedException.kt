b41258376e792bd06291fb9f52dda7e3f9744630
M Schalk <30376729+schalkms@users.noreply.github.com>
2021-03-13 20:47:17 +0100

    Update rule doc for SwallowedException config (#3547)
    This update explains the reason behind the config defaults.
    
    Related to #3524

--- detekt-rules-exceptions/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/SwallowedException.kt
+++ detekt-rules-exceptions/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/SwallowedException.kt
@@ -28,6 +28,11 @@ import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
  * Exceptions should not be swallowed. This rule reports all instances where exceptions are `caught` and not correctly
  * passed (e.g. as a cause) into a newly thrown exception.
  *
+ * The exception types configured in `ignoredExceptionTypes` indicate nonexceptional outcomes.
+ * These by default configured exception types are part of Java.
+ * Therefore, Kotlin developers have to handle them by using the catch clause.
+ * For that reason, this rule ignores that these configured exception types are caught.
+ *
  * <noncompliant>
  * fun foo() {
  *     try {
