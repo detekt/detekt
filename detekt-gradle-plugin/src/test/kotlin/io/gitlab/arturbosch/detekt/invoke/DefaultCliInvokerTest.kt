d99fab43e428de903f066fef11cd0553eab9a51e
zmunm <atheist2197@naver.com>
2021-03-18 01:42:31 +0900

    Modify existing file

--- detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/invoke/DefaultCliInvokerTest.kt
+++ detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/invoke/DefaultCliInvokerTest.kt
@@ -4,18 +4,13 @@ import io.gitlab.arturbosch.detekt.gradle.TestFileCollection
 import io.gitlab.arturbosch.detekt.internal.ClassLoaderCache
 import org.assertj.core.api.Assertions.assertThatCode
 import org.gradle.api.GradleException
-import org.gradle.api.file.FileCollection
 import org.spekframework.spek2.Spek
 import java.net.URLClassLoader
 
 internal class DefaultCliInvokerTest : Spek({
 
     test("catches ClassCastException and fails build") {
-        val stubbedCache = object : ClassLoaderCache {
-            override fun getOrCreate(classpath: FileCollection): URLClassLoader {
-                return URLClassLoader(emptyArray())
-            }
-        }
+        val stubbedCache = ClassLoaderCache { URLClassLoader(emptyArray()) }
 
         assertThatCode {
             DefaultCliInvoker(stubbedCache)
