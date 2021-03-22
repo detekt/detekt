9708cc08ae3b3ac1996eb8462c734c08a6ab1717
Markus Schwarz <post@markus-schwarz.net>
2021-03-22 17:34:53 +0100

    Update documentation with regard to RuleSetProvider

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/ActiveByDefault.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/ActiveByDefault.kt
@@ -1,7 +1,8 @@
 package io.gitlab.arturbosch.detekt.api.internal
 
 /**
- * Annotated [io.gitlab.arturbosch.detekt.api.Rule] is active by default.
+ * Annotated [io.gitlab.arturbosch.detekt.api.Rule] or [io.gitlab.arturbosch.detekt.api.RuleSetProvider]
+ * is active by default.
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
