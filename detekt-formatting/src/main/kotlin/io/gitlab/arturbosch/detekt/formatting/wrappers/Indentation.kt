11c37aeb96334d700d3975df1794ede26a194a09
Chao Zhang <cazhang@linkedin.com>
2021-03-27 11:36:20 -0700

    Remove useless config `continuationIndentSize`

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/Indentation.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/Indentation.kt
@@ -2,8 +2,6 @@ package io.gitlab.arturbosch.detekt.formatting.wrappers
 
 import com.pinterest.ktlint.ruleset.standard.IndentationRule
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.formatting.CONTINUATION_INDENT_SIZE_KEY
-import io.gitlab.arturbosch.detekt.formatting.DEFAULT_CONTINUATION_INDENT
 import io.gitlab.arturbosch.detekt.formatting.DEFAULT_INDENT
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 import io.gitlab.arturbosch.detekt.formatting.INDENT_SIZE_KEY
@@ -12,7 +10,6 @@ import io.gitlab.arturbosch.detekt.formatting.INDENT_SIZE_KEY
  * See <a href="https://ktlint.github.io/#rule-indentation">ktlint-website</a> for documentation.
  *
  * @configuration indentSize - indentation size (default: `4`)
- * @configuration continuationIndentSize - continuation indentation size (default: `4`)
  *
  * @autoCorrect since v1.0.0
  */
@@ -22,15 +19,12 @@ class Indentation(config: Config) : FormattingRule(config) {
     override val issue = issueFor("Reports mis-indented code")
 
     private val indentSize = valueOrDefault(INDENT_SIZE, DEFAULT_INDENT)
-    private val continuationIndentSize = valueOrDefault(CONTINUATION_INDENT_SIZE, DEFAULT_CONTINUATION_INDENT)
 
     override fun overrideEditorConfig() = mapOf(
-        INDENT_SIZE_KEY to indentSize,
-        CONTINUATION_INDENT_SIZE_KEY to continuationIndentSize
+        INDENT_SIZE_KEY to indentSize
     )
 
     companion object {
         const val INDENT_SIZE = "indentSize"
-        const val CONTINUATION_INDENT_SIZE = "continuationIndentSize"
     }
 }
