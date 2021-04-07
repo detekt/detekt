d1f25c6854072ea19cc06f26aec4f5df6dc8059c
Zoroark <utybodev@gmail.com>
2021-04-07 16:37:23 +0200

    Add link to official docs in DeprecatedBlockTag

--- detekt-rules-documentation/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/DeprecatedBlockTag.kt
+++ detekt-rules-documentation/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/DeprecatedBlockTag.kt
@@ -13,7 +13,8 @@ import org.jetbrains.kotlin.psi.KtDeclaration
 
 /**
  * This rule reports use of the `@deprecated` block tag in KDoc comments. Deprecation must be specified using a
- * `@Deprecated` annotation as adding a `@deprecated` block tag in KDoc comments has no effect. The `@Deprecated`
+ * `@Deprecated` annotation as adding a `@deprecated` block tag in KDoc comments
+ * [has no effect and is not supported](https://kotlinlang.org/docs/kotlin-doc.html#suppress). The `@Deprecated`
  * annotation constructor has dedicated fields for a message and a type (warning, error, etc.). You can also use the
  * `@ReplaceWith` annotation to specify how to solve the deprecation automatically via the IDE.
  *
