c0419e14d44d359f1c46db5810295b71260c2e49
Markus Schwarz <post@markus-schwarz.net>
2021-04-11 14:19:22 +0200

    Use annotation in LargeClass

--- detekt-rules-complexity/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LargeClassSpec.kt
+++ detekt-rules-complexity/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/LargeClassSpec.kt
@@ -2,18 +2,21 @@ package io.gitlab.arturbosch.detekt.rules.complexity
 
 import io.github.detekt.test.utils.resourceAsPath
 import io.gitlab.arturbosch.detekt.api.SourceLocation
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.assertThat
 import io.gitlab.arturbosch.detekt.test.compileAndLint
 import io.gitlab.arturbosch.detekt.test.lint
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
+private fun subject(threshold: Int) = LargeClass(TestConfig(mapOf("threshold" to threshold)))
+
 class LargeClassSpec : Spek({
 
     describe("nested classes are also considered") {
 
         it("should detect only the nested large class which exceeds threshold 70") {
-            val findings = LargeClass(threshold = 70).lint(resourceAsPath("NestedClasses.kt"))
+            val findings = subject(threshold = 70).lint(resourceAsPath("NestedClasses.kt"))
             assertThat(findings).hasSize(1)
             assertThat(findings).hasSourceLocations(SourceLocation(12, 15))
         }
@@ -30,7 +33,7 @@ class LargeClassSpec : Spek({
                     println()
                 }
             """
-            val rule = LargeClass(threshold = 2)
+            val rule = subject(threshold = 2)
             assertThat(rule.compileAndLint(code)).isEmpty()
         }
     }
