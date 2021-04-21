bad21fa41f8e0d7c8c3ea56dda002e3b05fdb6d1
Matthew Haughton <3flex@users.noreply.github.com>
2021-04-21 21:47:52 +1000

    Introduce `required` and `outputLocation` report properties
    This matches the property names & types used in Gradle's code quality
    plugins.

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/CustomDetektReport.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/CustomDetektReport.kt
@@ -1,18 +1,29 @@
 package io.gitlab.arturbosch.detekt.extensions
 
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
 import java.io.File
+import javax.inject.Inject
 
-class CustomDetektReport {
+open class CustomDetektReport @Inject constructor(objects: ObjectFactory) {
 
     @Internal
     var reportId: String? = null
 
+    @Deprecated("Use outputLocation.set(value)")
+    @get:Internal
+    var destination: File?
+        get() = outputLocation.asFile.getOrNull()
+        set(value) {
+            outputLocation.set(value)
+        }
+
     @OutputFile
-    var destination: File? = null
+    val outputLocation: RegularFileProperty = objects.fileProperty()
 
     override fun toString(): String {
-        return "CustomDetektReport(reportId=$reportId, destination=$destination)"
+        return "CustomDetektReport(reportId=$reportId, outputLocation=$outputLocation)"
     }
 }
--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/DetektAndroid.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/DetektAndroid.kt
@@ -124,10 +124,34 @@ internal fun Project.registerAndroidDetektTask(
         extension.baseline?.existingVariantOrBaseFile(variant.name)?.let { baselineFile ->
             baseline.set(layout.file(project.provider { baselineFile }))
         }
-        reports.xml.setDefaultIfUnset(File(extension.reportsDir, variant.name + ".xml"))
-        reports.html.setDefaultIfUnset(File(extension.reportsDir, variant.name + ".html"))
-        reports.txt.setDefaultIfUnset(File(extension.reportsDir, variant.name + ".txt"))
-        reports.sarif.setDefaultIfUnset(File(extension.reportsDir, variant.name + ".sarif"))
+        reports.xml.outputLocation.convention(
+            layout.projectDirectory.file(
+                providers.provider {
+                    File(extension.reportsDir, variant.name + ".xml").absolutePath
+                }
+            )
+        )
+        reports.html.outputLocation.convention(
+            layout.projectDirectory.file(
+                providers.provider {
+                    File(extension.reportsDir, variant.name + ".html").absolutePath
+                }
+            )
+        )
+        reports.txt.outputLocation.convention(
+            layout.projectDirectory.file(
+                providers.provider {
+                    File(extension.reportsDir, variant.name + ".txt").absolutePath
+                }
+            )
+        )
+        reports.sarif.outputLocation.convention(
+            layout.projectDirectory.file(
+                providers.provider {
+                    File(extension.reportsDir, variant.name + ".sarif").absolutePath
+                }
+            )
+        )
         description = "EXPERIMENTAL: Run detekt analysis for ${variant.name} classes with type resolution"
     }
 
