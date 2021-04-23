238c3cddbdf98599f24cb5f5b8926dca30f43f7c
Zoroark <matthieu.stombellini@outlook.fr>
2021-04-24 00:00:54 +0200

    Add DeprecatedBlockTag rule (#3660)
    * Add DeprecatedBlockTag rule
    
    * Refactor DeprecatedBlockTagSpec tests to remove Spek complexity
    
    * Fix incorrect deprecation test snippets on property set/get
    
    * Add link to official docs in DeprecatedBlockTag
    
    * Extract KDocStyle class out of EndOfSentenceFormat.kt
    
    * Fix EndOfSentenceFormat not visiting declarations on its own
    
    * Add DeprecatedBlockTag to KDocStyle
    
    * Fix code made useless by DeprecatedBlockTag move

---
+++ detekt-rules-documentation/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/DeprecatedBlockTag.kt
@@ -0,0 +1,70 @@
+package io.gitlab.arturbosch.detekt.rules.documentation
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.KtDeclaration
+
+/**
+ * This rule reports use of the `@deprecated` block tag in KDoc comments. Deprecation must be specified using a
+ * `@Deprecated` annotation as adding a `@deprecated` block tag in KDoc comments
+ * [has no effect and is not supported](https://kotlinlang.org/docs/kotlin-doc.html#suppress). The `@Deprecated`
+ * annotation constructor has dedicated fields for a message and a type (warning, error, etc.). You can also use the
+ * `@ReplaceWith` annotation to specify how to solve the deprecation automatically via the IDE.
+ *
+ * <noncompliant>
+ * /**
+ *  * This function prints a message followed by a new line.
+ *  *
+ *  * @deprecated Useless, the Kotlin standard library can already do this. Replace with println.
+ *  */
+ * fun printThenNewline(what: String) {
+ *     // ...
+ * }
+ * </noncompliant>
+ *
+ * <compliant>
+ * /**
+ *  * This function prints a message followed by a new line.
+ *  */
+ * &#64;Deprecated("Useless, the Kotlin standard library can already do this.")
+ * &#64;ReplaceWith("println(what)")
+ * fun printThenNewline(what: String) {
+ *     // ...
+ * }
+ * </compliant>
+ */
+class DeprecatedBlockTag(config: Config = Config.empty) : Rule(config) {
+    override val issue = Issue(
+        "DeprecatedBlockTag",
+        Severity.Defect,
+        "Do not use the @deprecated block tag, which is not supported by KDoc. " +
+            "Use the @Deprecated annotation instead.",
+        Debt.FIVE_MINS
+    )
+
+    override fun visitDeclaration(dcl: KtDeclaration) {
+        super.visitDeclaration(dcl)
+        verify(dcl)
+    }
+
+    fun verify(dcl: KtDeclaration) {
+        dcl.docComment?.getAllSections()?.forEach { section ->
+            section.findTagsByName("deprecated").forEach { tag ->
+                report(
+                    CodeSmell(
+                        issue,
+                        Entity.from(dcl),
+                        "@deprecated tag block does not properly report deprecation in Kotlin, use @Deprecated " +
+                            "annotation instead",
+                        references = listOf(Entity.from(tag))
+                    )
+                )
+            }
+        }
+    }
+}
