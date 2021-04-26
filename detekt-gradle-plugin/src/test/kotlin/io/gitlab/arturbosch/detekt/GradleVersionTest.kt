b4d5e47b9216c6ddfa5a924a9bb3adc8dd844add
Matthew Haughton <3flex@users.noreply.github.com>
2021-04-26 22:50:07 +1000

    Only test a single older Gradle version
    All other tests are run on the current Gradle wrapper version, so there's
    no need to also test a recent version in this test.

--- detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/GradleVersionTest.kt
+++ detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/GradleVersionTest.kt
@@ -5,34 +5,29 @@ import io.gitlab.arturbosch.detekt.testkit.DslTestBuilder.Companion.kotlin
 import org.assertj.core.api.Assertions.assertThat
 import org.gradle.testkit.runner.TaskOutcome
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.dsl.Skip
 import org.spekframework.spek2.style.specification.describe
 
 object GradleVersionTest : Spek({
 
-    val testedGradleVersions = getGradleVersionsUnderTest()
+    val gradleVersion = "5.4"
 
-    describe("detekt plugin running on different Gradle versions") {
+    describe(
+        "detekt plugin running on oldest supported Gradle version",
+        skip = if (getJdkVersion() < 13) Skip.No else Skip.Yes("Gradle $gradleVersion unsupported on this Java version")
+    ) {
         listOf(groovy().dryRun(), kotlin().dryRun()).forEach { builder ->
             describe("using ${builder.gradleBuildName}") {
-                testedGradleVersions.forEach { gradleVersion ->
-                    it("runs on version $gradleVersion of Gradle") {
-                        val gradleRunner = builder.withGradleVersion(gradleVersion).build()
-                        gradleRunner.runDetektTaskAndCheckResult { result ->
-                            assertThat(result.task(":detekt")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
-                        }
+                it("runs on version $gradleVersion of Gradle") {
+                    val gradleRunner = builder.withGradleVersion(gradleVersion).build()
+                    gradleRunner.runDetektTaskAndCheckResult { result ->
+                        assertThat(result.task(":detekt")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
                     }
                 }
             }
         }
     }
 })
-
-private fun getGradleVersionsUnderTest() =
-    if (getJdkVersion() < 13) {
-        listOf("5.4", "6.7")
-    } else {
-        listOf("6.7")
-    }
 
 private fun getJdkVersion(): Int {
     val version = System.getProperty("java.version")
