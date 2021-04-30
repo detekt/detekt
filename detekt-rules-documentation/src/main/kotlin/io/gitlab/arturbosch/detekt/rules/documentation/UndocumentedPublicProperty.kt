3159f46e7eeeda2176095099248539925cc3b4a8
arhont375 <arhont375@users.noreply.github.com>
2021-04-30 21:58:51 +0900

    Revert blank line removal in UndocumentedPublicProperty

--- detekt-rules-documentation/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/UndocumentedPublicProperty.kt
+++ detekt-rules-documentation/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/documentation/UndocumentedPublicProperty.kt
@@ -27,6 +27,7 @@ import org.jetbrains.kotlin.psi.psiUtil.isPublic
  * inline documentation (default: `false`).
  */
 class UndocumentedPublicProperty(config: Config = Config.empty) : Rule(config) {
+
     override val issue = Issue(
         javaClass.simpleName,
         Severity.Maintainability,
