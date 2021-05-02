d27789b1bb36339dd45e240c21112e4b5b1493a5
Markus Schwarz <post@markus-schwarz.net>
2021-05-02 20:28:11 +0200

    use annotations to configure ConstructorParameterNaming

--- detekt-rules-naming/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/naming/ConstructorParameterNaming.kt
+++ detekt-rules-naming/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/naming/ConstructorParameterNaming.kt
@@ -5,10 +5,11 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Debt
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
-import io.gitlab.arturbosch.detekt.api.LazyRegex
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
+import io.gitlab.arturbosch.detekt.api.internal.Configuration
+import io.gitlab.arturbosch.detekt.api.internal.config
 import io.gitlab.arturbosch.detekt.rules.identifierName
 import io.gitlab.arturbosch.detekt.rules.isOverride
 import io.gitlab.arturbosch.detekt.rules.naming.util.isContainingExcludedClassOrObject
@@ -17,11 +18,6 @@ import org.jetbrains.kotlin.psi.psiUtil.isPrivate
 
 /**
  * Reports constructor parameter names which do not follow the specified naming convention are used.
- *
- * @configuration parameterPattern - naming pattern (default: `'[a-z][A-Za-z0-9]*'`)
- * @configuration privateParameterPattern - naming pattern (default: `'[a-z][A-Za-z0-9]*'`)
- * @configuration excludeClassPattern - ignores variables in classes which match this regex (default: `'$^'`)
- * @configuration ignoreOverridden - ignores constructor properties that have the override modifier (default: `true`)
  */
 @ActiveByDefault(since = "1.0.0")
 class ConstructorParameterNaming(config: Config = Config.empty) : Rule(config) {
@@ -33,10 +29,17 @@ class ConstructorParameterNaming(config: Config = Config.empty) : Rule(config) {
         debt = Debt.FIVE_MINS
     )
 
-    private val parameterPattern by LazyRegex(PARAMETER_PATTERN, "[a-z][A-Za-z\\d]*")
-    private val privateParameterPattern by LazyRegex(PRIVATE_PARAMETER_PATTERN, "[a-z][A-Za-z\\d]*")
-    private val excludeClassPattern by LazyRegex(EXCLUDE_CLASS_PATTERN, "$^")
-    private val ignoreOverridden = valueOrDefault(IGNORE_OVERRIDDEN, true)
+    @Configuration("naming pattern")
+    private val parameterPattern: Regex by config("[a-z][A-Za-z0-9]*") { it.toRegex() }
+
+    @Configuration("naming pattern")
+    private val privateParameterPattern: Regex by config("[a-z][A-Za-z0-9]*") { it.toRegex() }
+
+    @Configuration("ignores variables in classes which match this regex")
+    private val excludeClassPattern: Regex by config("$^") { it.toRegex() }
+
+    @Configuration("ignores constructor properties that have the override modifier")
+    private val ignoreOverridden: Boolean by config(true)
 
     override fun visitParameter(parameter: KtParameter) {
         if (parameter.isContainingExcludedClassOrObject(excludeClassPattern) || isIgnoreOverridden(parameter)) {
@@ -69,11 +72,4 @@ class ConstructorParameterNaming(config: Config = Config.empty) : Rule(config) {
     }
 
     private fun isIgnoreOverridden(parameter: KtParameter) = ignoreOverridden && parameter.isOverride()
-
-    companion object {
-        const val PARAMETER_PATTERN = "parameterPattern"
-        const val PRIVATE_PARAMETER_PATTERN = "privateParameterPattern"
-        const val EXCLUDE_CLASS_PATTERN = "excludeClassPattern"
-        const val IGNORE_OVERRIDDEN = "ignoreOverridden"
-    }
 }
