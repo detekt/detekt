459d467c433508d3f691fcb5b370415a865a2359
Matthew Haughton <3flex@users.noreply.github.com>
2021-05-22 08:19:04 +1000

    Build & test on Java 16 (#3698)
    * Update Byte Buddy
    
    This transitive dependency of mockk is updated to a version that supports
    Java 16
    
    * Skip Android tests on JDK 16+
    
    Required until a compatible version of Android Gradle Plugin is released.
    
    * Build & test on Java 16

--- detekt-rules-style/build.gradle.kts
+++ detekt-rules-style/build.gradle.kts
@@ -7,4 +7,11 @@ dependencies {
     compileOnly(projects.detektMetrics)
     testImplementation(projects.detektMetrics)
     testImplementation(projects.detektTest)
+    testImplementation(libs.mockk)
+
+    constraints {
+        testImplementation("net.bytebuddy:byte-buddy:1.11.0") {
+            because("version 1.10.14 (pulled in by mockk 1.11.0) is not Java 16 compatible")
+        }
+    }
 }
