61eb1e01815e73be0c0eee7a35905b9a63ead841
Brais <braisgabin@gmail.com>
2021-05-22 13:33:19 +0200

    Simplify runner

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/v2/Runner.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/v2/Runner.kt
@@ -1,112 +1,47 @@
 package io.gitlab.arturbosch.detekt.core.v2
 
-import io.github.detekt.tooling.api.spec.ProcessingSpec
 import io.gitlab.arturbosch.detekt.api.v2.Detektion
 import io.gitlab.arturbosch.detekt.api.v2.FileProcessListener
 import io.gitlab.arturbosch.detekt.api.v2.ResolvedContext
 import io.gitlab.arturbosch.detekt.api.v2.Rule
-import io.gitlab.arturbosch.detekt.core.ProcessingSettings
-import io.gitlab.arturbosch.detekt.core.tooling.contentToKtFile
-import io.gitlab.arturbosch.detekt.core.tooling.pathToKtFile
-import io.gitlab.arturbosch.detekt.core.tooling.withSettings
 import io.gitlab.arturbosch.detekt.core.v2.providers.ConsoleReportersProvider
-import io.gitlab.arturbosch.detekt.core.v2.providers.ConsoleReportersProviderImpl
 import io.gitlab.arturbosch.detekt.core.v2.providers.FileProcessListenersProvider
-import io.gitlab.arturbosch.detekt.core.v2.providers.FileProcessListenersProviderImpl
 import io.gitlab.arturbosch.detekt.core.v2.providers.KtFilesProvider
-import io.gitlab.arturbosch.detekt.core.v2.providers.KtFilesProviderImpl
 import io.gitlab.arturbosch.detekt.core.v2.providers.OutputReportersProvider
-import io.gitlab.arturbosch.detekt.core.v2.providers.OutputReportersProviderImpl
 import io.gitlab.arturbosch.detekt.core.v2.providers.ReportingModifiersProvider
-import io.gitlab.arturbosch.detekt.core.v2.providers.ReportingModifiersProviderImpl
 import io.gitlab.arturbosch.detekt.core.v2.providers.ResolvedContextProvider
-import io.gitlab.arturbosch.detekt.core.v2.providers.ResolvedContextProviderImpl
-import io.gitlab.arturbosch.detekt.core.v2.providers.ResolvedContextProviderWithBindingContext
 import io.gitlab.arturbosch.detekt.core.v2.providers.RulesProvider
-import io.gitlab.arturbosch.detekt.core.v2.providers.RulesProviderImpl
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.runBlocking
-import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.resolve.BindingContext
-import java.nio.file.Path
-import java.nio.file.Paths
 
-class Runner(
-    private val spec: ProcessingSpec // This class probably needs a refactor too
-) : Detekt { // TODO all of this should be coroutines
-    override fun run(): Detektion {
-        return spec.withSettings {
-            runBlocking {
-                run(
-                    KtFilesProviderImpl(this@withSettings),
-                    ResolvedContextProviderImpl(environment, classpath),
-                )
-            }
-        }
-    }
-
-    override fun run(path: Path): Detektion {
-        return spec.withSettings {
-            runBlocking {
-                run(
-                    { pathToKtFile(path).invoke(this@withSettings).asFlow() },
-                    ResolvedContextProviderImpl(environment, classpath),
-                )
-            }
-        }
-    }
-
-    override fun run(sourceCode: String, filename: String): Detektion {
-        return spec.withSettings {
-            runBlocking {
-                run(
-                    { contentToKtFile(sourceCode, Paths.get(filename)).invoke(this@withSettings).asFlow() },
-                    ResolvedContextProviderImpl(environment, classpath),
-                )
-            }
-        }
-    }
-
-    override fun run(files: Collection<KtFile>, bindingContext: BindingContext): Detektion { // TODO Flow<KtFile>
-        return spec.withSettings {
-            runBlocking {
-                run(
-                    files::asFlow,
-                    ResolvedContextProviderWithBindingContext(
-                        bindingContext,
-                        environment.configuration.languageVersionSettings
-                    ),
-                )
-            }
-        }
-    }
-}
-
-private suspend fun ProcessingSettings.run(
+suspend fun run(
     filesProvider: KtFilesProvider,
     resolvedContextProvider: ResolvedContextProvider,
+    ruleProvider: RulesProvider,
+    fileProcessListenersProvider: FileProcessListenersProvider,
+    reportingModifiersProvider: ReportingModifiersProvider,
+    consoleReportersProvider: ConsoleReportersProvider,
+    outputReportersProvider: OutputReportersProvider,
 ): Detektion {
     return run(
         filesProvider,
         resolvedContextProvider,
-        RulesProviderImpl(pluginLoader, config),
-        FileProcessListenersProviderImpl(pluginLoader, this),
-        ReportingModifiersProviderImpl(pluginLoader, this),
-        ConsoleReportersProviderImpl(pluginLoader, this),
-        OutputReportersProviderImpl(pluginLoader, this),
+        ruleProvider,
+        fileProcessListenersProvider,
+        reportingModifiersProvider,
+        consoleReportersProvider,
+        outputReportersProvider,
         ::analyze,
     )
 }
 
-private suspend fun run(
+internal suspend fun run(
     filesProvider: KtFilesProvider,
     resolvedContextProvider: ResolvedContextProvider,
     ruleProvider: RulesProvider,
