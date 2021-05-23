09d842451a469ce050d236888073ca696f023d11
Chao Zhang <zhangchao6865@gmail.com>
2021-05-23 08:45:34 -0700

    Update detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/AutoCorrectable.kt
    Co-authored-by: M Schalk <30376729+schalkms@users.noreply.github.com>

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/AutoCorrectable.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/AutoCorrectable.kt
@@ -8,7 +8,7 @@ package io.gitlab.arturbosch.detekt.api.internal
 @Retention(AnnotationRetention.SOURCE)
 annotation class AutoCorrectable(
     /**
-     *  The Detekt version the rule was auto-correctable in the following format: <major>.<minor>.<patch>,
+     *  The Detekt version since the rule is auto-correctable in the following format: <major>.<minor>.<patch>,
      *  where major, minor and patch are non-negative integer numbers.
      */
     val since: String
