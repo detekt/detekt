382741b9113a356a556bd9b90a7c0cb54f120b5f
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-05-24 08:27:13 +0200

    Add a test case without `--dry-run` argument to make the test invoke default `DetektInvoker` implementation

--- detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/ConfigurationCacheTest.kt
+++ detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/ConfigurationCacheTest.kt
@@ -7,18 +7,23 @@ import org.spekframework.spek2.style.specification.describe
 
 object ConfigurationCacheTest : Spek({
     describe("Detekt task") {
-        it("can be loaded from the configuration cache") {
-            val gradleRunner = DslTestBuilder.kotlin()
-                .dryRun()
-                .build()
+        listOf(
+            "regular invocation" to arrayOf("detekt"),
+            "dry-run invocation" to arrayOf("detekt", "-Pdetekt-dry-run=true"),
+        ).forEach { (context, arguments) ->
+            context("given $context") {
+                it("can be loaded from the configuration cache") {
+                    val gradleRunner = DslTestBuilder.kotlin().build()
 
-            // First run primes the cache
-            gradleRunner.runTasks("--configuration-cache", "detekt")
+                    // First run primes the cache
+                    gradleRunner.runTasks("--configuration-cache", *arguments)
 
-            // Second run reuses the cache
-            val result = gradleRunner.runTasks("--configuration-cache", "detekt")
+                    // Second run reuses the cache
+                    val result = gradleRunner.runTasks("--configuration-cache", *arguments)
 
-            assertThat(result.output).contains("Reusing configuration cache.")
+                    assertThat(result.output).contains("Reusing configuration cache.")
+                }
+            }
         }
     }
 
