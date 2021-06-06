52fcd6e457af58344bad1a3b339961de831f69ef
Markus Schwarz <post@markus-schwarz.net>
2021-06-06 14:52:31 +0200

    remove checks that are no longer necessary after switching to annotations

--- detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/ConfigAssert.kt
+++ detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/ConfigAssert.kt
@@ -2,13 +2,11 @@ package io.gitlab.arturbosch.detekt.core
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Rule
-import io.gitlab.arturbosch.detekt.api.internal.Configuration
 import io.gitlab.arturbosch.detekt.core.config.DefaultConfig
 import io.gitlab.arturbosch.detekt.core.config.YamlConfig
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
 import org.reflections.Reflections
-import java.lang.reflect.Field
 import java.lang.reflect.Modifier
 
 class ConfigAssert(
@@ -38,32 +36,8 @@ class ConfigAssert(
             if (ymlDeclaration.keys.size != 1) {
                 fail<String>("${ruleClass.simpleName} rule is not correctly defined in ${DefaultConfig.RESOURCE_NAME}")
             }
-
-            @Suppress("UNCHECKED_CAST")
-            val options = ymlDeclaration.iterator().next().value as HashMap<String, *>
-            checkOptions(options, ruleClass)
         }
     }
-
-    private fun checkOptions(ymlOptions: HashMap<String, *>, ruleClass: Class<out Rule>) {
-        if (ruleClass.isConfiguredWithAnnotations()) return
-
-        val configFields = ruleClass.declaredFields.filter { isPublicStaticFinal(it) && it.name != "Companion" }
-        var filter = ymlOptions.filterKeys { it !in allowedOptions }
-        if (filter.containsKey(THRESHOLD)) {
-            assertThat(ruleClass.superclass.simpleName).isEqualTo(THRESHOLD_RULE)
-            filter = filter.filterKeys { it != THRESHOLD }
-        }
-        for (ymlOption in filter) {
-            val configField = configFields.singleOrNull { ymlOption.key == it.get(null) }
-            if (configField == null) {
-                fail<String>("${ymlOption.key} option for ${ruleClass.simpleName} rule is not correctly defined")
-            }
-        }
-    }
-
-    private fun Class<out Rule>.isConfiguredWithAnnotations(): Boolean =
-        declaredMethods.any { it.isAnnotationPresent(Configuration::class.java) }
 
     private fun getYmlRuleConfig() = config.subConfig(name) as? YamlConfig
         ?: error("yaml config expected but got ${config.javaClass}")
@@ -73,12 +47,4 @@ class ConfigAssert(
             .getSubTypesOf(Rule::class.java)
             .filter { !Modifier.isAbstract(it.modifiers) }
     }
-
-    private fun isPublicStaticFinal(it: Field): Boolean {
-        val modifiers = it.modifiers
-        return Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers) && Modifier.isFinal(modifiers)
-    }
 }
-
-private const val THRESHOLD_RULE = "ThresholdRule"
-private const val THRESHOLD = "threshold"
