159aa720da43ea9edffd38602032cea8cb2601b9
Markus Schwarz <post@markus-schwarz.net>
2021-06-29 11:28:45 +0200

    Add ReferentialEquality rule

---
+++ detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/ReferentialEquality.kt
@@ -0,0 +1,73 @@
+package io.gitlab.arturbosch.detekt.rules.bugs
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.api.internal.Configuration
+import io.gitlab.arturbosch.detekt.api.internal.RequiresTypeResolution
+import io.gitlab.arturbosch.detekt.api.internal.config
+import io.gitlab.arturbosch.detekt.rules.fqNameOrNull
+import org.jetbrains.kotlin.lexer.KtTokens.EQEQEQ
+import org.jetbrains.kotlin.lexer.KtTokens.EXCLEQEQEQ
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.calls.callUtil.getType
+
+/**
+ * Kotlin supports two types of equality: structural equality and referential equality. While there are
+ * use cases for both, checking for referential equality for some types (such as `String` or `List` is
+ * likely not intentional and may case unexpected results.
+ *
+ * <noncompliant>
+ *     val areEqual = "aString" === otherString
+ *     val areNotEqual = "aString" !== otherString
+ * </noncompliant>
+ *
+ * <compliant>
+ *     val areEqual = "aString" == otherString
+ *     val areNotEqual = "aString" != otherString
+ * </compliant>
+ */
+@RequiresTypeResolution
+class ReferentialEquality(config: Config) : Rule(config) {
+
+    override val issue = Issue(
+        "ReferentialEquality",
+        Severity.Warning,
+        "",
+        Debt.FIVE_MINS
+    )
+
+    @Configuration(
+        "specifies those types for which referential equality checks are considered a rule violation. " +
+            "The types are defined by a list of regular expressions that match the fully qualified type name."
+    )
+    private val forbiddenTypesRegex: List<Regex> by config(
+        listOf(
+            "kotlin.String"
+        )
+    ) { it.map(String::toRegex) }
+
+    override fun visitBinaryExpression(expression: KtBinaryExpression) {
+        super.visitBinaryExpression(expression)
+        if (bindingContext == BindingContext.EMPTY) return
+        if (expression.operationToken != EQEQEQ && expression.operationToken != EXCLEQEQEQ) return
+
+        val checkedType = expression.left?.getType(bindingContext)?.fqNameOrNull() ?: return
+        val fullyQualifiedType = checkedType.asString()
+
+        if (forbiddenTypesRegex.any { it.matches(fullyQualifiedType) }) {
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.from(expression),
+                    "Checking referential equality may lead to unwanted results."
+                )
+            )
+        }
+    }
+}
---
+++ detekt-rules-errorprone/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/ReferentialEqualitySpec.kt
@@ -0,0 +1,87 @@
+package io.gitlab.arturbosch.detekt.rules.bugs
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.rules.setupKotlinEnvironment
+import io.gitlab.arturbosch.detekt.test.TestConfig
+import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object ReferentialEqualitySpec : Spek({
+    setupKotlinEnvironment()
+
+    val env: KotlinCoreEnvironment by memoized()
+
+    describe("ReferentialEquality with defaults") {
+        val subject by memoized { ReferentialEquality(Config.empty) }
+        it("reports usage of === for strings") {
+            val code = """
+                val s = "a string" 
+                val b = s === "something"
+                fun f(other: String) = s === other
+                fun g(other: String) = if (s === other) 1 else 2
+            """.trimIndent()
+
+            val actual = subject.compileAndLintWithContext(env, code)
+
+            assertThat(actual).hasSize(3)
+        }
+        it("reports usage of === with nullable") {
+            val code = """
+                var s: String? = "a string" 
+                val b1 = s === "something"
+                val b2 = "something" === s
+                fun f(other: String) = s === other
+                fun g(other: String?) = s === other
+            """.trimIndent()
+
+            val actual = subject.compileAndLintWithContext(env, code)
+
+            assertThat(actual).hasSize(4)
+        }
+        it("reports usage of !== with nullable") {
+            val code = """
+                var s: String = "a string" 
+                val b = s !== "something"
+            """.trimIndent()
+
+            val actual = subject.compileAndLintWithContext(env, code)
+
+            assertThat(actual).hasSize(1)
+        }
+    }
+
+    describe("ReferentialEquality enabled for all types") {
+        val subject by memoized { ReferentialEquality(TestConfig("forbiddenTypesRegex" to ".*")) }
+        it("reports usage of === for strings") {
+            val code = """
+                val s = "a string" 
+                val i = 1 
+                val list = listOf(1)
+                val b = s === "other" || i === 42 || list === listOf(2)
+            """.trimIndent()
+
+            val actual = subject.compileAndLintWithContext(env, code)
+
+            assertThat(actual).hasSize(3)
+        }
+    }
+
+    describe("ReferentialEquality enabled for all lists") {
+        val pattern = """kotlin\.collections\..*List"""
+        val subject by memoized { ReferentialEquality(TestConfig("forbiddenTypesRegex" to pattern)) }
+        it("reports usage of === for strings") {
+            val code = """
+                val list = listOf(1)
+                val mutableList = mutableListOf(1)
+                val b = list === listOf(2) || mutableList === mutableListOf(2)
+            """.trimIndent()
+
+            val actual = subject.compileAndLintWithContext(env, code)
+
+            assertThat(actual).hasSize(2)
+        }
+    }
+})
