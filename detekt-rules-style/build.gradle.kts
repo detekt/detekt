d81d6a2979261c3d81d4b045f6bb9205f5b80e36
marschwar <marschwar@users.noreply.github.com>
2021-07-11 11:55:37 +0200

    Update mockk (#3937)
    Co-authored-by: Markus Schwarz <post@markus-schwarz.net>

--- detekt-rules-style/build.gradle.kts
+++ detekt-rules-style/build.gradle.kts
@@ -8,10 +8,4 @@ dependencies {
     testImplementation(projects.detektMetrics)
     testImplementation(projects.detektTest)
     testImplementation(libs.mockk)
-
-    constraints {
-        testImplementation("net.bytebuddy:byte-buddy:1.11.5") {
-            because("version 1.10.14 (pulled in by mockk 1.11.0) is not Java 16 compatible")
-        }
-    }
 }
