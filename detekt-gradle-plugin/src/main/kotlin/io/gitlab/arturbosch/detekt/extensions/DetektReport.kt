90bcb32629490d9c8b2fdec3a3f8fa7e9f65ec90
Markus Schwarz <post@markus-schwarz.net>
2021-08-09 20:55:00 +0200

    convert DetektReport to real data class

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/DetektReport.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/DetektReport.kt
@@ -2,16 +2,11 @@ package io.gitlab.arturbosch.detekt.extensions
 
 import java.io.File
 
-data class DetektReport(val type: DetektReportType) {
-
-    var enabled: Boolean? = null
-
+data class DetektReport(
+    val type: DetektReportType,
+    var enabled: Boolean? = null,
     var destination: File? = null
-
-    override fun toString(): String {
-        return "DetektReport(type='$type', enabled=$enabled, destination=$destination)"
-    }
-
+) {
     companion object {
         const val DEFAULT_FILENAME = "detekt"
     }
