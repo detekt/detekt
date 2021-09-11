a893dab1eeb68e98fbfaf9f93bc46bd2fbc670d6
Brais <braisgabin@gmail.com>
2021-09-11 17:42:08 +0200

    Make deprecated properties list dynamic

--- detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/DeprecatedPrinter.kt
+++ detekt-generator/src/main/kotlin/io/gitlab/arturbosch/detekt/generator/printer/DeprecatedPrinter.kt
@@ -1,15 +1,27 @@
 package io.gitlab.arturbosch.detekt.generator.printer
 
+import io.gitlab.arturbosch.detekt.generator.collection.Configuration
+import io.gitlab.arturbosch.detekt.generator.collection.Rule
 import io.gitlab.arturbosch.detekt.generator.collection.RuleSetPage
 
 object DeprecatedPrinter : DocumentationPrinter<List<RuleSetPage>> {
+    @Suppress("NestedBlockDepth")
     override fun print(item: List<RuleSetPage>): String {
-        return """
-            complexity>LongParameterList>threshold=Use 'functionThreshold' and 'constructorThreshold' instead
-            empty-blocks>EmptyFunctionBlock>ignoreOverriddenFunctions=Use 'ignoreOverridden' instead
-            naming>FunctionParameterNaming>ignoreOverriddenFunctions=Use 'ignoreOverridden' instead
-            naming>MemberNameEqualsClassName>ignoreOverriddenFunction=Use 'ignoreOverridden' instead
-            
-        """.trimIndent()
+        val builder = StringBuilder()
+        item.forEach { ruleSet ->
+            ruleSet.rules.forEach { rule ->
+                rule.configuration.forEach { configuration ->
+                    if (configuration.isDeprecated()) {
+                        builder.appendLine(writeProperty(ruleSet, rule, configuration))
+                    }
+                }
+            }
+        }
+        return builder.toString()
     }
+}
+
+private fun writeProperty(ruleSet: RuleSetPage, rule: Rule, configuration: Configuration): String {
+    @Suppress("UnsafeCallOnNullableType")
+    return "${ruleSet.ruleSet.name}>${rule.name}>${configuration.name}=${configuration.deprecated!!}"
 }
