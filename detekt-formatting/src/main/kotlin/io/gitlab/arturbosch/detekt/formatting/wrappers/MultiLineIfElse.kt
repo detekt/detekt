e0fa7a3e81312d6edf052b13efc82998c59972a6
Matthew Haughton <3flex@users.noreply.github.com>
2021-10-16 11:24:46 +1100

    Update detekt-formatting default rules (#4179)
    * Enable detekt-formatting Indentation rule by default
    
    This is a ktlint-ruleset-standard rule, run by ktlint by default.
    
    * Enable detekt-formatting ImportOrdering rule by default
    
    This is a ktlint-ruleset-standard rule, run by ktlint by default.
    
    * Disable detekt-formatting MultiLineIfElse rule by default
    
    This is a ktlint-ruleset-experimental rule, not run by ktlint by default.
    
    * Disable detekt-formatting PackageName rule by default
    
    This is a ktlint-ruleset-experimental rule, not run by ktlint by default.
    
    * Refactor KtLintMultiRule so standard & experimental rules are clearly shown

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/MultiLineIfElse.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/MultiLineIfElse.kt
@@ -2,14 +2,12 @@ package io.gitlab.arturbosch.detekt.formatting.wrappers
 
 import com.pinterest.ktlint.ruleset.experimental.MultiLineIfElseRule
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
 import io.gitlab.arturbosch.detekt.api.internal.AutoCorrectable
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
 /**
  * See <a href="https://ktlint.github.io/#rule-modifier-order">ktlint-website</a> for documentation.
  */
-@ActiveByDefault(since = "1.0.0")
 @AutoCorrectable(since = "1.0.0")
 class MultiLineIfElse(config: Config) : FormattingRule(config) {
 
