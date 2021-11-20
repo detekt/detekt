7179b6d8e013a295529888ac471accc3f453a93d
Brais <braisgabin@gmail.com>
2021-11-20 20:44:45 +0100

    Remove DefaultConfig

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/DefaultConfigProvider.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/DefaultConfigProvider.kt
@@ -2,9 +2,10 @@ package io.gitlab.arturbosch.detekt.core.tooling
 
 import io.github.detekt.tooling.api.DefaultConfigurationProvider
 import io.github.detekt.tooling.api.spec.ProcessingSpec
+import io.github.detekt.utils.getSafeResourceAsStream
 import io.github.detekt.utils.openSafeStream
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.core.config.DefaultConfig
+import io.gitlab.arturbosch.detekt.core.config.YamlConfig
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
@@ -16,10 +17,18 @@ class DefaultConfigProvider : DefaultConfigurationProvider {
         this.spec = spec
     }
 
-    override fun get(): Config = DefaultConfig.newInstance()
+    override fun get(): Config = spec.getDefaultConfiguration()
 
     override fun copy(targetLocation: Path) {
-        val configUrl = javaClass.getResource("/${DefaultConfig.RESOURCE_NAME}")!!
+        val configUrl = javaClass.getResource(DEFAULT_CONFIG_RESOURCES_PATH)!!
         Files.copy(configUrl.openSafeStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING)
     }
 }
+
+fun ProcessingSpec.getDefaultConfiguration(): Config {
+    return requireNotNull(javaClass.getSafeResourceAsStream(DEFAULT_CONFIG_RESOURCES_PATH))
+        .reader()
+        .use(YamlConfig::load)
+}
+
+private const val DEFAULT_CONFIG_RESOURCES_PATH = "/default-detekt-config.yml"
