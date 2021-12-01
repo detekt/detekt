ca65fb01f519fae9607b9c75febcc3b01286ffec
Severn Everett <severn.everett@gmail.com>
2021-12-01 14:46:56 +0100

    Removed EmptyIfElseBlock and replaced with a shared utility function

--- detekt-rules-empty/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyElseBlock.kt
+++ detekt-rules-empty/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyElseBlock.kt
@@ -1,6 +1,8 @@
 package io.gitlab.arturbosch.detekt.rules.empty
 
+import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
 import org.jetbrains.kotlin.psi.KtIfExpression
 
@@ -8,6 +10,17 @@ import org.jetbrains.kotlin.psi.KtIfExpression
  * Reports empty `else` blocks. Empty blocks of code serve no purpose and should be removed.
  */
 @ActiveByDefault(since = "1.0.0")
-class EmptyElseBlock(config: Config) : EmptyIfElseBlock(config, KtIfExpression::getElse) {
-    override val blockTypeStr = "else"
+class EmptyElseBlock(config: Config) : EmptyRule(config) {
+    override fun visitIfExpression(expression: KtIfExpression) {
+        super.visitIfExpression(expression)
+        expression.`else`?.addFindingIfBlockExprIsEmpty() ?: checkThenBodyForLoneSemicolon(expression) {
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.from(it),
+                    "This else block is empty and can be removed."
+                )
+            )
+        }
+    }
 }
--- detekt-rules-empty/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyIfBlock.kt
+++ detekt-rules-empty/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyIfBlock.kt
@@ -1,6 +1,8 @@
 package io.gitlab.arturbosch.detekt.rules.empty
 
+import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
 import org.jetbrains.kotlin.psi.KtIfExpression
 
@@ -8,6 +10,18 @@ import org.jetbrains.kotlin.psi.KtIfExpression
  * Reports empty `if` blocks. Empty blocks of code serve no purpose and should be removed.
  */
 @ActiveByDefault(since = "1.0.0")
-class EmptyIfBlock(config: Config) : EmptyIfElseBlock(config, KtIfExpression::getThen) {
-    override val blockTypeStr = "if"
+class EmptyIfBlock(config: Config) : EmptyRule(config) {
+    override fun visitIfExpression(expression: KtIfExpression) {
+        super.visitIfExpression(expression)
+        expression.then?.addFindingIfBlockExprIsEmpty() ?: checkThenBodyForLoneSemicolon(expression) {
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.from(it),
+                    "This if block is empty and can be removed."
+                )
+            )
+        }
+    }
 }
+
