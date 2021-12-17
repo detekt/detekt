05e0327dd5ef7006d689f5ded40c49cc038709e8
Matthew Haughton <3flex@users.noreply.github.com>
2021-12-17 22:50:14 +1100

    Use a simpler workaround for gradle/gradle#16774

--- detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/DetektJvmSpec.kt
+++ detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/DetektJvmSpec.kt
@@ -5,19 +5,11 @@ import io.gitlab.arturbosch.detekt.testkit.DslGradleRunner
 import io.gitlab.arturbosch.detekt.testkit.ProjectLayout
 import io.gitlab.arturbosch.detekt.testkit.triggerEvaluation
 import org.assertj.core.api.Assertions.assertThat
-import org.gradle.api.Project
-import org.gradle.api.internal.project.DefaultProject
-import org.gradle.api.provider.Provider
-import org.gradle.build.event.BuildEventsListenerRegistry
-import org.gradle.internal.service.DefaultServiceRegistry
-import org.gradle.internal.service.scopes.ProjectScopeServices
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.repositories
-import org.gradle.tooling.events.OperationCompletionListener
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import java.util.concurrent.atomic.AtomicReference
 
 object DetektJvmSpec : Spek({
     describe("When applying detekt in a JVM project") {
@@ -28,7 +20,6 @@ object DetektJvmSpec : Spek({
                 buildFileName = "build.gradle",
                 baselineFiles = listOf("detekt-baseline.xml", "detekt-baseline-main.xml", "detekt-baseline-test.xml"),
                 projectScript = {
-                    addFakeService()
                     apply<KotlinPluginWrapper>()
                     apply<DetektPlugin>()
                     repositories {
@@ -146,28 +137,3 @@ object DetektJvmSpec : Spek({
         }
     }
 })
-
-internal class FakeBuildEventsListenerRegistry : BuildEventsListenerRegistry {
-    override fun onTaskCompletion(provider: Provider<out OperationCompletionListener>) {
-        // Unit
-    }
-}
-
-// https://github.com/gradle/gradle/issues/16774
-@Suppress("TooGenericExceptionThrown")
-private fun Project.addFakeService() {
-    try {
-        val projectScopeServices = (this as DefaultProject).services as ProjectScopeServices
-        val state = ProjectScopeServices::class.java.superclass.getDeclaredField("state")
-        state.isAccessible = true
-        val stateValue = state[projectScopeServices] as AtomicReference<Any>
-        val enumClass = Class.forName(DefaultServiceRegistry::class.java.name + "\$State")
-        stateValue.set(enumClass.enumConstants[0])
-
-        // add service and set state so that future mutations are not allowed
-        projectScopeServices.add(BuildEventsListenerRegistry::class.java, FakeBuildEventsListenerRegistry())
-        stateValue.set(enumClass.enumConstants[1])
-    } catch (e: Throwable) {
-        throw RuntimeException(e)
-    }
-}
