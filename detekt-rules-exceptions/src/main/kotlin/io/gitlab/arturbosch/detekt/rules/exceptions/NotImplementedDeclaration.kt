df8e4da3ecafd2ecd865f9838d666a768b26e1a6
M Schalk <30376729+schalkms@users.noreply.github.com>
2021-12-27 23:39:40 +0100

    Formulate rule/exceptions descriptions consistently
    reference #4384

--- detekt-rules-exceptions/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/NotImplementedDeclaration.kt
+++ detekt-rules-exceptions/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/exceptions/NotImplementedDeclaration.kt
@@ -32,9 +32,9 @@ class NotImplementedDeclaration(config: Config = Config.empty) : Rule(config) {
     override val issue = Issue(
         "NotImplementedDeclaration",
         Severity.CodeSmell,
-        "The NotImplementedDeclaration should only be used when a method stub is necessary. " +
+        "The `NotImplementedDeclaration` should only be used when a method stub is necessary. " +
             "This defers the development of the functionality of this function. " +
-            "Hence, the NotImplementedDeclaration should only serve as a temporary declaration. " +
+            "Hence, the `NotImplementedDeclaration` should only serve as a temporary declaration. " +
             "Before releasing, this type of declaration should be removed.",
         Debt.TWENTY_MINS
     )
