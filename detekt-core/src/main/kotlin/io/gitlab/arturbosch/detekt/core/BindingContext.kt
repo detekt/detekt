400f15d9564865097fcb89324b70c3c84d817e80
Brais <braisgabin@gmail.com>
2021-12-29 15:30:35 +0100

    Make detekt less noisy

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/BindingContext.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/BindingContext.kt
@@ -3,8 +3,8 @@ package io.gitlab.arturbosch.detekt.core
 import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSourceLocation
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.common.messages.PlainTextMessageRenderer
-import org.jetbrains.kotlin.cli.common.messages.PrintingMessageCollector
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.cli.jvm.compiler.NoScopeRecordCliBindingTrace
 import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
@@ -12,20 +12,26 @@ import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.lazy.declarations.FileBasedDeclarationProviderFactory
-import java.io.PrintStream
 
 internal fun generateBindingContext(
     environment: KotlinCoreEnvironment,
     classpath: List<String>,
     files: List<KtFile>,
+    debugPrinter: (() -> String) -> Unit,
+    warningPrinter: (String) -> Unit,
 ): BindingContext {
     if (classpath.isEmpty()) {
         return BindingContext.EMPTY
     }
 
+    val messageCollector = DetektMessageCollector(
+        debugPrinter = debugPrinter,
+        minSeverity = CompilerMessageSeverity.ERROR,
+    )
+
     val analyzer = AnalyzerWithCompilerReport(
-        DetektMessageCollector(minSeverity = CompilerMessageSeverity.ERROR),
-        environment.configuration.languageVersionSettings
+        messageCollector,
+        environment.configuration.languageVersionSettings,
     )
     analyzer.analyzeAndReport(files) {
         TopDownAnalyzerFacadeForJVM.analyzeFilesWithJavaIntegration(
@@ -37,17 +43,28 @@ internal fun generateBindingContext(
             ::FileBasedDeclarationProviderFactory
         )
     }
+
+    if (messageCollector.messages > 0) {
+        warningPrinter(
+            "The BindingContext was created with ${messageCollector.messages} issues. " +
+                "Run detekt with --debug to see the error messages."
+        )
+    }
+
     return analyzer.analysisResult.bindingContext
 }
 
-internal class DetektMessageCollector(
-    errorStream: PrintStream = System.err,
-    verbose: Boolean = false,
-    private val minSeverity: CompilerMessageSeverity = CompilerMessageSeverity.ERROR
-) : PrintingMessageCollector(errorStream, DetektMessageRenderer, verbose) {
+private class DetektMessageCollector(
+    private val debugPrinter: (() -> String) -> Unit,
+    private val minSeverity: CompilerMessageSeverity,
+) : MessageCollector by MessageCollector.NONE {
+    var messages = 0
+        private set
+
     override fun report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageSourceLocation?) {
         if (severity.ordinal <= minSeverity.ordinal) {
-            super.report(severity, message, location)
+            debugPrinter { DetektMessageRenderer.render(severity, message, location) }
+            messages++
         }
     }
 }
