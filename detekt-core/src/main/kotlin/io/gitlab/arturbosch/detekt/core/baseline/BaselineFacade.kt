2a47b8afff620de9b7c77cf96fbe7edfeb91830f
Brais <braisgabin@gmail.com>
2022-01-04 10:22:46 +0100

    Only write the file if it is different than the current one

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/baseline/BaselineFacade.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/baseline/BaselineFacade.kt
@@ -19,19 +19,17 @@ class BaselineFacade {
 
     fun createOrUpdate(baselineFile: Path, findings: List<Finding>) {
         val ids = findings.map { it.baselineId }.toSortedSet()
-        val exists = baselineExists(baselineFile)
-        val baseline = if (exists) {
-            Baseline.load(baselineFile).copy(currentIssues = ids)
+        val oldBaseline = if (baselineExists(baselineFile)) {
+            Baseline.load(baselineFile)
         } else {
-            Baseline(emptySet(), ids)
+            Baseline(emptySet(), emptySet())
         }
-        if (baseline.isNotEmpty() || exists) {
+        val baseline = oldBaseline.copy(currentIssues = ids)
+        if (oldBaseline != baseline) {
             baselineFile.parent?.let { Files.createDirectories(it) }
             BaselineFormat().write(baseline, baselineFile)
         }
     }
 
     private fun baselineExists(baseline: Path) = baseline.exists() && baseline.isFile()
-
-    private fun Baseline.isNotEmpty() = currentIssues.isNotEmpty() || manuallySuppressedIssues.isNotEmpty()
 }
