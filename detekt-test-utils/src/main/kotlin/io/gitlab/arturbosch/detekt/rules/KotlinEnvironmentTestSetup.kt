a627da97c994dc0f9534b34674a7529ee039627d
Matthew Haughton <3flex@users.noreply.github.com>
2022-01-16 08:32:21 +1100

    Migrate custom-checks tests to JUnit 5 (#4467)
    * Setup JUnit 5
    
    * Migrate custom-checks tests to JUnit
    
    * Set the default JUnit 5 test instance lifecycle mode to "per_class"
    
    * Use extension instead of base class to inject KotlinCoreEnvironment
    
    Co-authored-by: marschwar <marschwar@users.noreply.github.com>
    
    * Update contributor guidelines to reflect usage of JUnit 5
    
    * Fix style issues
    
    * Update detekt-test-utils API definition
    
    * Update .github/CONTRIBUTING.md
    
    Co-authored-by: marschwar <marschwar@users.noreply.github.com>
    Co-authored-by: Chao Zhang <zhangchao6865@gmail.com>

--- detekt-test-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KotlinEnvironmentTestSetup.kt
+++ detekt-test-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KotlinEnvironmentTestSetup.kt
@@ -1,7 +1,12 @@
 package io.gitlab.arturbosch.detekt.rules
 
+import io.github.detekt.test.utils.KotlinCoreEnvironmentWrapper
 import io.github.detekt.test.utils.createEnvironment
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.ParameterContext
+import org.junit.jupiter.api.extension.ParameterResolver
 import org.spekframework.spek2.dsl.Root
 import org.spekframework.spek2.lifecycle.CachingMode
 import java.nio.file.Path
@@ -16,4 +21,37 @@ fun Root.setupKotlinEnvironment(additionalJavaSourceRootPath: Path? = null) {
     // name is used for delegation
     @Suppress("UNUSED_VARIABLE")
     val env: KotlinCoreEnvironment by memoized(CachingMode.EACH_GROUP) { wrapper.env }
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@ExtendWith(KotlinEnvironmentResolver::class)
+annotation class KotlinCoreEnvironmentTest
+
+internal class KotlinEnvironmentResolver : ParameterResolver {
+    private var ExtensionContext.wrapper: CloseableWrapper?
+        get() = getStore(NAMESPACE)[WRAPPER_KEY, CloseableWrapper::class.java]
+        set(value) = getStore(NAMESPACE).put(WRAPPER_KEY, value)
+
+    override fun supportsParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Boolean {
+        return parameterContext.parameter.type == KotlinCoreEnvironment::class.java
+    }
+
+    override fun resolveParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Any {
+        val closeableWrapper = extensionContext.wrapper
+            ?: CloseableWrapper(createEnvironment()).also { extensionContext.wrapper = it }
+        return closeableWrapper.wrapper.env
+    }
+
+    companion object {
+        private val NAMESPACE = ExtensionContext.Namespace.create("KotlinCoreEnvironment")
+        private const val WRAPPER_KEY = "wrapper"
+    }
+
+    private class CloseableWrapper(val wrapper: KotlinCoreEnvironmentWrapper) :
+        ExtensionContext.Store.CloseableResource {
+        override fun close() {
+            wrapper.dispose()
+        }
+    }
 }
