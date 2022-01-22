aa6481851400493706d16ac421855c217fba30bf
Matthew Haughton <3flex@users.noreply.github.com>
2022-01-22 14:50:24 +1100

    Remove unused dependencies

--- detekt-test-utils/build.gradle.kts
+++ detekt-test-utils/build.gradle.kts
@@ -7,8 +7,6 @@ dependencies {
     api(libs.kotlin.stdlibJdk8)
     compileOnly(libs.spek.dsl)
     implementation(projects.detektParser)
-    implementation(projects.detektPsiUtils)
-    implementation(libs.kotlin.scriptRuntime)
     implementation(libs.kotlin.scriptUtil)
     implementation(libs.kotlin.scriptingCompilerEmbeddable)
     implementation(libs.junit.api)
