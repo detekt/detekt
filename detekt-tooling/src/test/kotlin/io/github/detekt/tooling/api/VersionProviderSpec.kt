77204be660ca732a610ffe03f16309fbb3166c7f
Matthew Haughton <3flex@users.noreply.github.com>
2022-01-26 00:56:20 +1100

    Migrate detekt-tooling tests to JUnit 5 (#4524)

--- detekt-tooling/src/test/kotlin/io/github/detekt/tooling/api/VersionProviderSpec.kt
+++ detekt-tooling/src/test/kotlin/io/github/detekt/tooling/api/VersionProviderSpec.kt
@@ -1,18 +1,15 @@
 package io.github.detekt.tooling.api
 
 import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.junit.jupiter.api.Test
 
-class VersionProviderSpec : Spek({
+class VersionProviderSpec {
 
-    describe("version provider") {
-
-        it("retries current running detekt version") {
-            assertThat(VersionProvider.load().current()).isEqualTo("unknown")
-        }
+    @Test
+    fun `retries current running detekt version`() {
+        assertThat(VersionProvider.load().current()).isEqualTo("unknown")
     }
-})
+}
 
 internal class TestVersionProvider : VersionProvider {
 
