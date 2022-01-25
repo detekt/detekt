a236d80919002bf642e049c4b386169aad374f77
Matthew Haughton <3flex@users.noreply.github.com>
2022-01-26 00:55:52 +1100

    Migrate detekt-sample-extensions tests to JUnit (#4525)

--- detekt-sample-extensions/src/test/kotlin/io/gitlab/arturbosch/detekt/sample/extensions/processors/NumberOfLoopsProcessorSpec.kt
+++ detekt-sample-extensions/src/test/kotlin/io/gitlab/arturbosch/detekt/sample/extensions/processors/NumberOfLoopsProcessorSpec.kt
@@ -4,29 +4,26 @@ import io.github.detekt.test.utils.compileContentForTest
 import io.gitlab.arturbosch.detekt.api.DetektVisitor
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.kotlin.resolve.BindingContext
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.junit.jupiter.api.Test
 
-class NumberOfLoopsProcessorSpec : Spek({
+class NumberOfLoopsProcessorSpec {
 
-    describe("Number of Loops sample rule") {
-
-        it("should expect two loops") {
-            val code = """
-            fun main() {
-                for (i in 0..10) {
-                    while (i < 5) {
-                        println(i)
-                    }
+    @Test
+    fun `Number of loops sample rule should expect two loops`() {
+        val code = """
+        fun main() {
+            for (i in 0..10) {
+                while (i < 5) {
+                    println(i)
                 }
             }
+        }
         """
 
-            val ktFile = compileContentForTest(code)
-            ktFile.accept(DetektVisitor())
-            NumberOfLoopsProcessor().onProcess(ktFile, BindingContext.EMPTY)
+        val ktFile = compileContentForTest(code)
+        ktFile.accept(DetektVisitor())
+        NumberOfLoopsProcessor().onProcess(ktFile, BindingContext.EMPTY)
 
-            assertThat(ktFile.getUserData(NumberOfLoopsProcessor.numberOfLoopsKey)).isEqualTo(2)
-        }
+        assertThat(ktFile.getUserData(NumberOfLoopsProcessor.numberOfLoopsKey)).isEqualTo(2)
     }
-})
+}
