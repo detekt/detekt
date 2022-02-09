45e0ce6842b8e0f396dc29920e496aa4521cb87e
Markus Schwarz <post@markus-schwarz.net>
2022-02-09 22:45:19 +0100

    split rule documentation printer to improve testability

---
+++ detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/RulePrinterTest.kt
@@ -0,0 +1,80 @@
+package io.gitlab.arturbosch.detekt.generator.printer
+
+import io.gitlab.arturbosch.detekt.generator.collection.Active
+import io.gitlab.arturbosch.detekt.generator.collection.Inactive
+import io.gitlab.arturbosch.detekt.generator.collection.Rule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class RulePrinterTest {
+    val ruleTemplate = Rule(
+        name = "RuleName",
+        description = "rule description",
+        nonCompliantCodeExample = "",
+        compliantCodeExample = "",
+        defaultActivationStatus = Inactive,
+        severity = "Defect",
+        debt = "10min",
+        aliases = "alias1, alias2",
+        parent = "",
+    )
+
+    @Test
+    fun `rule name`() {
+        val rule = ruleTemplate.copy(name = "RuleName")
+        val actual = RulePrinter.print(rule)
+        assertThat(actual).contains("""### RuleName""")
+    }
+
+    @Nested
+    inner class ActiveByDefault {
+        @Test
+        fun inactive() {
+            val rule = ruleTemplate.copy(defaultActivationStatus = Inactive)
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).contains("""**Active by default**: No""")
+        }
+
+        @Test
+        fun active() {
+            val rule = ruleTemplate.copy(defaultActivationStatus = Active("1.2.3"))
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).contains("""**Active by default**: Yes - Since v1.2.3""")
+        }
+    }
+
+    @Nested
+    inner class Aliases {
+        @Test
+        fun `no alias`() {
+            val rule = ruleTemplate.copy(aliases = null)
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).doesNotContain("Aliases")
+        }
+
+        @Test
+        fun `with alias`() {
+            val rule = ruleTemplate.copy(aliases = "alias1, alias2")
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).contains("""**Aliases**: alias1, alias2""")
+        }
+    }
+
+    @Nested
+    inner class TypeResolution {
+        @Test
+        fun `no type resolution`() {
+            val rule = ruleTemplate.copy(requiresTypeResolution = false)
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).doesNotContainIgnoringCase("type resolution")
+        }
+
+        @Test
+        fun `with type resolution`() {
+            val rule = ruleTemplate.copy(requiresTypeResolution = true)
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).contains("""**Requires Type Resolution**""")
+        }
+    }
+}
