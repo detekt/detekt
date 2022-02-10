d50224e1b03d84238f5688d118eca7b99a23f6c5
marschwar <marschwar@users.noreply.github.com>
2022-02-10 23:34:51 +0100

    Split rule documentation printer to improve testability (#4578)
    * split rule documentation printer to improve testability
    
    * add tests for missed code paths
    
    Co-authored-by: Markus Schwarz <post@markus-schwarz.net>

--- detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/RuleSetPagePrinterSpec.kt
+++ detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/RuleSetPagePrinterSpec.kt
@@ -3,19 +3,15 @@ package io.gitlab.arturbosch.detekt.generator.printer
 import io.github.detekt.test.utils.resource
 import io.gitlab.arturbosch.detekt.generator.util.createRuleSetPage
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.io.File
 
 class RuleSetPagePrinterSpec {
 
-    @Nested
-    inner class `Ruleset page printer` {
-        @Test
-        fun `prints the correct markdown format`() {
-            val markdownString = RuleSetPagePrinter.print(createRuleSetPage())
-            val expectedMarkdownString = File(resource("/RuleSet.md")).readText()
-            assertThat(markdownString).isEqualTo(expectedMarkdownString)
-        }
+    @Test
+    fun `prints the correct markdown format`() {
+        val markdownString = RuleSetPagePrinter.print(createRuleSetPage())
+        val expectedMarkdownString = File(resource("/RuleSet.md")).readText()
+        assertThat(markdownString).isEqualTo(expectedMarkdownString)
     }
 }
