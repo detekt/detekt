143a59b6c4405683cc3ce96bba0145080a7469d8
Markus Schwarz <post@markus-schwarz.net>
2022-02-10 07:25:55 +0100

    add tests for missed code paths

--- detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/RulePrinterTest.kt
+++ detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/RulePrinterTest.kt
@@ -28,6 +28,24 @@ internal class RulePrinterTest {
     }
 
     @Nested
+    inner class Description {
+        @Test
+        fun `empty description`() {
+            val rule = ruleTemplate.copy(description = "")
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).contains("TODO: Specify description")
+        }
+
+        @Test
+        fun `with description`() {
+            val description = "This is the description"
+            val rule = ruleTemplate.copy(description = description)
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).contains(description)
+        }
+    }
+
+    @Nested
     inner class ActiveByDefault {
         @Test
         fun inactive() {
@@ -50,7 +68,14 @@ internal class RulePrinterTest {
         fun `no alias`() {
             val rule = ruleTemplate.copy(aliases = null)
             val actual = RulePrinter.print(rule)
-            assertThat(actual).doesNotContain("Aliases")
+            assertThat(actual).doesNotContainIgnoringCase("aliases")
+        }
+
+        @Test
+        fun `empty alias`() {
+            val rule = ruleTemplate.copy(aliases = "")
+            val actual = RulePrinter.print(rule)
+            assertThat(actual).doesNotContainIgnoringCase("aliases")
         }
 
         @Test
