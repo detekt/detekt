65ba52fb1de4a14e29a55992e9e4152996fdd459
Kevin Stieglitz <kevin.stieglitz@jambit.com>
2022-03-15 00:32:28 +0100

    deprecate indentSize in ParameterListWrapping.kt

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/ParameterListWrapping.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/ParameterListWrapping.kt
@@ -2,6 +2,7 @@ package io.gitlab.arturbosch.detekt.formatting.wrappers
 
 import com.pinterest.ktlint.ruleset.standard.ParameterListWrappingRule
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.config
 import io.gitlab.arturbosch.detekt.api.configWithAndroidVariants
 import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
 import io.gitlab.arturbosch.detekt.api.internal.AutoCorrectable
@@ -21,6 +22,11 @@ class ParameterListWrapping(config: Config) : FormattingRule(config) {
 
     @Configuration("maximum line length")
     private val maxLineLength: Int by configWithAndroidVariants(120, 100)
+
+    @Configuration("indentation size")
+    @Deprecated("`indentSize` is ignored by KtLint and will have no effect")
+    @Suppress("UnusedPrivateMember")
+    private val indentSize by config(4)
 
     override fun overrideEditorConfig() = mapOf(
         MAX_LINE_LENGTH_KEY to maxLineLength
--- detekt-formatting/src/test/kotlin/io/gitlab/arturbosch/detekt/formatting/UnnecessaryParenthesesBeforeTrailingLambdaSpec.kt
+++ detekt-formatting/src/test/kotlin/io/gitlab/arturbosch/detekt/formatting/UnnecessaryParenthesesBeforeTrailingLambdaSpec.kt
@@ -3,30 +3,30 @@ package io.gitlab.arturbosch.detekt.formatting
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.formatting.wrappers.UnnecessaryParenthesesBeforeTrailingLambda
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
+/**
+ * Test cases were used directly from KtLint to verify the wrapper rule:
+ *
+ * https://github.com/pinterest/ktlint/blob/master/ktlint-ruleset-experimental/src/test/kotlin/com/pinterest/ktlint/ruleset/experimental/UnnecessaryParenthesesBeforeTrailingLambdaRuleTest.kt
+ */
 class UnnecessaryParenthesesBeforeTrailingLambdaSpec {
 
-    @Nested
-    inner class `UnnecessaryParenthesesBeforeTrailingLambda rule` {
+    @Test
+    fun `reports unnecessary parentheses before trailing lambda`() {
+        val code = """
+            fun countDash(input: String) =
+                "some-string".count() { it == '-' }
+        """.trimIndent()
+        assertThat(UnnecessaryParenthesesBeforeTrailingLambda(Config.empty).lint(code)).hasSize(1)
+    }
 
-        @Test
-        fun `reports unnecessary parentheses before trailing lambda`() {
-            val code = """
-                fun countDash(input: String) =
-                    "some-string".count() { it == '-' }
-            """.trimIndent()
-            assertThat(UnnecessaryParenthesesBeforeTrailingLambda(Config.empty).lint(code)).hasSize(1)
-        }
-
-        @Test
-        fun `does not report unnecessary parentheses before trailing lambda`() {
-            val code = """
-                fun countDash(input: String) =
-                    "some-string".count { it == '-' }
-            """.trimIndent()
-            assertThat(UnnecessaryParenthesesBeforeTrailingLambda(Config.empty).lint(code)).isEmpty()
-        }
+    @Test
+    fun `does not report unnecessary parentheses before trailing lambda`() {
+        val code = """
+            fun countDash(input: String) =
+                "some-string".count { it == '-' }
+        """.trimIndent()
+        assertThat(UnnecessaryParenthesesBeforeTrailingLambda(Config.empty).lint(code)).isEmpty()
     }
 }
