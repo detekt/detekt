2f5e18e9b7a687b8b5c824a539975cd57d10a849
Chao Zhang <chao.zhang@instacart.com>
2022-04-08 09:43:44 -0700

    Update gradle enterprise server (#4691)
    Co-authored-by: Nicola Corti <corti.nico@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+
 rootProject.name = "detekt"
 
 pluginManagement {
@@ -40,16 +42,26 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 // build scan plugin can only be applied in settings file
 plugins {
     `gradle-enterprise`
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.6.5"
 }
 
 gradleEnterprise {
     val isCiBuild = System.getenv("CI") != null
 
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        if (isCiBuild) {
-            termsOfServiceAgree = "yes"
-            publishAlways()
+        publishAlways()
+
+        // Publish to scans.gradle.com when `--scan` is used explicitly
+        if (!gradle.startParameter.isBuildScan) {
+            server = "https://ge.detekt.dev"
+            this as BuildScanExtensionWithHiddenFeatures
+            publishIfAuthenticated()
+        }
+
+        isUploadInBackground = !isCiBuild
+
+        capture {
+            isTaskInputFiles = true
         }
     }
 }
