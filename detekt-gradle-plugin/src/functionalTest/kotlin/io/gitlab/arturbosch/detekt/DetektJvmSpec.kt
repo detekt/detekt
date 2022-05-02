010f6a12ad33c7d08fb34b137f86a560a1d4aa7c
Chao Zhang <chao.zhang@instacart.com>
2022-05-01 22:16:26 -0700

    Build a utility to copy resource files into temporary files

--- detekt-gradle-plugin/src/functionalTest/kotlin/io/gitlab/arturbosch/detekt/DetektJvmSpec.kt
+++ detekt-gradle-plugin/src/functionalTest/kotlin/io/gitlab/arturbosch/detekt/DetektJvmSpec.kt
@@ -3,71 +3,13 @@ package io.gitlab.arturbosch.detekt
 import io.gitlab.arturbosch.detekt.testkit.DslGradleRunner
 import io.gitlab.arturbosch.detekt.testkit.ProjectLayout
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-/**
- * When applying detekt in a JVM project
- */
 class DetektJvmSpec {
 
-    @Test
-    fun `type resolution on JVM`() {
-        val fileContent = """
-            package jvm.src.main.kotlin
-
-            import kotlin.system.exitProcess
-            
-            class Errors {
-                fun kotlinExit() {
-                    exitProcess(0)
-                }
-            
-                fun javaExit() {
-                    @Suppress("ReplaceJavaStaticMethodWithKotlinAnalog")
-                    System.exit(1)
-                }
-            }
-        """.trimIndent()
-
-        val detektConfigContent = """
-            potential-bugs:
-              ExitOutsideMain:
-                active: true
-        """.trimIndent()
-
-        val gradleRunner = DslGradleRunner(
-            projectLayout = ProjectLayout(
-                numberOfSourceFilesInRootPerSourceDir = 0,
-                numberOfCodeSmellsInRootPerSourceDir = 0,
-                srcDirs = listOf("src/main/kotlin"),
-            ),
-            buildFileName = "build.gradle.kts",
-            mainBuildFileContent = """
-                plugins {
-                    id("org.jetbrains.kotlin.jvm")
-                    id("io.gitlab.arturbosch.detekt")
-                }
-                
-                repositories {
-                    mavenCentral()
-                    mavenLocal()
-                }
-            """.trimMargin()
-        )
-
-        gradleRunner.setupProject()
-        gradleRunner.writeProjectFile("src/main/kotlin/Errors.kt", fileContent)
-        gradleRunner.writeProjectFile("config/detekt/detekt.yml", detektConfigContent)
-        gradleRunner.runTasksAndExpectFailure("detektMain") { buildResult ->
-            assertThat(buildResult.output).contains("failed with 2 weighted issues.")
-            assertThat(buildResult.output).contains("Do not directly exit the process outside the `main` function. Throw an exception(…)")
-            assertThat(buildResult.output).contains("Errors.kt:7:9")
-            assertThat(buildResult.output).contains("Errors.kt:12:16")
-        }
-    }
-
-    @Test
-    fun `reporting location set on extension & task logs a warning`() {
+    @Nested
+    inner class `report location set on extension & task` {
         val gradleRunner = DslGradleRunner(
             projectLayout = ProjectLayout(numberOfSourceFilesInRootPerSourceDir = 1),
             buildFileName = "build.gradle.kts",
@@ -76,34 +18,35 @@ class DetektJvmSpec {
                     kotlin("jvm")
                     id("io.gitlab.arturbosch.detekt")
                 }
-
                 repositories {
                     mavenCentral()
                     mavenLocal()
                 }
-
                 detekt {
                     reports {
                         txt.destination = file("output-path.txt")
                     }
                 }
-
                 tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
                     reports {
                         txt.destination = file("output-path2.txt")
                     }
                 }
-            """.trimIndent(),
-            dryRun = false
-        )
-        gradleRunner.setupProject()
-        gradleRunner.runTasksAndCheckResult(":detektMain") { buildResult ->
-            assertThat(buildResult.output).contains("TXT report location set on detekt {} extension will be ignored for detektMain task.")
+            """.trimIndent()
+        ).also {
+            it.setupProject()
+        }
+
+        @Test
+        fun `logs a warning`() {
+            gradleRunner.runTasksAndCheckResult(":detektMain") { buildResult ->
+                assertThat(buildResult.output).contains("TXT report location set on detekt {} extension will be ignored for detektMain task.")
+            }
         }
     }
 
-    @Test
-    fun `reporting location set on task only logs a warning`() {
+    @Nested
+    inner class `report location set on task only` {
         val gradleRunner = DslGradleRunner(
             projectLayout = ProjectLayout(numberOfSourceFilesInRootPerSourceDir = 1),
             buildFileName = "build.gradle.kts",
@@ -112,23 +55,25 @@ class DetektJvmSpec {
                     kotlin("jvm")
                     id("io.gitlab.arturbosch.detekt")
                 }
-
                 repositories {
                     mavenCentral()
                     mavenLocal()
                 }
-
                 tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
                     reports {
                         txt.destination = file("output-path2.txt")
                     }
                 }
-            """.trimIndent(),
-            dryRun = false
-        )
-        gradleRunner.setupProject()
-        gradleRunner.runTasksAndCheckResult(":detektMain") { buildResult ->
-            assertThat(buildResult.output).doesNotContain("report location set on detekt {} extension will be ignored")
+            """.trimIndent()
+        ).also {
+            it.setupProject()
+        }
+
+        @Test
+        fun `logs a warning`() {
+            gradleRunner.runTasksAndCheckResult(":detektMain") { buildResult ->
+                assertThat(buildResult.output).doesNotContain("report location set on detekt {} extension will be ignored")
+            }
         }
     }
 }
---
+++ detekt-gradle-plugin/src/functionalTest/kotlin/io/gitlab/arturbosch/detekt/JvmSpec.kt
@@ -0,0 +1,22 @@
+package io.gitlab.arturbosch.detekt
+
+import io.gitlab.arturbosch.detekt.testkit.withResourceDir
+import org.assertj.core.api.Assertions.assertThat
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.Test
+
+class JvmSpec {
+    @Test
+    fun `Type resolution on JVM`() {
+        val result = GradleRunner.create()
+            .withResourceDir("jvm")
+            .withPluginClasspath()
+            .withArguments("detektMain")
+            .buildAndFail()
+
+        assertThat(result.output).contains("failed with 2 weighted issues.")
+        assertThat(result.output).contains("Do not directly exit the process outside the `main` function. Throw an exception(…)")
+        assertThat(result.output).contains("Errors.kt:7:9")
+        assertThat(result.output).contains("Errors.kt:12:16")
+    }
+}
