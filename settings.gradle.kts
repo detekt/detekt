3512e3fd7610040b977db29895bb46ca2e4581a7
Matthew Haughton <3flex@users.noreply.github.com>
2022-05-20 13:10:53 +1000

    Use standard plugin notation to apply Gradle Enterprise Gradle Plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,7 +41,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 // build scan plugin can only be applied in settings file
 plugins {
     // check https://gradle.com/enterprise/releases with new versions. GE plugin version should not lag behind Gradle version
-    `gradle-enterprise` version "3.10"
+    id("com.gradle.enterprise") version "3.10"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "1.6.6"
 }
 
