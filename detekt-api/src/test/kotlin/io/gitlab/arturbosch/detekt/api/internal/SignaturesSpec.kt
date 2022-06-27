2b068e00426ad0acb83081575e0903b106e7d9fa
Vitaly V. Pinchuk <vetal.978@gmail.com>
2022-06-27 15:51:52 +0300

    Fix Signatures.kt:buildFunctionSignature (#4961)
    * Fix Signatures.kt:buildFunctionSignature
    
    'buildFunctionSignature' fails when function body has KDoc.
    
    * Simplify tests
    
    * Skip comments

---
+++ detekt-api/src/test/kotlin/io/gitlab/arturbosch/detekt/api/internal/SignaturesSpec.kt
@@ -0,0 +1,47 @@
+package io.gitlab.arturbosch.detekt.api.internal
+
+import io.github.detekt.test.utils.compileContentForTest
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.junit.jupiter.api.Test
+
+class SignaturesSpec {
+    @Test
+    fun `function with type reference`() {
+        val result = compileContentForTest("fun data(): Int = 0")
+            .findChildByClass(KtNamedFunction::class.java)!!
+            .buildFullSignature()
+
+        assertThat(result).isEqualTo("Test.kt\$fun data(): Int")
+    }
+
+    @Test
+    fun `function without type reference`() {
+        val result = compileContentForTest("fun data() = 0")
+            .findChildByClass(KtNamedFunction::class.java)!!
+            .buildFullSignature()
+
+        assertThat(result).isEqualTo("Test.kt\$fun data()")
+    }
+
+    @Test
+    fun `function with comments`() {
+        val result = compileContentForTest("/* comments */ fun data() = 0")
+            .findChildByClass(KtNamedFunction::class.java)!!
+            .buildFullSignature()
+
+        assertThat(result).isEqualTo("Test.kt\$fun data()")
+    }
+
+    @Test
+    fun `function throws exception`() {
+        assertThatThrownBy {
+            compileContentForTest("{ fun data() = 0 }")
+                .findChildByClass(KtNamedFunction::class.java)!!
+                .buildFullSignature()
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Error building function signature")
+    }
+}
