488e6fb8d1227469eacc80f625147eb200935167
Kevin Stieglitz <kevin.stieglitz@jambit.com>
2022-07-04 23:45:29 +0200

    Bump ktlint to v0.46.1

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/PackageName.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/PackageName.kt
@@ -1,7 +1,8 @@
 package io.gitlab.arturbosch.detekt.formatting.wrappers
 
-import com.pinterest.ktlint.ruleset.experimental.PackageNameRule
+import com.pinterest.ktlint.ruleset.standard.PackageNameRule
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
 import io.gitlab.arturbosch.detekt.api.internal.AutoCorrectable
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
@@ -9,6 +10,7 @@ import io.gitlab.arturbosch.detekt.formatting.FormattingRule
  * See [ktlint-website](https://ktlint.github.io) for documentation.
  */
 @AutoCorrectable(since = "1.0.0")
+@ActiveByDefault(since = "1.21.0")
 class PackageName(config: Config) : FormattingRule(config) {
 
     override val wrapping = PackageNameRule()
