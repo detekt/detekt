aa2a1bf37d6e667ca78cf2a868a272bb1ff2932a
Toshiaki Kameyama <kmymtsak@gmail.com>
2022-07-05 17:00:52 +0900

    MaxChainedCallsOnSameLine: don't count package references as chained calls (#5036)
    * MaxChainedCallsOnSameLine: don't count package references as chained calls
    
    * Use compileAndLintWithContext

--- detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxChainedCallsOnSameLine.kt
+++ detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/MaxChainedCallsOnSameLine.kt
@@ -9,11 +9,15 @@ import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.config
 import io.gitlab.arturbosch.detekt.api.internal.Configuration
+import io.gitlab.arturbosch.detekt.api.internal.RequiresTypeResolution
+import org.jetbrains.kotlin.descriptors.PackageViewDescriptor
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.psi.KtPackageDirective
 import org.jetbrains.kotlin.psi.KtQualifiedExpression
+import org.jetbrains.kotlin.psi.KtReferenceExpression
 import org.jetbrains.kotlin.psi.KtUnaryExpression
+import org.jetbrains.kotlin.resolve.BindingContext
 
 /**
  * Limits the number of chained calls which can be placed on a single line.
@@ -27,6 +31,7 @@ import org.jetbrains.kotlin.psi.KtUnaryExpression
  *   .d().e().f()
  * </compliant>
  */
+@RequiresTypeResolution
 class MaxChainedCallsOnSameLine(config: Config = Config.empty) : Rule(config) {
     override val issue = Issue(
         id = javaClass.simpleName,
@@ -38,8 +43,11 @@ class MaxChainedCallsOnSameLine(config: Config = Config.empty) : Rule(config) {
     @Configuration("maximum chained calls allowed on a single line")
     private val maxChainedCalls: Int by config(defaultValue = 5)
 
+    @Suppress("ReturnCount")
     override fun visitQualifiedExpression(expression: KtQualifiedExpression) {
         super.visitQualifiedExpression(expression)
+
+        if (bindingContext == BindingContext.EMPTY) return
 
         val parent = expression.parent
 
@@ -63,11 +71,19 @@ class MaxChainedCallsOnSameLine(config: Config = Config.empty) : Rule(config) {
 
     private fun KtExpression.countChainedCalls(): Int {
         return when (this) {
-            is KtQualifiedExpression ->
-                if (callOnNewLine()) 0 else receiverExpression.countChainedCalls() + 1
+            is KtQualifiedExpression -> when {
+                receiverExpression.isReferenceToPackage() || callOnNewLine() -> 0
+                else -> receiverExpression.countChainedCalls() + 1
+            }
             is KtUnaryExpression -> baseExpression?.countChainedCalls() ?: 0
             else -> 0
         }
+    }
+
+    private fun KtExpression.isReferenceToPackage(): Boolean {
+        val selectorOrThis = (this as? KtQualifiedExpression)?.selectorExpression ?: this
+        if (selectorOrThis !is KtReferenceExpression) return false
+        return bindingContext[BindingContext.REFERENCE_TARGET, selectorOrThis] is PackageViewDescriptor
     }
 
     private fun KtQualifiedExpression.callOnNewLine(): Boolean {
