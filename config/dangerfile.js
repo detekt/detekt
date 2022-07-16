eb342695244f39092a790433c6fed2738672b5e9
Brais Gabín <braisgabin@gmail.com>
2022-07-16 12:35:35 +0200

    Configure danger.js

---
+++ config/dangerfile.js
@@ -0,0 +1,18 @@
+import { danger, fail, markdown, warn, message } from "danger"
+import * as path from "path"
+
+// API reference: https://danger.systems/js/reference.html
+
+const pr = danger.github.pr
+
+// Find out if some particular files have changed in this PR
+const changelog = danger.git.fileMatch("CHANGELOG.md")
+
+// Rules
+
+// When there are app-changes and it us not a PR marked as trivial, expect
+// there to be CHANGELOG changes.
+const trivialPR = pr.body.includes("trivial")
+if (!changelog.edited && !trivialPR) {
+  fail("No CHANGELOG added.")
+}
