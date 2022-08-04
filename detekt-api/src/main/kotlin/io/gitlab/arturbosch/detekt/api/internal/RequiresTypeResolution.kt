7810ba846eaa9d2ed2510662dd9e0197e5971d22
Brais Gabín <braisgabin@gmail.com>
2022-08-04 16:31:15 +0200

    Improve documentation

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/RequiresTypeResolution.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/RequiresTypeResolution.kt
@@ -2,6 +2,9 @@ package io.gitlab.arturbosch.detekt.api.internal
 
 /**
  * Annotated [io.gitlab.arturbosch.detekt.api.Rule] requires type resolution to work.
+ *
+ * The detekt core will honor this annotation and it will not run any rule with this annotation if the bindingContext
+ * is empty.
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
