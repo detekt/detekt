1e67dc61ae9cb6cdfe4117b3d92aff15b0106ec9
3flex <3flex@users.noreply.github.com>
2022-10-11 20:30:21 +0000

    Deploying to gh-pages from @ detekt/detekt@7ad1b38408bbb313e806b4c56e7232429a17741d 🚀

--- kdoc/scripts/platform-content-handler.js
+++ kdoc/scripts/platform-content-handler.js
@@ -31,9 +31,10 @@ window.addEventListener('load', () => {
 const darkModeSwitch = () => {
     const localStorageKey = "dokka-dark-mode"
     const storage = localStorage.getItem(localStorageKey)
-    const savedDarkMode = storage ? JSON.parse(storage) : false
+    const osDarkSchemePreferred = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    const darkModeEnabled = storage ? JSON.parse(storage) : osDarkSchemePreferred
     const element = document.getElementById("theme-toggle-button")
-    initPlayground(savedDarkMode ? samplesDarkThemeName : samplesLightThemeName)
+    initPlayground(darkModeEnabled ? samplesDarkThemeName : samplesLightThemeName)
 
     element.addEventListener('click', () => {
         const enabledClasses = document.getElementsByTagName("html")[0].classList
