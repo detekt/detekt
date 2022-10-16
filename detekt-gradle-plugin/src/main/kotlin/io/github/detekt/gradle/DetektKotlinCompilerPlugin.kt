a4ab4c185752d528ef4385cce4dce6b728e7d51b
Matthew Haughton <3flex@users.noreply.github.com>
2022-10-16 17:23:33 +1100

    Allow both Gradle plugins to be used in the same project

--- detekt-gradle-plugin/src/main/kotlin/io/github/detekt/gradle/DetektKotlinCompilerPlugin.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/github/detekt/gradle/DetektKotlinCompilerPlugin.kt
@@ -1,7 +1,8 @@
 package io.github.detekt.gradle
 
 import io.github.detekt.gradle.extensions.KotlinCompileTaskDetektExtension
-import io.github.detekt.gradle.extensions.ProjectDetektExtension
+import io.gitlab.arturbosch.detekt.DetektPlugin
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.plugins.ReportingBasePlugin
@@ -25,22 +26,18 @@ class DetektKotlinCompilerPlugin : KotlinCompilerPluginSupportPlugin {
     override fun apply(target: Project) {
         target.pluginManager.apply(ReportingBasePlugin::class.java)
 
-        val projectExtension = target.extensions.create(DETEKT_NAME, ProjectDetektExtension::class.java).apply {
-            reportsDir.convention(
-                target.extensions.getByType(ReportingExtension::class.java).baseDirectory.dir(DETEKT_NAME)
-            )
-            excludes.add("**/${target.relativePath(target.buildDir)}/**")
+        val extension =
+            target.extensions.findByType(DetektExtension::class.java) ?: target.extensions.create(DETEKT_NAME, DetektExtension::class.java)
 
-            isEnabled.convention(true)
-            debug.convention(false)
-            buildUponDefaultConfig.convention(true)
-            allRules.convention(false)
-            disableDefaultRuleSets.convention(false)
-            parallel.convention(false)
-            config.from(target.rootProject.layout.projectDirectory.dir(CONFIG_DIR_NAME).file(CONFIG_FILE))
+        extension.reportsDir = target.extensions.getByType(ReportingExtension::class.java).file("detekt")
+
+        val defaultConfigFile =
+            target.file("${target.rootProject.layout.projectDirectory.dir(DetektPlugin.CONFIG_DIR_NAME)}/${DetektPlugin.CONFIG_FILE}")
+        if (defaultConfigFile.exists()) {
+            extension.config = target.files(defaultConfigFile)
         }
 
-        target.configurations.create(CONFIGURATION_DETEKT_PLUGINS).apply {
+        target.configurations.maybeCreate(CONFIGURATION_DETEKT_PLUGINS).apply {
             isVisible = false
             isTransitive = true
             description = "The $CONFIGURATION_DETEKT_PLUGINS libraries to be used for this project."
@@ -48,15 +45,14 @@ class DetektKotlinCompilerPlugin : KotlinCompilerPluginSupportPlugin {
 
         target.tasks.withType(KotlinCompile::class.java).configureEach { task ->
             task.extensions.create(DETEKT_NAME, KotlinCompileTaskDetektExtension::class.java, target).apply {
-                isEnabled.convention(projectExtension.isEnabled)
-                baseline.convention(projectExtension.baseline)
-                debug.convention(projectExtension.debug)
-                buildUponDefaultConfig.convention(projectExtension.buildUponDefaultConfig)
-                allRules.convention(projectExtension.allRules)
-                disableDefaultRuleSets.convention(projectExtension.disableDefaultRuleSets)
-                parallel.convention(projectExtension.parallel)
-                config.from(projectExtension.config)
-                excludes.convention(projectExtension.excludes)
+                baseline.convention(target.layout.file(target.provider { extension.baseline }))
+                debug.convention(target.provider { extension.debug })
+                buildUponDefaultConfig.convention(target.provider { extension.buildUponDefaultConfig })
+                allRules.convention(target.provider { extension.allRules })
+                disableDefaultRuleSets.convention(target.provider { extension.disableDefaultRuleSets })
+                parallel.convention(target.provider { extension.parallel })
+                config.from(extension.config)
+                excludes.convention(DetektPlugin.defaultExcludes)
             }
         }
     }
@@ -64,7 +60,7 @@ class DetektKotlinCompilerPlugin : KotlinCompilerPluginSupportPlugin {
     override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
         val project = kotlinCompilation.target.project
 
-        val projectExtension = project.extensions.getByType(ProjectDetektExtension::class.java)
+        val projectExtension = project.extensions.getByType(DetektExtension::class.java)
         val taskExtension =
             kotlinCompilation.compileKotlinTask.extensions.getByType(KotlinCompileTaskDetektExtension::class.java)
 
@@ -75,7 +71,7 @@ class DetektKotlinCompilerPlugin : KotlinCompilerPluginSupportPlugin {
         val options = project.objects.listProperty(SubpluginOption::class.java).apply {
             add(SubpluginOption("debug", taskExtension.debug.get().toString()))
             add(SubpluginOption("configDigest", taskExtension.config.toDigest()))
-            add(SubpluginOption("isEnabled", taskExtension.isEnabled.get().toString()))
+            add(SubpluginOption("isEnabled", taskExtension.isEnabled.getOrElse(false).toString()))
             add(SubpluginOption("useDefaultConfig", taskExtension.buildUponDefaultConfig.get().toString()))
             add(SubpluginOption("allRules", taskExtension.allRules.get().toString()))
             add(SubpluginOption("disableDefaultRuleSets", taskExtension.disableDefaultRuleSets.get().toString()))
@@ -86,7 +82,11 @@ class DetektKotlinCompilerPlugin : KotlinCompilerPluginSupportPlugin {
             taskExtension.reports.all { report ->
                 report.enabled.convention(true)
                 report.destination.convention(
-                    projectExtension.reportsDir.file("${kotlinCompilation.name}.${report.name}")
+                    project.layout.projectDirectory.file(
+                        project.providers.provider {
+                            File(projectExtension.reportsDir, "${kotlinCompilation.name}.${report.name}").absolutePath
+                        }
+                    )
                 )
 
                 if (report.enabled.get()) {
