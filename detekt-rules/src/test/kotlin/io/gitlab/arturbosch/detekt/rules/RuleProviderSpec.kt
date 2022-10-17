6445721a17677e92804ba17d1619b40d6a71dc19
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-10-17 10:11:50 +0100

    Remove unnecessary instantiation of rule providers in test.

--- detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/RuleProviderSpec.kt
+++ detekt-rules/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/RuleProviderSpec.kt
@@ -40,16 +40,16 @@ class RuleProviderSpec {
     }
 }
 
-private val ruleMap = mapOf<Class<*>, String>(
-    CommentSmellProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.documentation",
-    ComplexityProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.complexity",
-    EmptyCodeProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.empty",
-    ExceptionsProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.exceptions",
-    NamingProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.naming",
-    PerformanceProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.performance",
-    PotentialBugProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.bugs",
-    StyleGuideProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.style",
-    CoroutinesProvider().javaClass to "io.gitlab.arturbosch.detekt.rules.coroutines"
+private val ruleMap: Map<Class<out DefaultRuleSetProvider>, String> = mapOf(
+    CommentSmellProvider::class.java to "io.gitlab.arturbosch.detekt.rules.documentation",
+    ComplexityProvider::class.java to "io.gitlab.arturbosch.detekt.rules.complexity",
+    EmptyCodeProvider::class.java to "io.gitlab.arturbosch.detekt.rules.empty",
+    ExceptionsProvider::class.java to "io.gitlab.arturbosch.detekt.rules.exceptions",
+    NamingProvider::class.java to "io.gitlab.arturbosch.detekt.rules.naming",
+    PerformanceProvider::class.java to "io.gitlab.arturbosch.detekt.rules.performance",
+    PotentialBugProvider::class.java to "io.gitlab.arturbosch.detekt.rules.bugs",
+    StyleGuideProvider::class.java to "io.gitlab.arturbosch.detekt.rules.style",
+    CoroutinesProvider::class.java to "io.gitlab.arturbosch.detekt.rules.coroutines"
 )
 
 private fun getRulesPackageNameForProvider(providerType: Class<out RuleSetProvider>): String {
