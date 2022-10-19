4f64190108c7f59d24f328ed6e9d34b7484bbcdd
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-10-19 12:33:18 +0100

    Implement rule UseNamedLocation

--- detekt-rules-ruleauthors/src/main/kotlin/io/gitlab/arturbosch/detekt/authors/UseNamedLocation.kt
+++ detekt-rules-ruleauthors/src/main/kotlin/io/gitlab/arturbosch/detekt/authors/UseNamedLocation.kt
@@ -1,5 +1,6 @@
 package io.gitlab.arturbosch.detekt.authors
 
+import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Debt
 import io.gitlab.arturbosch.detekt.api.Entity
@@ -7,7 +8,15 @@ import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
+import org.jetbrains.kotlin.psi.KtPostfixExpression
+import org.jetbrains.kotlin.psi.psiUtil.getCallNameExpression
+import org.jetbrains.kotlin.psi.psiUtil.getReceiverExpression
 
 /**
  * If a rule reports issues using [Entity.from] with [KtNamedDeclaration.getNameIdentifier],
@@ -22,4 +31,38 @@ class UseNamedLocation(config: Config = Config.empty) : Rule(config) {
         "Prefer Entity.atName to Entity.from(....nameIdentifier).",
         Debt.FIVE_MINS
     )
+
+    override fun visitCallExpression(expression: KtCallExpression) {
+        super.visitCallExpression(expression)
+
+        if (isEntityFromCall(expression)
+            && isNameIdentifier(expression.valueArguments.single().getArgumentExpression()!!)
+        ) {
+            report(CodeSmell(issue, Entity.from(expression), "Use Entity.atName($expression) instead."))
+        }
+    }
+
+    private fun isNameIdentifier(expression: KtExpression): Boolean =
+        when {
+            expression is KtDotQualifiedExpression ->
+                expression.selectorExpression?.text == "nameIdentifier"
+
+            expression is KtCallExpression ->
+                expression.getCallNameExpression()?.text == "nameIdentifier"
+
+            expression is KtPostfixExpression && expression.operationToken == KtTokens.EXCLEXCL ->
+                isNameIdentifier(expression.baseExpression!!)
+
+            expression is KtBinaryExpression && expression.operationToken == KtTokens.ELVIS ->
+                isNameIdentifier(expression.left!!)
+
+            else ->
+                false
+        }
+
+    private fun isEntityFromCall(expression: KtCallExpression): Boolean {
+        val callNameExpression = expression.getCallNameExpression()
+        return callNameExpression?.text == "from" &&
+            callNameExpression.getReceiverExpression()?.text == "Entity"
+    }
 }
--- detekt-rules-ruleauthors/src/test/kotlin/io/gitlab/arturbosch/detekt/authors/UseNamedLocationSpec.kt
+++ detekt-rules-ruleauthors/src/test/kotlin/io/gitlab/arturbosch/detekt/authors/UseNamedLocationSpec.kt
@@ -14,8 +14,97 @@ internal class UseNamedLocationSpec(private val env: KotlinCoreEnvironment) {
     @Test
     fun `should not report calls when there's no name involved`() {
         val code = """
+            import io.gitlab.arturbosch.detekt.api.CodeSmell
+            import io.gitlab.arturbosch.detekt.api.Entity
+            import io.gitlab.arturbosch.detekt.api.Rule
+            import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+            
+            fun Rule.f(element: PsiElement) {
+                report(CodeSmell(issue, Entity.from(element), "message"))
+            }
         """.trimIndent()
         val findings = rule.compileAndLintWithContext(env, code)
         assertThat(findings).isEmpty()
+    }
+
+    @Test
+    fun `should not report calls when atName is already used`() {
+        val code = """
+            import io.gitlab.arturbosch.detekt.api.CodeSmell
+            import io.gitlab.arturbosch.detekt.api.Entity
+            import io.gitlab.arturbosch.detekt.api.Rule
+            import org.jetbrains.kotlin.psi.KtNamedDeclaration
+            
+            fun Rule.f(element: KtNamedDeclaration) {
+                report(CodeSmell(issue, Entity.atName(element), "message"))
+            }
+        """.trimIndent()
+        val findings = rule.compileAndLintWithContext(env, code)
+        assertThat(findings).isEmpty()
+    }
+
+    @Test
+    fun `should report calls where nameIdentifier is used directly with bang`() {
+        val code = """
+            import io.gitlab.arturbosch.detekt.api.CodeSmell
+            import io.gitlab.arturbosch.detekt.api.Entity
+            import io.gitlab.arturbosch.detekt.api.Rule
+            import org.jetbrains.kotlin.com.intellij.psi.PsiNameIdentifierOwner
+            
+            fun Rule.f(element: PsiNameIdentifierOwner) {
+                report(CodeSmell(issue, Entity.from(element.nameIdentifier!!), "message"))
+            }
+        """.trimIndent()
+        val findings = rule.compileAndLintWithContext(env, code)
+        assertThat(findings).hasSize(1)
+    }
+
+    @Test
+    fun `should report calls where nameIdentifier is used directly with double-bang`() {
+        val code = """
+            import io.gitlab.arturbosch.detekt.api.CodeSmell
+            import io.gitlab.arturbosch.detekt.api.Entity
+            import io.gitlab.arturbosch.detekt.api.Rule
+            import org.jetbrains.kotlin.com.intellij.psi.PsiNameIdentifierOwner
+            
+            fun Rule.f(element: PsiNameIdentifierOwner) {
+                report(CodeSmell(issue, Entity.from(element.nameIdentifier!!!!), "message"))
+            }
+        """.trimIndent()
+        val findings = rule.compileAndLintWithContext(env, code)
+        assertThat(findings).hasSize(1)
+    }
+
+    @Test
+    fun `should report calls where nameIdentifier is used with elvis with same fallback`() {
+        val code = """
+            import io.gitlab.arturbosch.detekt.api.CodeSmell
+            import io.gitlab.arturbosch.detekt.api.Entity
+            import io.gitlab.arturbosch.detekt.api.Rule
+            import org.jetbrains.kotlin.com.intellij.psi.PsiNameIdentifierOwner
+            
+            fun Rule.f(element: PsiNameIdentifierOwner) {
+                report(CodeSmell(issue, Entity.from(element.nameIdentifier ?: element), "message"))
+            }
+        """.trimIndent()
+        val findings = rule.compileAndLintWithContext(env, code)
+        assertThat(findings).hasSize(1)
+    }
+
+    @Test
+    fun `should report calls where nameIdentifier is used with elvis with other fallback`() {
+        val code = """
+            import io.gitlab.arturbosch.detekt.api.CodeSmell
+            import io.gitlab.arturbosch.detekt.api.Entity
+            import io.gitlab.arturbosch.detekt.api.Rule
+            import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+            import org.jetbrains.kotlin.com.intellij.psi.PsiNameIdentifierOwner
+            
+            fun Rule.f(element: PsiNameIdentifierOwner, element2: PsiElement) {
+                report(CodeSmell(issue, Entity.from(element.nameIdentifier ?: element2), "message"))
+            }
+        """.trimIndent()
+        val findings = rule.compileAndLintWithContext(env, code)
+        assertThat(findings).hasSize(1)
     }
 }
