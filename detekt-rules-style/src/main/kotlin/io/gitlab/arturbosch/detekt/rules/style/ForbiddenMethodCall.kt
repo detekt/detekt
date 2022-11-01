1ab21083334c1954c45445bde1e7bba6c898489e
Dominic Zirbel <dominiczirbel@gmail.com>
2022-11-01 15:41:11 -0700

    Document and test edge cases for ForbiddenMethodCall function signatures
    I was stuck trying to match some function signatures (from Timber on Android)
    for the ForbiddenMethodCall rule and had to test inside detekt to figure out how
    to do it properly. To make this more clear in the future, I've expanded the
    description of the methods configuration parameter and added more tests for
    these cases. Some of this might be obvious to some but it was not to me at the
    time so I think having some quick tips might be helpful; or perhaps a link to a
    more comprehensive guide on Kotlin function signatures (if one exists).

--- detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenMethodCall.kt
+++ detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenMethodCall.kt
@@ -58,10 +58,13 @@ class ForbiddenMethodCall(config: Config = Config.empty) : Rule(config) {
             "Methods can be defined without full signature (i.e. `java.time.LocalDate.now`) which will report " +
             "calls of all methods with this name or with full signature " +
             "(i.e. `java.time.LocalDate(java.time.Clock)`) which would report only call " +
-            "with this concrete signature. If you want to forbid an extension function like" +
+            "with this concrete signature. If you want to forbid an extension function like " +
             "`fun String.hello(a: Int)` you should add the receiver parameter as the first parameter like this: " +
             "`hello(kotlin.String, kotlin.Int)`. To forbid constructor calls you need to define them with `<init>`, " +
-            " for example `java.util.Date.<init>`."
+            "for example `java.util.Date.<init>`. To forbid calls involving type parameters, omit them, for example " +
+            "`fun hello(args: Array<Any>)` is referred to as simply `hello(kotlin.Array)` (also the signature for " +
+            "vararg parameters). To forbid methods from the companion object reference the Companion class, for " +
+            "example as `TestClass.Companion.hello()` (even if it is marked `@JvmStatic`)."
     )
     private val methods: List<Forbidden> by config(
         valuesWithReason(
