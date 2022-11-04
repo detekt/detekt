4dac97990640efdd0ed3ea9bc103a3a6a7f60203
Toshiaki Kameyama <kmymtsak@gmail.com>
2022-11-04 18:02:29 +0900

    MultilineLambdaItParameter: fix false positive for one-line statements with a lambda argument

--- detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/MultilineLambdaItParameterSpec.kt
+++ detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/MultilineLambdaItParameterSpec.kt
@@ -129,6 +129,24 @@ class MultilineLambdaItParameterSpec(val env: KotlinCoreEnvironment) {
             val findings = subject.compileAndLintWithContext(env, code)
             assertThat(findings).hasSize(1)
         }
+
+        @Test
+        fun `does not report when statement has no nested statements`() {
+            val code = """
+            data class Foo(val x: Int, val y: Int)
+            
+            fun f(i: Int?) {
+                val foo = i?.let {
+                    Foo(
+                        x = it,
+                        y = 2
+                    )
+                }
+            }
+            """.trimIndent()
+            val findings = subject.compileAndLintWithContext(env, code)
+            assertThat(findings).isEmpty()
+        }
     }
 
     @Nested
@@ -171,6 +189,19 @@ class MultilineLambdaItParameterSpec(val env: KotlinCoreEnvironment) {
             val code = """
             fun f() {
                 val digits = 1234.let { param -> listOf(param) }
+            }   
+            """.trimIndent()
+            val findings = subject.compileAndLintWithContext(env, code)
+            assertThat(findings).isEmpty()
+        }
+
+        @Test
+        fun `does not report when statement has one-line lambda argument`() {
+            val code = """
+            fun f() {
+                val digits = 1234.let {
+                    check(it > 0) { println(it) }
+                }
             }
             """.trimIndent()
             val findings = subject.compileAndLintWithContext(env, code)
