detekt/detekt

commit 505d6d7c09b874c372e6492b6595b98cb0927532

Get Android variant compile classpath from compileConfiguration (#5152)
* Get compile classpath from variant compileConfiguration

Using compileClasspath didn't resolve the right values at the right point
during configuration phase, causing problems with the configuration cache.

Fixes #5150

* Add detektTest test case for #5150

detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/DetektAndroid.kt
@@ -16,6 +16,7 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.attributes.Attribute
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.TaskProvider
@@ -119,7 +120,11 @@ internal fun Project.registerAndroidDetektTask(
         setSource(variant.sourceSets.map { it.javaDirectories + it.kotlinDirectories })
         extraInputSource?.let { source(it) }
         classpath.setFrom(
-            variant.getCompileClasspath(null).filter { it.exists() },
+            variant.compileConfiguration.incoming.artifactView { view ->
+                view.attributes {
+                    it.attribute(Attribute.of("artifactType", String::class.java), "jar")
+                }
+            }.files,
             bootClasspath,
             javaCompileDestination(variant),
         )
@@ -143,7 +148,11 @@ internal fun Project.registerAndroidCreateBaselineTask(
         setSource(variant.sourceSets.map { it.javaDirectories + it.kotlinDirectories })
         extraInputSource?.let { source(it) }
         classpath.setFrom(
-            variant.getCompileClasspath(null).filter { it.exists() },
+            variant.compileConfiguration.incoming.artifactView { view ->
+                view.attributes {
+                    it.attribute(Attribute.of("artifactType", String::class.java), "jar")
+                }
+            }.files,
             bootClasspath,
             javaCompileDestination(variant),
         )

detekt-gradle-plugin/src/functionalTest/kotlin/io/gitlab/arturbosch/detekt/DetektAndroidSpec.kt
@@ -3,7 +3,6 @@ package io.gitlab.arturbosch.detekt
 import io.gitlab.arturbosch.detekt.testkit.DslGradleRunner
 import io.gitlab.arturbosch.detekt.testkit.ProjectLayout
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -274,7 +273,6 @@ class DetektAndroidSpec {
 
         @Test
         @DisplayName("task :android_lib:detektMain")
-        @Disabled("https://github.com/detekt/detekt/issues/5150")
         fun libDetektMain() {
             gradleRunner.runTasksAndCheckResult(
                 "--configuration-cache",
@@ -289,6 +287,31 @@ class DetektAndroidSpec {
                 assertThat(buildResult.tasks.map { it.path }).containsAll(
                     listOf(
                         ":android_lib:detektMain",
+                    )
+                )
+            }
+        }
+
+        @Test
+        @DisplayName("task :android_lib:detektTest")
+        fun libDetektTest() {
+            gradleRunner.runTasksAndCheckResult(
+                "--configuration-cache",
+                ":android_lib:detektTest",
+            ) { buildResult ->
+                assertThat(buildResult.output).contains("Configuration cache")
+                assertThat(buildResult.output).containsPattern(
+                    """--baseline \S*[/\\]detekt-baseline-debugUnitTest.xml """
+                )
+                assertThat(buildResult.output).containsPattern(
+                    """--baseline \S*[/\\]detekt-baseline-debugAndroidTest.xml """
+                )
+                assertThat(buildResult.output).contains("--report xml:")
+                assertThat(buildResult.output).contains("--report sarif:")
+                assertThat(buildResult.output).doesNotContain("--report txt:")
+                assertThat(buildResult.tasks.map { it.path }).containsAll(
+                    listOf(
+                        ":android_lib:detektTest",
                     )
                 )
             }

commit 9b38f677e2073de53b49f426dcea53039fc64c8b

Fix issues introduced by #5152 (#5508)
* Revert behaviour change from 505d6d7c09b874c372e6492b6595b98cb0927532

* Use standard getter to determine CLI arguments

This avoids the issue with configuration cache originally reported in
#5150, while keeping the v1.21 method of determining the classpath for the
generated detekt tasks.

detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/Detekt.kt
@@ -208,8 +208,8 @@ abstract class Detekt @Inject constructor(
     }
 
     @get:Internal
-    internal val arguments: Provider<List<String>> = project.provider {
-        listOf(
+    internal val arguments
+        get() = listOf(
             InputArgument(source),
             ClasspathArgument(classpath),
             LanguageVersionArgument(languageVersionProp.orNull),
@@ -230,7 +230,6 @@ abstract class Detekt @Inject constructor(
             BasePathArgument(basePathProp.orNull),
             DisableDefaultRuleSetArgument(disableDefaultRuleSetsProp.getOrElse(false))
         ).plus(convertCustomReportsToArguments()).flatMap(CliArgument::toArgument)
-    }
 
     @InputFiles
     @SkipWhenEmpty
@@ -252,7 +251,7 @@ abstract class Detekt @Inject constructor(
     @TaskAction
     fun check() {
         DetektInvoker.create(task = this, isDryRun = isDryRun).invokeCli(
-            arguments = arguments.get(),
+            arguments = arguments,
             ignoreFailures = ignoreFailures,
             classpath = detektClasspath.plus(pluginClasspath),
             taskName = name

detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektCreateBaselineTask.kt
@@ -20,7 +20,6 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileTree
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Console
@@ -115,8 +114,8 @@ abstract class DetektCreateBaselineTask : SourceTask() {
     abstract val jdkHome: DirectoryProperty
 
     @get:Internal
-    internal val arguments: Provider<List<String>> = project.provider {
-        listOf(
+    internal val arguments
+        get() = listOf(
             CreateBaselineArgument,
             ClasspathArgument(classpath),
             JvmTargetArgument(jvmTargetProp.orNull),
@@ -131,7 +130,6 @@ abstract class DetektCreateBaselineTask : SourceTask() {
             BasePathArgument(basePathProp.orNull),
             DisableDefaultRuleSetArgument(disableDefaultRuleSets.getOrElse(false))
         ).flatMap(CliArgument::toArgument)
-    }
 
     @InputFiles
     @SkipWhenEmpty
@@ -142,7 +140,7 @@ abstract class DetektCreateBaselineTask : SourceTask() {
     @TaskAction
     fun baseline() {
         DetektInvoker.create(task = this).invokeCli(
-            arguments = arguments.get(),
+            arguments = arguments,
             ignoreFailures = ignoreFailures.getOrElse(false),
             classpath = detektClasspath.plus(pluginClasspath),
             taskName = name

detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektGenerateConfigTask.kt
@@ -10,7 +10,6 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
-import org.gradle.api.provider.Provider
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
 import org.gradle.api.tasks.CacheableTask
@@ -57,12 +56,11 @@ abstract class DetektGenerateConfigTask @Inject constructor(
         }
 
     @get:Internal
-    internal val arguments: Provider<List<String>> = project.provider {
-        listOf(
+    internal val arguments
+        get() = listOf(
             GenerateConfigArgument,
             ConfigArgument(configFile.get())
         ).flatMap(CliArgument::toArgument)
-    }
 
     @TaskAction
     fun generateConfig() {
@@ -74,7 +72,7 @@ abstract class DetektGenerateConfigTask @Inject constructor(
         Files.createDirectories(configFile.get().asFile.parentFile.toPath())
 
         DetektInvoker.create(task = this).invokeCli(
-            arguments = arguments.get(),
+            arguments = arguments,
             classpath = detektClasspath.plus(pluginClasspath),
             taskName = name,
         )

detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/internal/DetektAndroid.kt
@@ -16,7 +16,6 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.attributes.Attribute
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.TaskProvider
@@ -123,11 +122,7 @@ internal fun Project.registerAndroidDetektTask(
         setSource(variant.sourceSets.map { it.javaDirectories + it.kotlinDirectories })
         extraInputSource?.let { source(it) }
         classpath.setFrom(
-            variant.compileConfiguration.incoming.artifactView { view ->
-                view.attributes {
-                    it.attribute(Attribute.of("artifactType", String::class.java), "jar")
-                }
-            }.files,
+            variant.getCompileClasspath(null).filter { it.exists() },
             bootClasspath,
             javaCompileDestination(variant),
         )
@@ -151,11 +146,7 @@ internal fun Project.registerAndroidCreateBaselineTask(
         setSource(variant.sourceSets.map { it.javaDirectories + it.kotlinDirectories })
         extraInputSource?.let { source(it) }
         classpath.setFrom(
-            variant.compileConfiguration.incoming.artifactView { view ->
-                view.attributes {
-                    it.attribute(Attribute.of("artifactType", String::class.java), "jar")
-                }
-            }.files,
+            variant.getCompileClasspath(null).filter { it.exists() },
             bootClasspath,
             javaCompileDestination(variant),
         )

detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/DetektJvmSpec.kt
@@ -37,7 +37,7 @@ class DetektJvmSpec {
         val project = gradleRunner.buildProject()
 
         val detektTask = project.tasks.getByPath("detektMain") as Detekt
-        val argumentString = detektTask.arguments.get().joinToString(" ")
+        val argumentString = detektTask.arguments.joinToString(" ")
 
         assertThat(argumentString).containsPattern("""--baseline \S*[/\\]detekt-baseline-main.xml """)
         assertThat(argumentString).contains("--report xml:")
@@ -53,7 +53,7 @@ class DetektJvmSpec {
         val project = gradleRunner.buildProject()
 
         val detektTask = project.tasks.getByPath("detektTest") as Detekt
-        val argumentString = detektTask.arguments.get().joinToString(" ")
+        val argumentString = detektTask.arguments.joinToString(" ")
 
         assertThat(argumentString).containsPattern("""--baseline \S*[/\\]detekt-baseline-test.xml """)
         assertThat(argumentString).contains("--report xml:")

detekt-gradle-plugin/src/test/kotlin/io/gitlab/arturbosch/detekt/DetektPlainSpec.kt
@@ -68,7 +68,7 @@ class DetektPlainSpec {
             val project = gradleRunner.buildProject()
 
             val detektTask = project.tasks.getByPath("detekt") as Detekt
-            val argumentString = detektTask.arguments.get().joinToString(" ")
+            val argumentString = detektTask.arguments.joinToString(" ")
 
             assertThat(argumentString).containsPattern("""--baseline \S*[/\\]detekt-baseline.xml """)
             assertThat(argumentString).contains("--report xml:")