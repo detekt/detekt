5af7b73a4390668fb8dd1cf65a82704b4135b0ea
Chao Zhang <chao.zhang@instacart.com>
2022-11-06 19:27:20 -0800

    Update default config of TrailingComma to upstream

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/TrailingCommaOnDeclarationSite.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/TrailingCommaOnDeclarationSite.kt
@@ -3,13 +3,17 @@ package io.gitlab.arturbosch.detekt.formatting.wrappers
 import com.pinterest.ktlint.core.api.UsesEditorConfigProperties
 import com.pinterest.ktlint.ruleset.standard.TrailingCommaOnDeclarationSiteRule
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.config
+import io.gitlab.arturbosch.detekt.api.configWithAndroidVariants
 import io.gitlab.arturbosch.detekt.api.internal.AutoCorrectable
 import io.gitlab.arturbosch.detekt.api.internal.Configuration
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
 /**
  * See [ktlint docs](https://pinterest.github.io/ktlint/rules/standard/) for documentation.
+ *
+ * [Kotlin coding convention](https://kotlinlang.org/docs/coding-conventions.html#trailing-commas) recommends
+ * trailing comma encourage the use of trailing commas at the declaration site and
+ * leaves it at your discretion for the call site.
  */
 @AutoCorrectable(since = "1.22.0")
 class TrailingCommaOnDeclarationSite(config: Config) : FormattingRule(config) {
@@ -18,7 +22,10 @@ class TrailingCommaOnDeclarationSite(config: Config) : FormattingRule(config) {
     override val issue = issueFor("Rule to mandate/forbid trailing commas at declaration sites")
 
     @Configuration("Defines whether a trailing comma (or no trailing comma) should be enforced at declaration sites")
-    private val useTrailingCommaOnDeclarationSite by config(false)
+    private val useTrailingCommaOnDeclarationSite by configWithAndroidVariants(
+        defaultValue = true,
+        defaultAndroidValue = false,
+    )
 
     override fun overrideEditorConfigProperties(): Map<UsesEditorConfigProperties.EditorConfigProperty<*>, String> =
         mapOf(
