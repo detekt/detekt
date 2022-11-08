7513394cf4c5de17e5faf6bf4c20e581e9ad3800
Chao Zhang <chao.zhang@instacart.com>
2022-11-07 22:38:43 -0800

    Update doc to reflect ktlint to be an upstream

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/TrailingCommaOnDeclarationSite.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/TrailingCommaOnDeclarationSite.kt
@@ -11,9 +11,12 @@ import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 /**
  * See [ktlint docs](https://pinterest.github.io/ktlint/rules/standard/) for documentation.
  *
- * [Kotlin coding convention](https://kotlinlang.org/docs/coding-conventions.html#trailing-commas) recommends
+ * The default config comes from ktlint and follows these conventions:
+ * - [Kotlin coding convention](https://kotlinlang.org/docs/coding-conventions.html#trailing-commas) recommends
  * trailing comma encourage the use of trailing commas at the declaration site and
  * leaves it at your discretion for the call site.
+ * - [Android Kotlin style guide](https://developer.android.com/kotlin/style-guide) does not include
+ * trailing comma usage yet.
  */
 @AutoCorrectable(since = "1.22.0")
 class TrailingCommaOnDeclarationSite(config: Config) : FormattingRule(config) {
