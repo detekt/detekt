edddb48eac9cd9138b3d8963d9437df3da824dbb
Chao Zhang <chao.zhang@instacart.com>
2022-12-13 00:39:39 -0800

    Exclude operator functions in function min/max length (#5618)
    * Exclude operator functions in function min/max length
    
    * Fix detekt violations

--- detekt-rules-naming/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/naming/FunctionMinLength.kt
+++ detekt-rules-naming/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/naming/FunctionMinLength.kt
@@ -10,6 +10,7 @@ import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.config
 import io.gitlab.arturbosch.detekt.api.internal.Configuration
 import io.gitlab.arturbosch.detekt.rules.identifierName
+import io.gitlab.arturbosch.detekt.rules.isOperator
 import io.gitlab.arturbosch.detekt.rules.isOverride
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
@@ -32,7 +33,7 @@ class FunctionMinLength(config: Config = Config.empty) : Rule(config) {
     private val minimumFunctionNameLength: Int by config(3)
 
     override fun visitNamedFunction(function: KtNamedFunction) {
-        if (function.isOverride()) {
+        if (function.isOverride() || function.isOperator()) {
             return
         }
 
