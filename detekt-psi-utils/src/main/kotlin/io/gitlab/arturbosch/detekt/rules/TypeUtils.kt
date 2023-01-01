da180e862dddfb6a44af1e44fe5779ee0f2340d2
Atul Gupta <atlgpt17@gmail.com>
2023-01-01 23:53:36 +0530

    Refactor KtExpression utility to TypeUtils.kt
    Move isNullable and descriptor common utility that was common across
    NullableToStringCall.kt and CastNullableToNonNullableType.kt rules

--- detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/TypeUtils.kt
+++ detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/TypeUtils.kt
@@ -1,13 +1,20 @@
 package io.gitlab.arturbosch.detekt.rules
 
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.config.LanguageVersionSettings
+import org.jetbrains.kotlin.descriptors.CallableDescriptor
+import org.jetbrains.kotlin.diagnostics.Errors
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtSafeQualifiedExpression
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.calls.smartcasts.DataFlowValueFactory
+import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.TypeUtils
+import org.jetbrains.kotlin.types.isFlexible
+import org.jetbrains.kotlin.types.isNullable
 import org.jetbrains.kotlin.util.containingNonLocalDeclaration
 
 fun KotlinType.fqNameOrNull(): FqName? {
@@ -52,3 +59,47 @@ fun KtExpression.getDataFlowAwareTypes(
     return dataFlowInfo.getStableTypes(dataFlowValue, languageVersionSettings)
         .ifEmpty { setOf(originalType) }
 }
+
+/**
+ * Return if overall expression is nullable or not nullable
+ *
+ * ```kotlin
+ * var a: Int? = null
+ * val b = a // RHS expression will be nullable
+ * val c = a!! // RHS expression will be not nullable
+ * val d = (a ?: error("null assertion message")) // RHS expression will be not nullable
+ * val c = 1?.and(2) // RHS expression will be not nullable
+ * ```
+ * [shouldConsiderPlatformTypeAsNullable] determines the behaviour of platform type. Passing true considers
+ * the platform type as nullable otherwise it is not considered nullable in case of false
+ */
+@Suppress("ReturnCount")
+fun KtExpression.isNullable(
+    bindingContext: BindingContext,
+    languageVersionSettings: LanguageVersionSettings?,
+    dataFlowValueFactory: DataFlowValueFactory?,
+    shouldConsiderPlatformTypeAsNullable: Boolean,
+): Boolean {
+    languageVersionSettings ?: return false
+    dataFlowValueFactory ?: return false
+
+    val safeAccessOperation = safeAs<KtSafeQualifiedExpression>()?.operationTokenNode?.safeAs<PsiElement>()
+    if (safeAccessOperation != null) {
+        return bindingContext.diagnostics.forElement(safeAccessOperation).none {
+            it.factory == Errors.UNNECESSARY_SAFE_CALL
+        }
+    }
+    val originalType = descriptor(bindingContext)?.returnType?.takeIf {
+        it.isNullable() && (shouldConsiderPlatformTypeAsNullable || !it.isFlexible())
+    } ?: return false
+    val dataFlowTypes = getDataFlowAwareTypes(
+        bindingContext,
+        languageVersionSettings,
+        dataFlowValueFactory,
+        originalType
+    )
+    return dataFlowTypes.all { it.isNullable() }
+}
+
+fun KtExpression.descriptor(bindingContext: BindingContext): CallableDescriptor? =
+    getResolvedCall(bindingContext)?.resultingDescriptor
--- detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/CastNullableToNonNullableType.kt
+++ detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/CastNullableToNonNullableType.kt
@@ -8,18 +8,10 @@ import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.internal.RequiresTypeResolution
-import io.gitlab.arturbosch.detekt.rules.getDataFlowAwareTypes
-import io.gitlab.arturbosch.detekt.rules.safeAs
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.descriptors.CallableDescriptor
-import org.jetbrains.kotlin.diagnostics.Errors
+import io.gitlab.arturbosch.detekt.rules.isNullable
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtBinaryExpressionWithTypeRHS
-import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtNullableType
-import org.jetbrains.kotlin.psi.KtSafeQualifiedExpression
-import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
-import org.jetbrains.kotlin.types.isNullable
 import org.jetbrains.kotlin.utils.addToStdlib.ifFalse
 import org.jetbrains.kotlin.utils.addToStdlib.ifTrue
 
@@ -59,32 +51,16 @@ class CastNullableToNonNullableType(config: Config = Config.empty) : Rule(config
         if (expression.left.text == KtTokens.NULL_KEYWORD.value) return
         val typeElement = expression.right?.typeElement ?: return
         (typeElement is KtNullableType).ifTrue { return }
-        expression.left.isNullable().ifFalse { return }
+        expression.left.isNullable(
+            bindingContext,
+            compilerResources?.languageVersionSettings,
+            compilerResources?.dataFlowValueFactory,
+            shouldConsiderPlatformTypeAsNullable = true,
+        ).ifFalse { return }
 
         val message =
             "Use separate `null` assertion and type cast like ('(${expression.left.text} ?: " +
                 "error(\"null assertion message\")) as ${typeElement.text}') instead of '${expression.text}'."
         report(CodeSmell(issue, Entity.from(operationReference), message))
     }
-
-    private fun KtExpression.isNullable(): Boolean {
-        val compilerResources = compilerResources ?: return false
-
-        val safeAccessOperation = safeAs<KtSafeQualifiedExpression>()?.operationTokenNode?.safeAs<PsiElement>()
-        if (safeAccessOperation != null) {
-            return bindingContext.diagnostics.forElement(safeAccessOperation).none {
-                it.factory == Errors.UNNECESSARY_SAFE_CALL
-            }
-        }
-        val originalType = descriptor()?.returnType?.takeIf { it.isNullable() } ?: return false
-        val dataFlowTypes = getDataFlowAwareTypes(
-            bindingContext,
-            compilerResources.languageVersionSettings,
-            compilerResources.dataFlowValueFactory,
-            originalType
-        )
-        return dataFlowTypes.all { it.isNullable() }
-    }
-
-    private fun KtExpression.descriptor(): CallableDescriptor? = getResolvedCall(bindingContext)?.resultingDescriptor
 }
