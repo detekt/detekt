1fe9a9892e61a9a76808b9ec139b6eaa6220afd0
Atul Gupta <atlgpt17@gmail.com>
2023-01-12 23:37:02 +0530

    Add allowOperators in DataClassContainsFunctions (#5658)
    * Add allowOperators in DataClassContainsFunctions
    
    * Remove delegation related TCs
    
    Decouple if checks

--- detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/DataClassContainsFunctionsSpec.kt
+++ detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/DataClassContainsFunctionsSpec.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 private const val CONVERSION_FUNCTION_PREFIX = "conversionFunctionPrefix"
+private const val ALLOW_OPERATORS = "allowOperators"
 
 class DataClassContainsFunctionsSpec {
     val subject = DataClassContainsFunctions()
@@ -40,6 +41,35 @@ class DataClassContainsFunctionsSpec {
             val config = TestConfig(mapOf(CONVERSION_FUNCTION_PREFIX to emptyList<String>()))
             val rule = DataClassContainsFunctions(config)
             assertThat(rule.compileAndLint(code)).hasSize(2)
+        }
+    }
+
+    @Nested
+    inner class `operators in data class` {
+        val code = """
+            data class Vector2(val x: Float, val y: Float) {
+                operator fun plus(other: Vector2): Vector2 = Vector2(x + other.x, y + other.y)
+            }
+        """.trimIndent()
+
+        @Test
+        fun `reports operators if not allowed by default`() {
+            val rule = DataClassContainsFunctions()
+            assertThat(rule.compileAndLint(code)).hasSize(1)
+        }
+
+        @Test
+        fun `reports operators if not allowed`() {
+            val config = TestConfig(mapOf(ALLOW_OPERATORS to false))
+            val rule = DataClassContainsFunctions(config)
+            assertThat(rule.compileAndLint(code)).hasSize(1)
+        }
+
+        @Test
+        fun `does not report operators if allowed`() {
+            val config = TestConfig(mapOf(ALLOW_OPERATORS to true))
+            val rule = DataClassContainsFunctions(config)
+            assertThat(rule.compileAndLint(code)).isEmpty()
         }
     }
 
