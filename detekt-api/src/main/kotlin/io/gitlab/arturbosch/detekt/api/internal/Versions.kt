a46bd37a85af3abb14ce914bd448be5ca2dc4e69
Matthew Haughton <3flex@users.noreply.github.com>
2023-01-27 09:59:24 +1100

    Refactor common code into its own function

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/Versions.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/Versions.kt
@@ -18,22 +18,16 @@ fun whichJava(): String = System.getProperty("java.runtime.version")
 /**
  * Returns the version of Kotlin that detekt was compiled with
  */
-fun whichKotlin(): String {
-    fun readVersion(resource: URL): String? = resource.openSafeStream()
-        .use { Manifest(it).mainAttributes.getValue("KotlinImplementationVersion") }
-
-    return Extension::class.java.classLoader.getResources("META-INF/MANIFEST.MF")
-        .asSequence()
-        .mapNotNull { runCatching { readVersion(it) }.getOrNull() }
-        .first()
-}
+fun whichKotlin(): String = getManifestValue("KotlinImplementationVersion")
 
 /**
  * Returns the bundled detekt version.
  */
-fun whichDetekt(): String {
+fun whichDetekt(): String = getManifestValue("DetektVersion")
+
+private fun getManifestValue(key: String): String {
     fun readVersion(resource: URL): String? = resource.openSafeStream()
-        .use { Manifest(it).mainAttributes.getValue("DetektVersion") }
+        .use { Manifest(it).mainAttributes.getValue(key) }
 
     return Extension::class.java.classLoader.getResources("META-INF/MANIFEST.MF")
         .asSequence()
