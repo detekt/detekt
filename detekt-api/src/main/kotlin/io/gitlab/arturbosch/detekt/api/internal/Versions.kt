d5962222cf1898dc5578c422421315a9de43860d
Matthew Haughton <3flex@users.noreply.github.com>
2023-01-27 09:57:45 +1100

    Stop returning null when versions can't be retrieved from manifest
    This should never happen and simplifies things at call sites.

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/Versions.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/Versions.kt
@@ -18,25 +18,25 @@ fun whichJava(): String = System.getProperty("java.runtime.version")
 /**
  * Returns the version of Kotlin that detekt was compiled with
  */
-fun whichKotlin(): String? {
+fun whichKotlin(): String {
     fun readVersion(resource: URL): String? = resource.openSafeStream()
         .use { Manifest(it).mainAttributes.getValue("KotlinImplementationVersion") }
 
     return Extension::class.java.classLoader.getResources("META-INF/MANIFEST.MF")
         .asSequence()
         .mapNotNull { runCatching { readVersion(it) }.getOrNull() }
-        .firstOrNull()
+        .first()
 }
 
 /**
  * Returns the bundled detekt version.
  */
-fun whichDetekt(): String? {
+fun whichDetekt(): String {
     fun readVersion(resource: URL): String? = resource.openSafeStream()
         .use { Manifest(it).mainAttributes.getValue("DetektVersion") }
 
     return Extension::class.java.classLoader.getResources("META-INF/MANIFEST.MF")
         .asSequence()
         .mapNotNull { runCatching { readVersion(it) }.getOrNull() }
-        .firstOrNull()
+        .first()
 }
