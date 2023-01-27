891b5435f056b75fcb256f38320c3de2f467db9f
Matthew Haughton <3flex@users.noreply.github.com>
2023-01-27 12:42:01 +1100

    Migrate detekt compiler extension to CompilerPluginRegistrar
    The ComponentRegistrar interface is deprecated.

--- detekt-compiler-plugin/src/main/kotlin/io/github/detekt/compiler/plugin/DetektCompilerPluginRegistrar.kt
+++ detekt-compiler-plugin/src/main/kotlin/io/github/detekt/compiler/plugin/DetektCompilerPluginRegistrar.kt
@@ -3,20 +3,18 @@ package io.github.detekt.compiler.plugin
 import io.github.detekt.compiler.plugin.internal.toSpec
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
-import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.resolve.jvm.extensions.AnalysisHandlerExtension
 import java.nio.file.Paths
 
-@Suppress("DEPRECATION")
 @OptIn(ExperimentalCompilerApi::class)
-class DetektComponentRegistrar : org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar {
+class DetektCompilerPluginRegistrar : CompilerPluginRegistrar() {
 
-    override fun registerProjectComponents(
-        project: MockProject,
-        configuration: CompilerConfiguration
-    ) {
+    override val supportsK2 = false
+
+    override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
         if (configuration.get(Keys.IS_ENABLED) == false) {
             return
         }
@@ -24,7 +22,6 @@ class DetektComponentRegistrar : org.jetbrains.kotlin.compiler.plugin.ComponentR
         val messageCollector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
 
         AnalysisHandlerExtension.registerExtension(
-            project,
             DetektAnalysisExtension(
                 messageCollector,
                 configuration.toSpec(messageCollector),
