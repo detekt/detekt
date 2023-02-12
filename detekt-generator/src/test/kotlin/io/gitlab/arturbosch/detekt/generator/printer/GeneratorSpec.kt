5d45cba5ffb915cd25ef61ef87e022650d1f728c
Matthew Haughton <3flex@users.noreply.github.com>
2023-02-13 04:45:57 +1100

    Remove unnecessary usage of BeforeAll (#5781)
    * Refactor to remove companion object
    
    * Remove unnecessary usage of BeforeAll
    
    Tests would sometimes fail due to reuse of the builder. Tests were
    reconfiguring the builder and that configuration persisted when other
    tests ran causing failures in some scenarios. Isolating the builder
    config to individual test classes avoids this issue.

--- detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/GeneratorSpec.kt
+++ detekt-generator/src/test/kotlin/io/gitlab/arturbosch/detekt/generator/printer/GeneratorSpec.kt
@@ -10,6 +10,24 @@ import java.nio.file.Paths
 import kotlin.io.path.readText
 
 class GeneratorSpec {
+    private val configPath = "/src/main/resources/config/config.yml"
+
+    private val tempDir1: File = Files.createTempDirectory(null).toFile()
+    private val tempDir2: File = Files.createTempDirectory(null).toFile()
+
+    @BeforeAll
+    fun init() {
+        Paths.get("../detekt-rules-complexity").toFile().copyRecursively(tempDir1)
+        Paths.get("../detekt-rules-coroutines").toFile().copyRecursively(tempDir2)
+
+        val args = arrayOf(
+            "--generate-custom-rule-config",
+            "--input",
+            "$tempDir1, $tempDir2",
+        )
+        io.gitlab.arturbosch.detekt.generator.main(args)
+    }
+
     @Test
     fun `config files generated successfully`() {
         assertThat(Paths.get(tempDir1.toString(), configPath)).exists()
@@ -27,33 +45,9 @@ class GeneratorSpec {
             .doesNotContain("complexity:")
     }
 
-    companion object {
-        private const val sourceDir1 = "../detekt-rules-complexity"
-        private const val sourceDir2 = "../detekt-rules-coroutines"
-        private const val configPath = "/src/main/resources/config/config.yml"
-
-        private val tempDir1: File = Files.createTempDirectory(null).toFile()
-        private val tempDir2: File = Files.createTempDirectory(null).toFile()
-
-        @JvmStatic
-        @BeforeAll
-        fun init() {
-            File(Paths.get(sourceDir1).toString()).copyRecursively(tempDir1)
-            File(Paths.get(sourceDir2).toString()).copyRecursively(tempDir2)
-
-            val args = arrayOf(
-                "--generate-custom-rule-config",
-                "--input",
-                "$tempDir1, $tempDir2",
-            )
-            io.gitlab.arturbosch.detekt.generator.main(args)
-        }
-
-        @JvmStatic
-        @AfterAll
-        fun tearDown() {
-            tempDir1.deleteRecursively()
-            tempDir2.deleteRecursively()
-        }
+    @AfterAll
+    fun tearDown() {
+        tempDir1.deleteRecursively()
+        tempDir2.deleteRecursively()
     }
 }
