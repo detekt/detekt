43742bb182cd18513f93da94fdf65bef06ad46f4
Goooler <wangzongler@gmail.com>
2023-03-20 02:47:04 +0800

    Set test toolchain via gradleProperty

--- build-logic/src/main/kotlin/module.gradle.kts
+++ build-logic/src/main/kotlin/module.gradle.kts
@@ -1,4 +1,3 @@
-import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -24,6 +23,13 @@ jacoco.toolVersion = versionCatalog.findVersion("jacoco").get().requiredVersion
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+    val testJavaVersion = providers.gradleProperty("io.github.detekt.javaToolchain.test")
+        .getOrElse(Versions.JVM_TARGET.toString())
+    javaLauncher.set(
+        javaToolchains.launcherFor {
+            languageVersion.set(JavaLanguageVersion.of(testJavaVersion))
+        }
+    )
     systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_class")
     val compileTestSnippets = providers.gradleProperty("compile-test-snippets").orNull.toBoolean()
     systemProperty("compile-test-snippets", compileTestSnippets)
@@ -39,34 +45,6 @@ tasks.withType<Test>().configureEach {
         showExceptions = true
         showCauses = true
         showStackTraces = true
-    }
-}
-
-@Suppress("MagicNumber")
-listOf(8, 11, 17).forEach { jdkVersion ->
-    // Windows with JDK8 are really flaky
-    if (jdkVersion == 8 && Os.isFamily(Os.FAMILY_WINDOWS)) return@forEach
-    // https://github.com/detekt/detekt/issues/5646
-    if (project.name == "detekt-compiler-plugin") return@forEach
-
-    val jdkTest = tasks.register<Test>("testJdk$jdkVersion") {
-        val javaToolchains = project.extensions.getByType(JavaToolchainService::class)
-        javaLauncher.set(
-            javaToolchains.launcherFor {
-                languageVersion.set(JavaLanguageVersion.of(jdkVersion))
-            }
-        )
-
-        description = "Runs the test suite on JDK $jdkVersion"
-        group = LifecycleBasePlugin.VERIFICATION_GROUP
-
-        // Copy inputs from normal Test task.
-        val testTask = tasks.test.get()
-        classpath = testTask.classpath
-        testClassesDirs = testTask.testClassesDirs
-    }
-    tasks.check {
-        dependsOn(jdkTest)
     }
 }
 
