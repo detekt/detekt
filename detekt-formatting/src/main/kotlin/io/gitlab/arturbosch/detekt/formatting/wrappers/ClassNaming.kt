53bd0f44386e09eb7e0f4ab93a2fc81db5720871
Atul Gupta <atlgpt17@gmail.com>
2023-04-23 17:56:44 +0530

    Add wrapper for KtLint rules

---
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/ClassNaming.kt
@@ -0,0 +1,15 @@
+package io.gitlab.arturbosch.detekt.formatting.wrappers
+
+import com.pinterest.ktlint.ruleset.standard.rules.ClassNamingRule
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.formatting.FormattingRule
+
+/**
+ * See [ktlint docs](https://pinterest.github.io/ktlint/rules/standard/#classobject-naming) for
+ * documentation.
+ */
+class ClassNaming(config: Config) : FormattingRule(config) {
+    override val wrapping = ClassNamingRule()
+    override val issue =
+        issueFor("Class or object name should start with an uppercase letter and use camel case.")
+}
