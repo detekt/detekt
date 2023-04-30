17ddae47de9886aced278a6b0a5b0dbf478399b2
Atul Gupta <atlgpt17@gmail.com>
2023-04-30 16:25:05 +0530

    Remove Jetbrains copyright as we are attributing to original source

--- detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/movelambdaout/MoveLambdaOutsideParenthesesInspection.kt
+++ detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/movelambdaout/MoveLambdaOutsideParenthesesInspection.kt
@@ -1,5 +1,3 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-
 package io.gitlab.arturbosch.detekt.rules.style.movelambdaout
 
 import org.jetbrains.kotlin.builtins.isFunctionOrSuspendFunctionType
