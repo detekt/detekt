63d8904d5f3d21b8b588f5a80a809c098c22807d
Markus Schwarz <post@codekenner.de>
2023-05-07 14:19:04 +0200

    remove unused autoCorrect property

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/FormattingProvider.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/FormattingProvider.kt
@@ -28,8 +28,5 @@ class FormattingProvider : RuleSetProvider {
     companion object {
         @Configuration("if android style guides should be preferred")
         val android by ruleSetConfig(false)
-
-        @Configuration("if rules should auto correct style violation")
-        val autoCorrect by ruleSetConfig(true)
     }
 }
