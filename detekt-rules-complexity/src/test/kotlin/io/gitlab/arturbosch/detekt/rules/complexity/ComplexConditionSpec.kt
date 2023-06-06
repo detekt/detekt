d546db6a405ba2f4e09cef109967f311041bc6e4
Matthias Schenk <matthias.schenk@sevdesk.de>
2023-06-06 21:01:25 +0200

    Replaced "threshold" property of ComplexCondition rule by "allowedComplexity" (#3679)
    The rule reported an issue when the complexity of conditions is greater or equal the value defined for the "threshold" property.
    It is intended that the value defined in the rule is the maximum allowed complexity. So the property is renamed to
    "allowedComplexity" and the check for reporting an issue is changed to only use greater and no longer equal.

--- detekt-rules-complexity/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/ComplexConditionSpec.kt
+++ detekt-rules-complexity/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/complexity/ComplexConditionSpec.kt
@@ -1,22 +1,60 @@
 package io.gitlab.arturbosch.detekt.rules.complexity
 
+import io.gitlab.arturbosch.detekt.test.TestConfig
+import io.gitlab.arturbosch.detekt.test.assertThat
 import io.gitlab.arturbosch.detekt.test.compileAndLint
-import org.assertj.core.api.Assertions.assertThat
+import io.gitlab.arturbosch.detekt.test.isThresholded
 import org.junit.jupiter.api.Test
 
 class ComplexConditionSpec {
 
-    val code = """
-        val a = if (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5)) { 42 } else { 24 }
-        
-        fun complexConditions() {
-            while (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5)) {}
-            do { } while (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5))
-        }
-    """.trimIndent()
+    private val testConfig = TestConfig("allowedComplexity" to 4)
 
     @Test
-    fun `reports some complex conditions`() {
-        assertThat(ComplexCondition().compileAndLint(code)).hasSize(3)
+    fun `should report complex conditions exceeding allowed complexity`() {
+        val code = """
+           val a = if (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5)) { 42 } else { 24 }
+                    
+           fun complexConditions() {
+               while (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5) && 2 % 2 == 0) {}
+               do { } while (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5))
+           }
+        """.trimIndent()
+
+        val actual = ComplexCondition(testConfig).compileAndLint(code)
+
+        assertThat(actual).hasSize(1)
+        assertThat(actual.first()).isThresholded().withValue(5)
+    }
+
+    @Test
+    fun `should not report conditions that has exactly the allowed complexity`() {
+        val code = """
+            val a = if (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5)) { 42 } else { 24 }
+                    
+            fun complexConditions() {
+                while (5 > 4 && 4 < 6 || (3 < 5 || 2 < 5)) {}
+            }
+        """.trimIndent()
+
+        val actual = ComplexCondition(testConfig).compileAndLint(code)
+
+        assertThat(actual).isEmpty()
+    }
+
+    @Test
+    fun `should not report conditions that are below the allowed complexity`() {
+        val code = """
+             fun simpleCondition(a: Int): Boolean {
+                 if(a == 1) {
+                     return true
+                 }
+                 return false
+             }
+        """.trimIndent()
+
+        val actual = ComplexCondition(testConfig).compileAndLint(code)
+
+        assertThat(actual).isEmpty()
     }
 }
