faab009bd18e73b10aca750f1ff76ba9af70ba7e
Matthew Haughton <3flex@users.noreply.github.com>
2023-07-16 09:06:30 +1000

    Remove deprecated elements from Gradle plugin (#6180)
    * Remove deprecated elements from Gradle plugin
    
    * Update tests

--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektGenerateConfigTask.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/DetektGenerateConfigTask.kt
@@ -1,7 +1,5 @@
 package io.gitlab.arturbosch.detekt
 
-import io.gitlab.arturbosch.detekt.DetektPlugin.Companion.CONFIG_DIR_NAME
-import io.gitlab.arturbosch.detekt.DetektPlugin.Companion.CONFIG_FILE
 import io.gitlab.arturbosch.detekt.invoke.CliArgument
 import io.gitlab.arturbosch.detekt.invoke.ConfigArgument
 import io.gitlab.arturbosch.detekt.invoke.DetektInvoker
@@ -10,7 +8,6 @@ import io.gitlab.arturbosch.detekt.invoke.GenerateConfigArgument
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ProviderFactory
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
@@ -21,13 +18,11 @@ import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import org.gradle.language.base.plugins.LifecycleBasePlugin
 import org.gradle.workers.WorkerExecutor
-import java.io.File
 import java.nio.file.Files
 import javax.inject.Inject
 
 @CacheableTask
 abstract class DetektGenerateConfigTask @Inject constructor(
-    objects: ObjectFactory,
     private val workerExecutor: WorkerExecutor,
     private val providers: ProviderFactory,
 ) : DefaultTask() {
@@ -44,21 +39,7 @@ abstract class DetektGenerateConfigTask @Inject constructor(
     abstract val pluginClasspath: ConfigurableFileCollection
 
     @get:OutputFile
-    val configFile: RegularFileProperty = objects.fileProperty().convention { configurationToUse }
-
-    @get:Internal
-    @get:Deprecated("Replaced with configFile property")
-    abstract val config: ConfigurableFileCollection
-
-    private val defaultConfigPath = project.rootDir.toPath().resolve(CONFIG_DIR_NAME).resolve(CONFIG_FILE)
-
-    @Suppress("DEPRECATION")
-    private val configurationToUse: File
-        get() = if (config.isEmpty) {
-            defaultConfigPath.toFile()
-        } else {
-            config.last()
-        }
+    abstract val configFile: RegularFileProperty
 
     @get:Internal
     internal val arguments
--- detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/DetektReport.kt
+++ detekt-gradle-plugin/src/main/kotlin/io/gitlab/arturbosch/detekt/extensions/DetektReport.kt
@@ -4,23 +4,9 @@ import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
-import java.io.File
 import javax.inject.Inject
 
 abstract class DetektReport @Inject constructor(val type: DetektReportType) {
-
-    @Deprecated("Use required.set(value)")
-    var enabled: Boolean?
-        get() = required.get()
-        set(value) = required.set(value)
-
-    @Deprecated("Use outputLocation.set(value)")
-    var destination: File?
-        get() = outputLocation.asFile.orNull
-        set(value) {
-            outputLocation.set(value)
-        }
-
     @get:Input
     abstract val required: Property<Boolean>
 
