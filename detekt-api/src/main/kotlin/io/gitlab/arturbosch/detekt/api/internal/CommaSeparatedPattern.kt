ea7b19ee506a61aeeb76570fc010e9003b213402
Brais Gabín <braisgabin@gmail.com>
2023-09-02 11:08:40 +0200

    Remove unused code

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/CommaSeparatedPattern.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/internal/CommaSeparatedPattern.kt
@@ -2,40 +2,11 @@ package io.gitlab.arturbosch.detekt.api.internal
 
 import io.gitlab.arturbosch.detekt.api.commaSeparatedPattern
 
-class CommaSeparatedPattern(
-    text: String,
-    delimiters: String = ","
-) {
+class CommaSeparatedPattern(text: String) {
 
-    @Suppress("detekt.SpreadOperator")
     private val excludes = text
-        .commaSeparatedPattern(*delimiters.toCharArray().map { it.toString() }.toTypedArray())
+        .commaSeparatedPattern(",")
         .toList()
 
     fun mapToRegex(): Set<Regex> = excludes.map { it.toRegex() }.toSet()
-
-    /**
-     * Does any part contain given [value]?
-     */
-    fun contains(value: String?): Boolean = excludes.any { value?.contains(it, ignoreCase = true) == true }
-
-    /**
-     * Is there any element which matches the given [value]?
-     */
-    fun any(value: String?): Boolean = excludes.any { value?.equals(it, ignoreCase = true) == true }
-
-    /**
-     * Tests if none of the parts contain the given [value].
-     */
-    fun none(value: String): Boolean = !contains(value)
-
-    /**
-     * Finds all parts which match the given [value].
-     */
-    fun matches(value: String): List<String> = excludes.filter { value.contains(it, ignoreCase = true) }
-
-    /**
-     * Tests if any part starts with the given [value]
-     */
-    fun startWith(value: String?): Boolean = excludes.any { value?.startsWith(it) ?: false }
 }
