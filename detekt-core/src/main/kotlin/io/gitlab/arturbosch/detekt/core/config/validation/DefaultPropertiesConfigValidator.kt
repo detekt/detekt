d6b68fc3abff0a4b6dfcc03a9d702118938bb900
Brais Gabín <braisgabin@gmail.com>
2023-09-06 11:05:46 +0200

    Remove `SplitPattern` and `CommaSeparatedPattern` (#6448)
    * Merge SplitPattern and CommaSeparatedPattern
    
    * Simplify code
    
    * Remove unused code
    
    * Don't use CommaSeparatedPattern in ConfigValidation
    
    * Remove CommaSeparatedPattern

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/validation/DefaultPropertiesConfigValidator.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/validation/DefaultPropertiesConfigValidator.kt
@@ -3,7 +3,7 @@ package io.gitlab.arturbosch.detekt.core.config.validation
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.ConfigValidator
 import io.gitlab.arturbosch.detekt.api.Notification
-import io.gitlab.arturbosch.detekt.api.internal.CommaSeparatedPattern
+import io.gitlab.arturbosch.detekt.api.commaSeparatedPattern
 import io.gitlab.arturbosch.detekt.api.internal.DefaultRuleSetProvider
 import io.gitlab.arturbosch.detekt.core.ProcessingSettings
 import io.gitlab.arturbosch.detekt.core.rules.RuleSetLocator
@@ -17,10 +17,15 @@ internal class DefaultPropertiesConfigValidator(
         fun patterns(): Set<Regex> {
             val pluginExcludes = RuleSetLocator(settings).load()
                 .filter { it !is DefaultRuleSetProvider }
-                .joinToString(",") { "${it.ruleSetId}.*" }
+                .map { "${it.ruleSetId}.*".toRegex() }
             val configExcludes = config.subConfig("config").valueOrDefault("excludes", "")
-            val allExcludes = "$configExcludes,$DEFAULT_PROPERTY_EXCLUDES,$pluginExcludes"
-            return CommaSeparatedPattern(allExcludes).mapToRegex()
+                .commaSeparatedPattern(",")
+                .map { it.toRegex() }
+            return buildSet {
+                addAll(pluginExcludes)
+                addAll(DEFAULT_PROPERTY_EXCLUDES)
+                addAll(configExcludes)
+            }
         }
         return validateConfig(config, baseline, patterns())
     }
