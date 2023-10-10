721fe386d29e4a0341b3a02503c544f732bfaf7a
Matthew Haughton <3flex@users.noreply.github.com>
2023-10-10 14:11:01 +0800

    Opt in to ExperimentalCompilerApi in all compilations in compiler-plugin (#6534)

--- detekt-compiler-plugin/src/test/kotlin/io/github/detekt/compiler/plugin/util/CompilerTestUtils.kt
+++ detekt-compiler-plugin/src/test/kotlin/io/github/detekt/compiler/plugin/util/CompilerTestUtils.kt
@@ -5,11 +5,9 @@ import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import io.github.detekt.compiler.plugin.DetektCommandLineProcessor
 import org.intellij.lang.annotations.Language
-import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 
 object CompilerTestUtils {
 
-    @OptIn(ExperimentalCompilerApi::class)
     fun compile(@Language("kotlin") vararg kotlinSources: String): JvmCompilationResult {
         val sourceFiles = kotlinSources.map {
             SourceFile.kotlin("KClass.kt", it, trimIndent = true)
