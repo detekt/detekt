23397c258db199645f49bd8c1a433132d0ce3b01
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-11-12 11:42:03 +0000

    Fix broken hashes for v1.0 upgrade

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/NoConsecutiveComments.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/NoConsecutiveComments.kt
@@ -6,7 +6,7 @@ import io.gitlab.arturbosch.detekt.api.internal.ActiveByDefault
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
 /**
- * See [ktlint docs](https://pinterest.github.io/ktlint/<ktlintVersion/>/rules/standard/#disallow-consecutive-comments) for documentation.
+ * See [ktlint docs](https://pinterest.github.io/ktlint/<ktlintVersion/>/rules/standard/#no-consecutive-comments) for documentation.
  */
 @ActiveByDefault(since = "2.0.0")
 class NoConsecutiveComments(config: Config) : FormattingRule(config) {
