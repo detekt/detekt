0bbed92977eda639ef3ce6a45fe21b22e7a710a1
Matthew Haughton <3flex@users.noreply.github.com>
2023-12-02 17:06:34 +1100

    Suppress deprecation of URL constructor

--- detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/StringExtensions.kt
+++ detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/StringExtensions.kt
@@ -5,6 +5,7 @@ import java.net.URL
 fun String.lastArgumentMatchesUrl(): Boolean {
     val lastArgument = trimEnd().split(Regex("\\s+")).last()
     return runCatching {
+        @Suppress("DEPRECATION")
         URL(lastArgument).toURI()
     }.isSuccess
 }
