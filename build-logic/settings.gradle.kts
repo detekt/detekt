7d967eb9590111a0c28782a2dcdb10f928e9ca0c
Matthew Haughton <3flex@users.noreply.github.com>
2023-12-07 10:25:17 +1100

    Keep build cache configuration consistent in all builds (#6693)
    It is recommended to keep the build cache configuration of the root
    build consistent with the build cache configuration of early evaluated
    included builds.
    
    Included builds applied using `pluginManagement` do not inherit build
    cache configuration from the top-level build so were not saving task
    output to the remote cache.

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -7,3 +7,20 @@ dependencyResolutionManagement {
         }
     }
 }
+
+val isCiBuild = providers.environmentVariable("CI").isPresent
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+    remote<HttpBuildCache> {
+        isPush = isCiBuild
+        isEnabled = true
+        url = uri("https://ge.detekt.dev/cache/")
+        credentials {
+            username = providers.environmentVariable("GRADLE_CACHE_USERNAME").orNull
+            password = providers.environmentVariable("GRADLE_CACHE_PASSWORD").orNull
+        }
+    }
+}
