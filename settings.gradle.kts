77b98ea27eba2bb8b8473620e3dfd907dbda5f71
Matthew Haughton <3flex@users.noreply.github.com>
2023-12-16 14:40:04 +1100

    Use consistent JDK for Kotlin compilation (#6706)
    * Use consistent JDK for Kotlin compilation
    
    If toolchains are not used the version of Java that Gradle is running with
    is passed as an input to Kotlin compilation tasks. Using toolchains means
    compilation tasks have a consistent JDK version as the input. Builds can
    then run on any Java version and hit remote cache primed with builds run on
    different Java versions.
    
    * Apply Foojay Toolchains Plugin
    
    The plugin provides a repository for downloading JVMs when they're not
    already installed on the machine running the build.
    
    * Suppress MagicNumber violation
    
    * Move compilerOptions to kotlin block

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -47,6 +47,7 @@ enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 plugins {
     id("com.gradle.enterprise") version "3.16.1"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "1.12.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
 val isCiBuild = providers.environmentVariable("CI").isPresent
