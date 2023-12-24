b8c8dce4bda5e8fbf36c04659c8546b640b30e49
Brais Gabín <braisgabin@gmail.com>
2023-12-24 21:46:14 +0100

    Remove deprecated code (#6726)

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
@@ -1,9 +1,6 @@
 package io.gitlab.arturbosch.detekt.api
 
-import io.gitlab.arturbosch.detekt.api.internal.PathFilters
 import io.gitlab.arturbosch.detekt.api.internal.validateIdentifier
-import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.resolve.BindingContext
 
 typealias RuleSetId = String
 
@@ -15,26 +12,4 @@ class RuleSet(val id: RuleSetId, val rules: List<BaseRule>) {
     init {
         validateIdentifier(id)
     }
-
-    /**
-     * Is used to determine if a given [KtFile] should be analyzed at all.
-     */
-    @Deprecated("Exposes detekt-core implementation details.")
-    var pathFilters: PathFilters? = null
-
-    /**
-     * Visits given file with all rules of this rule set, returning a list
-     * of all code smell findings.
-     */
-    @Suppress("DEPRECATION")
-    @Deprecated("Exposes detekt-core implementation details.")
-    fun accept(file: KtFile, bindingContext: BindingContext = BindingContext.EMPTY): List<Finding> =
-        if (pathFilters?.isIgnored(file) == true) {
-            emptyList()
-        } else {
-            rules.flatMap {
-                it.visitFile(file, bindingContext)
-                it.findings
-            }
-        }
 }
