189fab070898487a82561fdb47d75c48f890f466
Matthew Haughton <3flex@users.noreply.github.com>
2024-01-04 15:30:12 +1100

    Move empty config declaration to its callsite
    This helps tidy the internal package a little.

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Config.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Config.kt
@@ -1,7 +1,5 @@
 package io.gitlab.arturbosch.detekt.api
 
-import io.gitlab.arturbosch.detekt.api.internal.EmptyConfig
-
 /**
  * A configuration holds information about how to configure specific rules.
  */
@@ -50,7 +48,15 @@ interface Config {
          * This config should only be used in test cases.
          * Always returns the default value except when 'active' is queried, it returns true.
          */
-        val empty: Config = EmptyConfig
+        val empty: Config = object : Config {
+            override fun subConfig(key: String): Config = this
+
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : Any> valueOrNull(key: String): T? = when (key) {
+                ACTIVE_KEY -> true as? T
+                else -> null
+            }
+        }
 
         const val ACTIVE_KEY: String = "active"
         const val AUTO_CORRECT_KEY: String = "autoCorrect"
