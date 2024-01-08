a82ecd85815eaa01639f86cdebe2fbbf4cb4057c
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-08 13:59:50 +0100

    Simplify code

--- detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/rules/RuleSetSpec.kt
+++ detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/rules/RuleSetSpec.kt
@@ -1,32 +1,32 @@
 package io.gitlab.arturbosch.detekt.core.rules
 
-import io.github.detekt.test.utils.compileForTest
-import io.github.detekt.test.utils.resourceAsPath
+import io.github.detekt.test.utils.compileContentForTest
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.test.TestConfig
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
 
 class RuleSetSpec {
 
     @Nested
     inner class `should rule analyze a file` {
 
-        private val file = compileForTest(resourceAsPath("/cases/Default.kt"))
+        private val file = compileContentForTest("", path = Path("/cases/Default.kt"))
 
         @Test
         fun `analyzes file with an empty config`() {
             val config = Config.empty
-            assertThat(config.subConfig("comments").shouldAnalyzeFile(file)).isTrue()
+            assertThat(config.shouldAnalyzeFile(file)).isTrue()
         }
 
         @Test
         @DisplayName("should not analyze file with **/*.kt excludes")
         fun ignoreExcludedKt() {
             val config = TestConfig(Config.EXCLUDES_KEY to listOf("**/*.kt"))
-            assertThat(config.subConfig("comments").shouldAnalyzeFile(file)).isFalse()
+            assertThat(config.shouldAnalyzeFile(file)).isFalse()
         }
 
         @Test
@@ -35,7 +35,7 @@ class RuleSetSpec {
                 Config.EXCLUDES_KEY to listOf("**/*.kt"),
                 Config.INCLUDES_KEY to listOf("**/*.kt"),
             )
-            assertThat(config.subConfig("comments").shouldAnalyzeFile(file)).isFalse()
+            assertThat(config.shouldAnalyzeFile(file)).isFalse()
         }
     }
 }
