e3713e00ca25cb0dc2db19182e2c6e2d63fb5ddc
mgroth0 <mgroth49@gmail.com>
2024-01-09 19:56:45 -0500

    fix code smell

--- detekt-rules-empty/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyKotlinFileSpec.kt
+++ detekt-rules-empty/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/empty/EmptyKotlinFileSpec.kt
@@ -2,37 +2,31 @@ package io.gitlab.arturbosch.detekt.rules.empty
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.test.TestConfig
+import io.gitlab.arturbosch.detekt.test.assertThat
 import io.gitlab.arturbosch.detekt.test.compileAndLint
-import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
-
-class EmptyKotlinFileBlockSpec {
+class EmptyKotlinFileSpec {
     private val subject = EmptyKotlinFile(Config.empty)
     private val ignorePackageDirectiveSubject = EmptyKotlinFile(TestConfig("ignorePackageDeclaration" to true))
 
+    private val codeWithPackageStatement = """
+            package my.package
+    """.trimIndent()
 
     @Test
     fun `reports empty if file is blank`() {
         val code = ""
-        Assertions.assertThat(subject.compileAndLint(code)).hasSize(1)
+        assertThat(subject.compileAndLint(code)).hasSize(1)
     }
-
-
-    private val codeWithPackageStatement = """
-            package my.package
-        """.trimIndent()
 
     @Test
     fun `does not reports file with package statement`() {
-        Assertions.assertThat(subject.compileAndLint(codeWithPackageStatement)).hasSize(0)
+        assertThat(subject.compileAndLint(codeWithPackageStatement)).hasSize(0)
     }
 
     @Test
     fun `reports file with package statement if configured to`() {
-        Assertions.assertThat(ignorePackageDirectiveSubject.compileAndLint(codeWithPackageStatement)).hasSize(1)
+        assertThat(ignorePackageDirectiveSubject.compileAndLint(codeWithPackageStatement)).hasSize(1)
     }
-
-
-
 }
