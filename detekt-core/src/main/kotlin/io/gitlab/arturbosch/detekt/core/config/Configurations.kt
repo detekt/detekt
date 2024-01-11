f259cab7fe1ca15c66a49e1f8c524a1074ad81fc
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-11 17:11:20 +0100

    Simplify loadConfiguration

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/Configurations.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/config/Configurations.kt
@@ -3,31 +3,12 @@ package io.gitlab.arturbosch.detekt.core.config
 import io.github.detekt.tooling.api.spec.ProcessingSpec
 import io.github.detekt.utils.openSafeStream
 import io.gitlab.arturbosch.detekt.api.Config
-import java.net.URL
-import java.nio.file.Path
 
 internal fun ProcessingSpec.loadConfiguration(): Config = with(configSpec) {
     return when {
-        configPaths.isNotEmpty() -> parsePathConfig(configPaths)
-        resources.isNotEmpty() -> parseResourceConfig(resources)
-        else -> Config.empty
+        configPaths.isNotEmpty() -> configPaths.map { YamlConfig.load(it) }
+        resources.isNotEmpty() -> resources.map { it.openSafeStream().reader().use(YamlConfig::load) }
+        else -> listOf(Config.empty)
     }
+        .reduce { composite, config -> CompositeConfig(config, composite) }
 }
-
-private fun parseResourceConfig(urls: Collection<URL>): Config =
-    if (urls.size == 1) {
-        urls.single().openSafeStream().reader().use(YamlConfig::load)
-    } else {
-        urls.asSequence()
-            .map { it.openSafeStream().reader().use(YamlConfig::load) }
-            .reduce { composite, config -> CompositeConfig(config, composite) }
-    }
-
-private fun parsePathConfig(paths: Collection<Path>): Config =
-    if (paths.size == 1) {
-        YamlConfig.load(paths.single())
-    } else {
-        paths.asSequence()
-            .map { YamlConfig.load(it) }
-            .reduce { composite, config -> CompositeConfig(config, composite) }
-    }
