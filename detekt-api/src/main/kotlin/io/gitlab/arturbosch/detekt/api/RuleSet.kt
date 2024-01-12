623e9860219d0aa2fdbb2e4e6efb361e60a9199d
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-13 00:03:55 +0100

    Make the abi a bit cleaner

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
@@ -5,8 +5,9 @@ import io.gitlab.arturbosch.detekt.api.internal.validateIdentifier
 /**
  * A rule set is a collection of rules and must be defined within a rule set provider implementation.
  */
-class RuleSet(val id: Id, val rules: Map<String, (Config) -> Rule>) {
+class RuleSet(@get:JvmName("getId") val id: Id, val rules: Map<String, (Config) -> Rule>) {
     companion object {
+        @JvmName("invoke")
         operator fun invoke(id: Id, rules: List<(Config) -> Rule>): RuleSet {
             return RuleSet(id, rules.associateBy { it(Config.empty).ruleId })
         }
