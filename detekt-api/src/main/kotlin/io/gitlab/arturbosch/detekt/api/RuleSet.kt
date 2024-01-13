ba0d5e7344c621ce4c3b94b1303e38af35082b75
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-13 21:23:41 +0100

    Don't use value class

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
@@ -5,22 +5,33 @@ import io.gitlab.arturbosch.detekt.api.internal.validateIdentifier
 /**
  * A rule set is a collection of rules and must be defined within a rule set provider implementation.
  */
-class RuleSet(@get:JvmName("getId") val id: Id, val rules: Map<String, (Config) -> Rule>) {
+class RuleSet(val id: Id, val rules: Map<String, (Config) -> Rule>) {
     companion object {
-        @JvmName("invoke")
         operator fun invoke(id: Id, rules: List<(Config) -> Rule>): RuleSet {
             return RuleSet(id, rules.associateBy { it(Config.empty).ruleId })
         }
     }
 
-    @JvmInline
-    value class Id(val value: String) {
+    class Id(val value: String) {
         init {
             validateIdentifier(value)
         }
 
         override fun toString(): String {
             return value
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Id
+
+            return value == other.value
+        }
+
+        override fun hashCode(): Int {
+            return value.hashCode()
         }
     }
 }
