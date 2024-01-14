c94c72b577741a7768adcc17ebb466f388d31afc
Brais Gabín <braisgabin@gmail.com>
2024-01-14 01:51:49 +0100

    Don't use value class for RuleSet.Id (#6861)
    * Don't use value class
    
    * Use Poko

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/RuleSet.kt
@@ -1,20 +1,20 @@
 package io.gitlab.arturbosch.detekt.api
 
+import dev.drewhamilton.poko.Poko
 import io.gitlab.arturbosch.detekt.api.internal.validateIdentifier
 
 /**
  * A rule set is a collection of rules and must be defined within a rule set provider implementation.
  */
-class RuleSet(@get:JvmName("getId") val id: Id, val rules: Map<Rule.Id, (Config) -> Rule>) {
+class RuleSet(val id: Id, val rules: Map<Rule.Id, (Config) -> Rule>) {
     companion object {
-        @JvmName("invoke")
         operator fun invoke(id: Id, rules: List<(Config) -> Rule>): RuleSet {
             return RuleSet(id, rules.associateBy { it(Config.empty).ruleId })
         }
     }
 
-    @JvmInline
-    value class Id(val value: String) {
+    @Poko
+    class Id(val value: String) {
         init {
             validateIdentifier(value)
         }
