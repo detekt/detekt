125bd9cc44f86d1ede3d3e35a6da93ff9b52de94
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-15 22:52:03 +0100

    Add javadoc

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Findings2.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Findings2.kt
@@ -2,6 +2,12 @@ package io.gitlab.arturbosch.detekt.api
 
 import dev.drewhamilton.poko.Poko
 
+/**
+ * Represents a problem detected by detekt on the source code
+ *
+ * A finding has an issue (information about the rule that detected the problem), a severity and a source code position
+ * described as an entity. Entity references can also be considered for deeper characterization.
+ */
 interface Finding2 : HasEntity {
     val issue: Issue
     val references: List<Entity>
