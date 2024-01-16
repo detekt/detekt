fec71709912f81d9fd06b2c610584719a49abd8c
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-16 15:03:56 +0100

    Simplify code

--- detekt-metrics/src/main/kotlin/io/github/detekt/metrics/ComplexityMetric.kt
+++ detekt-metrics/src/main/kotlin/io/github/detekt/metrics/ComplexityMetric.kt
@@ -15,5 +15,5 @@ class ComplexityMetric(detektion: Detektion) {
     val sloc = detektion.getUserData(sourceLinesKey)
     val lloc = detektion.getUserData(logicalLinesKey)
     val cloc = detektion.getUserData(commentLinesKey)
-    val findings = detektion.findings.entries
+    val findingsCount = detektion.findings.values.sumOf { it.size }
 }
