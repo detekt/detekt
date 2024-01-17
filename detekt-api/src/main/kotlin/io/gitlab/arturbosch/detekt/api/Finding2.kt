38ebebc3c8c6ae22faaa5a4f8d9045761d082493
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-17 09:20:16 +0100

    Improve description

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding2.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding2.kt
@@ -3,8 +3,8 @@ package io.gitlab.arturbosch.detekt.api
 /**
  * Represents a problem detected by detekt on the source code
  *
- * A finding has information about the rule that detected the problem, a severity and a source code position
- * described as an entity. Entity references can also be considered for deeper characterization.
+ * A finding has information about the rule that detected the problem, a severity and an entity with information
+ * about the position. Entity references can also be considered for deeper characterization.
  */
 interface Finding2 : HasEntity {
     val rule: RuleInfo
