164618cee785865658b8eb8da622af9ecc9647eb
Brais Gabín <braisgabin@gmail.com>
2024-02-02 20:30:11 +0100

    Simplify RuleDescriptors.kt (#6936)

--- detekt-report-sarif/src/main/kotlin/io/github/detekt/report/sarif/RuleDescriptors.kt
+++ detekt-report-sarif/src/main/kotlin/io/github/detekt/report/sarif/RuleDescriptors.kt
@@ -4,7 +4,6 @@ import io.github.detekt.sarif4k.MultiformatMessageString
 import io.github.detekt.sarif4k.ReportingConfiguration
 import io.github.detekt.sarif4k.ReportingDescriptor
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
@@ -15,43 +14,31 @@ import java.util.ServiceLoader
  * Given the existing config, return a list of [ReportingDescriptor] for the rules.
  */
 internal fun toReportingDescriptors(config: Config): List<ReportingDescriptor> {
-    val sets = ServiceLoader.load(RuleSetProvider::class.java, SarifOutputReport::class.java.classLoader)
+    return ServiceLoader.load(RuleSetProvider::class.java, SarifOutputReport::class.java.classLoader)
         .map { it.instance() }
-    val ruleSetIdAndRules = sets.flatMap { ruleSet ->
-        val ruleSetConfig = config.subConfig(ruleSet.id.value)
-        ruleSet.rules.map { (id, provider) ->
-            val rule = provider(ruleSetConfig.subConfig(id.value))
-            val severity = rule.computeSeverity()
-            RuleInfo(ruleSet.id, rule.issue, severity)
+        .flatMap { ruleSet ->
+            val ruleSetConfig = config.subConfig(ruleSet.id.value)
+            ruleSet.rules.map { (id, provider) ->
+                val rule = provider(ruleSetConfig.subConfig(id.value))
+                rule.toDescriptor(ruleSet.id)
+            }
         }
-    }
-    val descriptors = mutableListOf<ReportingDescriptor>()
-    ruleSetIdAndRules.forEach { (ruleSetId, issue, severity) ->
-        descriptors.add(issue.toDescriptor(ruleSetId, severity))
-    }
-    return descriptors
 }
 
-private fun Issue.toDescriptor(ruleSetId: RuleSet.Id, severity: Severity): ReportingDescriptor {
+private fun Rule.toDescriptor(ruleSetId: RuleSet.Id): ReportingDescriptor {
     val formattedRuleSetId = ruleSetId.value.lowercase()
-    val formattedRuleId = id.value.lowercase()
+    val formattedRuleId = ruleId.value.lowercase()
 
     return ReportingDescriptor(
-        id = "detekt.$ruleSetId.$id",
-        name = id.value,
-        shortDescription = MultiformatMessageString(text = description),
+        id = "detekt.$ruleSetId.$ruleId",
+        name = ruleId.value,
+        shortDescription = MultiformatMessageString(text = issue.description),
         helpURI = "https://detekt.dev/$formattedRuleSetId.html#$formattedRuleId",
         defaultConfiguration = ReportingConfiguration(
-            level = severity.toResultLevel()
+            level = computeSeverity().toResultLevel()
         )
     )
 }
-
-private data class RuleInfo(
-    val id: RuleSet.Id,
-    val severity: Issue,
-    val issue: Severity
-)
 
 private fun Rule.computeSeverity(): Severity {
     val configValue: String = config.valueOrNull(Config.SEVERITY_KEY)
