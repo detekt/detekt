be4a910f2d67142950ec6c167fa6d0460f97b8d3
Matthew Haughton <3flex@users.noreply.github.com>
2024-03-05 19:07:41 +1100

    Throw exception when KotlinTargetsContainer extension isn't found
    This should never happen on a KMP project unless KGP API interfaces are
    changed unexpectedly.

--- detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/plugin/internal/DetektKmpJvmCompilations.kt
+++ detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/plugin/internal/DetektKmpJvmCompilations.kt
@@ -8,8 +8,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinTargetsContainer
 
 internal object DetektKmpJvmCompilations {
     fun registerTasks(project: Project, extension: DetektExtension) {
-        val kotlinExtension = project.extensions.getByName("kotlin")
-        if (kotlinExtension !is KotlinTargetsContainer) return
+        val kotlinExtension = project.extensions.getByType(KotlinTargetsContainer::class.java)
 
         kotlinExtension.targets.matching { it.platformType in setOf(jvm, androidJvm) }.all { target ->
             target.compilations.all { compilation ->
