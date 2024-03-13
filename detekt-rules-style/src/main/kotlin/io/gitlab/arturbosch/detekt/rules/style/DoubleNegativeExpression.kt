963bc6596f50216a890464c006c821ac0ac9b290
Toshiaki Kameyama <kmymtsak@gmail.com>
2024-03-13 18:33:47 +0900

    Add DoubleNegativeExpression rule

---
+++ detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/DoubleNegativeExpression.kt
@@ -0,0 +1,92 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.RequiresTypeResolution
+import io.gitlab.arturbosch.detekt.api.Rule
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtPrefixExpression
+import org.jetbrains.kotlin.psi.KtQualifiedExpression
+import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
+
+/**
+ * Detects expressions with two or more calls of operator `not` could be simplified.
+ *
+ * <noncompliant>
+ * isValid.not().not()
+ * !isValid.not()
+ * !!isValid
+ * </noncompliant>
+ *
+ * <compliant>
+ * isValid
+ * </compliant>
+ */
+@RequiresTypeResolution
+class DoubleNegativeExpression(config: Config) : Rule(
+    config,
+    "Expression with two or more calls of operator `not` could be simplified.",
+) {
+
+    override fun visitPrefixExpression(expression: KtPrefixExpression) {
+        super.visitPrefixExpression(expression)
+        check(expression)
+    }
+
+    override fun visitQualifiedExpression(expression: KtQualifiedExpression) {
+        super.visitQualifiedExpression(expression)
+        check(expression)
+    }
+
+    private fun check(expression: KtExpression) {
+        val parent = expression.parent
+        if (parent is KtPrefixExpression || parent is KtQualifiedExpression) return
+        if (expression.isDoubleNegativeExpression()) {
+            val codeSmell = CodeSmell(
+                Entity.from(expression),
+                "Expression with two or more calls of operator `not` could be simplified.",
+            )
+            report(codeSmell)
+        }
+    }
+
+    private fun KtExpression.isDoubleNegativeExpression(): Boolean {
+        var expr: KtExpression? = this
+        var count = 0
+        while (count < 2) {
+            when {
+                expr is KtPrefixExpression && expr.operationToken == KtTokens.EXCL -> {
+                    count++
+                    expr = expr.baseExpression
+                }
+
+                expr is KtQualifiedExpression && expr.selectorExpression.isBooleanNotCall() -> {
+                    count++
+                    expr = expr.receiverExpression
+                }
+
+                expr is KtCallExpression && expr.isBooleanNotCall() -> {
+                    count++
+                    expr = null
+                }
+
+                else -> break
+            }
+        }
+        return count == 2
+    }
+
+    private fun KtExpression?.isBooleanNotCall(): Boolean {
+        return this is KtCallExpression &&
+            getResolvedCall(bindingContext)?.resultingDescriptor?.fqNameSafe == booleanNotFqName
+    }
+
+    companion object {
+        private val booleanNotFqName = FqName("kotlin.Boolean.not")
+    }
+}
---
+++ detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/DoubleNegativeExpressionSpec.kt
@@ -0,0 +1,113 @@
+package io.gitlab.arturbosch.detekt.rules.style
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
+import io.gitlab.arturbosch.detekt.test.assertThat
+import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.jupiter.api.Test
+
+@KotlinCoreEnvironmentTest
+class DoubleNegativeExpressionSpec(private val env: KotlinCoreEnvironment) {
+    private val subject = DoubleNegativeExpression(Config.empty)
+
+    @Test
+    fun not() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return b.not()
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).isEmpty()
+    }
+
+    @Test
+    fun `not not`() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return b.not().not()
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).hasSize(1)
+    }
+
+    @Test
+    fun `not not not`() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return b.not().not().not()
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).hasSize(1)
+    }
+
+    @Test
+    fun `!`() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return !b
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).isEmpty()
+    }
+
+    @Test
+    fun `!!`() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return !!b
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).hasSize(1)
+    }
+
+    @Test
+    fun `!!!`() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return !!!b
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).hasSize(1)
+    }
+
+    @Test
+    fun `! not`() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return !b.not()
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).hasSize(1)
+    }
+
+    @Test
+    fun `!! not`() {
+        val code = """
+            fun test(b: Boolean): Boolean {
+                return !!b.not()
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).hasSize(1)
+    }
+
+    @Test
+    fun `implicit receiver`() {
+        val code = """
+            fun Boolean.test() {
+                val x = !not()
+                val y = not().not()
+            }
+        """.trimIndent()
+        val actual = subject.compileAndLintWithContext(env, code)
+        assertThat(actual).hasSize(2)
+    }
+}
