1b3a20ed505c8a8f57c551341761c205398c603b
Brais Gabín <brais.gabin@scrm.lidl>
2024-03-17 23:55:17 +0100

    Remove HasEntity

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding2.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding2.kt
@@ -6,24 +6,19 @@ package io.gitlab.arturbosch.detekt.api
  * A finding has information about the rule that detected the problem, a severity and an entity with information
  * about the position. Entity references can also be considered for deeper characterization.
  */
-interface Finding2 : HasEntity {
+interface Finding2 {
     val rule: RuleInfo
+    val entity: Entity
     val references: List<Entity>
     val message: String
     val severity: Severity
     val autoCorrectEnabled: Boolean
 
+    val location: Location
+        get() = entity.location
+
     interface RuleInfo {
         val id: Rule.Id
         val description: String
     }
-}
-
-/**
- * Describes a source code position.
- */
-interface HasEntity {
-    val entity: Entity
-    val location: Location
-        get() = entity.location
 }
