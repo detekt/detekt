ac294223ba5b36a6b274624aba72e8363ea6e905
Brais Gabín <braisgabin@gmail.com>
2024-03-22 10:35:15 +0100

    Update detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding.kt
    Co-authored-by: schalkms <30376729+schalkms@users.noreply.github.com>

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding.kt
@@ -3,7 +3,7 @@ package io.gitlab.arturbosch.detekt.api
 /**
  * Represents a problem in the source code detected by a rule.
  *
- * A finding has an issue (information about the rule that detected the problem), a severity and a source code position
+ * A finding has an issue (information about the rule that detected the problem), a severity and a source code position.
  * described as an entity. Entity references can also be considered for deeper characterization.
  */
 interface Finding {
