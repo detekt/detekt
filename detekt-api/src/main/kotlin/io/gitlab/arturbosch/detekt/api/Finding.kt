0814e50c05e53880b1769108555afa2be4f3c51f
Brais Gabín <brais.gabin@scrm.lidl>
2024-03-22 10:39:30 +0100

    Fix kdoc

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Finding.kt
@@ -1,10 +1,10 @@
 package io.gitlab.arturbosch.detekt.api
 
 /**
- * Represents a problem in the source code detected by a rule.
+ * Represents a detected problem in the source code.
  *
- * A finding has an issue (information about the rule that detected the problem), a severity and a source code position.
- * described as an entity. Entity references can also be considered for deeper characterization.
+ * A finding has a source code position described as an entity and a message.
+ * Entity references can also be considered for deeper characterization.
  */
 interface Finding {
     val entity: Entity
