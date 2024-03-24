51b988a963d55f3f8538a0073bb58c6b72e5d738
Toshiaki Kameyama <kmymtsak@gmail.com>
2024-03-24 13:49:13 +0900

    Add DoubleNegativeExpression rule (#7048)
    * Add DoubleNegativeExpression rule
    
    * Update DoubleNegativeExpressionSpec.kt
    
    * Fix unnecessary backticks
    
    ---------
    
    Co-authored-by: Chao Zhang <chao2zhang2@gmail.com>

--- detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/StyleGuideProvider.kt
+++ detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/StyleGuideProvider.kt
@@ -112,6 +112,7 @@ class StyleGuideProvider : DefaultRuleSetProvider {
             ::UseSumOfInsteadOfFlatMapSize,
             ::DoubleNegativeLambda,
             ::UseLet,
+            ::DoubleNegativeExpression,
         )
     )
 }
