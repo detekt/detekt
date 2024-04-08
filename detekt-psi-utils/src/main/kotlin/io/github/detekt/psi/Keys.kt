77f9f90e90c30e037c35a448f6427adb0d9bcfdf
Brais Gabín <brais.gabin@scrm.lidl>
2024-04-08 12:54:06 +0200

    No nullable line separator

--- detekt-psi-utils/src/main/kotlin/io/github/detekt/psi/Keys.kt
+++ detekt-psi-utils/src/main/kotlin/io/github/detekt/psi/Keys.kt
@@ -2,9 +2,10 @@ package io.github.detekt.psi
 
 import org.jetbrains.kotlin.com.intellij.openapi.util.Key
 import org.jetbrains.kotlin.com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.psi.NotNullableUserDataProperty
 import org.jetbrains.kotlin.psi.UserDataProperty
 import java.nio.file.Path
 
 var PsiFile.relativePath: Path? by UserDataProperty(Key("relativePath"))
 var PsiFile.basePath: Path? by UserDataProperty(Key("basePath"))
-var PsiFile.lineSeparator: String? by UserDataProperty(Key("lineSeparator"))
+var PsiFile.lineSeparator: String by NotNullableUserDataProperty(Key("lineSeparator"), System.lineSeparator())
