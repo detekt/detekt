19b1784165efce16cdca7d196ca2a59c05ea25f3
Matthew Haughton <3flex@users.noreply.github.com>
2024-04-20 00:03:30 +1000

    Add CLI test that checks multiple autocorrectable rules can be applied to a given file (#7204)
    * Fix test assertion for single autocorrectable rule
    
    * Add test for multiple autocorrectable rules

---
+++ detekt-cli/src/test/resources/autocorrect/CompliantSample.kt
@@ -0,0 +1,12 @@
+class Test {
+
+    val foo =
+        listOf(1, 2, 3)
+            .filter { it > 2 }!!
+            .takeIf { it.count() > 100 }
+            ?.sum()
+    val foobar =
+        foo()
+            ?: bar
+
+}
