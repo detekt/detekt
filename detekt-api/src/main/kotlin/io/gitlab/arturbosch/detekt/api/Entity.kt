22e888bb14c18da4c12fbdd1c8d5dafd03eb9da7
Matthew Haughton <3flex@users.noreply.github.com>
2024-04-30 17:39:18 +1000

    Remove unused compactWithSignature

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Entity.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/Entity.kt
@@ -74,10 +74,4 @@ interface Compactable {
      * Contract to format implementing object to a string representation.
      */
     fun compact(): String
-
-    /**
-     * Same as [compact] except the content should contain a substring which represents
-     * this exact findings via a custom identifier.
-     */
-    fun compactWithSignature(): String = compact()
 }
