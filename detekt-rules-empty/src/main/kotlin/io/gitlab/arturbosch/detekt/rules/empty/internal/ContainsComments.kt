8ae8ccc5dda52e559c66ec39c03e95acd0b8e6e5
Matthew Haughton <3flex@users.noreply.github.com>
2024-05-07 23:20:13 +1000

    Simplify implementation of PsiElement.hasCommentInside

--- detekt-rules-empty/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/internal/ContainsComments.kt
+++ detekt-rules-empty/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/empty/internal/ContainsComments.kt
@@ -1,19 +1,10 @@
 package io.gitlab.arturbosch.detekt.rules.empty.internal
 
-import org.jetbrains.kotlin.com.intellij.openapi.util.Key
 import org.jetbrains.kotlin.com.intellij.psi.PsiComment
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtClassOrObject
-import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
+import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
 
 fun KtClassOrObject.hasCommentInside() = this.body?.hasCommentInside() ?: false
 
-fun PsiElement.hasCommentInside(): Boolean {
-    val commentKey = Key<Boolean>("comment")
-    this.acceptChildren(object : KtTreeVisitorVoid() {
-        override fun visitComment(comment: PsiComment) {
-            putUserData(commentKey, true)
-        }
-    })
-    return getUserData(commentKey) == true
-}
+fun PsiElement.hasCommentInside(): Boolean = getChildrenOfType<PsiComment>().isNotEmpty()
