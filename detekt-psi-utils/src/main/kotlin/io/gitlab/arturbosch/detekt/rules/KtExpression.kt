f10ab2880d9f4c95458f0facf9242465d33919f1
Matthew Haughton <3flex@users.noreply.github.com>
2024-05-19 20:43:41 +1000

    Remove KtExpression.isUnitExpression()

--- detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtExpression.kt
+++
@@ -1,6 +0,0 @@
-package io.gitlab.arturbosch.detekt.rules
-
-import org.jetbrains.kotlin.builtins.StandardNames
-import org.jetbrains.kotlin.psi.KtExpression
-
-fun KtExpression.isUnitExpression() = text == StandardNames.FqNames.unit.shortName().asString()
