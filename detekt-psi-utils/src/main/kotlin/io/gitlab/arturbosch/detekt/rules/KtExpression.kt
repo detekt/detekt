6ebc5f6a76ede7028919b6b60f5597849bc29201
Matthew Haughton <3flex@users.noreply.github.com>
2024-05-20 20:14:53 +1000

    Remove PsiElement.getParentExpressionAfterParenthesis (#7315)

--- detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtExpression.kt
+++ detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtExpression.kt
@@ -1,15 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules
 
 import org.jetbrains.kotlin.builtins.StandardNames
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.kotlin.psi.KtParenthesizedExpression
-import org.jetbrains.kotlin.psi.psiUtil.getParentOfTypesAndPredicate
 
 fun KtExpression.isUnitExpression() = text == StandardNames.FqNames.unit.shortName().asString()
-
-fun PsiElement.getParentExpressionAfterParenthesis(strict: Boolean = true): PsiElement? =
-    this.getParentOfTypesAndPredicate(
-        strict,
-        PsiElement::class.java,
-    ) { it !is KtParenthesizedExpression }
