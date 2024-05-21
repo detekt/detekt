1aaa6ab1d9f74db58c7ce3cbc8e6e022cc21fccd
Matthew Haughton <3flex@users.noreply.github.com>
2024-05-21 13:27:47 +1000

    Remove ResolvedCall.isCalling (#7317)
    * Simplify isCalling implementations
    
    * Remove ResolvedCall.isCalling
    
    This was only used by functions in this utils file.
    
    * DRY

--- detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtCallExpression.kt
+++ detekt-psi-utils/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/KtCallExpression.kt
@@ -1,31 +1,19 @@
 package io.gitlab.arturbosch.detekt.rules
 
-import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtBinaryExpression
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
 import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 
-fun KtCallExpression.isCalling(fqName: FqName, bindingContext: BindingContext): Boolean {
-    return bindingContext != BindingContext.EMPTY &&
-        calleeExpression?.text == fqName.shortName().asString() &&
-        getResolvedCall(bindingContext)?.isCalling(fqName) == true
-}
+fun KtCallExpression.isCalling(fqName: FqName, bindingContext: BindingContext): Boolean =
+    isCalling(listOf(fqName), bindingContext)
 
-@Suppress("ReturnCount")
 fun KtCallExpression.isCalling(fqNames: List<FqName>, bindingContext: BindingContext): Boolean {
     if (bindingContext == BindingContext.EMPTY) return false
-    val calleeText = calleeExpression?.text ?: return false
-    val targetFqNames = fqNames.filter { it.shortName().asString() == calleeText }
-    if (targetFqNames.isEmpty()) return false
-    val resolvedCall = getResolvedCall(bindingContext) ?: return false
-    return targetFqNames.any { resolvedCall.isCalling(it) }
+    return getResolvedCall(bindingContext)?.resultingDescriptor?.fqNameOrNull() in fqNames
 }
-
-fun ResolvedCall<out CallableDescriptor>.isCalling(fqName: FqName): Boolean = resultingDescriptor.fqNameSafe == fqName
 
 fun KtCallExpression.isCallingWithNonNullCheckArgument(
     fqName: FqName,
