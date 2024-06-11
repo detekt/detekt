a8fef9bfabc62498938a2df9998f5e8551dccf35
Matthew Haughton <3flex@users.noreply.github.com>
2024-06-11 20:58:25 +1000

    Fix issue

--- detekt-rules-naming/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/MatchingDeclarationNameSpec.kt
+++ detekt-rules-naming/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/naming/MatchingDeclarationNameSpec.kt
@@ -6,7 +6,7 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.assertThat
 import io.gitlab.arturbosch.detekt.test.lint
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.kotlin.com.intellij.psi.PsiFile
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -268,31 +268,31 @@ class MatchingDeclarationNameSpec {
         @Test
         fun `should remove kt suffix`() {
             val filename = makeFile("C.kt").fileNameWithoutSuffix()
-            Assertions.assertThat(filename).isEqualTo("C")
+            assertThat(filename).isEqualTo("C")
         }
 
         @Test
         fun `should remove kts suffix`() {
             val filename = makeFile("C.kts").fileNameWithoutSuffix()
-            Assertions.assertThat(filename).isEqualTo("C")
+            assertThat(filename).isEqualTo("C")
         }
 
         @Test
         fun `should not remove non kotlin suffixes`() {
             val filename = makeFile("C.java").fileNameWithoutSuffix()
-            Assertions.assertThat(filename).isEqualTo("C.java")
+            assertThat(filename).isEqualTo("C.java")
         }
 
         @Test
         fun `should not remove common suffix`() {
             val filename = makeFile("C.common").fileNameWithoutSuffix()
-            Assertions.assertThat(filename).isEqualTo("C.common")
+            assertThat(filename).isEqualTo("C.common")
         }
 
         @Test
         fun `should not remove common_ktx suffix`() {
             val filename = makeFile("C.common.ktx").fileNameWithoutSuffix()
-            Assertions.assertThat(filename).isEqualTo("C.common.ktx")
+            assertThat(filename).isEqualTo("C.common.ktx")
         }
     }
 
