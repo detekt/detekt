794583d7f200863b35f08fdb1a0a8f744bc5fba4
Brais Gabín <brais.gabin@scrm.lidl>
2024-06-17 00:57:44 +0200

    Improve tests of RuleSetLocator

--- detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/rules/RuleSetLocatorSpec.kt
+++ detekt-core/src/test/kotlin/io/gitlab/arturbosch/detekt/core/rules/RuleSetLocatorSpec.kt
@@ -1,25 +1,34 @@
 package io.gitlab.arturbosch.detekt.core.rules
 
-import io.github.classgraph.ClassGraph
-import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+import io.github.detekt.test.utils.resourceAsPath
 import io.gitlab.arturbosch.detekt.core.createNullLoggingSpec
 import io.gitlab.arturbosch.detekt.core.tooling.withSettings
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.Test
-import java.lang.reflect.Modifier
 
 class RuleSetLocatorSpec {
 
     @Test
-    fun `contains all RuleSetProviders`() {
+    fun `loads all the RuleSetProviders`() {
         val providers = createNullLoggingSpec().withSettings { RuleSetLocator(this).load() }
-        val providerClasses = getProviderClasses()
 
-        assertThat(providerClasses).isNotEmpty
-        providerClasses
-            .filter { clazz -> providers.none { it.javaClass == clazz } }
-            .forEach { fail("$it rule set is not loaded by the RuleSetLocator") }
+        assertThat(providers.map { it.ruleSetId.value })
+            .containsExactlyInAnyOrder(
+                "sample-rule-set",
+                *defaultRuleSets.toTypedArray(),
+            )
+    }
+
+    @Test
+    fun `loads all the DefaultRuleSetProviders except the disabled`() {
+        val providers = createNullLoggingSpec { extensions { disableExtension("style") } }
+            .withSettings { RuleSetLocator(this).load() }
+
+        assertThat(providers.map { it.ruleSetId.value })
+            .containsExactlyInAnyOrder(
+                "sample-rule-set",
+                *defaultRuleSets.minus("style").toTypedArray(),
+            )
     }
 
     @Test
@@ -27,18 +36,46 @@ class RuleSetLocatorSpec {
         val providers = createNullLoggingSpec { extensions { disableDefaultRuleSets = true } }
             .withSettings { RuleSetLocator(this).load() }
 
-        val defaultProviders = getProviderClasses().toSet()
+        assertThat(providers.map { it.ruleSetId.value })
+            .containsExactlyInAnyOrder("sample-rule-set")
+    }
 
-        assertThat(providers).noneMatch { it.javaClass in defaultProviders }
+    /**
+     * This test runs a precompiled jar with a custom rule provider.
+     * When any breaking change in 'detekt-api' is done, this test will break.
+     *
+     * The procedure to repair this test is:
+     *  1. 'gradle build -x test publishToMavenLocal'
+     *  2. 'gradle build' again to let the 'sample' project pick up the new api changes.
+     *  3. 'cp detekt-sample-extensions/build/libs/detekt-sample-extensions-<version>.jar
+     *          detekt-core/src/test/resources/sample-rule-set.jar'
+     *  4. Now 'gradle build' should be green again.
+     */
+    @Test
+    fun `loads custom rule sets through jars`() {
+        val providers = createNullLoggingSpec {
+            extensions {
+                fromPaths { listOf(resourceAsPath("sample-rule-set.jar")) }
+            }
+        }.withSettings { RuleSetLocator(this).load() }
+
+        assertThat(providers.map { it.ruleSetId.value })
+            .containsExactlyInAnyOrder(
+                "sample-rule-set",
+                "sample",
+                *defaultRuleSets.toTypedArray(),
+            )
     }
 }
 
-private fun getProviderClasses(): List<Class<out RuleSetProvider>> =
-    ClassGraph()
-        .acceptPackages("io.gitlab.arturbosch.detekt.rules")
-        .scan()
-        .use { scanResult ->
-            scanResult.getClassesImplementing(RuleSetProvider::class.java)
-                .loadClasses(RuleSetProvider::class.java)
-                .filter { !Modifier.isAbstract(it.modifiers) }
-        }
+val defaultRuleSets = listOf(
+    "potential-bugs",
+    "complexity",
+    "coroutines",
+    "comments",
+    "empty-blocks",
+    "exceptions",
+    "naming",
+    "performance",
+    "style",
+)
