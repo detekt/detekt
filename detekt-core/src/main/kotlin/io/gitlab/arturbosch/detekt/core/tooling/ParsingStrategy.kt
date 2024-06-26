a1c1104ade08dc37678828e7d0ebee392a38a990
Brais Gabín <brais.gabin@scrm.lidl>
2024-06-26 12:03:56 +0200

    Use KotlinFirLoader on ParsingStrategy

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/ParsingStrategy.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/ParsingStrategy.kt
@@ -1,14 +1,20 @@
 package io.gitlab.arturbosch.detekt.core.tooling
 
-import io.github.detekt.parser.KtCompiler
+import io.github.detekt.parser.KotlinFirLoader
 import io.gitlab.arturbosch.detekt.core.ProcessingSettings
+import org.jetbrains.kotlin.fir.psi
 import org.jetbrains.kotlin.psi.KtFile
+import java.io.File
 
 typealias ParsingStrategy = (settings: ProcessingSettings) -> List<KtFile>
 
 val inputPathsToKtFiles: ParsingStrategy = { settings ->
-    val compiler = KtCompiler(settings.environment)
-    settings.spec.projectSpec.inputPaths.map { path ->
-        compiler.compile(path)
-    }
+    KotlinFirLoader(
+        sources = settings.spec.projectSpec.inputPaths.map { it.toFile() },
+        classpath = settings.classpath.map { File(it) },
+    )
+        .use { it.load() }
+        .outputs
+        .flatMap { it.fir }
+        .map { firFile -> firFile.psi!! as KtFile }
 }
