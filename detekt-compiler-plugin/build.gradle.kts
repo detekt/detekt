4ef1ad2d782f9584ab76f49c0d27fdde158e1f18
Matthew Haughton <3flex@users.noreply.github.com>
2024-07-22 11:52:09 +1000

    Update build.gradle.kts

--- detekt-compiler-plugin/build.gradle.kts
+++ detekt-compiler-plugin/build.gradle.kts
@@ -8,7 +8,7 @@ version = "$kotlinVersion-$detektVersion"
 
 plugins {
     id("module")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.github.goooler.shadow") version "8.1.8"
     id("de.undercouch.download") version "5.6.0"
 }
 
