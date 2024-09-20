5576d51e11f056e8cf799840530c4197a42855f1
Matthew Haughton <3flex@users.noreply.github.com>
2024-09-20 18:18:39 +1000

    Remove compatibility function no longer needed with Gradle 7.6.3

--- detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/plugin/internal/GradleCompat.kt
+++ detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/plugin/internal/GradleCompat.kt
@@ -4,18 +4,8 @@ import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.Directory
-import org.gradle.api.provider.Provider
-import org.gradle.api.provider.ProviderFactory
 import org.gradle.api.tasks.VerificationException
 import org.gradle.util.GradleVersion
-
-internal fun ProviderFactory.gradlePropertyAtConfigTimeCompat(propertyName: String): Provider<String> =
-    if (GradleVersion.current() >= GradleVersion.version("7.4")) {
-        gradleProperty(propertyName)
-    } else {
-        @Suppress("DEPRECATION")
-        gradleProperty(propertyName).forUseAtConfigurationTime()
-    }
 
 internal fun Project.rootProjectDirectoryCompat(): Directory =
     if (GradleVersion.current() >= GradleVersion.version("8.8")) {
