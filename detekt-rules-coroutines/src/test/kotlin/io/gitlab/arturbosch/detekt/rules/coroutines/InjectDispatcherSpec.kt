8ffe4ac6a7282faf58d8d8eeb9d8915b7aba01f9
Eliezer Graber <me@eygraber.com>
2024-12-07 19:35:40 -0800

    Fix InjectDispatcher false positives (#7797)
    Fixes #6200

--- detekt-rules-coroutines/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/coroutines/InjectDispatcherSpec.kt
+++ detekt-rules-coroutines/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/coroutines/InjectDispatcherSpec.kt
@@ -100,6 +100,154 @@ class InjectDispatcherSpec(val env: KotlinCoreEnvironment) {
             """.trimIndent()
             assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
         }
+
+        @Test
+        fun `reports when a custom dispatchers class is used inside of a function`() {
+            val code = """
+                import kotlinx.coroutines.launch
+                import kotlinx.coroutines.runBlocking
+                import kotlinx.coroutines.CoroutineDispatcher
+                
+                interface IDispatchers {
+                    val IO: CoroutineDispatcher
+                }
+                
+                @Suppress("InjectDispatcher")
+                object Dispatchers : IDispatchers {
+                    override val IO = kotlinx.coroutines.Dispatchers.IO
+                }
+                
+                class DispatcherUser(private val dispatchers: Dispatchers) {
+                    fun dispatch() {
+                        runBlocking { launch(Dispatchers.IO) { } }
+                    }
+                }
+            """.trimIndent()
+            assertThat(subject.compileAndLintWithContext(env, code)).hasSize(1)
+        }
+
+        @Test
+        fun `does not report when a custom dispatchers class is used as a function parameter`() {
+            val code = """
+                import kotlinx.coroutines.launch
+                import kotlinx.coroutines.runBlocking
+                import kotlinx.coroutines.CoroutineDispatcher
+                
+                interface IDispatchers {
+                    val IO: CoroutineDispatcher
+                }
+                
+                @Suppress("InjectDispatcher")
+                object Dispatchers : IDispatchers {
+                    override val IO = kotlinx.coroutines.Dispatchers.IO
+                }
+                
+                fun useDispatchers(dispatchers: Dispatchers) {
+                    runBlocking {
+                        launch(dispatchers.IO) { }
+                    }
+                }
+            """.trimIndent()
+            assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
+        }
+
+        @Test
+        fun `does not report when a custom dispatchers class is used as a constructor parameter`() {
+            val code = """
+                import kotlinx.coroutines.launch
+                import kotlinx.coroutines.runBlocking
+                import kotlinx.coroutines.CoroutineDispatcher
+                
+                interface IDispatchers {
+                    val IO: CoroutineDispatcher
+                }
+                
+                @Suppress("InjectDispatcher")
+                object Dispatchers : IDispatchers {
+                    override val IO = kotlinx.coroutines.Dispatchers.IO
+                }
+                
+                class MyRepository(dispatchers: Dispatchers) {
+                    init {
+                        runBlocking { launch(dispatchers.IO) { } }
+                    }
+                }
+            """.trimIndent()
+            assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
+        }
+
+        @Test
+        fun `does not report when a custom dispatchers class is used as a secondary constructor parameter`() {
+            val code = """
+                import kotlinx.coroutines.launch
+                import kotlinx.coroutines.runBlocking
+                import kotlinx.coroutines.CoroutineDispatcher
+                
+                interface IDispatchers {
+                    val IO: CoroutineDispatcher
+                }
+                
+                @Suppress("InjectDispatcher")
+                object Dispatchers : IDispatchers {
+                    override val IO = kotlinx.coroutines.Dispatchers.IO
+                }
+                
+                class MyRepository(dispatchers: Dispatchers) {
+                    constructor() : this(Dispatchers)
+                }
+            """.trimIndent()
+            assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
+        }
+
+        @Test
+        fun `does not report when a custom dispatchers class is used as a property`() {
+            val code = """
+                import kotlinx.coroutines.launch
+                import kotlinx.coroutines.runBlocking
+                import kotlinx.coroutines.CoroutineDispatcher
+                
+                interface IDispatchers {
+                    val IO: CoroutineDispatcher
+                }
+                
+                @Suppress("InjectDispatcher")
+                object Dispatchers : IDispatchers {
+                    override val IO = kotlinx.coroutines.Dispatchers.IO
+                }
+                
+                class DispatcherUser(private val dispatchers: Dispatchers) {
+                    fun dispatch() {
+                        runBlocking { launch(dispatchers.IO) { } }
+                    }
+                }
+            """.trimIndent()
+            assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
+        }
+
+        @Test
+        fun `does not report when a custom dispatchers class is used as a function parameter and there is a property with the same name but different type`() {
+            val code = """
+                import kotlinx.coroutines.launch
+                import kotlinx.coroutines.runBlocking
+                import kotlinx.coroutines.CoroutineDispatcher
+                
+                interface IDispatchers {
+                    val IO: CoroutineDispatcher
+                }
+                
+                @Suppress("InjectDispatcher")
+                object Dispatchers : IDispatchers {
+                    override val IO = kotlinx.coroutines.Dispatchers.IO
+                }
+                
+                class DispatcherUser(private val dispatchers: String) {
+                    fun dispatch(dispatchers: Dispatchers) {
+                        runBlocking { launch(dispatchers.IO) { } }
+                    }
+                }
+            """.trimIndent()
+            assertThat(subject.compileAndLintWithContext(env, code)).isEmpty()
+        }
     }
 
     @Nested
