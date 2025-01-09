dda67f21d9f6b100b9f472108e5d6d921690f85a
Atul Gupta <atlgpt17@gmail.com>
2025-01-09 20:56:14 +0530

    [Deprecation] Add config `ignoreRuleParameterThreshold`
    This allows deprecation in imports

--- detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/Deprecation.kt
+++ detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/Deprecation.kt
@@ -3,12 +3,17 @@ package io.gitlab.arturbosch.detekt.rules.bugs
 import com.intellij.psi.PsiElement
 import io.gitlab.arturbosch.detekt.api.Alias
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Configuration
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Finding
 import io.gitlab.arturbosch.detekt.api.RequiresFullAnalysis
 import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.config
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 
 /**
  * Deprecated elements are expected to be removed in the future. Alternatives should be found if possible.
@@ -22,14 +27,29 @@ class Deprecation(config: Config) :
     ),
     RequiresFullAnalysis {
 
+    @Configuration("Ignore deprecation in import statements")
+    private val ignoreImport: Boolean by config(false)
+
     override fun visitElement(element: PsiElement) {
         val diagnostic = hasDeprecationCompilerWarnings(element)
         if (diagnostic != null) {
-            val entity = if (element is KtNamedDeclaration) Entity.atName(element) else Entity.from(element)
-            report(Finding(entity, """${element.text} is deprecated with message "${diagnostic.b}""""))
+            val entity =
+                if (element is KtNamedDeclaration) Entity.atName(element) else Entity.from(element)
+            if (shouldIgnoreThisElement(element)) {
+                // not reporting for import statements
+                report(
+                    Finding(
+                        entity,
+                        """${element.text} is deprecated with message "${diagnostic.b}""""
+                    )
+                )
+            }
         }
         super.visitElement(element)
     }
+
+    private fun shouldIgnoreThisElement(element: PsiElement) =
+        !(ignoreImport && element.getParentOfType<KtImportDirective>(true) != null)
 
     private fun hasDeprecationCompilerWarnings(element: PsiElement) =
         bindingContext.diagnostics
--- detekt-rules-errorprone/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/DeprecationSpec.kt
+++ detekt-rules-errorprone/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/DeprecationSpec.kt
@@ -3,8 +3,12 @@ package io.gitlab.arturbosch.detekt.rules.bugs
 import io.github.detekt.test.utils.KotlinEnvironmentContainer
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
+import io.gitlab.arturbosch.detekt.test.TestConfig
 import io.gitlab.arturbosch.detekt.test.lintWithContext
+import io.gitlab.arturbosch.detekt.test.location
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 @KotlinCoreEnvironmentTest
@@ -47,5 +51,137 @@ class DeprecationSpec(private val env: KotlinEnvironmentContainer) {
             }
         """.trimIndent()
         assertThat(subject.lintWithContext(env, code)).isEmpty()
+    }
+
+    @Test
+    fun `report when property delegate is deprecated`() {
+        val stateFile = """
+            package state
+
+            import kotlin.reflect.KProperty
+
+            interface State {
+                val value: Double
+            }
+
+            @Deprecated("Some reason")
+            operator fun State.getValue(thisObj: Any?, property: KProperty<*>): Double = value
+        """.trimIndent()
+        val code = """
+            import state.State
+            import state.getValue
+            fun foo(state: State) {
+                val d by state
+            }
+        """.trimIndent()
+        assertThat(subject.lintWithContext(env, code, stateFile))
+            .hasSize(1)
+            .first()
+            .extracting {
+                it.message
+            }
+            .isEqualTo("""state is deprecated with message "Some reason"""")
+    }
+
+    @Test
+    fun `does not report import location`() {
+        val deprecatedFile = """
+            package com.example.detekt.featureflag
+
+            @Deprecated("Moved to other service")
+            enum class LegacyFeatureFlags {
+                FEATURE_A, FEATURE_B,
+            }
+        """.trimIndent()
+        val code = """
+            import com.example.detekt.featureflag.LegacyFeatureFlags
+            class FeatureFlagManager {
+                @Suppress("DEPRECATION")
+                fun getFeatureFlagValue(featureFlag: LegacyFeatureFlags): Boolean {
+                    return true
+                }
+            }
+
+            class Manager(private val featureFlagManager: FeatureFlagManager) {
+                fun doSomething() {
+                    @Suppress("DEPRECATION")
+                    val isFeatureAEnabled =
+                        featureFlagManager.getFeatureFlagValue(LegacyFeatureFlags.FEATURE_A)
+                }
+            }
+        """.trimIndent()
+        assertThat(subject.lintWithContext(env, code, deprecatedFile))
+            .hasSize(1)
+            .first()
+            .extracting { it.location.source.line }
+            .isEqualTo(1)
+    }
+
+    @Nested
+    inner class `With ignore import true` {
+        private val ignoredImportSubject =
+            Deprecation(TestConfig(IGNORED_IMPORT to true))
+
+        @Test
+        fun `does not report import location - #7402`() {
+            val deprecatedFile = """
+                package com.example.detekt.featureflag
+
+                @Deprecated("Moved to other service")
+                enum class LegacyFeatureFlags {
+                    FEATURE_A, FEATURE_B,
+                }
+            """.trimIndent()
+            val code = """
+                import com.example.detekt.featureflag.LegacyFeatureFlags
+                class FeatureFlagManager {
+                    @Suppress("DEPRECATION")
+                    fun getFeatureFlagValue(featureFlag: LegacyFeatureFlags): Boolean {
+                        return true
+                    }
+                }
+
+                class Manager(private val featureFlagManager: FeatureFlagManager) {
+                    fun doSomething() {
+                        @Suppress("DEPRECATION")
+                        val isFeatureAEnabled =
+                            featureFlagManager.getFeatureFlagValue(LegacyFeatureFlags.FEATURE_A)
+                    }
+                }
+            """.trimIndent()
+            assertThat(ignoredImportSubject.lintWithContext(env, code, deprecatedFile))
+                .isEmpty()
+        }
+
+        @Test
+        fun `report when property delegate is deprecated`() {
+            val stateFile = """
+                package state
+                import kotlin.reflect.KProperty
+                interface State {
+                    val value: Double
+                }
+                @Deprecated("Some reason")
+                operator fun State.getValue(thisObj: Any?, property: KProperty<*>): Double = value
+            """.trimIndent()
+            val code = """
+                import state.State
+                import state.getValue
+                fun foo(state: State) {
+                    val d by state
+                }
+            """.trimIndent()
+            assertThat(ignoredImportSubject.lintWithContext(env, code, stateFile))
+                .hasSize(1)
+                .first()
+                .extracting {
+                    it.message
+                }
+                .isEqualTo("""state is deprecated with message "Some reason"""")
+        }
+    }
+
+    companion object {
+        private const val IGNORED_IMPORT = "ignoreImport"
     }
 }
