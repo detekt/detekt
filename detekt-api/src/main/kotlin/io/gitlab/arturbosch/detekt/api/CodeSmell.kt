6c8f571d886aa581b62b02964e00ef7008559e72
Brais Gabín <braisgabin@gmail.com>
2025-01-13 16:27:51 +0100

    Don't open CodeSmell (#7868)

--- detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/CodeSmell.kt
+++ detekt-api/src/main/kotlin/io/gitlab/arturbosch/detekt/api/CodeSmell.kt
@@ -7,11 +7,11 @@ package io.gitlab.arturbosch.detekt.api
  * If the design problem manifests by different source locations, references to these
  * locations can be stored in additional [Entity]'s.
  */
-open class CodeSmell(
-    final override val entity: Entity,
-    final override val message: String,
-    final override val references: List<Entity> = emptyList(),
-    final override val suppressReasons: List<String> = emptyList(),
+class CodeSmell(
+    override val entity: Entity,
+    override val message: String,
+    override val references: List<Entity> = emptyList(),
+    override val suppressReasons: List<String> = emptyList(),
 ) : Finding {
     init {
         require(message.isNotBlank()) { "The message should not be empty" }
