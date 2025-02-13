b7b368b14452d63619bc85efd5bd7763ad56afb5
Matthew Haughton <3flex@users.noreply.github.com>
2025-02-13 13:33:12 +1100

    Remove unused(?) getModel override
    This called ReflectionFactory which is not available in the temurin build used in CI. If it's not available, and this has never caused a bug, then why do we need it?
    
    In theory removing should have no effect.

--- detekt-parser/src/main/kotlin/io/github/detekt/parser/DetektPomModel.kt
+++ detekt-parser/src/main/kotlin/io/github/detekt/parser/DetektPomModel.kt
@@ -2,11 +2,8 @@ package io.github.detekt.parser
 
 import org.jetbrains.kotlin.com.intellij.openapi.util.UserDataHolderBase
 import org.jetbrains.kotlin.com.intellij.pom.PomModel
-import org.jetbrains.kotlin.com.intellij.pom.PomModelAspect
 import org.jetbrains.kotlin.com.intellij.pom.PomTransaction
 import org.jetbrains.kotlin.com.intellij.pom.impl.PomTransactionBase
-import org.jetbrains.kotlin.com.intellij.pom.tree.TreeAspect
-import sun.reflect.ReflectionFactory
 
 /**
  * Adapted from https://github.com/pinterest/ktlint/blob/0.50.0/ktlint-rule-engine/src/main/kotlin/com/pinterest/ktlint/rule/engine/internal/KotlinPsiFileFactory.kt
@@ -22,15 +19,5 @@ object DetektPomModel : UserDataHolderBase(), PomModel {
         }
 
         pomTransaction.run()
-    }
-
-    override fun <T : PomModelAspect?> getModelAspect(aspect: Class<T>): T? {
-        if (aspect == TreeAspect::class.java) {
-            val constructor = ReflectionFactory.getReflectionFactory()
-                .newConstructorForSerialization(aspect, Any::class.java.getDeclaredConstructor())
-            @Suppress("UNCHECKED_CAST")
-            return constructor.newInstance() as T
-        }
-        return null
     }
 }
