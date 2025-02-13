fbed5f9bf38cabe65abd3c037b82bed81334006d
Matthew Haughton <3flex@users.noreply.github.com>
2025-02-13 13:43:33 +1100

    Ignore two previous...

--- detekt-parser/src/main/kotlin/io/github/detekt/parser/DetektPomModel.kt
+++ detekt-parser/src/main/kotlin/io/github/detekt/parser/DetektPomModel.kt
@@ -5,6 +5,8 @@ import org.jetbrains.kotlin.com.intellij.pom.PomModel
 import org.jetbrains.kotlin.com.intellij.pom.PomModelAspect
 import org.jetbrains.kotlin.com.intellij.pom.PomTransaction
 import org.jetbrains.kotlin.com.intellij.pom.impl.PomTransactionBase
+import org.jetbrains.kotlin.com.intellij.pom.tree.TreeAspect
+import sun.reflect.ReflectionFactory
 
 /**
  * Adapted from https://github.com/pinterest/ktlint/blob/0.50.0/ktlint-rule-engine/src/main/kotlin/com/pinterest/ktlint/rule/engine/internal/KotlinPsiFileFactory.kt
@@ -23,6 +25,12 @@ object DetektPomModel : UserDataHolderBase(), PomModel {
     }
 
     override fun <T : PomModelAspect?> getModelAspect(aspect: Class<T>): T? {
+        if (aspect == TreeAspect::class.java) {
+            val constructor = ReflectionFactory.getReflectionFactory()
+                .newConstructorForSerialization(aspect, Any::class.java.getDeclaredConstructor())
+            @Suppress("UNCHECKED_CAST")
+            return constructor.newInstance() as T
+        }
         return null
     }
 }
