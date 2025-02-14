b705cbb5f86b2ab99d50962b4e1d3f18600c5b3d
Matthew Haughton <3flex@users.noreply.github.com>
2025-02-14 22:59:25 +1100

    Enable BackingPropertyNaming by default (#7940)
    * Enable BackingPropertyNaming by default
    
    * Remove redundant config enabling BackingPropertyNaming in project config
    
    * Update ktlint doc URL for BackingPropertyNaming
    
    Co-authored-by: Brais Gabín <braisgabin@gmail.com>
    
    ---------
    
    Co-authored-by: Brais Gabín <braisgabin@gmail.com>

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/BackingPropertyNaming.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/BackingPropertyNaming.kt
@@ -1,13 +1,15 @@
 package io.gitlab.arturbosch.detekt.formatting.wrappers
 
 import com.pinterest.ktlint.ruleset.standard.rules.BackingPropertyNamingRule
+import io.gitlab.arturbosch.detekt.api.ActiveByDefault
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
 /**
- * See [ktlint docs](https://pinterest.github.io/ktlint/<ktlintVersion/>/rules/experimental/#backing-property-naming)
+ * See [ktlint docs](https://pinterest.github.io/ktlint/<ktlintVersion/>/rules/standard/#backing-property-naming)
  * for documentation.
  */
+@ActiveByDefault(since = "2.0.0")
 class BackingPropertyNaming(config: Config) : FormattingRule(
     config,
     "Reports incorrect property name."
