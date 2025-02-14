969e246051970f3adddca926678ff3a6adecd1ee
Matthew Haughton <3flex@users.noreply.github.com>
2025-02-14 22:58:00 +1100

    Fix ktlint doc URL for BinaryExpressionWrapping

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/BinaryExpressionWrapping.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/BinaryExpressionWrapping.kt
@@ -13,7 +13,7 @@ import io.gitlab.arturbosch.detekt.api.internal.AutoCorrectable
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
 /**
- * See [ktlint docs](https://pinterest.github.io/ktlint/<ktlintVersion/>/rules/experimental/#binary-expression-wrapping) for
+ * See [ktlint docs](https://pinterest.github.io/ktlint/<ktlintVersion/>/rules/standard/#binary-expression-wrapping) for
  * documentation.
  */
 @ActiveByDefault(since = "2.0.0")
