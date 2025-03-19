5fc77fd03e54002d89e6350f736856389fefa870
Matthew Haughton <3flex@users.noreply.github.com>
2025-03-19 22:54:46 +1100

    Avoid unnecessary package relocation
    This relocation was only required to make detekt dependencies that used
    kotlin-compiler-embeddable compatible with the compiler itself.

--- detekt-compiler-plugin/build.gradle.kts
+++ detekt-compiler-plugin/build.gradle.kts
@@ -42,7 +42,6 @@ publishing {
 }
 
 tasks.shadowJar {
-    relocate("org.jetbrains.kotlin.com.intellij", "com.intellij")
     relocate("org.snakeyaml.engine", "dev.detekt.shaded.snakeyaml")
     mergeServiceFiles()
     dependencies {
