b0efee3d906895ce7f786591073807c0f805eba0
Matthew Haughton <3flex@users.noreply.github.com>
2025-03-22 13:03:16 +1100

    Add support for compiling test snippets using Kotlin Analysis API

--- detekt-test-utils/build.gradle.kts
+++ detekt-test-utils/build.gradle.kts
@@ -3,9 +3,16 @@ plugins {
     id("public-api")
 }
 
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+}
+
 dependencies {
     api(libs.kotlin.stdlib)
     api(libs.junit.jupiterApi)
+    runtimeOnly(projects.detektKotlinAnalysisApi)
+    compileOnly(libs.bundles.kotlin.analysisApi) { isTransitive = false }
     implementation(projects.detektParser)
     implementation(libs.kotlin.mainKts) {
         isTransitive = false
