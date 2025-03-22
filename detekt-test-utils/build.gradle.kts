60f5c909fa7cef7c303e4ef4d450c2410f3075f8
Matthew Haughton <3flex@users.noreply.github.com>
2025-03-23 10:12:14 +1100

    Use kotlin-scripting-compiler dependency for kotlin-main-kts
    The scripting-compiler-embeddable dependency used by kotlin-main-kts is not
    compatible with the dependencies required by the Analysis API. The non-
    embeddable kotlin-scripting-compiler lib works with the current
    kotlin-scripting version of test snippet compilation and will work with
    Analysis API as well so both can be used in parallel in future.

--- detekt-test-utils/build.gradle.kts
+++ detekt-test-utils/build.gradle.kts
@@ -7,7 +7,11 @@ dependencies {
     api(libs.kotlin.stdlib)
     api(libs.junit.jupiterApi)
     implementation(projects.detektParser)
-    implementation(libs.kotlin.mainKts)
+    implementation(libs.kotlin.mainKts) {
+        // Manually add the kotlin-scripting-compiler dependency instead to avoid future conflicts with Analysis API dependencies.
+        isTransitive = false
+    }
+    implementation(libs.kotlin.scriptingCompiler)
     implementation(libs.kotlinx.coroutinesCore)
 
     testImplementation(libs.assertj.core)
