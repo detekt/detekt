56e66401ca26014b9eb306a9378d435a9b81ea3e
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-06 21:56:17 +1000

    Stop using KotlinCoreEnvironment in detekt-test & detekt-test-utils

--- detekt-test-utils/src/main/kotlin/io/github/detekt/test/utils/KotlinCoreEnvironmentWrapper.kt
+++ detekt-test-utils/src/main/kotlin/io/github/detekt/test/utils/KotlinCoreEnvironmentWrapper.kt
@@ -1,24 +1,25 @@
 package io.github.detekt.test.utils
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.config.CompilerConfiguration
 import java.io.File
+
+class KotlinEnvironmentContainer(val project: Project, val configuration: CompilerConfiguration)
 
 /**
  * Make sure to always call [dispose] or use a [use] block when working with [KotlinCoreEnvironment]s.
  */
 class KotlinCoreEnvironmentWrapper(
-    private var environment: KotlinCoreEnvironment?,
+    private var environment: KotlinCoreEnvironment,
     private val disposable: Disposable,
+    val env: KotlinEnvironmentContainer = KotlinEnvironmentContainer(environment.project, environment.configuration)
 ) {
-
-    val env: KotlinCoreEnvironment
-        get() = checkNotNull(environment) { "Environment already disposed." }
 
     fun dispose() {
         Disposer.dispose(disposable)
-        environment = null
     }
 }
 
