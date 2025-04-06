a4bf7a56989f04197580ac0c3a28771a8e570387
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-06 17:08:29 +1000

    Allow creation of BindingContext in tests without KotlinCoreEnvironment

--- detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/KotlinCoreEnvironmentExtensions.kt
+++ detekt-test/src/main/kotlin/io/gitlab/arturbosch/detekt/test/KotlinCoreEnvironmentExtensions.kt
@@ -1,16 +1,40 @@
 package io.gitlab.arturbosch.detekt.test
 
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.impl.jar.CoreJarFileSystem
+import org.jetbrains.kotlin.analysis.api.standalone.base.projectStructure.StandaloneProjectFactory
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.cli.jvm.compiler.NoScopeRecordCliBindingTrace
 import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
+import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
+import org.jetbrains.kotlin.cli.jvm.index.JavaRoot
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingContext
 
-fun KotlinCoreEnvironment.createBindingContext(files: List<KtFile>): BindingContext =
-    TopDownAnalyzerFacadeForJVM.analyzeFilesWithJavaIntegration(
-        this.project,
+fun createBindingContext(files: List<KtFile>, configuration: CompilerConfiguration, project: Project): BindingContext {
+    val vfsFs = CoreJarFileSystem()
+
+    val classpathRoots = configuration.jvmClasspathRoots
+        .filter { it.extension == "jar" }
+        .mapNotNull { vfsFs.findFileByPath("${it.absolutePath}!/") }
+        .map { JavaRoot(it, JavaRoot.RootType.BINARY) }
+
+    val packagePartProvider = StandaloneProjectFactory.createPackagePartsProvider(
+        classpathRoots,
+        configuration.languageVersionSettings
+    )
+
+    return TopDownAnalyzerFacadeForJVM.analyzeFilesWithJavaIntegration(
+        project,
         files,
-        NoScopeRecordCliBindingTrace(this.project),
-        this.configuration,
-        this::createPackagePartProvider
+        NoScopeRecordCliBindingTrace(project),
+        configuration,
+        packagePartProvider
     ).bindingContext
+}
+
+@Deprecated("Use createBindingContext function directly")
+fun KotlinCoreEnvironment.createBindingContext(files: List<KtFile>): BindingContext =
+    createBindingContext(files, configuration, project)
