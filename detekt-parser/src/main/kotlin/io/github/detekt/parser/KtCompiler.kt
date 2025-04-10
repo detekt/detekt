0e9b44bb5a4cf6fecb7dca1996f3964446afdb5a
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-10 23:01:02 +1000

    Remove KotlinCoreEnvironment usage from KtCompiler

--- detekt-parser/src/main/kotlin/io/github/detekt/parser/KtCompiler.kt
+++ detekt-parser/src/main/kotlin/io/github/detekt/parser/KtCompiler.kt
@@ -1,14 +1,21 @@
 package io.github.detekt.parser
 
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.pom.PomModel
 import com.intellij.psi.util.PsiUtilCore
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.analysis.api.standalone.base.projectStructure.StandaloneProjectFactory
+import org.jetbrains.kotlin.cli.common.CliModuleVisibilityManagerImpl
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreApplicationEnvironmentMode
+import org.jetbrains.kotlin.cli.jvm.compiler.setupIdeaStandaloneExecution
+import org.jetbrains.kotlin.load.kotlin.ModuleVisibilityManager
 import org.jetbrains.kotlin.psi.KtFile
 import java.nio.file.Path
 import kotlin.io.path.isRegularFile
 
 open class KtCompiler(
-    protected val environment: KotlinCoreEnvironment = createKotlinCoreEnvironment(printStream = System.err),
+    protected val project: Project = createDefaultProject(),
 ) {
 
     fun compile(path: Path): KtFile {
@@ -18,8 +25,22 @@ open class KtCompiler(
             "$path cannot be retrieved as a VirtualFile"
         }
 
-        return requireNotNull(PsiUtilCore.getPsiFile(environment.project, virtualFile) as? KtFile) {
+        return requireNotNull(PsiUtilCore.getPsiFile(project, virtualFile) as? KtFile) {
             "$path is not a Kotlin file"
         }
     }
+}
+
+private fun createDefaultProject(): Project {
+    setupIdeaStandaloneExecution()
+
+    val project = StandaloneProjectFactory.createProjectEnvironment(
+        Disposer.newDisposable(),
+        KotlinCoreApplicationEnvironmentMode.fromUnitTestModeFlag(false)
+    ).project
+
+    project.registerService(PomModel::class.java, DetektPomModel)
+    project.registerService(ModuleVisibilityManager::class.java, CliModuleVisibilityManagerImpl(true))
+
+    return project
 }
