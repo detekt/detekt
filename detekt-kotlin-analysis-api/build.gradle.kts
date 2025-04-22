163a7109109c6dce37889f97bfbbb243a050e128
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-22 21:56:08 +1000

    Declare Analysis API repo for all projects
    This fixes resolution for local builds when projects start using the API.

--- detekt-kotlin-analysis-api/build.gradle.kts
+++ detekt-kotlin-analysis-api/build.gradle.kts
@@ -5,11 +5,6 @@ plugins {
     id("com.gradleup.shadow") version "8.3.6"
 }
 
-repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
-}
-
 dependencies {
     implementation(libs.bundles.kotlin.analysisApi) {
         // https://youtrack.jetbrains.com/issue/KT-61639/Standalone-Analysis-API-cannot-find-transitive-dependencies
