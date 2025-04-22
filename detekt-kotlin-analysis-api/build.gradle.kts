448d3f6dd0286be4aef0dd0bd82e84868bdd1148
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-22 22:01:48 +1000

    Explicitly declare required bundling attribute for Caffeine dependency

--- detekt-kotlin-analysis-api/build.gradle.kts
+++ detekt-kotlin-analysis-api/build.gradle.kts
@@ -14,7 +14,12 @@ dependencies {
     implementation(libs.kotlin.analysisApiPlatformInterface) { isTransitive = false }
     implementation(libs.kotlin.lowLevelApiFir) { isTransitive = false }
     implementation(libs.kotlin.symbolLightClasses) { isTransitive = false }
-    implementation(libs.caffeine)
+    implementation(libs.caffeine) {
+        attributes {
+            // https://github.com/ben-manes/caffeine/issues/716
+            // Remove on upgrade to Caffeine 3.x or if https://youtrack.jetbrains.com/issue/KT-73751 is fixed
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
     }
 }
 
