71f6b78b8f7ee4f46b39f72f72be0670f5116c5a
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-23 18:00:34 +1000

    Create packagePartProvider without KotlinCoreEnvironment

--- detekt-parser/src/main/kotlin/io/github/detekt/parser/BindingContextUtils.kt
+++ detekt-parser/src/main/kotlin/io/github/detekt/parser/BindingContextUtils.kt
@@ -1,5 +1,8 @@
 package io.github.detekt.parser
 
+import com.intellij.openapi.vfs.impl.jar.CoreJarFileSystem
+import com.intellij.openapi.vfs.local.CoreLocalFileSystem
+import org.jetbrains.kotlin.analysis.api.standalone.base.projectStructure.StandaloneProjectFactory
 import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSourceLocation
@@ -8,6 +11,8 @@ import org.jetbrains.kotlin.cli.common.messages.PlainTextMessageRenderer
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.cli.jvm.compiler.NoScopeRecordCliBindingTrace
 import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
+import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
+import org.jetbrains.kotlin.cli.jvm.index.JavaRoot
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingContext
@@ -29,13 +34,32 @@ fun generateBindingContext(
         environment.configuration.languageVersionSettings,
         false,
     )
+
+    val vfsFs = CoreJarFileSystem()
+    val coreLocalFilesystem = CoreLocalFileSystem()
+
+    val jarJavaRoots = environment.configuration.jvmClasspathRoots
+        .filter { it.extension == "jar" }
+        .mapNotNull { vfsFs.findFileByPath("${it.absolutePath}!/") }
+        .map { JavaRoot(it, JavaRoot.RootType.BINARY) }
+
+    val dirJavaRoots = environment.configuration.jvmClasspathRoots
+        .filter { it.extension != "jar" }
+        .mapNotNull { coreLocalFilesystem.findFileByPath("${it.absolutePath}") }
+        .map { JavaRoot(it, JavaRoot.RootType.BINARY) }
+
+    val packagePartProvider = StandaloneProjectFactory.createPackagePartsProvider(
+        jarJavaRoots + dirJavaRoots,
+        environment.configuration.languageVersionSettings
+    )
+
     analyzer.analyzeAndReport(files) {
         TopDownAnalyzerFacadeForJVM.analyzeFilesWithJavaIntegration(
             environment.project,
             files,
             NoScopeRecordCliBindingTrace(environment.project),
             environment.configuration,
-            environment::createPackagePartProvider
+            packagePartProvider
         )
     }
 
