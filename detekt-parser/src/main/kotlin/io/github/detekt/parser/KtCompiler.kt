3c94c64e902fc9b06f6152ebad70299950fe86e8
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-25 14:24:29 +1000

    Remove direct usage of KotlinCoreEnvironment in KtCompiler

--- detekt-parser/src/main/kotlin/io/github/detekt/parser/KtCompiler.kt
+++ detekt-parser/src/main/kotlin/io/github/detekt/parser/KtCompiler.kt
@@ -1,20 +1,24 @@
 package io.github.detekt.parser
 
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.util.PsiUtilCore
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.psi.KtFile
 import java.nio.file.Path
 import kotlin.io.path.isRegularFile
 
 open class KtCompiler(
-    protected val environment: KotlinCoreEnvironment = createKotlinCoreEnvironment(printStream = System.err),
+    protected val project: Project = createKotlinCoreEnvironment(printStream = System.err).project,
 ) {
 
     fun compile(path: Path): KtFile {
         require(path.isRegularFile()) { "Given path '$path' should be a regular file!" }
 
-        val virtualFile = requireNotNull(environment.findLocalFile(path.toString()))
-        return requireNotNull(PsiUtilCore.getPsiFile(environment.project, virtualFile) as? KtFile) {
+        val virtualFile = requireNotNull(VirtualFileManager.getInstance().findFileByNioPath(path)) {
+            "$path cannot be retrieved as a VirtualFile"
+        }
+
+        return requireNotNull(PsiUtilCore.getPsiFile(project, virtualFile) as? KtFile) {
             "$path is not a Kotlin file"
         }
     }
