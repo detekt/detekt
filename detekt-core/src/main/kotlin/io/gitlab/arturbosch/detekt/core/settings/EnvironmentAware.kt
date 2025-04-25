d295199f11606c0a826fe1e12956500609f2a72b
Matthew Haughton <3flex@users.noreply.github.com>
2025-04-25 21:00:10 +1000

    Use StandaloneAnalysisAPISession to setup analysis environment

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/settings/EnvironmentAware.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/settings/EnvironmentAware.kt
@@ -3,15 +3,19 @@ package io.gitlab.arturbosch.detekt.core.settings
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.pom.PomModel
+import io.github.detekt.parser.DetektPomModel
 import io.github.detekt.parser.createCompilerConfiguration
-import io.github.detekt.parser.createKotlinCoreEnvironment
 import io.github.detekt.tooling.api.spec.CompilerSpec
 import io.github.detekt.tooling.api.spec.LoggingSpec
 import io.github.detekt.tooling.api.spec.ProjectSpec
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.analysis.api.standalone.buildStandaloneAnalysisAPISession
+import org.jetbrains.kotlin.cli.common.CliModuleVisibilityManagerImpl
+import org.jetbrains.kotlin.cli.jvm.compiler.CliTraceHolder
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.JVMConfigurationKeys
+import org.jetbrains.kotlin.config.friendPaths
 import org.jetbrains.kotlin.load.kotlin.ModuleVisibilityManager
+import org.jetbrains.kotlin.resolve.CodeAnalyzerInitializer
 import java.io.Closeable
 import java.io.File
 import java.io.OutputStream
@@ -24,32 +28,13 @@ interface EnvironmentAware {
 }
 
 internal class EnvironmentFacade(
-    private val projectSpec: ProjectSpec,
-    private val compilerSpec: CompilerSpec,
+    projectSpec: ProjectSpec,
+    compilerSpec: CompilerSpec,
     loggingSpec: LoggingSpec,
 ) : AutoCloseable, Closeable, EnvironmentAware {
 
     private val printStream = if (loggingSpec.debug) loggingSpec.errorChannel.asPrintStream() else NullPrintStream
-    private val environment: KotlinCoreEnvironment by lazy {
-        val env = createKotlinCoreEnvironment(
-            configuration,
-            disposable,
-            printStream,
-        )
-
-        val moduleVisibilityManager = ModuleVisibilityManager.SERVICE.getInstance(env.project)
-        configuration.getList(JVMConfigurationKeys.FRIEND_PATHS).forEach(moduleVisibilityManager::addFriendPath)
-
-        env
-    }
-
-    override val disposable: Disposable = Disposer.newDisposable()
-
-    override val project: Project by lazy {
-        environment.project
-    }
-
-    override val configuration: CompilerConfiguration by lazy {
+    override val configuration: CompilerConfiguration =
         createCompilerConfiguration(
             projectSpec.inputPaths.toList(),
             compilerSpec.classpathEntries(),
@@ -60,6 +45,25 @@ internal class EnvironmentFacade(
             compilerSpec.freeCompilerArgs,
             printStream,
         )
+
+    override val disposable: Disposable = Disposer.newDisposable()
+
+    private val analysisSession = buildStandaloneAnalysisAPISession(disposable) {
+        // Required for autocorrect support
+        registerProjectService(PomModel::class.java, DetektPomModel)
+
+        // Required by K1 compiler setup
+        registerProjectService(CodeAnalyzerInitializer::class.java, CliTraceHolder(project))
+        registerProjectService(ModuleVisibilityManager::class.java, CliModuleVisibilityManagerImpl(true))
+        val moduleVisibilityManager = ModuleVisibilityManager.SERVICE.getInstance(project)
+        configuration.friendPaths.forEach(moduleVisibilityManager::addFriendPath)
+
+        @Suppress("DEPRECATION") // Required until fully transitioned to setting up Kotlin Analysis API session
+        buildKtModuleProviderByCompilerConfiguration(configuration)
+    }
+
+    override val project: Project by lazy {
+        analysisSession.project
     }
 
     override fun close() {
