49feaab5a0b31719f4052b57412277c42a6d787a
Matthew Haughton <3flex@users.noreply.github.com>
2025-05-03 15:08:48 +1000

    Update to Shadow plugin 9.0.0-beta17

--- detekt-formatting/ktlint-repackage/build.gradle.kts
+++ detekt-formatting/ktlint-repackage/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "9.0.0-beta17"
 }
 
 dependencies {
--- detekt-generator/build.gradle.kts
+++ detekt-generator/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.io.ByteArrayOutputStream
 
 plugins {
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "9.0.0-beta17"
     id("module")
     id("application")
 }
