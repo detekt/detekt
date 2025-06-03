8e5034e98d0d0698f5c22d4584fc1f1785772b38
Matthew Haughton <3flex@users.noreply.github.com>
2025-06-03 20:47:03 +1000

    Show diagnostic errors on console when using Analysis API

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/Lifecycle.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/Lifecycle.kt
@@ -1,5 +1,6 @@
 package io.gitlab.arturbosch.detekt.core.tooling
 
+import io.github.detekt.parser.DetektMessageCollector
 import io.github.detekt.parser.generateBindingContext
 import io.github.detekt.tooling.api.AnalysisMode
 import io.gitlab.arturbosch.detekt.api.Config
@@ -17,6 +18,12 @@ import io.gitlab.arturbosch.detekt.core.reporting.OutputFacade
 import io.gitlab.arturbosch.detekt.core.rules.createRuleProviders
 import io.gitlab.arturbosch.detekt.core.util.PerformanceMonitor.Phase
 import io.gitlab.arturbosch.detekt.core.util.getOrCreateMonitor
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.components.KaDiagnosticCheckerFilter
+import org.jetbrains.kotlin.analysis.api.diagnostics.KaSeverity
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocationWithRange
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
+import org.jetbrains.kotlin.diagnostics.DiagnosticUtils.getLineAndColumnRangeInPsiFile
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingContext
 
@@ -69,6 +76,33 @@ internal class DefaultLifecycle(
     override val bindingProvider: (files: List<KtFile>) -> BindingContext =
         {
             if (settings.spec.projectSpec.analysisMode == AnalysisMode.full) {
+                val collector = DetektMessageCollector(CompilerMessageSeverity.ERROR, settings::debug, settings::info)
+
+                it.forEach { file: KtFile ->
+                    analyze(file) {
+                        file.collectDiagnostics(KaDiagnosticCheckerFilter.ONLY_COMMON_CHECKERS).forEach { diagnostic ->
+                            val lineAndColumnRange =
+                                getLineAndColumnRangeInPsiFile(diagnostic.psi.containingFile, diagnostic.psi.textRange)
+
+                            val location = CompilerMessageLocationWithRange.create(
+                                diagnostic.psi.containingFile.virtualFile.path,
+                                lineAndColumnRange.start.line,
+                                lineAndColumnRange.start.column,
+                                lineAndColumnRange.end.line,
+                                lineAndColumnRange.end.column,
+                                lineAndColumnRange.start.lineContent
+                            )
+                            collector.report(
+                                diagnostic.severity.toCompilerMessageSeverity(),
+                                diagnostic.defaultMessage,
+                                location
+                            )
+                        }
+                    }
+                }
+
+                collector.printIssuesCountIfAny(k2Mode = true)
+
                 generateBindingContext(
                     settings.project,
                     settings.configuration,
@@ -85,3 +119,10 @@ internal class DefaultLifecycle(
     override val ruleSetsProvider: () -> List<RuleSetProvider> =
         { settings.createRuleProviders() },
 ) : Lifecycle
+
+private fun KaSeverity.toCompilerMessageSeverity(): CompilerMessageSeverity =
+    when (this) {
+        KaSeverity.ERROR -> CompilerMessageSeverity.ERROR
+        KaSeverity.WARNING -> CompilerMessageSeverity.WARNING
+        KaSeverity.INFO -> CompilerMessageSeverity.INFO
+    }
