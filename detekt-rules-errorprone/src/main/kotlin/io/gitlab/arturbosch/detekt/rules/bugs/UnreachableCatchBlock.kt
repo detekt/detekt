63dff43d7e31e9d7327a00bd15e48560b2f049f4
Toshiaki Kameyama <kmymtsak@gmail.com>
2025-06-30 13:27:36 +0900

    Migrate UnreachableCatchBlock to Analysis API

--- detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnreachableCatchBlock.kt
+++ detekt-rules-errorprone/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/bugs/UnreachableCatchBlock.kt
@@ -4,14 +4,14 @@ import io.gitlab.arturbosch.detekt.api.ActiveByDefault
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Finding
-import io.gitlab.arturbosch.detekt.api.RequiresFullAnalysis
+import io.gitlab.arturbosch.detekt.api.RequiresAnalysisApi
 import io.gitlab.arturbosch.detekt.api.Rule
-import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.types.KaType
 import org.jetbrains.kotlin.psi.KtCatchClause
 import org.jetbrains.kotlin.psi.KtTryExpression
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.descriptorUtil.isSubclassOf
 
 /**
  * Reports unreachable catch blocks.
@@ -49,7 +49,7 @@ class UnreachableCatchBlock(config: Config) :
         config,
         "Unreachable catch block detected."
     ),
-    RequiresFullAnalysis {
+    RequiresAnalysisApi {
 
     override fun visitCatchSection(catchClause: KtCatchClause) {
         super.visitCatchSection(catchClause)
@@ -57,21 +57,18 @@ class UnreachableCatchBlock(config: Config) :
         val tryExpression = catchClause.getStrictParentOfType<KtTryExpression>() ?: return
         val prevCatchClauses = tryExpression.catchClauses.takeWhile { it != catchClause }
         if (prevCatchClauses.isEmpty()) return
-        val catchClassDescriptor = catchClause.catchClassDescriptor() ?: return
-        if (prevCatchClauses.any { catchClassDescriptor.isSubclassOf(it) }) {
-            report(Finding(Entity.from(catchClause), "This catch block is unreachable."))
+
+        analyze(catchClause) {
+            val catchType = catchType(catchClause) ?: return
+            val isSubType = prevCatchClauses.any {
+                val prevCatchType = catchType(it)
+                prevCatchType != null && catchType.isSubtypeOf(prevCatchType)
+            }
+            if (isSubType) {
+                report(Finding(Entity.from(catchClause), "This catch block is unreachable."))
+            }
         }
     }
 
-    private fun KtCatchClause.catchClassDescriptor(): ClassDescriptor? {
-        val typeReference = catchParameter?.typeReference ?: return null
-        return bindingContext[BindingContext.TYPE, typeReference]
-            ?.constructor
-            ?.declarationDescriptor as? ClassDescriptor
-    }
-
-    private fun ClassDescriptor.isSubclassOf(catchClause: KtCatchClause): Boolean {
-        val catchClassDescriptor = catchClause.catchClassDescriptor() ?: return false
-        return isSubclassOf(catchClassDescriptor)
-    }
+    private fun KaSession.catchType(catchClause: KtCatchClause): KaType? = catchClause.catchParameter?.returnType
 }
