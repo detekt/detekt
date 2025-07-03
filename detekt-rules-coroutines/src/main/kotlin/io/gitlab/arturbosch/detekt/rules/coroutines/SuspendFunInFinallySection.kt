92f04ea207dcc5aae2d064de42ca43b2df5d2b8a
Mark Saif <43094629+markoeltiger@users.noreply.github.com>
2025-07-03 02:05:22 +0000

    Migrate SuspendFunInFinallySection to Analysis API

--- detekt-rules-coroutines/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/coroutines/SuspendFunInFinallySection.kt
+++ detekt-rules-coroutines/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/coroutines/SuspendFunInFinallySection.kt
@@ -1,19 +1,24 @@
 package io.gitlab.arturbosch.detekt.rules.coroutines
 
 import com.intellij.psi.PsiElement
-import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Entity
-import io.gitlab.arturbosch.detekt.api.Finding
-import io.gitlab.arturbosch.detekt.api.RequiresFullAnalysis
 import io.gitlab.arturbosch.detekt.api.Rule
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtFinallySection
 import org.jetbrains.kotlin.psi.KtValueArgument
+import org.jetbrains.kotlin.psi.KtValueArgument
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.RequiresAnalysisApi
+import io.gitlab.arturbosch.detekt.api.Rule
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.calls.singleFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.calls.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaFunctionSymbol
+import org.jetbrains.kotlin.analysis.api.types.KaType
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.psiUtil.forEachDescendantOfType
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 
 /**
  * Report usage of suspending functions within a `finally` section that are not enclosed in a non-cancellable context.
@@ -46,50 +51,74 @@ class SuspendFunInFinallySection(config: Config) :
         "Suspend functions should not be called from a 'finally' section without using 'NonCancellable' " +
             "context as they won't execute if parent coroutine scope is cancelled."
     ),
-    RequiresFullAnalysis {
+    RequiresAnalysisApi {
 
     override fun visitFinallySection(finallySection: KtFinallySection) {
         finallySection.forEachDescendantOfType<KtCallExpression> { expression ->
-            if (shouldReport(expression, finallySection)) {
-                report(Finding(Entity.from(expression.calleeExpression as PsiElement), description))
-            }
+          analyze(expression) {
+                    val call = expression.resolveCall()?.singleFunctionCallOrNull()
+                    val functionSymbol = call?.symbol as? KaFunctionSymbol
+                    
+                    if (functionSymbol?.isSuspend == true) {
+                        report(
+                            CodeSmell(
+                                entity = Entity.from(expression),
+                                message = "Suspending function '${functionSymbol.name}' should not be called inside a finally block"
+                            )
+                        )
+                    }
+                }
         }
     }
 
-    private fun shouldReport(
+     private fun shouldReport(
         expression: KtCallExpression,
         topParent: KtFinallySection,
     ): Boolean {
-        if (!expression.isSuspendCall()) {
-            return false
-        }
+        return analyze(expression) {
+            if (!expression.isSuspendCall()) {
+                return@analyze false
+            }
 
-        val parentCalls = expression.parentCallsUpTo(topParent)
-        val withContextFun = parentCalls.findFunction("kotlinx.coroutines.withContext") ?: return true
-        val firstArgument = withContextFun.valueArguments.first()
-        return !isNonCancellableArgument(firstArgument, bindingContext)
+            val parentCalls = expression.parentCallsUpTo(topParent)
+            val withContextFun = parentCalls.findFunction("kotlinx.coroutines.withContext")
+                ?: return@analyze true
+            
+            val firstArgument = withContextFun.valueArguments.firstOrNull()
+                ?: return@analyze true
+            
+            !isNonCancellableArgument(firstArgument)
+        }
     }
 
-    private fun KtCallExpression.isSuspendCall() =
-        (getResolvedCall(bindingContext)?.resultingDescriptor as FunctionDescriptor).isSuspend
-
+  private fun KtCallExpression.isSuspendCall(): Boolean {
+        return analyze(this) {
+            val call = this@isSuspendCall.resolveCall()?.singleFunctionCallOrNull()
+            val functionSymbol = call?.symbol as? KaFunctionSymbol
+            functionSymbol?.isSuspend == true
+        }
+    }
     private fun KtCallExpression.parentCallsUpTo(topParent: PsiElement) =
         generateSequence(this as PsiElement) { it.parent }
             .takeWhile { it != topParent }
             .filter { it is KtCallExpression }
             .map { it as KtCallExpression }
 
-    private fun Sequence<KtCallExpression>.findFunction(fqName: String) = firstOrNull {
-        it.getResolvedCall(bindingContext)
-            ?.resultingDescriptor
-            ?.fqNameOrNull()
-            ?.asString() == fqName
+      private fun Sequence<KtCallExpression>.findFunction(fqName: String): KtCallExpression? {
+        return firstOrNull { call ->
+            analyze(call) {
+                val resolvedCall = call.resolveCall()?.singleFunctionCallOrNull()
+                val functionSymbol = resolvedCall?.symbol as? KaFunctionSymbol
+                functionSymbol?.callableId?.asSingleFqName()?.asString() == fqName
+            }
+        }
     }
 
-    private fun isNonCancellableArgument(arg: KtValueArgument, context: BindingContext) =
-        arg.getArgumentExpression()
-            .getResolvedCall(context)
-            ?.resultingDescriptor
-            ?.returnType
-            .toString() == "NonCancellable"
-}
+  private fun isNonCancellableArgument(arg: KtValueArgument): Boolean {
+        val argumentExpression = arg.getArgumentExpression() ?: return false
+        return analyze(argumentExpression) {
+            val call = argumentExpression.resolveCall()?.singleFunctionCallOrNull()
+            val symbol = call?.symbol
+            symbol?.callableId?.asSingleFqName()?.asString() == "kotlinx.coroutines.NonCancellable"
+        }
+    }
