aa5bf11e269217a2b527e7a3ca1c55a986a60646
Matthew Haughton <3flex@users.noreply.github.com>
2025-07-13 18:37:00 +1000

    Configure publishing for ktlint-repackage

--- detekt-formatting/ktlint-repackage/build.gradle.kts
+++ detekt-formatting/ktlint-repackage/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    id("packaging")
     id("com.gradleup.shadow") version "8.3.8"
 }
 
@@ -11,4 +11,23 @@ dependencies {
 
 tasks.shadowJar {
     relocate("org.jetbrains.kotlin.com.intellij", "com.intellij")
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+configurations.shadowRuntimeElements {
+    attributes {
+        // This is not needed in shadow plugin 9+: https://github.com/GradleUp/shadow/pull/1199
+        attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, JavaVersion.VERSION_1_8.majorVersion.toInt())
+    }
+}
+
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["apiElements"]) {
+    skip()
+}
+javaComponent.withVariantsFromConfiguration(configurations["runtimeElements"]) {
+    skip()
 }
