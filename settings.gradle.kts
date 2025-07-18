2f0753ac6cdabdd19320927c95657222dda139ea
Matthew Haughton <3flex@users.noreply.github.com>
2025-07-18 19:57:06 +1000

    Apply build-health plugin to build (#8319)
    * Apply build-health plugin to build
    
    * Print build-health report to console
    
    * Run buildHealth task on CI
    
    * Clarify reason for including Kotlin plugin on classpath

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -47,6 +47,9 @@ plugins {
     id("com.gradle.develocity") version "4.1"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.3"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.autonomousapps.build-health") version "2.19.0"
+    // Kotlin plugin must be added to classpath to support build-health analysis
+    id("org.jetbrains.kotlin.jvm") version "2.2.0" apply false
 }
 
 val isCiBuild = providers.environmentVariable("CI").isPresent
