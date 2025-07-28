73a8878489a903797bf294fce4a361ef0bd3e076
Brais Gabín <brais.gabin@scrm.lidl>
2025-07-28 11:18:19 +0200

    Merge SharedTasks.kt

--- detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/plugin/internal/SharedTasks.kt
+++ detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/plugin/internal/SharedTasks.kt
@@ -6,8 +6,12 @@ import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import io.gitlab.arturbosch.detekt.internal.addVariantName
 import io.gitlab.arturbosch.detekt.internal.existingVariantOrBaseFile
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaBasePlugin
+import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.provider.Provider
+import org.gradle.jvm.toolchain.JavaToolchainService
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
@@ -107,3 +111,64 @@ internal fun Project.mapExplicitArgMode(): Provider<String> =
             else -> null
         }
     }
+
+internal fun Project.setDetektTaskDefaults(extension: DetektExtension) {
+    tasks.withType(Detekt::class.java) {
+        project.plugins.withType(JavaBasePlugin::class.java) { _ ->
+            val toolchain = project.extensions.getByType(JavaPluginExtension::class.java).toolchain
+
+            // acquire a provider that returns the launcher for the toolchain
+            val service = project.extensions.getByType(JavaToolchainService::class.java)
+            val defaultLauncher = service.launcherFor(toolchain)
+            it.jdkHome.convention(defaultLauncher.map { launcher -> launcher.metadata.installationPath })
+            it.jvmTarget.convention(
+                defaultLauncher.map { launcher ->
+                    JavaVersion.toVersion(launcher.metadata.languageVersion.asInt()).toString()
+                }
+            )
+        }
+
+        it.debug.convention(extension.debug)
+        it.parallel.convention(extension.parallel)
+        it.disableDefaultRuleSets.convention(extension.disableDefaultRuleSets)
+        it.buildUponDefaultConfig.convention(extension.buildUponDefaultConfig)
+        it.autoCorrect.convention(extension.autoCorrect)
+        it.ignoreFailures.convention(extension.ignoreFailures)
+        it.failOnSeverity.convention(extension.failOnSeverity)
+        it.config.conventionCompat(provider { extension.config })
+        it.basePath.convention(extension.basePath.map { basePath -> basePath.asFile.absolutePath })
+        it.allRules.convention(extension.allRules)
+        it.noJdk.convention(false)
+        it.multiPlatformEnabled.convention(false)
+    }
+}
+
+internal fun Project.setCreateBaselineTaskDefaults(extension: DetektExtension) {
+    tasks.withType(DetektCreateBaselineTask::class.java) {
+        project.plugins.withType(JavaBasePlugin::class.java) { _ ->
+            val toolchain = project.extensions.getByType(JavaPluginExtension::class.java).toolchain
+
+            // acquire a provider that returns the launcher for the toolchain
+            val service = project.extensions.getByType(JavaToolchainService::class.java)
+            val defaultLauncher = service.launcherFor(toolchain)
+            it.jdkHome.convention(defaultLauncher.map { launcher -> launcher.metadata.installationPath })
+            it.jvmTarget.convention(
+                defaultLauncher.map { launcher ->
+                    JavaVersion.toVersion(launcher.metadata.languageVersion.asInt()).toString()
+                }
+            )
+        }
+
+        it.config.conventionCompat(project.provider { extension.config })
+        it.debug.convention(extension.debug)
+        it.parallel.convention(extension.parallel)
+        it.disableDefaultRuleSets.convention(extension.disableDefaultRuleSets)
+        it.buildUponDefaultConfig.convention(extension.buildUponDefaultConfig)
+        it.autoCorrect.convention(extension.autoCorrect)
+        it.ignoreFailures.convention(extension.ignoreFailures)
+        it.basePath.convention(extension.basePath.map { basePath -> basePath.asFile.absolutePath })
+        it.allRules.convention(extension.allRules)
+        it.noJdk.convention(false)
+        it.multiPlatformEnabled.convention(false)
+    }
+}
