58c145c7b6abd6e45f20913b14cd1a647e6a7aa6
Brais Gabín <brais.gabin@scrm.lidl>
2025-07-30 14:13:33 +0200

    detekt-test does not depend on detekt-test-assertj

--- detekt-test/build.gradle.kts
+++ detekt-test/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 
 dependencies {
     api(projects.detektApi)
-    api(projects.detektTestUtils)
     api(projects.detektTestAssertj)
+    api(projects.detektTestUtils)
     api(libs.kotlin.compiler)
     implementation(projects.detektKotlinAnalysisApiStandalone)
     implementation(projects.detektUtils)
