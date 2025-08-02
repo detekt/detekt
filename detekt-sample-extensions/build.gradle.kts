7b4cbeaba89a9e48da6b0cc1491f48747078d1f4
Matthew Haughton <3flex@users.noreply.github.com>
2025-08-02 13:19:32 +1000

    Properly configure test suite in detekt-sample-extensions
    Gradle 9 has a check to ensure at least one test is executed if test
    sources are present. The sample extension was not executing any
    tests.

--- detekt-sample-extensions/build.gradle.kts
+++ detekt-sample-extensions/build.gradle.kts
@@ -11,3 +11,11 @@ dependencies {
     testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.23.8")
     testImplementation("org.assertj:assertj-core:3.27.3")
 }
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+    }
+}
