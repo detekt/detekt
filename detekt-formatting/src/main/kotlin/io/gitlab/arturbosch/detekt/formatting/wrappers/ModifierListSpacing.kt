70c10f46ccdaeabc70b60068f5bc5dbecfb3cbe1
Matthew Haughton <3flex@users.noreply.github.com>
2025-08-03 13:47:36 +1000

    Add indentSize config option to ModifierListSpacing

--- detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/ModifierListSpacing.kt
+++ detekt-formatting/src/main/kotlin/io/gitlab/arturbosch/detekt/formatting/wrappers/ModifierListSpacing.kt
@@ -1,8 +1,12 @@
 package io.gitlab.arturbosch.detekt.formatting.wrappers
 
+import com.pinterest.ktlint.rule.engine.core.api.editorconfig.EditorConfigProperty
+import com.pinterest.ktlint.rule.engine.core.api.editorconfig.INDENT_SIZE_PROPERTY
 import com.pinterest.ktlint.ruleset.standard.rules.ModifierListSpacingRule
 import dev.detekt.api.ActiveByDefault
 import dev.detekt.api.Config
+import dev.detekt.api.Configuration
+import dev.detekt.api.config
 import dev.detekt.api.internal.AutoCorrectable
 import io.gitlab.arturbosch.detekt.formatting.FormattingRule
 
@@ -17,4 +21,12 @@ class ModifierListSpacing(config: Config) : FormattingRule(
 ) {
 
     override val wrapping = ModifierListSpacingRule()
+
+    @Configuration("indentation size")
+    private val indentSize by config(4)
+
+    override fun overrideEditorConfigProperties(): Map<EditorConfigProperty<*>, String> =
+        mapOf(
+            INDENT_SIZE_PROPERTY to indentSize.toString(),
+        )
 }
