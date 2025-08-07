b4595fdccb2871c7f3ebe0cffb65182e579ca300
Brais Gabín <brais.gabin@scrm.lidl>
2025-08-07 19:23:45 +0200

    Migrate AnnotationExcluder to Analysis API

--- detekt-psi-utils/src/main/kotlin/dev/detekt/psi/AnnotationExcluder.kt
+++ detekt-psi-utils/src/main/kotlin/dev/detekt/psi/AnnotationExcluder.kt
@@ -1,11 +1,12 @@
 package dev.detekt.psi
 
 import dev.detekt.psi.internal.FullQualifiedNameGuesser
-import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.types.KaClassType
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtTypeReference
-import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.utils.KotlinExceptionWithAttachments
 
 /**
  * Primary use case for an AnnotationExcluder is to decide if a KtElement should be
@@ -15,7 +16,6 @@ import org.jetbrains.kotlin.resolve.BindingContext
 class AnnotationExcluder(
     root: KtFile,
     private val excludes: List<Regex>,
-    private val context: BindingContext,
 ) {
 
     private val fullQualifiedNameGuesser = FullQualifiedNameGuesser(root)
@@ -25,35 +25,35 @@ class AnnotationExcluder(
      * which basically describes entries to exclude.
      */
     fun shouldExclude(annotations: List<KtAnnotationEntry>): Boolean =
-        annotations.any { annotation -> annotation.typeReference?.let { isExcluded(it, context) } ?: false }
+        annotations.any { annotation -> annotation.typeReference?.let { isExcluded(it) } ?: false }
 
-    private fun isExcluded(annotation: KtTypeReference, context: BindingContext): Boolean {
-        val fqName = if (context == BindingContext.EMPTY) null else annotation.fqNameOrNull(context)
+    private fun isExcluded(annotation: KtTypeReference): Boolean {
+        val fqName = annotation.fqNameOrNull()
         val possibleNames = if (fqName == null) {
             fullQualifiedNameGuesser.getFullQualifiedName(annotation.text.toString())
                 .map { it.getPackage() to it }
+                .flatMap { (packaage, fqName) ->
+                    fqName.substringAfter("$packaage.", "")
+                        .split(".")
+                        .reversed()
+                        .scan("") { acc, name -> if (acc.isEmpty()) name else "$name.$acc" }
+                        .drop(1) + fqName
+                }
         } else {
-            listOf(fqName.getPackage() to fqName.toString())
-        }
-            .flatMap { (packaage, fqName) ->
-                fqName.substringAfter("$packaage.", "")
-                    .split(".")
-                    .reversed()
-                    .scan("") { acc, name -> if (acc.isEmpty()) name else "$name.$acc" }
-                    .drop(1) + fqName
+            buildList {
+                add(fqName.first + "." + fqName.second)
+                addAll(
+                    fqName.second
+                        .split(".")
+                        .reversed()
+                        .scan("") { acc, name -> if (acc.isEmpty()) name else "$name.$acc" }
+                        .drop(1)
+                )
             }
+        }
 
         return possibleNames.any { name -> name in excludes }
     }
-}
-
-private fun FqName.getPackage(): String {
-    /* This is a shortcut. Right now we are using the same heuristic that we use when we don't have type solving
-     * information. With the type solving information we should know exactly which part is package and which part is
-     * class name. But right now I don't know how to extract that information. There is a disabled test that should be
-     * enabled once this is solved.
-     */
-    return this.toString().getPackage()
 }
 
 private fun String.getPackage(): String {
@@ -69,8 +69,19 @@ private fun String.getPackage(): String {
         .joinToString(".")
 }
 
-private fun KtTypeReference.fqNameOrNull(bindingContext: BindingContext): FqName? =
-    bindingContext[BindingContext.TYPE, this]?.fqNameOrNull()
+private fun KtTypeReference.fqNameOrNull(): Pair<String, String>? = try {
+    analyze(this) {
+        val type = type as? KaClassType ?: return null
+        val classId = type.symbol.classId ?: return null
+        classId.packageFqName.toString() to classId.relativeClassName.toString()
+    }
+} catch (ex: KotlinExceptionWithAttachments) {
+    if (ex.message == "Cannot find a KaModule for the VirtualFile") {
+        null
+    } else {
+        throw ex
+    }
+}
 
 private operator fun Iterable<Regex>.contains(a: String?): Boolean {
     if (a == null) return false
