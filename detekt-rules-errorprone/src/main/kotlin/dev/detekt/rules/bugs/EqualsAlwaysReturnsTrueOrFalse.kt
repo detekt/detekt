f631aec437a1f3eaf5acf4b5b5e69d537ea035f9
Brais Gabín <braisgabin@gmail.com>
2025-08-20 14:02:11 +0200

    Change package of :detekt-rules-errorprone to dev.detekt.rules.bugs (#8493)

---
+++ detekt-rules-errorprone/src/main/kotlin/dev/detekt/rules/bugs/EqualsAlwaysReturnsTrueOrFalse.kt
@@ -0,0 +1,81 @@
+package dev.detekt.rules.bugs
+
+import com.intellij.psi.PsiElement
+import dev.detekt.api.ActiveByDefault
+import dev.detekt.api.Config
+import dev.detekt.api.Entity
+import dev.detekt.api.Finding
+import dev.detekt.api.Rule
+import dev.detekt.psi.isEqualsFunction
+import org.jetbrains.kotlin.KtNodeTypes
+import org.jetbrains.kotlin.psi.KtAnnotatedExpression
+import org.jetbrains.kotlin.psi.KtConstantExpression
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtReturnExpression
+import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
+
+/**
+ * Reports `equals()` methods which will always return true or false.
+ *
+ * Equals methods should always report if some other object is equal to the current object.
+ * See the Kotlin documentation for Any.equals(other: Any?):
+ * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-any/equals.html
+ *
+ * <noncompliant>
+ * override fun equals(other: Any?): Boolean {
+ *     return true
+ * }
+ * </noncompliant>
+ *
+ * <compliant>
+ * override fun equals(other: Any?): Boolean {
+ *     return this === other
+ * }
+ * </compliant>
+ */
+@ActiveByDefault(since = "1.2.0")
+class EqualsAlwaysReturnsTrueOrFalse(config: Config) : Rule(
+    config,
+    "Having an `equals()` method that always returns true or false is not a good idea. " +
+        "It does not follow the contract of this method. " +
+        "Consider a good default implementation (e.g. `this == other`)."
+) {
+
+    override fun visitNamedFunction(function: KtNamedFunction) {
+        if (function.isEqualsFunction() && function.returnsBooleanConstant()) {
+            report(
+                Finding(
+                    Entity.atName(function),
+                    "This equals function always returns the same " +
+                        "result regardless of the input parameters."
+                )
+            )
+        }
+    }
+
+    private fun KtNamedFunction.returnsBooleanConstant(): Boolean {
+        val bodyExpression = bodyExpression ?: return false
+        return if (bodyExpression is KtConstantExpression) {
+            bodyExpression.isBooleanConstant()
+        } else {
+            isSingleReturnWithBooleanConstant(bodyExpression)
+        }
+    }
+
+    private fun isSingleReturnWithBooleanConstant(bodyExpression: KtExpression): Boolean {
+        val returnExpressionsInBlock = bodyExpression.collectDescendantsOfType<KtReturnExpression> {
+            it.parent == bodyExpression || it.parent is KtAnnotatedExpression && it.parent.parent == bodyExpression
+        }
+        val lastValidReturnExpression = returnExpressionsInBlock.firstOrNull()?.returnedExpression
+        val allReturnExpressions = bodyExpression.collectDescendantsOfType<KtReturnExpression>()
+        val hasNoNestedReturnExpression = allReturnExpressions.size == returnExpressionsInBlock.size
+        return lastValidReturnExpression?.isBooleanConstant() == true &&
+            (
+                hasNoNestedReturnExpression ||
+                    allReturnExpressions.all { it.returnedExpression?.text == lastValidReturnExpression.text }
+                )
+    }
+
+    private fun PsiElement.isBooleanConstant() = node.elementType == KtNodeTypes.BOOLEAN_CONSTANT
+}
---
+++ detekt-rules-errorprone/src/test/kotlin/dev/detekt/rules/bugs/EqualsAlwaysReturnsTrueOrFalseSpec.kt
@@ -0,0 +1,155 @@
+package dev.detekt.rules.bugs
+
+import dev.detekt.api.Config
+import dev.detekt.test.lint
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class EqualsAlwaysReturnsTrueOrFalseSpec {
+    private val subject = EqualsAlwaysReturnsTrueOrFalse(Config.empty)
+
+    @Test
+    fun `reports equals() methods`() {
+        @Suppress("EqualsOrHashCode")
+        val code = """
+            // reports 1 for every equals method
+            class EqualsReturnsTrue {
+            
+                override fun equals(other: Any?): Boolean {
+                    return true
+                }
+            }
+            
+            class EqualsReturnsFalse {
+            
+                override fun equals(other: Any?): Boolean {
+                    return false
+                }
+            }
+            
+            class EqualsReturnsFalseWithUnreachableReturnStatement {
+            
+                override fun equals(other: Any?): Boolean {
+                    return false
+                    return true
+                }
+            }
+            
+            class EqualsReturnsFalseWithUnreachableCode {
+            
+                override fun equals(other: Any?): Boolean {
+                    return false
+                    val i = 0
+                }
+            }
+            
+            class EqualsReturnsConstantExpression {
+            
+                override fun equals(other: Any?) = false
+            }
+            
+            class EqualsWithTwoReturnExpressions {
+            
+                override fun equals(other: Any?): Boolean {
+                    if (other is Int) {
+                        return true
+                    }
+                    return true
+                }
+            }
+        """.trimIndent()
+        assertThat(subject.lint(code)).hasSize(6)
+    }
+
+    @Test
+    fun `does not report equals() methods`() {
+        @Suppress("EqualsOrHashCode")
+        val code = """
+            class EqualsReturnsTrueOrFalse {
+            
+                override fun equals(other: Any?): Boolean {
+                    if (other is Int) {
+                        return true
+                    }
+                    return false
+                }
+            }
+            
+            class CorrectEquals {
+            
+                override fun equals(other: Any?): Boolean {
+                    return this.toString() == other.toString()
+                }
+            }
+            
+            class ReferentialEquality {
+            
+                override fun equals(other: Any?): Boolean {
+                    return this === other
+                }
+            }
+            
+            fun equals(other: Any?): Boolean {
+                return false
+            }
+            
+            class NotOverridingEquals {
+            
+                fun equal(other: Any?): Boolean {
+                    return true
+                }
+            }
+            
+            class WrongEqualsParameterList {
+            
+                fun equals(other: Any, i: Int): Boolean {
+                    return true
+                }
+            }
+        """.trimIndent()
+        assertThat(subject.lint(code)).isEmpty()
+    }
+
+    @Test
+    fun `detects and doesn't crash when return expression is annotated - #2021`() {
+        val code = """
+            class C {
+                override fun equals(other: Any?): Boolean {
+                    @Suppress("UnsafeCallOnNullableType")
+                    return true
+                }
+            }
+        """.trimIndent()
+        assertThat(subject.lint(code)).hasSize(1)
+    }
+
+    @Test
+    fun `detects and doesn't crash when the equals method contains no return expression - #2103`() {
+        val code = """
+            open class SuperClass
+            
+            data class Item(val text: String) : SuperClass() {
+                override fun equals(other: Any?): Boolean = (other as? Item)?.text == this.text
+                override fun hashCode(): Int = text.hashCode()
+            }
+        """.trimIndent()
+        assertThat(subject.lint(code)).isEmpty()
+    }
+
+    @Test
+    fun `doesn't report when custom method named equals with extension receiver is used - #6569`() {
+        val code = """
+            open class Base1 {
+                open fun Base1.equals(other: Any?): Boolean {
+                    return false
+                }
+            }
+            class A1 : Base1() {
+                override fun Base1.equals(other: Any?): Boolean {
+                    return true
+                }
+            }
+        """.trimIndent()
+        assertThat(subject.lint(code)).isEmpty()
+    }
+}
