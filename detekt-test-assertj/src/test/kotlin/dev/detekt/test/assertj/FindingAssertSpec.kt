3d00ea4207f6b01497a751200832eb5591db0aaa
Brais Gabín <brais.gabin@scrm.lidl>
2025-08-20 14:37:15 +0200

    Test SourceLocation

--- detekt-test-assertj/src/test/kotlin/dev/detekt/test/assertj/FindingAssertSpec.kt
+++ detekt-test-assertj/src/test/kotlin/dev/detekt/test/assertj/FindingAssertSpec.kt
@@ -1,12 +1,18 @@
 package dev.detekt.test.assertj
 
+import dev.detekt.api.SourceLocation
+import dev.detekt.api.testfixtures.createEntity
 import dev.detekt.api.testfixtures.createFinding
+import dev.detekt.api.testfixtures.createLocation
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class FindingAssertSpec {
-    private val finding = createFinding(message = "TestMessage")
+    private val finding = createFinding(
+        entity = createEntity(createLocation(source = 1 to 1, endSource = 1 to 11)),
+        message = "TestMessage",
+    )
 
     @Nested
     inner class Message {
@@ -65,6 +71,99 @@ class FindingAssertSpec {
                 .hasMessage("Expect no suppressions but [reason1, reason2] was found")
                 .hasActual(listOf("reason1", "reason2"))
                 .hasExpected(emptyList<String>())
+        }
+    }
+
+    @Nested
+    inner class SourceLocationChecks {
+        @Nested
+        inner class Start {
+            @Test
+            fun `hasStartSourceLocation with null value`() {
+                assertThatThrownBy { FindingAssert(null).hasStartSourceLocation(SourceLocation(1, 1)) }
+                    .isExactlyInstanceOf(AssertionError::class.java)
+                    .hasMessage("\nExpecting actual not to be null")
+            }
+
+            @Test
+            fun `hasStartSourceLocationInt with null value`() {
+                assertThatThrownBy { FindingAssert(null).hasStartSourceLocation(1, 1) }
+                    .isExactlyInstanceOf(AssertionError::class.java)
+                    .hasMessage("\nExpecting actual not to be null")
+            }
+
+            @Test
+            fun hasStartSourceLocation() {
+                FindingAssert(finding).hasStartSourceLocation(SourceLocation(1, 1))
+            }
+
+            @Test
+            fun hasStartSourceLocationInt() {
+                FindingAssert(finding).hasStartSourceLocation(1, 1)
+            }
+
+            @Test
+            fun `hasStartSourceLocation failing`() {
+                assertThatThrownBy { FindingAssert(finding).hasStartSourceLocation(SourceLocation(2, 5)) }
+                    .isInstanceOfAssertionFailedError()
+                    .hasMessage("Expected start source location to be 2:5 but was 1:1")
+                    .hasActual(SourceLocation(1, 1))
+                    .hasExpected(SourceLocation(2, 5))
+            }
+
+            @Test
+            fun `hasStartSourceLocationInt failing`() {
+                assertThatThrownBy { FindingAssert(finding).hasStartSourceLocation(2, 5) }
+                    .isInstanceOfAssertionFailedError()
+                    .hasMessage("Expected start source location to be 2:5 but was 1:1")
+                    .hasActual(SourceLocation(1, 1))
+                    .hasExpected(SourceLocation(2, 5))
+            }
+        }
+
+        @Nested
+        inner class End {
+            @Test
+            fun `hasEndSourceLocation with null value`() {
+                assertThatThrownBy { FindingAssert(null).hasEndSourceLocation(SourceLocation(1, 1)) }
+                    .isExactlyInstanceOf(AssertionError::class.java)
+                    .hasMessage("\nExpecting actual not to be null")
+            }
+
+            @Test
+            fun `hasEndSourceLocationInt with null value`() {
+                assertThatThrownBy { FindingAssert(null).hasEndSourceLocation(1, 1) }
+                    .isExactlyInstanceOf(AssertionError::class.java)
+                    .hasMessage("\nExpecting actual not to be null")
+            }
+
+            @Test
+            fun hasEndSourceLocation() {
+                FindingAssert(finding).hasEndSourceLocation(SourceLocation(1, 11))
+            }
+
+            @Test
+            fun hasEndSourceLocationInt() {
+                FindingAssert(finding).hasEndSourceLocation(1, 11)
+            }
+
+            @Test
+            fun `hasEndSourceLocation failing`() {
+                assertThatThrownBy { FindingAssert(finding).hasEndSourceLocation(SourceLocation(2, 14)) }
+                    .isInstanceOfAssertionFailedError()
+                    .hasMessage("Expected end source location to be 2:14 but was 1:11")
+                    .hasActual(SourceLocation(1, 11))
+                    .hasExpected(SourceLocation(2, 14))
+            }
+
+            @Test
+            fun `hasEndSourceLocationInt failing`() {
+                assertThatThrownBy { FindingAssert(finding).hasEndSourceLocation(2, 14) }
+                    .isInstanceOfAssertionFailedError()
+                    .hasMessage("Expected end source location to be 2:14 but was 1:11")
+                    .hasActual(SourceLocation(1, 11))
+                    .hasExpected(SourceLocation(2, 14))
+            }
         }
     }
 }
