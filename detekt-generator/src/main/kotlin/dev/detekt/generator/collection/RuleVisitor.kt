17d1b24d315198cf6a70dcc4b8148c5558caae64
Brais Gabín <braisgabin@gmail.com>
2025-08-26 23:14:18 +0200

    Update detekt-generator/src/main/kotlin/dev/detekt/generator/collection/RuleVisitor.kt
    Co-authored-by: marschwar <marschwar@users.noreply.github.com>

--- detekt-generator/src/main/kotlin/dev/detekt/generator/collection/RuleVisitor.kt
+++ detekt-generator/src/main/kotlin/dev/detekt/generator/collection/RuleVisitor.kt
@@ -131,7 +131,7 @@ internal class RuleVisitor(textReplacements: Map<String, String>) : DetektVisito
             // which needs :detekt-rules-ktlint-wrapper:processResources task output,
             // which needs output of this class.
             "Rule", // dev.detekt.api.Rule
-            "KtlintRule", // dev.detekt.rules.ktlintwrapper.wrappers.KtlintRule
+            "KtlintRule", // dev.detekt.rules.ktlintwrapper.KtlintRule
             "EmptyRule", // dev.detekt.rules.empty.EmptyRule
         )
     }
