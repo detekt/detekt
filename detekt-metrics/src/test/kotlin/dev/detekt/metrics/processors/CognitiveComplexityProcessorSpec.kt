8e9057dd40f332106fce294d617273c7b100bdac
Brais Gabín <brais.gabin@scrm.lidl>
2025-08-26 20:08:13 +0200

    Simplify

--- detekt-metrics/src/test/kotlin/dev/detekt/metrics/processors/CognitiveComplexityProcessorSpec.kt
+++ detekt-metrics/src/test/kotlin/dev/detekt/metrics/processors/CognitiveComplexityProcessorSpec.kt
@@ -1,8 +1,16 @@
 package dev.detekt.metrics.processors
 
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.UserDataHolderBase
+import dev.detekt.api.Detektion
+import dev.detekt.api.Issue
+import dev.detekt.api.Notification
+import dev.detekt.api.ProjectMetric
+import dev.detekt.api.RuleInstance
 import dev.detekt.metrics.CognitiveComplexity
 import dev.detekt.test.utils.compileContentForTest
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.kotlin.psi.KtFile
 import org.junit.jupiter.api.Test
 
 class CognitiveComplexityProcessorSpec {
@@ -15,5 +23,37 @@ class CognitiveComplexityProcessorSpec {
             .test(ProjectCognitiveComplexityProcessor(), CognitiveComplexity.KEY)
 
         assertThat(value).isEqualTo(50)
+    }
+}
+
+private class MetricProcessorTester(
+    private val file: KtFile,
+    private val result: Detektion = MetricResults(),
+) {
+
+    fun <T : Any> test(processor: AbstractProcessor, key: Key<T>): T {
+        with(processor) {
+            onStart(listOf(file))
+            onProcess(file)
+            onProcessComplete(file, emptyList())
+            onFinish(listOf(file), result)
+        }
+        return checkNotNull(result.getUserData(key))
+    }
+}
+
+private class MetricResults : Detektion, UserDataHolderBase() {
+    override val issues: List<Issue>
+        get() = throw UnsupportedOperationException()
+    override val rules: List<RuleInstance>
+        get() = throw UnsupportedOperationException()
+    override val notifications: Collection<Notification>
+        get() = throw UnsupportedOperationException()
+    override val metrics: MutableList<ProjectMetric> = mutableListOf()
+
+    override fun add(notification: Notification): Unit = throw UnsupportedOperationException()
+
+    override fun add(projectMetric: ProjectMetric) {
+        metrics.add(projectMetric)
     }
 }
