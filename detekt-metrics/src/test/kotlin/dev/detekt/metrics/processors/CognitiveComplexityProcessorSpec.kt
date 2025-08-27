f5230b9550890ef985bbbe9419707eb1b0ef1ac1
Brais Gabín <braisgabin@gmail.com>
2025-08-27 19:52:56 +0200

    Metric results (#8544)
    * Simplify
    
    * Remove MetricResults in favor of TestDetektion

--- detekt-metrics/src/test/kotlin/dev/detekt/metrics/processors/CognitiveComplexityProcessorSpec.kt
+++ detekt-metrics/src/test/kotlin/dev/detekt/metrics/processors/CognitiveComplexityProcessorSpec.kt
@@ -1,8 +1,12 @@
 package dev.detekt.metrics.processors
 
+import com.intellij.openapi.util.Key
+import dev.detekt.api.Detektion
+import dev.detekt.api.testfixtures.TestDetektion
 import dev.detekt.metrics.CognitiveComplexity
 import dev.detekt.test.utils.compileContentForTest
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.kotlin.psi.KtFile
 import org.junit.jupiter.api.Test
 
 class CognitiveComplexityProcessorSpec {
@@ -15,5 +19,21 @@ class CognitiveComplexityProcessorSpec {
             .test(ProjectCognitiveComplexityProcessor(), CognitiveComplexity.KEY)
 
         assertThat(value).isEqualTo(50)
+    }
+}
+
+private class MetricProcessorTester(
+    private val file: KtFile,
+    private val result: Detektion = TestDetektion(),
+) {
+
+    fun <T : Any> test(processor: AbstractProcessor, key: Key<T>): T {
+        with(processor) {
+            onStart(listOf(file))
+            onProcess(file)
+            onProcessComplete(file, emptyList())
+            onFinish(listOf(file), result)
+        }
+        return checkNotNull(result.getUserData(key))
     }
 }
