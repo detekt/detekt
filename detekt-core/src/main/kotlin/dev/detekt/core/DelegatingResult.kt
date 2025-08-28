addd82c0db58c2e41d09ae5b24df04c17a562596
Brais Gabín <braisgabin@gmail.com>
2025-08-28 14:01:54 +0200

    Refactor `Detektion` implementations in core (#8547)
    * DetektResult should not be a data class
    
    * Move DelegatingResult near to its use

--- detekt-core/src/main/kotlin/dev/detekt/core/DelegatingResult.kt
+++
@@ -1,9 +0,0 @@
-package dev.detekt.core
-
-import dev.detekt.api.Detektion
-import dev.detekt.api.Issue
-
-class DelegatingResult(
-    result: Detektion,
-    override val issues: List<Issue>,
-) : Detektion by result
