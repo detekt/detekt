72cb988b6db4e38dd392a5c57e8c87e751144411
Brais Gabín <brais.gabin@scrm.lidl>
2025-08-29 11:08:46 +0200

    Improve tests

--- detekt-core/src/test/kotlin/dev/detekt/core/reporting/OutputFacadeSpec.kt
+++ detekt-core/src/test/kotlin/dev/detekt/core/reporting/OutputFacadeSpec.kt
@@ -12,17 +12,14 @@ import dev.detekt.report.md.MdOutputReport
 import dev.detekt.report.xml.XmlOutputReport
 import dev.detekt.test.utils.StringPrintStream
 import dev.detekt.test.utils.createTempFileForTest
-import dev.detekt.test.utils.resourceAsPath
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.nio.file.Path
 
 class OutputFacadeSpec {
 
     @Test
     fun `Running the output facade with multiple reports`() {
         val printStream = StringPrintStream()
-        val inputPath: Path = resourceAsPath("/cases")
         val defaultResult = DetektResult(
             issues = listOf(
                 createIssue(
@@ -37,9 +34,6 @@ class OutputFacadeSpec {
         val mdOutputPath = createTempFileForTest("detekt", ".md")
 
         val spec = createNullLoggingSpec {
-            project {
-                inputPaths = listOf(inputPath)
-            }
             reports {
                 report { "html" to htmlOutputPath }
                 report { "xml" to xmlOutputPath }
@@ -57,5 +51,8 @@ class OutputFacadeSpec {
             "Successfully generated ${HtmlOutputReport().id} at ${htmlOutputPath.toUri()}",
             "Successfully generated ${MdOutputReport().id} at ${mdOutputPath.toUri()}"
         )
+        assertThat(xmlOutputPath).isNotEmptyFile()
+        assertThat(htmlOutputPath).isNotEmptyFile()
+        assertThat(mdOutputPath).isNotEmptyFile()
     }
 }
