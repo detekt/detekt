2b510029d6bdcf8aad0544ce816de6bafd764325
Brais Gabín <brais.gabin@scrm.lidl>
2025-09-08 18:00:49 +0200

    Simplify OutputReport

--- detekt-api/src/main/kotlin/dev/detekt/api/OutputReport.kt
+++ detekt-api/src/main/kotlin/dev/detekt/api/OutputReport.kt
@@ -1,9 +1,5 @@
 package dev.detekt.api
 
-import java.nio.file.Path
-import kotlin.io.path.createParentDirectories
-import kotlin.io.path.writeText
-
 /**
  * Translates detekt's result container - [Detektion] - into an output report
  * which is written inside a file.
@@ -14,16 +10,6 @@ abstract class OutputReport : Extension {
      * Supported ending of this report type.
      */
     abstract val ending: String
-
-    /**
-     * Renders result and writes it to the given [filePath].
-     */
-    fun write(filePath: Path, detektion: Detektion) {
-        val reportData = render(detektion)
-        if (reportData != null) {
-            filePath.createParentDirectories().writeText(reportData)
-        }
-    }
 
     /**
      * Defines the translation process of detekt's result into a string.
--- detekt-core/src/main/kotlin/dev/detekt/core/reporting/OutputFacade.kt
+++ detekt-core/src/main/kotlin/dev/detekt/core/reporting/OutputFacade.kt
@@ -3,8 +3,12 @@ package dev.detekt.core.reporting
 import dev.detekt.api.Detektion
 import dev.detekt.api.Notification
 import dev.detekt.api.Notification.Level
+import dev.detekt.api.OutputReport
 import dev.detekt.core.ProcessingSettings
 import dev.detekt.tooling.api.spec.ReportsSpec
+import java.nio.file.Path
+import kotlin.io.path.createParentDirectories
+import kotlin.io.path.writeText
 
 class OutputFacade(
     private val settings: ProcessingSettings,
@@ -37,5 +41,15 @@ class OutputFacade(
                 result.add(Notification("Successfully generated ${report.id} at ${filePath.toUri()}", Level.Error))
             }
         }
+    }
+}
+
+/*
+ * Renders result and writes it to the given [filePath].
+ */
+private fun OutputReport.write(filePath: Path, detektion: Detektion) {
+    val reportData = render(detektion)
+    if (reportData != null) {
+        filePath.createParentDirectories().writeText(reportData)
     }
 }
