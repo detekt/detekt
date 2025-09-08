7dec130c829674217261ec5528f6a5db15c404ac
Matthew Haughton <3flex@users.noreply.github.com>
2025-09-08 21:32:04 +1000

    Remove use of ReflectionFactory to setup DetektPomModel

--- detekt-parser/src/main/kotlin/dev/detekt/parser/DetektPomModel.kt
+++ detekt-parser/src/main/kotlin/dev/detekt/parser/DetektPomModel.kt
@@ -1,18 +1,20 @@
 package dev.detekt.parser
 
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.pom.PomModel
 import com.intellij.pom.PomModelAspect
 import com.intellij.pom.PomTransaction
 import com.intellij.pom.impl.PomTransactionBase
 import com.intellij.pom.tree.TreeAspect
-import sun.reflect.ReflectionFactory
 
 /**
  * Adapted from https://github.com/pinterest/ktlint/blob/0.50.0/ktlint-rule-engine/src/main/kotlin/com/pinterest/ktlint/rule/engine/internal/KotlinPsiFileFactory.kt
  * Licenced under the MIT licence - https://github.com/pinterest/ktlint/blob/master/LICENSE
  */
-object DetektPomModel : UserDataHolderBase(), PomModel {
+class DetektPomModel(project: Project) : UserDataHolderBase(), PomModel {
+
+    val treeAspect: TreeAspect = project.getService(TreeAspect::class.java)
 
     override fun runTransaction(transaction: PomTransaction) {
         val transactionCandidate = transaction as? PomTransactionBase
@@ -24,13 +26,11 @@ object DetektPomModel : UserDataHolderBase(), PomModel {
         pomTransaction.run()
     }
 
-    override fun <T : PomModelAspect?> getModelAspect(aspect: Class<T>): T? {
-        if (aspect == TreeAspect::class.java) {
-            val constructor = ReflectionFactory.getReflectionFactory()
-                .newConstructorForSerialization(aspect, Any::class.java.getDeclaredConstructor())
+    override fun <T : PomModelAspect> getModelAspect(aspect: Class<T>): T? =
+        if (aspect == treeAspect::class.java) {
             @Suppress("UNCHECKED_CAST")
-            return constructor.newInstance() as T
+            treeAspect as T
+        } else {
+            null
         }
-        return null
-    }
 }
