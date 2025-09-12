e5a32b6ec6afe221d5b16c15084d5e06a19af76f
Atul Gupta <atlgpt17@gmail.com>
2025-09-13 01:49:52 +0530

    Delete unused `bindingContext` using methods for `firstParameter`

--- detekt-psi-utils/src/main/kotlin/dev/detekt/psi/KtLambdaExpression.kt
+++ detekt-psi-utils/src/main/kotlin/dev/detekt/psi/KtLambdaExpression.kt
@@ -2,28 +2,14 @@ package dev.detekt.psi
 
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.symbols.KaValueParameterSymbol
-import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
 import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.psi.KtLambdaExpression
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.psi.psiUtil.anyDescendantOfType
-import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
-
-private fun KtLambdaExpression.firstParameter(bindingContext: BindingContext) =
-    bindingContext[BindingContext.FUNCTION, functionLiteral]?.valueParameters?.singleOrNull()
 
 fun KtLambdaExpression.firstParameterOrNull(): KaValueParameterSymbol? = analyze(this) {
     functionLiteral.symbol.valueParameters.singleOrNull()
 }
-
-fun KtLambdaExpression.implicitParameter(bindingContext: BindingContext): ValueParameterDescriptor? =
-    if (valueParameters.isNotEmpty()) {
-        null
-    } else {
-        firstParameter(bindingContext)
-    }
 
 fun KtLambdaExpression.implicitParameterOrNull(): KaValueParameterSymbol? =
     if (valueParameters.isNotEmpty()) {
@@ -31,23 +17,6 @@ fun KtLambdaExpression.implicitParameterOrNull(): KaValueParameterSymbol? =
     } else {
         firstParameterOrNull()
     }
-
-fun KtLambdaExpression.hasImplicitParameterReference(
-    implicitParameter: ValueParameterDescriptor,
-    bindingContext: BindingContext,
-): Boolean =
-    anyDescendantOfType<KtNameReferenceExpression> {
-        it.isImplicitParameterReference(this, implicitParameter, bindingContext)
-    }
-
-private fun KtNameReferenceExpression.isImplicitParameterReference(
-    lambda: KtLambdaExpression,
-    implicitParameter: ValueParameterDescriptor,
-    bindingContext: BindingContext,
-): Boolean =
-    text == "it" &&
-        getStrictParentOfType<KtLambdaExpression>() == lambda &&
-        getResolvedCall(bindingContext)?.resultingDescriptor == implicitParameter
 
 fun KtLambdaExpression.hasImplicitParameterReference(): Boolean {
     if (valueParameters.isNotEmpty()) return false
