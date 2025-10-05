50bf99fdc3f62bd048f7aefcaf6d184bd87e58a6
Matthew Haughton <3flex@users.noreply.github.com>
2025-10-05 22:18:00 +1100

    Remove unused test task configuration from detekt-parser (#8729)

--- detekt-parser/build.gradle.kts
+++ detekt-parser/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
-
 plugins {
     id("module")
 }
@@ -9,12 +7,4 @@ dependencies {
     implementation(projects.detektKotlinAnalysisApiStandalone)
     testImplementation(projects.detektTestUtils)
     testImplementation(libs.assertj.core)
-}
-
-tasks.withType<Test>().configureEach {
-    systemProperty("kotlinVersion", getKotlinPluginVersion())
-
-    doFirst {
-        systemProperty("testClasspath", classpath.joinToString(";"))
-    }
 }
