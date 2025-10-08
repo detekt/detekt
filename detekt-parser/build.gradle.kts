8a7e287f7b07a38c7d30c2fed23ae2e9cc1a8de5
Matthew Haughton <3flex@users.noreply.github.com>
2025-10-08 22:24:29 +1100

    Remove KtCompiler from detekt-parser (#8728)

--- detekt-parser/build.gradle.kts
+++ detekt-parser/build.gradle.kts
@@ -5,6 +5,5 @@ plugins {
 dependencies {
     api(libs.kotlin.compiler)
     implementation(projects.detektKotlinAnalysisApiStandalone)
-    testImplementation(projects.detektTestUtils)
     testImplementation(libs.assertj.core)
 }
