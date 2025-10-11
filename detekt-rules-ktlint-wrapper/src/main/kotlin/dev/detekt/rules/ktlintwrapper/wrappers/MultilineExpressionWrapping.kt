00d9468f5aa6766596397ecc47a748f1d2bf2d68
Matthew Haughton <3flex@users.noreply.github.com>
2025-10-11 14:54:45 +1100

    Don't enable MultilineExpressionWrapping by default
    From https://pinterest.github.io/ktlint/1.7.1/rules/standard/#multiline-expression-wrapping
    
    > This rule is only run when ktlint_code_style is set to ktlint_official
    > or when the rule is enabled explicitly.
    
    detekt uses code style intellij_idea by default.

--- detekt-rules-ktlint-wrapper/src/main/kotlin/dev/detekt/rules/ktlintwrapper/wrappers/MultilineExpressionWrapping.kt
+++ detekt-rules-ktlint-wrapper/src/main/kotlin/dev/detekt/rules/ktlintwrapper/wrappers/MultilineExpressionWrapping.kt
@@ -4,7 +4,6 @@ import com.pinterest.ktlint.rule.engine.core.api.editorconfig.EditorConfigProper
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.INDENT_SIZE_PROPERTY
 import com.pinterest.ktlint.ruleset.standard.rules.FunctionSignatureRule.Companion.FUNCTION_BODY_EXPRESSION_WRAPPING_PROPERTY
 import com.pinterest.ktlint.ruleset.standard.rules.MultilineExpressionWrappingRule
-import dev.detekt.api.ActiveByDefault
 import dev.detekt.api.Config
 import dev.detekt.api.Configuration
 import dev.detekt.api.config
@@ -14,8 +13,11 @@ import dev.detekt.rules.ktlintwrapper.KtlintRule
 /**
  * See [ktlint docs](https://pinterest.github.io/ktlint/<ktlintVersion/>/rules/standard/#multiline-expression-wrapping) for
  * documentation.
+ *
+ * Although this is a standard rule it is not enabled by default like other standard ktlint rules. ktlint only enables
+ * this rule when the rule is explicitly enabled or if the code style is set to ktlint_official. detekt uses
+ * intellij_idea by default.
  */
-@ActiveByDefault(since = "2.0.0")
 @AutoCorrectable(since = "1.23.0")
 class MultilineExpressionWrapping(config: Config) : KtlintRule(
     config,
