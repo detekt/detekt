63f0b100ff3ce47924f264de1e21a5cafdcdd026
Matthew Haughton <3flex@users.noreply.github.com>
2025-12-22 10:09:58 +1100

    Add direct testImplementation dependency on Kotlin compiler (#8943)
    Addresses this advice from Dependency Analysis Gradle Plugin:
    
    Advice for :detekt-test-assertj
    These transitive dependencies should be declared directly:
      testImplementation(libs.kotlin.compiler)

--- detekt-test-assertj/build.gradle.kts
+++ detekt-test-assertj/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     compileOnly(projects.detektApi)
     api(libs.assertj.coreMinimum)
 
+    testImplementation(libs.kotlin.compiler)
     testImplementation(testFixtures(projects.detektApi))
     testImplementation(projects.detektTestUtils)
     testImplementation(libs.opentest4j)
