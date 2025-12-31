c3b7af90d2d3e6d78e10e03f32ce675991142d65
Atul Gupta <11026203+atulgpt@users.noreply.github.com>
2025-12-31 05:30:05 +0530

    Ignore param in baselines (#8886)
    * Ignore param in baselines
    
    * Fix TC in EntitySpec.kt

--- detekt-api/src/test/kotlin/dev/detekt/api/internal/SignaturesSpec.kt
+++ detekt-api/src/test/kotlin/dev/detekt/api/internal/SignaturesSpec.kt
@@ -3,6 +3,7 @@ package dev.detekt.api.internal
 import dev.detekt.test.utils.compileContentForTest
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.jetbrains.kotlin.psi.KtConstructor
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.findDescendantOfType
 import org.junit.jupiter.api.Test
@@ -14,7 +15,7 @@ class SignaturesSpec {
             .findDescendantOfType<KtNamedFunction>()!!
             .buildFullSignature()
 
-        assertThat(result).isEqualTo("fun data(): Int")
+        assertThat(result).isEqualTo("fun data: Int")
     }
 
     @Test
@@ -23,7 +24,7 @@ class SignaturesSpec {
             .findDescendantOfType<KtNamedFunction>()!!
             .buildFullSignature()
 
-        assertThat(result).isEqualTo("A\$fun data(): Int")
+        assertThat(result).isEqualTo($$"A$fun data: Int")
     }
 
     @Test
@@ -32,7 +33,7 @@ class SignaturesSpec {
             .findDescendantOfType<KtNamedFunction>()!!
             .buildFullSignature()
 
-        assertThat(result).isEqualTo("fun data()")
+        assertThat(result).isEqualTo("fun data")
     }
 
     @Test
@@ -41,7 +42,7 @@ class SignaturesSpec {
             .findDescendantOfType<KtNamedFunction>()!!
             .buildFullSignature()
 
-        assertThat(result).isEqualTo("fun data()")
+        assertThat(result).isEqualTo("fun data")
     }
 
     @Test
@@ -53,5 +54,53 @@ class SignaturesSpec {
         }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("Error building function signature")
+    }
+
+    @Test
+    fun `function with params`() {
+        assertThat(
+            compileContentForTest("class A { fun data(a: Int, b: Int): Int = a + b }")
+                .findDescendantOfType<KtNamedFunction>()!!
+                .buildFullSignature(),
+        ).isEqualTo($$"A$fun data: Int")
+    }
+
+    @Test
+    fun `primary constructor with params`() {
+        assertThat(
+            compileContentForTest("class A(private val a: String)")
+                .findDescendantOfType<KtConstructor<*>>()!!
+                .buildFullSignature(),
+        ).isEqualTo("A")
+    }
+
+    @Test
+    fun `secondary constructor without params`() {
+        assertThat(
+            compileContentForTest(
+                """
+                    class A {
+                        constructor()
+                    }
+                """.trimIndent(),
+            )
+                .findDescendantOfType<KtConstructor<*>>()!!
+                .buildFullSignature(),
+        ).isEqualTo($$"A$constructor")
+    }
+
+    @Test
+    fun `secondary constructor with params`() {
+        assertThat(
+            compileContentForTest(
+                """
+                    class A {
+                        constructor(a: String)
+                    }
+                """.trimIndent(),
+            )
+                .findDescendantOfType<KtConstructor<*>>()!!
+                .buildFullSignature(),
+        ).isEqualTo($$"A$constructor")
     }
 }
