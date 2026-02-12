2eb800b2d360ca01991a26e4bddc54ff4623a39d
Paolo Rotolo <rotolopao@gmail.com>
2026-02-12 17:23:51 +0100

    Add new rule and tests

---
+++ detekt-rules-complexity/src/test/kotlin/dev/detekt/rules/complexity/ReplaceTryFinallyWithUseSpec.kt
@@ -0,0 +1,118 @@
+package dev.detekt.rules.complexity
+
+import dev.detekt.api.Config
+import dev.detekt.test.junit.KotlinCoreEnvironmentTest
+import dev.detekt.test.lintWithContext
+import dev.detekt.test.utils.KotlinEnvironmentContainer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+@KotlinCoreEnvironmentTest
+class ReplaceTryFinallyWithUseSpec(val env: KotlinEnvironmentContainer) {
+
+    val subject = ReplaceTryFinallyWithUse(Config.empty)
+
+    @Test
+    fun `reports close in try-catch-finally block`() {
+        val code = """
+                class ExampleCloseable: AutoCloseable {
+                    override fun close() { }
+                }
+            
+                val closeable = ExampleCloseable()
+                fun test() {
+                    try {
+            
+                    } catch (e: Exception) {
+                    
+                    } finally {
+                        closeable.close()
+                    }
+                }
+        """.trimIndent()
+
+        assertThat(subject.lintWithContext(env, code)).hasSize(1)
+    }
+
+    @Test
+    fun `reports close in try-finally block`() {
+        val code = """
+                class ExampleCloseable: AutoCloseable {
+                    override fun close() { }
+                }
+            
+                val closeable = ExampleCloseable()
+                fun test() {
+                    try {
+            
+                    } finally {
+                        closeable.close()
+                    }
+                }
+        """.trimIndent()
+
+        assertThat(subject.lintWithContext(env, code)).hasSize(1)
+    }
+
+    @Test
+    fun `does not report close overload in finally block`() {
+        val code = """
+                class ExampleCloseable: AutoCloseable {
+                    override fun close() { }
+                    fun close(test: String) { }
+                }
+            
+                val closeable = ExampleCloseable()
+                fun test() {
+                    try {
+            
+                    } finally {
+                        closeable.close("")
+                    }
+                }
+        """.trimIndent()
+
+        assertThat(subject.lintWithContext(env, code)).isEmpty()
+    }
+
+    @Test
+    fun `does not report with no close calls`() {
+        val code = """
+                class ExampleCloseable: AutoCloseable {
+                    override fun close() { }
+                    fun close(test: String) { }
+                }
+            
+                val closeable = ExampleCloseable()
+                fun test() {
+                    try {
+            
+                    } finally {
+                        // empty
+                    }
+                }
+        """.trimIndent()
+
+        assertThat(subject.lintWithContext(env, code)).isEmpty()
+    }
+
+    @Test
+    fun `only reports when receiver is AutoCloseable`() {
+        val code = """
+                class NotCloseableInterface {
+                    fun close() { }
+                }
+            
+                val closeable = NotCloseableInterface()
+                fun test() {
+                    try {
+            
+                    } finally {
+                        closeable.close()
+                    }
+                }
+        """.trimIndent()
+
+        assertThat(subject.lintWithContext(env, code)).isEmpty()
+    }
+}
