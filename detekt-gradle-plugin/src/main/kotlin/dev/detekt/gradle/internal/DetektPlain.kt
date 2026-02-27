8b65a1f3f523bb6756c1ec4e983ee0f2f242289b
Jon Poulton <jpoulton@pm.me>
2026-02-27 20:52:14 +0000

    Register explicit API config for detekt task

--- detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/internal/DetektPlain.kt
+++ detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/internal/DetektPlain.kt
@@ -4,10 +4,12 @@ import dev.detekt.gradle.Detekt
 import dev.detekt.gradle.DetektCreateBaselineTask
 import dev.detekt.gradle.extensions.DetektExtension
 import dev.detekt.gradle.plugin.DetektPlugin
+import dev.detekt.gradle.plugin.internal.mapExplicitArgMode
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.gradle.api.provider.Provider
 import org.gradle.language.base.plugins.LifecycleBasePlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinBasePlugin
 
 internal class DetektPlain(private val project: Project) {
 
@@ -24,15 +26,28 @@ internal class DetektPlain(private val project: Project) {
             detektTask.setExcludes(DetektPlugin.defaultExcludes)
         }
 
+        plugins.withType(KotlinBasePlugin::class.java) {
+            detektTaskProvider.configure { detektTask ->
+                detektTask.explicitApi.convention(mapExplicitArgMode())
+            }
+        }
+
         tasks.matching { it.name == LifecycleBasePlugin.CHECK_TASK_NAME }.configureEach {
             it.dependsOn(detektTaskProvider)
         }
     }
 
     private fun Project.registerCreateBaselineTask(extension: DetektExtension) {
-        tasks.register(DetektPlugin.BASELINE_TASK_NAME, DetektCreateBaselineTask::class.java) {
-            it.baseline.convention(extension.baseline)
-            it.setSource(existingInputDirectoriesProvider(project, extension))
+        val baselineTaskProvider =
+            tasks.register(DetektPlugin.BASELINE_TASK_NAME, DetektCreateBaselineTask::class.java) {
+                it.baseline.convention(extension.baseline)
+                it.setSource(existingInputDirectoriesProvider(project, extension))
+            }
+
+        plugins.withType(KotlinBasePlugin::class.java) {
+            baselineTaskProvider.configure { baselineTask ->
+                baselineTask.explicitApi.convention(mapExplicitArgMode())
+            }
         }
     }
 
