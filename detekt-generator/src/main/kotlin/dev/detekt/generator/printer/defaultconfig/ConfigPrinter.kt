2aeea1f8d64fce84a1c859389ad7ee3678a610ea
Brais Gabín <braisgabin@gmail.com>
2026-03-09 01:05:28 +0100

    Don't add config header

--- detekt-generator/src/main/kotlin/dev/detekt/generator/printer/defaultconfig/ConfigPrinter.kt
+++ detekt-generator/src/main/kotlin/dev/detekt/generator/printer/defaultconfig/ConfigPrinter.kt
@@ -8,63 +8,7 @@ object ConfigPrinter : DocumentationPrinter<List<RuleSetPage>> {
 
     override fun print(item: List<RuleSetPage>): String =
         yaml {
-            yaml { defaultConfigConfiguration() }
-            emptyLine()
-            yaml { defaultProcessorsConfiguration() }
-            emptyLine()
-            yaml { defaultConsoleReportsConfiguration() }
-            emptyLine()
-
             item.sortedBy { it.ruleSet.name }
                 .forEach { printRuleSetPage(it) }
         }
-
-    fun printCustomRuleConfig(item: List<RuleSetPage>): String =
-        yaml {
-            item.sortedBy { it.ruleSet.name }
-                .forEach { printRuleSetPage(it) }
-        }
-
-    private fun defaultConfigConfiguration(): String =
-        """
-            config:
-              validation: true
-              warningsAsErrors: false
-              checkExhaustiveness: false
-              # when writing own rules with new properties, exclude the property path e.g.: ['my_rule_set', '.*>.*>[my_property]']
-              excludes: []
-        """.trimIndent()
-
-    private fun defaultProcessorsConfiguration(): String =
-        """
-            processors:
-              active: true
-              exclude:
-                - 'DetektProgressListener'
-              # - 'KtFileCountProcessor'
-              # - 'PackageCountProcessor'
-              # - 'ClassCountProcessor'
-              # - 'FunctionCountProcessor'
-              # - 'PropertyCountProcessor'
-              # - 'ProjectComplexityProcessor'
-              # - 'ProjectCognitiveComplexityProcessor'
-              # - 'ProjectLLOCProcessor'
-              # - 'ProjectCLOCProcessor'
-              # - 'ProjectLOCProcessor'
-              # - 'ProjectSLOCProcessor'
-              # - 'LicenseHeaderLoaderExtension'
-        """.trimIndent()
-
-    private fun defaultConsoleReportsConfiguration(): String =
-        """
-            console-reports:
-              active: true
-              exclude:
-                 - 'ProjectStatisticsReport'
-                 - 'ComplexityReport'
-                 - 'NotificationReport'
-                 - 'IssuesReport'
-                 - 'FileBasedIssuesReport'
-              #  - 'LiteIssuesReport'
-        """.trimIndent()
 }
