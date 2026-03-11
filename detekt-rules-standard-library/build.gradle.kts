58e41f6537a0d7b26efd1e88bae365137542bfc2
Brais Gabín <braisgabin@gmail.com>
2026-03-11 23:19:44 +0100

    Create standard-library rule-set

---
+++ detekt-rules-standard-library/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("module")
+}
+
+dependencies {
+    compileOnly(projects.detektApi)
+    compileOnly(projects.detektPsiUtils)
+
+    testImplementation(libs.kotlin.compiler)
+    testImplementation(projects.detektApi)
+    testImplementation(projects.detektTest)
+    testImplementation(projects.detektTestAssertj)
+    testImplementation(projects.detektTestJunit)
+    testImplementation(libs.assertj.core)
+}
