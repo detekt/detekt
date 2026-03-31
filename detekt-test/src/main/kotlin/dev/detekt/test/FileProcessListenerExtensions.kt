9dede47bc6242f10cce0dbedbd905faa4a9a64d1
Brais Gabín <braisgabin@gmail.com>
2026-03-31 23:28:58 +0200

    Extend FileProcessListenerExtensions

--- detekt-test/src/main/kotlin/dev/detekt/test/FileProcessListenerExtensions.kt
+++ detekt-test/src/main/kotlin/dev/detekt/test/FileProcessListenerExtensions.kt
@@ -2,17 +2,25 @@ package dev.detekt.test
 
 import dev.detekt.api.Detektion
 import dev.detekt.api.FileProcessListener
+import dev.detekt.api.Issue
 import dev.detekt.api.testfixtures.TestDetektion
 import org.jetbrains.kotlin.psi.KtFile
 
-fun FileProcessListener.invoke(vararg files: KtFile) = invoke(files.toList())
+fun FileProcessListener.invoke(
+    vararg files: KtFile,
+    detektion: Detektion = TestDetektion(),
+    issues: List<Issue> = emptyList(),
+) = invoke(files.toList(), detektion, issues)
 
-fun FileProcessListener.invoke(files: List<KtFile>): Detektion {
-    val result = TestDetektion()
+fun FileProcessListener.invoke(
+    files: List<KtFile>,
+    detektion: Detektion = TestDetektion(),
+    issues: List<Issue> = emptyList(),
+): Detektion {
     onStart(files)
     files.forEach {
         onProcess(it)
-        onProcessComplete(it, emptyList())
+        onProcessComplete(it, issues)
     }
-    return onFinish(files, result)
+    return onFinish(files, detektion)
 }
