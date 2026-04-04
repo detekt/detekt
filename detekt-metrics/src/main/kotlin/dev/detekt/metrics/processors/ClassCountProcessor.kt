ba8bba76072b5bb35c8c23349faec560f7d50e61
Brais Gabín <braisgabin@gmail.com>
2026-04-04 09:50:03 +0200

    Optimize how we get project statistics (#9217)
    * Don't test visitor, they are implementation details
    
    * Visit a KtFile isn't free. This implementation avoids that

--- detekt-metrics/src/main/kotlin/dev/detekt/metrics/processors/ClassCountProcessor.kt
+++ detekt-metrics/src/main/kotlin/dev/detekt/metrics/processors/ClassCountProcessor.kt
@@ -1,24 +1,21 @@
 package dev.detekt.metrics.processors
 
 import com.intellij.openapi.util.Key
-import dev.detekt.api.DetektVisitor
+import dev.detekt.api.Detektion
+import dev.detekt.api.FileProcessListener
+import dev.detekt.api.ProjectMetric
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 
-class ClassCountProcessor : AbstractProjectMetricProcessor() {
-
+class ClassCountProcessor : FileProcessListener {
     override val id: String = "ClassCountProcessor"
-    override val visitor = ClassCountVisitor()
-    override val key = numberOfClassesKey
+
+    override fun onFinish(files: List<KtFile>, result: Detektion): Detektion {
+        val count = files.sumOf { it.collectDescendantsOfType<KtClass>().size }
+        result.add(ProjectMetric(numberOfClassesKey.toString(), count))
+        return result
+    }
 }
 
 val numberOfClassesKey = Key<Int>("number of classes")
-
-class ClassCountVisitor : DetektVisitor() {
-
-    override fun visitKtFile(file: KtFile) {
-        super.visitKtFile(file)
-        file.putUserData(numberOfClassesKey, file.collectDescendantsOfType<KtClass>().size)
-    }
-}
--- detekt-metrics/src/main/kotlin/dev/detekt/metrics/processors/PropertyCountProcessor.kt
+++ detekt-metrics/src/main/kotlin/dev/detekt/metrics/processors/PropertyCountProcessor.kt
@@ -1,24 +1,21 @@
 package dev.detekt.metrics.processors
 
 import com.intellij.openapi.util.Key
-import dev.detekt.api.DetektVisitor
+import dev.detekt.api.Detektion
+import dev.detekt.api.FileProcessListener
+import dev.detekt.api.ProjectMetric
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 
-class PropertyCountProcessor : AbstractProjectMetricProcessor() {
-
+class PropertyCountProcessor : FileProcessListener {
     override val id: String = "PropertyCountProcessor"
-    override val visitor = PropertyCountVisitor()
-    override val key = numberOfFieldsKey
+
+    override fun onFinish(files: List<KtFile>, result: Detektion): Detektion {
+        val count = files.sumOf { it.collectDescendantsOfType<KtProperty>().size }
+        result.add(ProjectMetric(numberOfFieldsKey.toString(), count))
+        return result
+    }
 }
 
 val numberOfFieldsKey = Key<Int>("number of properties")
-
-class PropertyCountVisitor : DetektVisitor() {
-
-    override fun visitKtFile(file: KtFile) {
-        super.visitKtFile(file)
-        file.putUserData(numberOfFieldsKey, file.collectDescendantsOfType<KtProperty>().size)
-    }
-}
