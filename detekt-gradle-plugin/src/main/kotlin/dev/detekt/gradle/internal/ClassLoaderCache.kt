0e0ffa789a1aeab5025e44ee16e7bb57f947d1f2
Brais Gabín <braisgabin@gmail.com>
2026-04-14 00:45:21 +0200

    Use the sha1 of the classpath content

--- detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/internal/ClassLoaderCache.kt
+++ detekt-gradle-plugin/src/main/kotlin/dev/detekt/gradle/internal/ClassLoaderCache.kt
@@ -2,6 +2,8 @@ package dev.detekt.gradle.internal
 
 import java.io.File
 import java.net.URLClassLoader
+import java.security.MessageDigest
+import java.util.Locale
 import java.util.concurrent.ConcurrentHashMap
 
 internal fun interface ClassLoaderCache {
@@ -10,11 +12,10 @@ internal fun interface ClassLoaderCache {
 }
 
 internal class DefaultClassLoaderCache : ClassLoaderCache {
-
-    private val classpathFilesHashWithLoaders = ConcurrentHashMap<Int, URLClassLoader>()
+    private val classpathFilesHashWithLoaders = ConcurrentHashMap<String, URLClassLoader>()
 
     override fun getOrCreate(classpath: Set<File>): URLClassLoader {
-        val classpathHashCode = classpath.sorted().hashCode()
+        val classpathHashCode = sha1(classpath.sorted())
         return classpathFilesHashWithLoaders.getOrPut(classpathHashCode) {
             URLClassLoader(
                 classpath.map { it.toURI().toURL() }.toTypedArray(),
@@ -25,3 +26,29 @@ internal class DefaultClassLoaderCache : ClassLoaderCache {
 }
 
 internal object GlobalClassLoaderCache : ClassLoaderCache by DefaultClassLoaderCache()
+
+private fun sha1(paths: Collection<File>): String {
+    val digest = MessageDigest.getInstance("SHA-1")
+    val buffer = ByteArray(8192)
+
+    paths
+        .flatMap { file ->
+            if (file.isDirectory) {
+                file.walkTopDown().filter { it.isFile }.sortedBy { it.absolutePath }
+            } else if (file.exists()) {
+                sequenceOf(file)
+            } else {
+                emptySequence()
+            }
+        }
+        .forEach { file ->
+            file.inputStream().use { input ->
+                var bytesRead = input.read(buffer)
+                while (bytesRead != -1) {
+                    digest.update(buffer, 0, bytesRead)
+                    bytesRead = input.read(buffer)
+                }
+            }
+        }
+    return digest.digest().joinToString("") { "%02x".format(Locale.ROOT, it) }
+}
