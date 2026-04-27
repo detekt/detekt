6fb6214a0e8897b9efc3bfa86bfcb359afbed4ec
Matthew Haughton <3flex@users.noreply.github.com>
2026-04-27 13:44:18 +1000

    Publish sources for repackaged analysis-api artifacts

--- detekt-kotlin-analysis-api-standalone/build.gradle.kts
+++ detekt-kotlin-analysis-api-standalone/build.gradle.kts
@@ -10,8 +10,25 @@ dependencies {
     api(libs.kotlin.analysisApiStandalone) { isTransitive = false }
 }
 
+val sourcesJar = tasks.register<Jar>("sourcesJar") {
+    archiveClassifier = "sources"
+
+    from(
+        configurations.runtimeClasspath.map {
+            it.incoming.artifactView {
+                withVariantReselection()
+                attributes {
+                    attribute(Category.CATEGORY_ATTRIBUTE, named(Category.DOCUMENTATION))
+                    attribute(DocsType.DOCS_TYPE_ATTRIBUTE, named(DocsType.SOURCES))
+                }
+            }.files.map { jar -> zipTree(jar) }
+        }
+    )
+}
+
 java {
     targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
 }
 
 val javaComponent = components["java"] as AdhocComponentWithVariants
