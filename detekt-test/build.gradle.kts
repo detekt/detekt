a25d17a32f9f293a82d922e16a57c43bda4bcd60
Matthew Haughton <3flex@users.noreply.github.com>
2026-05-11 22:09:28 +1000

    Don't flag unused junit-jupiter in modules that have no tests

--- detekt-test/build.gradle.kts
+++ detekt-test/build.gradle.kts
@@ -10,3 +10,13 @@ dependencies {
     api(libs.kotlin.compiler)
     implementation(libs.kotlin.reflect)
 }
+
+dependencyAnalysis {
+    issues {
+        onUnusedDependencies {
+            // All modules have a test suite that uses JUnit Jupiter which adds this dependency.
+            // This module has no tests so this dependency is unused and is flagged by DAGP unless excluded.
+            exclude("org.junit.jupiter:junit-jupiter")
+        }
+    }
+}
