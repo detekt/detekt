ab562da77ab7ae01845e8705835a042f40d2d862
Brais Gabín <braisgabin@gmail.com>
2026-05-23 23:10:06 +0200

    Create test for parcelize

---
+++ detekt-gradle-plugin/src/functionalTest/kotlin/dev/detekt/gradle/CompilerPluginsTests.kt
@@ -0,0 +1,20 @@
+package dev.detekt.gradle
+
+import dev.detekt.gradle.testkit.withResourceDir
+import org.assertj.core.api.Assertions.assertThat
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.Test
+
+class CompilerPluginsTests {
+    @Test
+    fun `detekt works with @Parzelize`() {
+        val result = GradleRunner.create()
+            .withResourceDir("android-parcelize")
+            .withPluginClasspath()
+            .withArguments("detektMain")
+            .build()
+
+        assertThat(result.output)
+            .doesNotContainPattern("There were [0-9]+ compiler errors found during analysis".toRegex().toPattern())
+    }
+}
