56279ae73f140f6ad4a1b592f857a3b7af6e9860
Brais Gabín <braisgabin@gmail.com>
2026-08-21 10:51:47 +0200

    Improve error message for unused private properties in contructor

--- detekt-rules-style/src/main/kotlin/dev/detekt/rules/style/UnusedPrivateProperty.kt
+++ detekt-rules-style/src/main/kotlin/dev/detekt/rules/style/UnusedPrivateProperty.kt
@@ -103,10 +103,19 @@ private class UnusedPrivatePropertyVisitor(private val allowedNames: Regex) : De
             .filter { it.psiOrParent !in usedClassProperties }
             .filter { !allowedNames.matches(it.nameAsSafeName.identifier) }
             .map {
-                Finding(
-                    entity = Entity.atName(it),
-                    message = "Private property `${it.nameAsSafeName.identifier}` is unused."
-                )
+                if (it.psiOrParent in usedConstructorParameters) {
+                    it as KtParameter
+                    Finding(
+                        entity = Entity.from(checkNotNull(it.valOrVarKeyword)),
+                        message = "Private property `${it.nameAsSafeName.identifier}` is only used as constructor " +
+                            "parameter."
+                    )
+                } else {
+                    Finding(
+                        entity = Entity.atName(it),
+                        message = "Private property `${it.nameAsSafeName.identifier}` is unused."
+                    )
+                }
             }
 
         val constructorParametersReport = constructorParameters
@@ -195,6 +204,8 @@ private class UnusedPrivatePropertyVisitor(private val allowedNames: Regex) : De
                         psi is KtParameter && psi.hasValOrVar() -> {
                             if (!expression.isInPropertyInitializer(psi)) {
                                 usedClassProperties.add(psi)
+                            } else {
+                                usedConstructorParameters.add(psi)
                             }
                         }
 
