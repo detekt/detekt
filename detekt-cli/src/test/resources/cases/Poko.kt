4f89e00be2443c9409b314e54d33455fc40e3d45
Matthew Haughton <3flex@users.noreply.github.com>
2026-09-13 23:53:30 +1000

    Update test to avoid relying on ktlint rule set (#9722)
    Update rule to trigger MagicNumber instead of a ktlint rule. detekt-cli
    tests should rely only on first party rules to pass.

--- detekt-cli/src/test/resources/cases/Poko.kt
+++ detekt-cli/src/test/resources/cases/Poko.kt
@@ -2,8 +2,12 @@ package cases
 
 class Poko {
 
-    val name: String = "Poko"
-    fun hello() = "Hello, $name"
-
-
+    // Reports MagicNumber
+    fun test(x: Int): Int {
+        when (x) {
+            5 -> return 5
+            4 -> return 4
+            3 -> return 3
+        }
+    }
 }
