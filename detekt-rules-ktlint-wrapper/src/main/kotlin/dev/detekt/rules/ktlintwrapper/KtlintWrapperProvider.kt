b9c8fae6e3dde6709347db5e2c66c0560c695cb3
Matthew Haughton <3flex@users.noreply.github.com>
2026-09-26 10:22:50 +1000

    Rename ContextReceiverListWrapping to ContextParameterListWrapping (#9747)
    Aligns naming to upcoming changes in ktlint 2.0.0
    
    https://github.com/ktlint/ktlint/pull/3367

--- detekt-rules-ktlint-wrapper/src/main/kotlin/dev/detekt/rules/ktlintwrapper/KtlintWrapperProvider.kt
+++ detekt-rules-ktlint-wrapper/src/main/kotlin/dev/detekt/rules/ktlintwrapper/KtlintWrapperProvider.kt
@@ -22,7 +22,7 @@ import dev.detekt.rules.ktlintwrapper.wrappers.ClassName
 import dev.detekt.rules.ktlintwrapper.wrappers.ClassSignature
 import dev.detekt.rules.ktlintwrapper.wrappers.CommentSpacing
 import dev.detekt.rules.ktlintwrapper.wrappers.CommentWrapping
-import dev.detekt.rules.ktlintwrapper.wrappers.ContextReceiverListWrapping
+import dev.detekt.rules.ktlintwrapper.wrappers.ContextParameterListWrapping
 import dev.detekt.rules.ktlintwrapper.wrappers.ContextReceiverMapping
 import dev.detekt.rules.ktlintwrapper.wrappers.EnumEntryNameCase
 import dev.detekt.rules.ktlintwrapper.wrappers.EnumWrapping
@@ -144,7 +144,7 @@ class KtlintWrapperProvider : RuleSetProvider {
                 ::CommentSpacing,
                 ::CommentWrapping,
                 ::ContextReceiverMapping,
-                ::ContextReceiverListWrapping,
+                ::ContextParameterListWrapping,
                 ::EnumEntryNameCase,
                 ::EnumWrapping,
                 ::Filename,
